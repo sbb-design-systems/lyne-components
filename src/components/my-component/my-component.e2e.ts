6f546f37b7ac4f29dd2d046c28c9914bddb1fdcc
feerglas <info@feerglas.com>
2020-03-02 21:09:12 +0100

    Fix test

--- src/components/my-component/my-component.e2e.ts
+++ src/components/my-component/my-component.e2e.ts
@@ -6,6 +6,6 @@ describe('my-component', () => {
 
     await page.setContent('<my-component></my-component>');
     const element = await page.find('my-component');
-    expect(element).toHaveClass('hydrateds');
+    expect(element).toHaveClass('hydrated');
   });
 });
