9cc168c4d327b7d5973d0e705cbe4b55f21dc3d5
feerglas <info@feerglas.com>
2020-03-06 16:24:59 +0100

    fix: try to add release version to netlify deploy message

--- ci/netlify_deploy.sh
+++ ci/netlify_deploy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+# -e: make sure command returns non-zero exit code as soon as somehting fails
+# -v: verbose mode
 set -ev
+
+# version file written by .releaserc
 VERSION_FILE=.version
 
 if [ -f "$VERSION_FILE" ]; then
-  npm run build:storybook && npm install netlify-cli -g &&netlify deploy --prod --message "::`cat .version`::" --site $NETLIFY_SITE_ID --auth $NETLIFY_AUTH_TOKEN --dir ./storybook-static/
+  npm run build:storybook
+  npm install netlify-cli -g
+  netlify deploy --prod --message "::`cat .version`::" --site $NETLIFY_SITE_ID --auth $NETLIFY_AUTH_TOKEN --dir ./storybook-static/
 fi
