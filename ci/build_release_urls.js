4f32ae91606dec6adbea152c736b7dc29e0e5f55
feerglas <info@feerglas.com>
2020-03-08 18:16:25 +0100

    fix: use async/await to refactor code

--- ci/build_release_urls.js
+++ ci/build_release_urls.js
@@ -20,44 +20,62 @@
  * 4. it pushes the .md file to our repo
  */
 
+ const axios = require('axios');
+ const simpleGit = require('simple-git')('./');
+ const shell = require('shelljs');
+ const fs = require('fs');
+ const argv = require('yargs').argv;
+ const { promisify } = require("util");
 
-const axios = require('axios');
-const simpleGit = require('simple-git')('./');
-const shell = require('shelljs');
-const fs = require('fs');
-const argv = require('yargs').argv;
+ const netlifyToken = argv.netlifyToken;
+ const netlifySiteId = argv.netlifySiteId;
+ const netlifyGetDeploysUrl = 'https://api.netlify.com/api/v1/sites/' + netlifySiteId + '/deploys' + '?access_token=' + netlifyToken;
+ const gitUser = argv.gitUser;
+ const gitToken = argv.gitToken;
+ const gitMail = argv.gitMail;
+ const gitUrl = `https://${gitUser}:${gitToken}@github.com/lyne-design-system/lyne-components`;
 
-const netlifyToken = argv.netlifyToken;
-const netlifySiteId = argv.netlifySiteId;
-const netlifyGetDeploysUrl = 'https://api.netlify.com/api/v1/sites/' + netlifySiteId + '/deploys' + '?access_token=' + netlifyToken;
-const gitUser = argv.gitUser;
-const gitToken = argv.gitToken;
-const gitMail = argv.gitMail;
-const gitUrl = `https://${gitUser}:${gitToken}@github.com/lyne-design-system/lyne-components`;
+(async () => {
 
-// ---------------------------------------------------------------------------
-// CALL API
-// ---------------------------------------------------------------------------
-axios.request({
-  method: "GET",
-  url: netlifyGetDeploysUrl
-}).then(response => {
-  if (!response.data) {
-    console.log('-->> ! BUILD RELEASE URLS: There are no deployments...');
+  try {
+
+    // get results
+    const deployments = await axios.request({
+      method: "GET",
+      url: netlifyGetDeploysUrl
+    });
+
+    if (!deployments.data) {
+      throw new Error('ERROR: no deployments received.');
+    }
+
+    if (deployments.data.length < 1) {
+      throw new Error('ERROR: no deployments received.');
+    }
+
+    // create an array of deployments with all needed data
+    const results = processDeploys(deployments.data);
+
+    if (results.length < 1) {
+      throw new Error('ERROR: no deployments with valid version number received.');
+    }
+
+    // prepare content for .md file
+    const formatedResults = formatResults(results);
+
+    // write .md file
+    const writeFile = promisify(fs.writeFile);
+    await writeFile('./DEPLOYMENTS.md', formatedResults)
+
+    // commit and push .md file to git repo
+    await pushToGit();
+
+  } catch (error) {
+    console.log('-->> BUILD RELEASE URLS: error');
+    console.log(error);
     shell.exit(0);
-    return;
   }
-
-  console.log('-->> BUILD RELEASE URLS: response received from netlify');
-  const results = processDeploys(response.data);
-  const formatedResults = formatResults(results);
-  writeResultsToFile(formatedResults);
-
-}).catch(error => {
-  console.log('-->> ! BUILD RELEASE URLS: There was an error getting or parsing the results from netlify api');
-  console.log(error);
-  shell.exit(0);
-});
+})();
 
 // ---------------------------------------------------------------------------
 // PROCESS DATA
@@ -98,108 +116,19 @@ const formatResults = ((data) => {
 });
 
 // ---------------------------------------------------------------------------
-// WRITE DATA TO FILE
-// ---------------------------------------------------------------------------
-const writeResultsToFile = ((results) => {
-
-  if (results.length < 1) {
-    console.log('-->> ! BUILD RELEASE URLS: There are no deployments with valid version numbers');
-    shell.exit(0);
-    return;
-  }
-
-  console.log('-->> BUILD RELEASE URLS: data is prepared and ready to be saved');
-
-  fs.writeFile("./DEPLOYMENTS.md", results, function(error) {
-    if(error) {
-      console.log('-->> ! BUILD RELEASE URLS: Error writing the file');
-      console.log(error);
-      shell.exit(0);
-      return;
-    }
-
-    console.log('-->> BUILD RELEASE URLS: data saved to file');
-
-    pushToGit();
-
-  });
-
-});
-
-// ---------------------------------------------------------------------------
 // PUSH TO GIT
 // ---------------------------------------------------------------------------
-const pushToGit = (() => {
-
-  simpleGit.removeRemote('origin', (err) => {
-    if (err) {
-      console.log('-->> ! BUILD RELEASE URLS: Error removing git remote');
-      console.log(err);
-      shell.exit(0);
-      return;
-    }
-
-    simpleGit.addConfig('user.email', gitMail);
-    simpleGit.addConfig('user.name', gitUser);
-
-    simpleGit.addRemote('origin', gitUrl, (err) => {
-      if (err) {
-        console.log('-->> ! BUILD RELEASE URLS: Error adding git remote');
-        console.log(err);
-        shell.exit(0);
-        return;
-      }
-
-      simpleGit.checkout('master', (err) => {
-        if (err) {
-          console.log('-->> ! BUILD RELEASE URLS: Error checking out the master branch');
-          console.log(err);
-          shell.exit(0);
-          return;
-        }
-
-        simpleGit.pull('origin', 'master', (err) => {
-          if (err) {
-            console.log('-->> ! BUILD RELEASE URLS: Error pulling latest changes');
-            console.log(err);
-            shell.exit(0);
-            return;
-          }
-
-          simpleGit.add('DEPLOYMENTS.md', (err) => {
-            if (err) {
-              console.log('-->> ! BUILD RELEASE URLS: Error adding DEPLOYMENTS.md file');
-              console.log(err);
-              shell.exit(0);
-              return;
-            }
-
-            simpleGit.commit('chore(release): update DEPLOYMENTS.md [skip ci]', function(err) {
-              if (err) {
-                console.log('-->> ! BUILD RELEASE URLS: Error making commit');
-                console.log(err);
-                shell.exit(0);
-                return;
-              }
-
-              simpleGit.push(['-u', 'origin', 'master'], (err) => {
-                if (err) {
-                  console.log('-->> ! BUILD RELEASE URLS: Error pushing to master');
-                  console.log(err);
-                  shell.exit(0);
-                  return;
-                }
-
-                console.log('-->> BUILD RELEASE URLS: push finished');
-                shell.exit(0);
-              });
-            });
-          });
-        });
-      });
-    });
-  });
-});
+const pushToGit = async () => {
+  await simpleGit.removeRemote('origin');
+  simpleGit.addConfig('user.email', gitMail);
+  simpleGit.addConfig('user.name', gitUser);
+  await simpleGit.addRemote('origin', gitUrl);
+  await simpleGit.checkout('master');
+  await simpleGit.pull('origin', 'master');
+  await simpleGit.add('DEPLOYMENTS.md');
+  await simpleGit.commit('chore(release): update DEPLOYMENTS.md [skip ci]');
+  await simpleGit.push(['-u', 'origin', 'master']);
+};
 
 // ---------------------------------------------------------------------------
 // HELPER METHODS
