008cc53e64dad6d1b6a2ef7c68311c48ca360cd8
feerglas <info@feerglas.com>
2020-03-09 10:54:53 +0100

    fix: try to add dynamic components version as meta

--- ci/netlify_deploy.sh
+++ ci/netlify_deploy.sh
@@ -16,14 +16,16 @@ VERSION_FILE=.version
 if [ -f "$VERSION_FILE" ];
 then
 
+  VERSION=cat .version
+
   # make storybook build
-  STORYBOOK_COMPONENTS_VERSION=5.2.3 npm run build:storybook
+  STORYBOOK_COMPONENTS_VERSION=$VERSION npm run build:storybook
 
   # install netlify-cli
   npm install netlify-cli -g
 
   # deploy on netlify
-  netlify deploy --prod --message "::`cat .version`::" --site $NETLIFY_SITE_ID --auth $NETLIFY_AUTH_TOKEN --dir ./storybook-static/
+  netlify deploy --prod --message "::`$VERSION`::" --site $NETLIFY_SITE_ID --auth $NETLIFY_AUTH_TOKEN --dir ./storybook-static/
 else
   echo "-->> Skipping netlify depoly"
 fi
