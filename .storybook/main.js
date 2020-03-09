cd4cd80977cc9cf723198f2862f9c836d327d721
feerglas <info@feerglas.com>
2020-03-09 17:06:30 +0100

    fix: fix issue with live-reload for css files not working

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,23 @@ const WriteFilePlugin = require('write-file-webpack-plugin');
 const OUTPUT_DIR = '../dist';
 const PROJECT_NAME = 'lyne-components';
 
+// THIS IS HACKY... We have the issue that live reload is not working for
+// css files... Stencil adds <style sty-id="..."></style> for a component
+// in the head of the html document.
+// The Problem: Storybook's default webpack-plugin
+// will extract the style and put it in a generic <style></style> tag.
+// Instead of the tag with the id, this tag get's updated by webpack.
+// Solution: So removing the forst use-rule from the webpack-config seems to fix
+// the issue.
+const removeStyleLoaderFile = (rules) => {
+  return rules.map((rule) => {
+    if (rule.test.toString().indexOf('.css$') !== -1) {
+      rule.use.splice(0, 1);
+    }
+    return rule;
+  });
+};
+
 module.exports = {
   stories: ['../src/**/*.stories.js'],
   addons: [
@@ -15,6 +32,9 @@ module.exports = {
   ],
   // Custom webpack config to tell Storybook where to find the compiled files from Stencil
   async webpackFinal(config) {
+
+    config.module.rules = removeStyleLoaderFile(config.module.rules);
+
     config.entry.push(path.join(__dirname, OUTPUT_DIR, `${PROJECT_NAME}.js`));
     fs.readdirSync(path.join(__dirname, OUTPUT_DIR, 'collection/components')).map(file => {
       jsFilePath = path.join(__dirname, OUTPUT_DIR, `collection/components/${file}/${file}.js`);
