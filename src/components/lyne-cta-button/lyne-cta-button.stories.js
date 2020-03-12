d9bbcb8eb2c35ff51f9c385807b4400a8e657a00
feerglas <info@feerglas.com>
2020-03-12 08:41:28 +0100

    refactor: use new event name

--- src/components/lyne-cta-button/lyne-cta-button.stories.js
+++ src/components/lyne-cta-button/lyne-cta-button.stories.js
@@ -3,11 +3,12 @@ import { withKnobs, text } from '@storybook/addon-knobs';
 import { withActions } from '@storybook/addon-actions';
 import { h } from 'jsx-dom';
 import { withA11y } from '@storybook/addon-a11y';
+import events from './lyne-cta-button.events.ts';
 
 import readme from './readme.md';
 
 storiesOf('lyne-cta-button', module)
-  .addDecorator(withActions('onClick'))
+  .addDecorator(withActions(events.click))
   .addDecorator(withKnobs)
   .addDecorator(withA11y)
   .add(
