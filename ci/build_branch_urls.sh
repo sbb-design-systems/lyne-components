a86e98a2f0b801fa8676b27367565ee579f54bea
feerglas <info@feerglas.com>
2020-03-13 11:22:56 +0100

    fix: pass branch name instead of branch url

--- ci/build_branch_urls.sh
+++ ci/build_branch_urls.sh
@@ -9,7 +9,5 @@
 # -e: make sure command returns non-zero exit code as soon as somehting fails
 set -e
 
-BRANCH_NAME="https://github.com/lyne-design-system/lyne-components/tree/$1"
-
 # run build_release_urls.js with neccessary arguments
-node ./ci/build_deploy_urls.js --netlifyToken=$NETLIFY_AUTH_TOKEN --netlifySiteId=$NETLIFY_SITE_ID --gitUser=lyne-design-system --gitToken=$GH_PERSONAL_TOKEN --gitMail=$GH_MAIL --branch=$BRANCH_NAME
+node ./ci/build_deploy_urls.js --netlifyToken=$NETLIFY_AUTH_TOKEN --netlifySiteId=$NETLIFY_SITE_ID --gitUser=lyne-design-system --gitToken=$GH_PERSONAL_TOKEN --gitMail=$GH_MAIL --branch=$1
