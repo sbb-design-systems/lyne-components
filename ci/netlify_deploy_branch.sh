5ab2d5f903990b422a4018fec62bc4f149f857ab
feerglas <info@feerglas.com>
2020-03-13 10:06:34 +0100

    fix: add full branch url to netlify deploy title

--- ci/netlify_deploy_branch.sh
+++ ci/netlify_deploy_branch.sh
@@ -9,12 +9,14 @@
 # -e: make sure command returns non-zero exit code as soon as somehting fails
 set -e
 
+BRANCH_NAME="https://github.com/lyne-design-system/lyne-components/tree/$1"
+
 # make storybook build
-STORYBOOK_COMPONENTS_VERSION=$VERSION npm run build:storybook
+npm run build:storybook
 
 # install netlify-cli. We send stdout/stderr to /dev/null since we're not
 # interested in the output from netlify-cli
 npm install netlify-cli -g > "/dev/null" 2>&1
 
 # deploy on netlify
-netlify deploy --message "++$1++" --site $NETLIFY_SITE_ID --auth $NETLIFY_AUTH_TOKEN --dir ./storybook-static/
+netlify deploy --message "++$BRANCH_NAME++" --site $NETLIFY_SITE_ID --auth $NETLIFY_AUTH_TOKEN --dir ./storybook-static/
