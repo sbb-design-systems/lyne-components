d7111e7c098b9e64f4767869ee69422ebbc25f7e
feerglas <info@feerglas.com>
2020-03-14 15:27:39 +0100

    build: fix typo

--- ci/build_branch_urls.sh
+++ ci/build_branch_urls.sh
@@ -15,5 +15,5 @@ node ./ci/prepare_git.js --gitUser=lyne-design-system --gitToken=$GH_PERSONAL_TO
 # prcoess deployments and write to json file
 node ./ci/netlify_deployments.js --netlifyToken=$NETLIFY_AUTH_TOKEN --netlifySiteId=$NETLIFY_SITE_ID
 
-# run build_release_urls.js with neccessary arguments
+# run build_deploy_urls.js with neccessary arguments
 node ./ci/build_deploy_urls.js --gitUser=lyne-design-system --gitToken=$GH_PERSONAL_TOKEN --gitMail=$GH_MAIL --branch=$1 --prod=false
