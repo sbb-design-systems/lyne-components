8aa8370547d11885bb3fac2eab76fcb4b68c12f7
feerglas <info@feerglas.com>
2020-03-23 16:17:03 +0100

    build: remove unneded code

--- ci/prepare_git.js
+++ ci/prepare_git.js
@@ -1,8 +1,6 @@
 const simpleGit = require('simple-git/promise')('./');
 const shell = require('shelljs');
 const argv = require('yargs').argv;
-const fs = require('fs');
-const config = require('./deployments_config');
 
 const gitUser = argv.gitUser;
 const gitToken = argv.gitToken;
@@ -23,38 +21,10 @@ const branch = isProdDeploy ? 'master' : branchName;
     simpleGit.addConfig('user.email', gitMail);
     simpleGit.addConfig('user.name', gitUser);
     await simpleGit.addRemote('origin', gitUrl);
+
+    // checkout and pull the specific branch
     await simpleGit.checkout(branch);
-
     await simpleGit.pull('origin', branch);
-
-    // remove PREVIEWS.md
-    await fs.access(`./${config.branchFileName}`, fs.F_OK, async (err) => {
-      if (!err) {
-        await simpleGit.rm([`${config.branchFileName}`]);
-        await simpleGit.commit(`chore: remove ${config.branchFileName} [skip ci]`);
-      }
-    });
-
-
-    // remove RELEASES.md
-    if (isProdDeploy) {
-      await fs.access(`./${config.prodFileName}`, fs.F_OK, async (err) => {
-        if (!err) {
-          await simpleGit.rm([`${config.prodFileName}`]);
-          await simpleGit.commit(`chore: remove ${config.prodFileName} [skip ci]`);
-        }
-      });
-    }
-
-    // remove releases.json
-    await fs.access(`./ci/${config.deploymentsJsonName}`, fs.F_OK, async (err) => {
-      if (!err) {
-        await simpleGit.rm([`./ci/${config.deploymentsJsonName}`]);
-        await simpleGit.commit(`chore: remove ${config.deploymentsJsonName} [skip ci]`);
-      }
-    });
-
-    await simpleGit.push(['-u', 'origin', branch]);
 
     console.log('-->> PREPARE GIT: successcull');
     shell.exit(0);
