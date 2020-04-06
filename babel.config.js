1c6b279e509a9bad1d4d758ab6d63c2ab34b7ae6
feerglas <info@feerglas.com>
2020-04-06 11:12:26 +0200

    build: use babel.config.js instead of babelrc

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
-{
-  "plugins": [
+module.exports = {
+  plugins: [
     "@babel/plugin-syntax-jsx",
     [
       "@babel/plugin-transform-react-jsx",
       {
-        "pragma": "h"
+        pragma: "h"
       }
     ]
   ]
