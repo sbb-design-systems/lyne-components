a7cfa79b1f1f16984f264baf235fadfa92e6eb30
feerglas <info@feerglas.com>
2020-04-20 10:15:00 +0200

    feat: add level knob to title story

--- src/components/lyne-title/lyne-title.stories.js
+++ src/components/lyne-title/lyne-title.stories.js
@@ -1,21 +1,35 @@
 import { storiesOf } from '@storybook/html';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { withKnobs, text, radios } from '@storybook/addon-knobs';
 import { h } from 'jsx-dom';
 import { withA11y } from '@storybook/addon-a11y';
 import readme from './readme.md';
 
-storiesOf('lyne-cta-button', module)
+storiesOf('lyne-title', module)
   .addDecorator(withKnobs)
   .addDecorator(withA11y)
   .add(
     'Default',
     () => {
 
-      const textProp = text('text', 'Title Text');
+      const groupId = 'General'
+
+      const textLabel = 'Title Text';
+      const textDefaultValue = 'Sample Title';
+      const textValue = text(textLabel, textDefaultValue, groupId);
+
+      const levelLabel = 'Title Level';
+      const levelOptions = {
+        'Level 1': '1',
+        'Level 2': '2',
+        'Level 3': '3'
+      };
+      const levelDefaultValue = '1';
+      const levelValue = radios(levelLabel, levelOptions, levelDefaultValue, groupId);
 
       return (
         <lyne-title
-          text={textProp}
+          text={textValue}
+          level={levelValue}
         ></lyne-title>
       );
 
