af37641f8aa90a1bb7a8cff1f1c040913338360f
feerglas <info@feerglas.com>
2020-04-28 08:14:26 +0200

    fix: remove duplicated const declarations

--- src/components/lyne-title/lyne-title.e2e.ts
+++ src/components/lyne-title/lyne-title.e2e.ts
@@ -11,10 +11,6 @@ describe('lyne-title', () => {
 	});
 
   it('renders', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<lyne-title></lyne-title>');
-
-    const element = await page.find('lyne-title');
     expect(element).toHaveClass('hydrated');
   });
 
