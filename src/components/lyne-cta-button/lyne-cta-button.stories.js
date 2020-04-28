3dbad2bd4662206fe6c4224315506a24b0f794d8
feerglas <info@feerglas.com>
2020-04-28 15:17:26 +0200

    fix: rewrite story in cfm instead of storiesOf api

--- src/components/lyne-cta-button/lyne-cta-button.stories.js
+++ src/components/lyne-cta-button/lyne-cta-button.stories.js
@@ -1,32 +1,18 @@
-import { storiesOf } from '@storybook/html';
 import { withKnobs, text } from '@storybook/addon-knobs';
-import { withActions } from '@storybook/addon-actions';
+import { withActions, action } from '@storybook/addon-actions';
 import { h } from 'jsx-dom';
 import { withA11y } from '@storybook/addon-a11y';
 import events from './lyne-cta-button.events.ts';
 
-import readme from './readme.md';
+export const defaultButton = () => <lyne-cta-button
+  label={text('label', 'Label')}
+  onClick={action(events.click)}
+/>;
 
-storiesOf('lyne-cta-button', module)
-  .addDecorator(withActions(events.click))
-  .addDecorator(withKnobs)
-  .addDecorator(withA11y)
-  .add(
-    'Default',
-    () => {
-
-      const label = text('label', 'Label');
-
-      return (
-        <lyne-cta-button
-          label={label}
-        ></lyne-cta-button>
-      );
-
-    },
-    {
-      notes: {
-        markdown: readme,
-      },
-    }
-  );
+export default {
+  decorators: [
+    withKnobs,
+    withA11y
+  ],
+  title: 'Button'
+};
