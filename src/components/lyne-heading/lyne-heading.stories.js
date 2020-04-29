1ac20fd96ed248b69f8f5e22d28cc50e90354591
feerglas <info@feerglas.com>
2020-04-29 08:04:18 +0200

    feat: include visual and semantic level in stories

--- src/components/lyne-heading/lyne-heading.stories.js
+++ src/components/lyne-heading/lyne-heading.stories.js
@@ -2,9 +2,10 @@ import { radios, text, withKnobs } from '@storybook/addon-knobs';
 import { h } from 'jsx-dom';
 import { withA11y } from '@storybook/addon-a11y';
 
-const textLabel = 'Title Text';
-const textDefaultValue = 'Sample Title';
-const levelLabel = 'Title Level';
+const textLabel = 'Heading Text';
+const textDefaultValue = 'Sample Heading';
+const levelLabel = 'Heading Level';
+const visualLevelLabel = 'Visual Heading Level';
 const levelOptions = {
   'Level 1': '1',
   'Level 2': '2',
@@ -13,16 +14,19 @@ const levelOptions = {
 
 export const level1 = () => <lyne-heading
   level={radios(levelLabel, levelOptions, '1')}
+  visual-level={radios(visualLevelLabel, levelOptions, '1')}
   text={text(textLabel, textDefaultValue)}
 />;
 
 export const level2 = () => <lyne-heading
   level={radios(levelLabel, levelOptions, '2')}
+  visual-level={radios(visualLevelLabel, levelOptions, '2')}
   text={text(textLabel, textDefaultValue)}
 />;
 
 export const level3 = () => <lyne-heading
   level={radios(levelLabel, levelOptions, '3')}
+  visual-level={radios(visualLevelLabel, levelOptions, '3')}
   text={text(textLabel, textDefaultValue)}
 />;
 
