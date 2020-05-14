a655bfa94a8a451e600b36d244134c5a235b3560
feerglas <info@feerglas.com>
2020-05-14 10:23:14 +0200

    Revert "fix: remove addon docs"
    This reverts commit 04baa84c537f0b787ce4896b8ec0ff9958732694.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: ['../src/**/*.stories.(js|mdx)'],
   addons: [
+    '@storybook/addon-docs',
     '@storybook/addon-knobs/register',
     '@storybook/addon-actions/register',
     '@storybook/addon-a11y/register'
