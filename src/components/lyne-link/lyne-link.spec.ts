f5a2f24be42a3ff3f0fb456a8f68b7f2a29d1b35
feerglas <info@feerglas.com>
2020-05-14 16:51:38 +0200

    test: remove build test, add render test

--- src/components/lyne-link/lyne-link.spec.ts
+++ src/components/lyne-link/lyne-link.spec.ts
@@ -1,9 +1,21 @@
+import { newSpecPage } from '@stencil/core/testing';
 import { LyneLink } from './lyne-link';
 
 describe('lyne-link', () => {
 
-  it('builds', () => {
-    expect(new LyneLink()).toBeTruthy();
+  it('renders', async () => {
+    const {root} = await newSpecPage({
+      components: [LyneLink],
+      html: '<lyne-link text="Link text" link="https://www.sbb.ch" open-in-new-window="false"></lyne-link>'
+    });
+
+    expect(root).toEqualHtml(`
+      <lyne-link text="Link text" link="https://www.sbb.ch" open-in-new-window="false">
+        <mock:shadow-root>
+          <a class="link" href="https://www.sbb.ch">Link text</a>
+        </mock:shadow-root>
+      </lyne-heading>
+    `);
   });
 
 });
