04baa84c537f0b787ce4896b8ec0ff9958732694
feerglas <info@feerglas.com>
2020-05-14 09:53:46 +0200

    fix: remove addon docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   stories: ['../src/**/*.stories.(js|mdx)'],
   addons: [
-    '@storybook/addon-docs',
     '@storybook/addon-knobs/register',
     '@storybook/addon-actions/register',
     '@storybook/addon-a11y/register'
