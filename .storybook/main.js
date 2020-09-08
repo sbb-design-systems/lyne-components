cd198298f18ee9473bd49e5157e4b99e88a6c19e
feerglas <info@feerglas.com>
2020-09-08 09:36:49 +0200

    fix: update to latest storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,7 @@ module.exports = {
   addons: [
     '@storybook/addon-knobs/register',
     '@storybook/addon-actions/register',
-    '@storybook/addon-a11y/register'
+    '@storybook/addon-a11y/register',
+    '@storybook/addon-docs/preset'
   ]
 };
