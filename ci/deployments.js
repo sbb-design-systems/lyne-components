ebe6bb9e9113967c8fca1bf1d79aae9587bfb3bd
feerglas <info@feerglas.com>
2020-09-10 15:59:39 +0200

    chore: fix linting issues

--- ci/deployments.js
+++ ci/deployments.js
@@ -30,6 +30,121 @@ const json = {};
 json[config.deploymentsJsonKeyProd] = [];
 json[config.deploymentsJsonKeyPreview] = [];
 
+/*
+ * ---------------------------------------------------------------------------
+ * PROCESS DATA
+ * ---------------------------------------------------------------------------
+ */
+
+const getDeployType = ((title) => {
+  if (!title) {
+    return false;
+  }
+
+  if (title.length < 1) {
+    return false;
+  }
+
+  const prodSplit = title.split(config.netlifyTitleSeparatorProd);
+
+  if (prodSplit.length === 3) {
+    return config.deploymentsJsonKeyProd;
+  }
+
+  const previewSplit = title.split(config.netlifyTitleSeparatorPreview);
+
+  if (previewSplit.length === 3) {
+    return config.deploymentsJsonKeyPreview;
+  }
+
+  return false;
+});
+
+const findDeployment = ((deployTag, deployType) => {
+  let resultFound = false;
+
+  json[deployType].forEach((deployment) => {
+    if (deployment[config.deploymentsJsonKeyTag] === deployTag) {
+      resultFound = true;
+    }
+  });
+
+  return resultFound;
+});
+
+const getDeployTag = ((titleString, type) => {
+  const emptyTag = '';
+  const isProd = type === config.deploymentsJsonKeyProd;
+  const separator = isProd
+    ? config.netlifyTitleSeparatorProd
+    : config.netlifyTitleSeparatorPreview;
+
+  if (!titleString) {
+    return emptyTag;
+  }
+
+  if (titleString.length < 1) {
+    return emptyTag;
+  }
+
+  const separatorSplits = titleString.split(separator);
+
+  if (separatorSplits.length !== 3) {
+    return emptyTag;
+  }
+
+  const [
+    ,
+    tag
+  ] = separatorSplits;
+
+  if (isProd) {
+    const splitByDots = tag.split('.');
+
+    if (splitByDots.length !== 3) {
+      return emptyTag;
+    }
+  } else {
+    if (tag.length < 1) {
+      return emptyTag;
+    }
+  }
+
+  return tag;
+});
+
+const processDeploys = ((data) => {
+  data.forEach((deploy) => {
+    const deployType = getDeployType(deploy.title);
+
+    if (deployType) {
+      const deployTag = getDeployTag(deploy.title, deployType);
+
+      if (deployTag.length < 1) {
+        return;
+      }
+
+      const url = deploy.deploy_ssl_url;
+      const date = deploy.created_at;
+
+      /*
+       * this is only relevant for branch deploys. but it does not hurt release
+       * deploys, since we should never have 2 deploys with the same title
+       */
+      const alreadyThere = findDeployment(deployTag, deployType);
+
+      if (!alreadyThere) {
+        const newDeploy = {};
+
+        newDeploy[config.deploymentsJsonKeyTag] = deployTag;
+        newDeploy[config.deploymentsJsonKeyDate] = date;
+        newDeploy[config.deploymentsJsonKeyUrl] = url;
+        json[deployType].push(newDeploy);
+      }
+    }
+  });
+});
+
 (async () => {
 
   const netlifyGetDeploysUrl = `https://api.netlify.com/api/v1/sites/${netlifySiteId}/deploys?access_token=${netlifyToken}`;
@@ -77,117 +192,3 @@ json[config.deploymentsJsonKeyPreview] = [];
     shell.exit(0);
   }
 })();
-
-/*
- * ---------------------------------------------------------------------------
- * PROCESS DATA
- * ---------------------------------------------------------------------------
- */
-const processDeploys = ((data) => {
-  data.forEach((deploy) => {
-    const deployType = getDeployType(deploy.title);
-
-    if (deployType) {
-      const deployTag = getDeployTag(deploy.title, deployType);
-
-      if (deployTag.length < 1) {
-        return;
-      }
-
-      const url = deploy.deploy_ssl_url;
-      const date = deploy.created_at;
-
-      /*
-       * this is only relevant for branch deploys. but it does not hurt release
-       * deploys, since we should never have 2 deploys with the same title
-       */
-      const alreadyThere = findDeployment(deployTag, deployType);
-
-      if (!alreadyThere) {
-        const newDeploy = {};
-
-        newDeploy[config.deploymentsJsonKeyTag] = deployTag;
-        newDeploy[config.deploymentsJsonKeyDate] = date;
-        newDeploy[config.deploymentsJsonKeyUrl] = url;
-        json[deployType].push(newDeploy);
-      }
-    }
-  });
-});
-
-const getDeployType = ((title) => {
-  if (!title) {
-    return false;
-  }
-
-  if (title.length < 1) {
-    return false;
-  }
-
-  const prodSplit = title.split(config.netlifyTitleSeparatorProd);
-
-  if (prodSplit.length === 3) {
-    return config.deploymentsJsonKeyProd;
-  }
-
-  const previewSplit = title.split(config.netlifyTitleSeparatorPreview);
-
-  if (previewSplit.length === 3) {
-    return config.deploymentsJsonKeyPreview;
-  }
-
-  return false;
-});
-
-const findDeployment = ((deployTag, deployType) => {
-  let resultFound = false;
-
-  json[deployType].forEach((deployment) => {
-    if (deployment[config.deploymentsJsonKeyTag] === deployTag) {
-      resultFound = true;
-    }
-  });
-
-  return resultFound;
-});
-
-const getDeployTag = ((titleString, type) => {
-  const emptyTag = '';
-  const isProd = type === config.deploymentsJsonKeyProd;
-  const separator = isProd
-  ? config.netlifyTitleSeparatorProd
-  : config.netlifyTitleSeparatorPreview;
-
-  if (!titleString) {
-    return emptyTag;
-  }
-
-  if (titleString.length < 1) {
-    return emptyTag;
-  }
-
-  const separatorSplits = titleString.split(separator);
-
-  if (separatorSplits.length !== 3) {
-    return emptyTag;
-  }
-
-  const [
-    ,
-    tag
-  ] = separatorSplits;
-
-  if (isProd) {
-    const splitByDots = tag.split('.');
-
-    if (splitByDots.length !== 3) {
-      return emptyTag;
-    }
-  } else {
-    if (tag.length < 1) {
-      return emptyTag;
-    }
-  }
-
-  return tag;
-});
