9e68828b9b6616cd89d97e564a0ca5b10cbc3e6d
Yves Tscherry <info@feerglas.com>
2021-06-14 10:48:37 +0200

    fix: remove knobs

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,6 @@ module.exports = {
   stories: ['../src/**/*.stories.@(js|mdx)'],
   addons: [
     '@storybook/addon-essentials',
-    '@storybook/addon-knobs/register',
     '@storybook/addon-a11y',
   ],
   features: {
