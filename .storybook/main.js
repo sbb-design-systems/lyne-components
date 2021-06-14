ffa0d84d38176917d02ddc18fa0c579d3602ccd1
Yves Tscherry <info@feerglas.com>
2021-06-14 09:28:17 +0200

    fix: properly register a11y plugin for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,7 @@ module.exports = {
   addons: [
     '@storybook/addon-knobs/register',
     '@storybook/addon-actions/register',
-    '@storybook/addon-a11y/register',
+    '@storybook/addon-a11y',
     '@storybook/addon-docs/preset'
   ],
   features: {
