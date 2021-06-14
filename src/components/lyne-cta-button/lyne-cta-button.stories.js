cfe99f5328b596d4cbd1a8c7c5a9656c8cfebf0e
Yves Tscherry <info@feerglas.com>
2021-06-14 10:13:35 +0200

    fix: handle actions according to addon-essentials

--- src/components/lyne-cta-button/lyne-cta-button.stories.js
+++ src/components/lyne-cta-button/lyne-cta-button.stories.js
@@ -1,11 +1,9 @@
 import { withKnobs, text } from '@storybook/addon-knobs';
-import { withActions, action } from '@storybook/addon-actions';
 import { h } from 'jsx-dom';
 import events from './lyne-cta-button.events.ts';
 
 export const defaultButton = () => <lyne-cta-button
   label={text('label', 'Label')}
-  onClick={action(events.click)}
 />;
 
 export default {
@@ -14,6 +12,11 @@ export default {
   ],
   title: 'Button',
   parameters: {
+    actions: {
+      handles: [
+        events.click
+      ],
+    },
     chromatic: {
       delay: 1000,
       viewports: [
