3af85a6d3c8df6224880ca9922f35b8cfe42278b
Yves Tscherry <info@feerglas.com>
2021-06-24 09:39:26 +0200

    chore: fix linting issues

--- src/components/lyne-seo-test-shadow-slot/lyne-seo-test-shadow-slot.stories.js
+++ src/components/lyne-seo-test-shadow-slot/lyne-seo-test-shadow-slot.stories.js
@@ -2,9 +2,8 @@ import { h } from 'jsx-dom';
 import readme from './readme.md';
 
 export const seoWithShadow = () => <lyne-seo-test-shadow-slot><h1>Default heading</h1><p>Default text</p></lyne-seo-test-shadow-slot>;
-const foo = 'bar';
+
 export default {
-  title: 'seoWithShadowSlot',
   parameters: {
     chromatic: {
       delay: 1000,
@@ -15,9 +14,8 @@ export default {
       ]
     },
     docs: {
-      extractComponentDescription: () => {
-        return readme;
-      }
+      extractComponentDescription: () => readme
     }
-  }
+  },
+  title: 'seoWithShadowSlot'
 };
