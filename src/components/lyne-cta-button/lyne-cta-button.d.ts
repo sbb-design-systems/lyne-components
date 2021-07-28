1e3e4b3979d679db00d8e1f8ab092f02163f9be0
Yves Tscherry <info@feerglas.com>
2021-07-28 11:01:49 +0200

    chore: add interface declarations for button type

---
+++ src/components/lyne-cta-button/lyne-cta-button.d.ts
@@ -0,0 +1,3 @@
+export interface InterfaceButtonAttributes {
+  type: 'primary' | 'primary-negative' | 'secondary' | 'secondary-negative' | 'tertiary' | 'tertiary-negative' | 'transparent' | 'transparent-negative';
+}
