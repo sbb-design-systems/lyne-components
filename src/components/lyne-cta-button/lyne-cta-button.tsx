284680f8c721bf0e164c672b31e509322de816b7
Yves Tscherry <info@feerglas.com>
2021-08-12 17:13:16 +0200

    fix: make label required

--- src/components/lyne-cta-button/lyne-cta-button.tsx
+++ src/components/lyne-cta-button/lyne-cta-button.tsx
@@ -20,7 +20,7 @@ import { InterfaceButtonAttributes } from './lyne-cta-button.d';
 export class LyneCtaButton {
 
   /** Label text to show on the button */
-  @Prop() public label? = 'Default button text';
+  @Prop() public label = 'Default button text';
 
   /** Variant of the button, like primary, secondary etc. */
   @Prop() public variant?: InterfaceButtonAttributes['variant'] = 'primary';
