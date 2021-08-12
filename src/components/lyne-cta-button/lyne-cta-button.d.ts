9f2527dd1c356b88f1ca4dc43d18862f65025fa0
Yves Tscherry <info@feerglas.com>
2021-08-12 20:06:27 +0200

    chore: add typings for type property

--- src/components/lyne-cta-button/lyne-cta-button.d.ts
+++ src/components/lyne-cta-button/lyne-cta-button.d.ts
@@ -1,4 +1,5 @@
 export interface InterfaceButtonAttributes {
   variant: 'primary' | 'primary-negative' | 'secondary' | 'secondary-negative' | 'tertiary' | 'tertiary-negative' | 'transparent' | 'transparent-negative';
   size: 'large' | 'small';
+  type: 'button' | 'reset' | 'submit';
 }
