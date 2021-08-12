eaf23a41480288458450a4f7202273bb78d1f249
Yves Tscherry <info@feerglas.com>
2021-08-12 17:49:06 +0200

    test: fix test

--- src/components/lyne-cta-button/lyne-cta-button.spec.ts
+++ src/components/lyne-cta-button/lyne-cta-button.spec.ts
@@ -1,5 +1,6 @@
 import events from './lyne-cta-button.events';
 import { LyneCtaButton } from './lyne-cta-button';
+import lyneIcons from 'lyne-icons/dist/icons.json';
 import { newSpecPage } from '@stencil/core/testing';
 
 describe('lyne-cta-button', () => {
@@ -8,18 +9,23 @@ describe('lyne-cta-button', () => {
       root
     } = await newSpecPage({
       components: [LyneCtaButton],
-      html: '<lyne-cta-button label="Label" variant="secondary-negative"></lyne-cta-button>'
+      html: `<lyne-cta-button label="Label" variant="secondary-negative" icon="true">${lyneIcons.icons['arrow-right-small']}</lyne-cta-button>`
     });
 
     expect(root)
       .toEqualHtml(`
-        <lyne-cta-button label="Label" variant="secondary-negative">
+        <lyne-cta-button icon="true" label="Label" variant="secondary-negative">
           <mock:shadow-root>
-            <button class="button button--secondary-negative">
-              <span class="button__icon"></span>
+            <button class="button button--large button--secondary-negative">
+              <span class="button__icon">
+                <slot></slot>
+              </span>
               <span class="button__label">Label</span>
             </button>
           </mock:shadow-root>
+          <svg height="24" width="24" xmlns="http://www.w3.org/2000/svg">
+            <path clip-rule="evenodd" d="m17.8436,12.1382-3.99-3.99196-.7072.70693,3.1366,3.13823H5v1h11.287l-3.1413,3.1555.7086.7056,3.99-4.008.3519-.3535-.3526-.3528z" fill-rule="evenodd"></path>
+          </svg>
         </lyne-cta-button>
       `);
   });
