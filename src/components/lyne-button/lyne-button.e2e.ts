99afc1516268eb7426ba87fb5feac70381efed7c
Yves Tscherry <info@feerglas.com>
2021-08-13 16:27:39 +0200

    fix: rename lyne-cta-button to lyne-button

---
+++ src/components/lyne-button/lyne-button.e2e.ts
@@ -0,0 +1,72 @@
+import events from './lyne-cta-button.events';
+import { newE2EPage } from '@stencil/core/testing';
+
+describe('lyne-cta-button', () => {
+  let element,
+    page;
+
+  beforeEach(async () => {
+    page = await newE2EPage();
+    await page.setContent('<lyne-cta-button></lyne-cta-button>');
+    element = await page.find('lyne-cta-button');
+  });
+
+  it('renders', async () => {
+    page = await newE2EPage();
+    await page.setContent('<lyne-cta-button></lyne-cta-button>');
+
+    element = await page.find('lyne-cta-button');
+    expect(element)
+      .toHaveClass('hydrated');
+  });
+
+  it('renders button text', async () => {
+    const buttonText = 'Custom Button Text';
+
+    element.setProperty('label', buttonText);
+    await page.waitForChanges();
+    const button = await page.find('lyne-cta-button >>> .button__label');
+
+    expect(button.textContent)
+      .toEqual(buttonText);
+  });
+
+  describe('events', () => {
+    it('dispatches event on click', async () => {
+      element.setProperty('text', 'Custom Button Text');
+      await page.waitForChanges();
+      const button = await page.find('lyne-cta-button >>> button');
+      const changeSpy = await page.spyOnEvent(events.click);
+
+      await button.click();
+      expect(changeSpy)
+        .toHaveReceivedEventTimes(1);
+    });
+
+    it('dispatches correct event payload on click with no id', async () => {
+      element.setProperty('text', 'Custom Button Text');
+      await page.waitForChanges();
+      const button = await page.find('lyne-cta-button >>> button');
+      const changeSpy = await page.spyOnEvent(events.click);
+
+      await button.click();
+      expect(changeSpy)
+        .toHaveReceivedEventDetail(null);
+    });
+
+    it('dispatches correct event payload on click with id', async () => {
+      const buttonId = 'buttonId';
+
+      element.setProperty('text', 'Custom Button Text');
+      element.setProperty('eventId', buttonId);
+      await page.waitForChanges();
+      const button = await page.find('lyne-cta-button >>> button');
+      const changeSpy = await page.spyOnEvent(events.click);
+
+      await button.click();
+      expect(changeSpy)
+        .toHaveReceivedEventDetail(buttonId);
+    });
+  });
+
+});
