100a5c06da9c76ee6fb8a5b00e1afb82500728b4
Yves Tscherry <info@feerglas.com>
2021-08-13 14:03:42 +0200

    chore: add types for popup property

--- src/components/lyne-cta-button/lyne-cta-button.d.ts
+++ src/components/lyne-cta-button/lyne-cta-button.d.ts
@@ -2,4 +2,5 @@ export interface InterfaceButtonAttributes {
   variant: 'primary' | 'primary-negative' | 'secondary' | 'secondary-negative' | 'tertiary' | 'tertiary-negative' | 'transparent' | 'transparent-negative';
   size: 'large' | 'small';
   type: 'button' | 'reset' | 'submit';
+  popup: 'true' | 'dialog' | 'menu' | 'listbox' | 'tree' | 'grid';
 }
