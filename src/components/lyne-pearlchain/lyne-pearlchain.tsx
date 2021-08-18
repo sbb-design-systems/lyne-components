18b0b49738f706afef2d7b8baaff0da3910edfeb
Yves Tscherry <info@feerglas.com>
2021-08-18 18:28:11 +0200

    feat: add tsx file for pearlchain, add basic properties

---
+++ src/components/lyne-pearlchain/lyne-pearlchain.tsx
@@ -0,0 +1,72 @@
+import {
+  Component,
+  h,
+  Prop
+} from '@stencil/core';
+import { InterfacePearlchainAttributes } from './lyne-pearlchain.custom.d';
+
+@Component({
+  shadow: true,
+  styleUrl: 'lyne-pearlchain.scss',
+  tag: 'lyne-pearlchain'
+})
+
+export class LynePearlchain {
+
+  /** If set, the pearlchain will be displayed vertically. */
+  @Prop() public vertical?: boolean;
+
+  /**
+   * Define, if the pearlchain represents a connection in the past,
+   * in the future or if it is a currently running connection.
+   * If it is currently running, provide a number between 0 and 100,
+   * which will represent the current location on the pearl-chain.
+   */
+  @Prop() public status?: InterfacePearlchainAttributes['status'] = 'future';
+
+  /** If set to true, the departure point will be marked as cancelled */
+  @Prop() public departurePointCancellation?: boolean;
+
+  /** If set to true, the arrival point will be marked as cancelled */
+  @Prop() public arrivalPointCancellation?: boolean;
+
+  /**
+   * Stringified JSON to define the stations on the pearl-chain.
+   * Format:
+   * `{stations: [{location: number, cancellation?: boolean}]}`
+   * `location`: number between 0 and 100, which will represent the
+   * station on the pearl-chain
+   * `cancellation`: if set, the station will be marked as canceled. In
+   * this case, the connections to the previous and next stations will
+   * be marked as cancelled as well.
+   */
+  @Prop() public stations?: string;
+
+  public render(): JSX.Element {
+    this.vertical = false;
+    this.status = 'future';
+    this.departurePointCancellation = false;
+    this.arrivalPointCancellation = false;
+    this.stations = JSON.stringify({
+      stations: [
+        {
+          location: 25
+        },
+        {
+          location: 75
+        }
+      ]
+    });
+
+    const verticalClass = this.vertical
+      ? 'pearlchain--vertical'
+      : 'pearlchain--horizontal';
+
+    const classes = `pearlchain ${verticalClass}`;
+
+    return (
+      <div class={classes}>
+      </div>
+    );
+  }
+}
