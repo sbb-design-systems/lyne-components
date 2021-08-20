366d36ff6ac32fd3bfa045c99ff45584f15ab3a2
Yves Tscherry <info@feerglas.com>
2021-08-20 19:06:38 +0200

    feat: start defining stories

--- src/components/lyne-pearlchain/lyne-pearlchain.stories.js
+++ src/components/lyne-pearlchain/lyne-pearlchain.stories.js
@@ -1,19 +1,151 @@
 import { h } from 'jsx-dom';
 import readme from './readme.md';
 
-const Template = (args) => (
-  <lyne-pearlchain
-    {...args}
-  />
+const legsData1 = {
+  legs: [
+    {
+      cancellation: false,
+      duration: 25
+    },
+    {
+      cancellation: false,
+      duration: 75
+    }
+  ]
+};
 
+const legsData2 = {
+  legs: [
+    {
+      cancellation: false,
+      duration: 25
+    },
+    {
+      cancellation: false,
+      duration: 10
+    },
+    {
+      cancellation: false,
+      duration: 65
+    }
+  ]
+};
+
+const legsData3 = {
+  legs: [
+    {
+      cancellation: false,
+      duration: 25
+    },
+    {
+      cancellation: false,
+      duration: 10
+    },
+    {
+      cancellation: false,
+      duration: 50
+    },
+    {
+      cancellation: false,
+      duration: 15
+    }
+  ]
+};
+
+const legsData4 = {
+  legs: [
+    {
+      cancellation: false,
+      duration: 25
+    },
+    {
+      cancellation: false,
+      duration: 10
+    },
+    {
+      cancellation: false,
+      duration: 8
+    },
+    {
+      cancellation: false,
+      duration: 15
+    },
+    {
+      cancellation: false,
+      duration: 42
+    }
+  ]
+};
+
+const legsData5 = {
+  legs: [
+    {
+      cancellation: false,
+      duration: 10
+    },
+    {
+      cancellation: false,
+      duration: 5
+    },
+    {
+      cancellation: false,
+      duration: 5
+    },
+    {
+      cancellation: false,
+      duration: 10
+    },
+    {
+      cancellation: false,
+      duration: 10
+    },
+    {
+      cancellation: false,
+      duration: 5
+    },
+    {
+      cancellation: false,
+      duration: 5
+    },
+    {
+      cancellation: false,
+      duration: 10
+    },
+    {
+      cancellation: false,
+      duration: 15
+    },
+    {
+      cancellation: false,
+      duration: 25
+    }
+  ]
+};
+
+export const FutureConnection = () => (
+  <lyne-pearlchain
+    legs={JSON.stringify(legsData5)}
+  />
 );
 
-export const lynePearlchain = Template.bind({});
+export const PastConnection = () => (
+  <lyne-pearlchain
+    legs={JSON.stringify(legsData5)}
+    status='past'
+  />
+);
+
+export const RunningConnection = () => (
+  <lyne-pearlchain
+    legs={JSON.stringify(legsData5)}
+    status='50'
+  />
+);
 
 export default {
   decorators: [
     (Story) => (
-      <div style={'width: 20rem; height: 20rem;'}>
+      <div style={'max-width: 20rem;'}>
         <Story/>
       </div>
     )
