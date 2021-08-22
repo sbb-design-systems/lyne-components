296508ee5d8a5fef326073634c1bc2f30f470e6f
Yves Tscherry <info@feerglas.com>
2021-08-22 10:40:46 +0200

    feat:  add stories for future,  past and running connections

--- src/components/lyne-pearlchain/lyne-pearlchain.stories.js
+++ src/components/lyne-pearlchain/lyne-pearlchain.stories.js
@@ -122,25 +122,89 @@ const legsData5 = {
   ]
 };
 
-export const FutureConnection = () => (
+const legsData = {
+  legsData0: '',
+  legsData1: JSON.stringify(legsData1),
+  legsData2: JSON.stringify(legsData2),
+  legsData3: JSON.stringify(legsData3),
+  legsData4: JSON.stringify(legsData4),
+  legsData5: JSON.stringify(legsData5)
+};
+
+const Template = (args) => (
   <lyne-pearlchain
-    legs={JSON.stringify(legsData5)}
+    {...args}
   />
 );
 
-export const PastConnection = () => (
-  <lyne-pearlchain
-    legs={JSON.stringify(legsData5)}
-    status='past'
-  />
-);
+export const FutureConnection = Template.bind({});
+export const PastConnection = Template.bind({});
+export const RunningConnection = Template.bind({});
 
-export const RunningConnection = () => (
-  <lyne-pearlchain
-    legs={JSON.stringify(legsData5)}
-    status='50'
-  />
-);
+const legs = {
+  control: {
+    labels: {
+      legsData0: '0 stops',
+      legsData1: '1 stop',
+      legsData2: '2 stops',
+      legsData3: '3 stops',
+      legsData4: '4 stops',
+      legsData5: '9 stops'
+    },
+    type: 'inline-radio'
+  },
+  mapping: legsData,
+  options: Object.keys(legsData)
+};
+
+const status = {
+  control: {
+    max: 100,
+    min: 0,
+    step: 1,
+    type: 'range'
+  }
+};
+
+const globalArgTypes = {
+  legs
+};
+
+const globalArgs = {
+  legs: 'legsData0'
+};
+
+FutureConnection.argTypes = {
+  ...globalArgTypes
+};
+
+FutureConnection.args = {
+  ...globalArgs
+};
+
+PastConnection.argTypes = {
+  ...globalArgTypes,
+  status: {
+    table: {
+      disable: true
+    }
+  }
+};
+
+PastConnection.args = {
+  ...globalArgs,
+  status: 'past'
+};
+
+RunningConnection.argTypes = {
+  ...globalArgTypes,
+  status
+};
+
+RunningConnection.args = {
+  ...globalArgs,
+  status: 50
+};
 
 export default {
   decorators: [
