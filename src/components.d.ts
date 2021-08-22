18478bf23eb59ae62fcbdcdbe3a6e86112880911
Yves Tscherry <info@feerglas.com>
2021-08-22 16:45:46 +0200

    chore: update after build

--- src/components.d.ts
+++ src/components.d.ts
@@ -87,7 +87,7 @@ export namespace Components {
         /**
           * Stringified JSON to define the legs of the pearl-chain. Format: `{"legs": [{"cancellation": true, "duration": 25}, ...]}` `duration`: number between 0 and 100. Duration of the leg is relative to the total travel time. Example: departure 16:30, change at 16:40, arrival at 17:00. So the change should have a duration of 33.33%. `cancellation`: if set, the leg will be marked as canceled.
          */
-        "legs"?: string;
+        "legs": string;
         /**
           * Define, if the pearlchain represents a connection in the past, in the future or if it is a currently running connection. If it is currently running, provide a number between 0 and 100, which will represent the current location on the pearl-chain.
          */
@@ -205,7 +205,7 @@ declare namespace LocalJSX {
         /**
           * Stringified JSON to define the legs of the pearl-chain. Format: `{"legs": [{"cancellation": true, "duration": 25}, ...]}` `duration`: number between 0 and 100. Duration of the leg is relative to the total travel time. Example: departure 16:30, change at 16:40, arrival at 17:00. So the change should have a duration of 33.33%. `cancellation`: if set, the leg will be marked as canceled.
          */
-        "legs"?: string;
+        "legs": string;
         /**
           * Define, if the pearlchain represents a connection in the past, in the future or if it is a currently running connection. If it is currently running, provide a number between 0 and 100, which will represent the current location on the pearl-chain.
          */
