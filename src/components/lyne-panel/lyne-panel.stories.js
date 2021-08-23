aba29096278a27261c7efe6af97c4ab9f4f12dd0
Yves Tscherry <info@feerglas.com>
2021-08-23 18:26:59 +0200

    feat: add empty stories file for panel

---
+++ src/components/lyne-panel/lyne-panel.stories.js
@@ -0,0 +1,30 @@
+import { h } from 'jsx-dom';
+import readme from './readme.md';
+
+// --- Component
+
+const Template = (args) => (
+  <lyne-panel {...args} />
+);
+
+export const lynePanel = Template.bind({});
+
+export default {
+  parameters: {
+    backgrounds: {
+      disable: true
+    },
+    chromatic: {
+      delay: 1000,
+      viewports: [
+        320,
+        764,
+        1201
+      ]
+    },
+    docs: {
+      extractComponentDescription: () => readme
+    }
+  },
+  title: 'lyne-panel'
+};
