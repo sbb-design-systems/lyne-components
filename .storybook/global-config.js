f748083493581a5114c66de02cd26fe2426bbfcb
Yves Tscherry <info@feerglas.com>
2021-08-24 13:20:49 +0200

    fix: add px unit to viewports, respect max of 1800 px

--- .storybook/global-config.js
+++ .storybook/global-config.js
@@ -1,11 +1,15 @@
 import * as DesignTokens from 'lyne-design-tokens/dist/js/tokens.es6';
 
+/**
+ * As per chromatic docu, viewport can be any whole number
+ * between 320 and 1800 pixels.
+ */
 export const viewports = [
-  DesignTokens.BreakpointZeroMax,
-  DesignTokens.BreakpointMicroMax,
-  DesignTokens.BreakpointSmallMax,
-  DesignTokens.BreakpointMediumMax,
-  DesignTokens.BreakpointLargeMax,
-  DesignTokens.BreakpointWideMax,
-  DesignTokens.BreakpointUltraMax
+  `${DesignTokens.BreakpointZeroMax}px`,
+  `${DesignTokens.BreakpointMicroMax}px`,
+  `${DesignTokens.BreakpointSmallMax}px`,
+  `${DesignTokens.BreakpointMediumMax}px`,
+  `${DesignTokens.BreakpointLargeMax}px`,
+  `${DesignTokens.BreakpointWideMax}px`,
+  `${DesignTokens.BreakpointUltraMin}px`
 ];
