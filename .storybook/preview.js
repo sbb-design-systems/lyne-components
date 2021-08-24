f83b108c0da6bb2bc9176cbc26a2f94dbe7f7daa
Yves Tscherry <info@feerglas.com>
2021-08-24 13:25:42 +0200

    fix: define chormatic config globally

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,20 @@
+import * as DesignTokens from 'lyne-design-tokens/dist/js/tokens.es6';
 import {defineCustomElements} from '../dist/esm/loader';
 
 defineCustomElements();
+
+export const parameters = {
+  // Set the viewports in Chromatic globally.
+  chromatic: {
+    delay: 1000,
+    viewports: [
+      `${DesignTokens.BreakpointZeroMax}px`,
+      `${DesignTokens.BreakpointMicroMax}px`,
+      `${DesignTokens.BreakpointSmallMax}px`,
+      `${DesignTokens.BreakpointMediumMax}px`,
+      `${DesignTokens.BreakpointLargeMax}px`,
+      `${DesignTokens.BreakpointWideMax}px`,
+      `${DesignTokens.BreakpointUltraMin}px`
+    ]
+  }
+};
