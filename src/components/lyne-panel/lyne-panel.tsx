b3ffbd363ef27ea55ac58d86d8038dcde9f77412
Yves Tscherry <info@feerglas.com>
2021-08-24 10:54:50 +0200

    feat: make tag for the text configurable via prop

--- src/components/lyne-panel/lyne-panel.tsx
+++ src/components/lyne-panel/lyne-panel.tsx
@@ -3,6 +3,7 @@ import {
   h,
   Prop
 } from '@stencil/core';
+import { InterfacePanelAttributes } from './lyne-panel.custom.d';
 
 @Component({
   shadow: true,
@@ -15,15 +16,27 @@ export class LynePanel {
   /** The text to show in the panel */
   @Prop() public text? = 'Text inside of the panel.';
 
+  /** Id which is sent in the click event payload for the button*/
+  @Prop() public eventId?: string;
+
+  /** The tag to use for the text element */
+  @Prop() public tag?: InterfacePanelAttributes['tag'] = 'p';
+
   public render(): JSX.Element {
+    const TAGNAME = this.tag;
+
     return (
       <div class='lyne-panel'>
-        <p>{this.text}</p>
+        <TAGNAME class='lyne-panel__text'>{this.text}</TAGNAME>
+
         <lyne-button
+          class='lyne-panel__button'
           label='Button text'
           variant='secondary-negative'
           size='small'
+          event-id={this.eventId}
         ></lyne-button>
+
       </div>
     );
   }
