bb11ace9d6eda007f427f44a1bf2b5af700652cb
Yves Tscherry <info@feerglas.com>
2021-08-24 10:59:44 +0200

    test: fix texts

--- src/components/lyne-panel/lyne-panel.spec.ts
+++ src/components/lyne-panel/lyne-panel.spec.ts
@@ -7,13 +7,19 @@ describe('lyne-panel', () => {
       root
     } = await newSpecPage({
       components: [LynePanel],
-      html: '<lyne-panel />'
+      html: '<lyne-panel text= "Example panel text"/>'
     });
 
     expect(root)
       .toEqualHtml(`
-        <lyne-panel>
+        <lyne-panel text= "Example panel text">
           <mock:shadow-root>
+            <div class="lyne-panel">
+              <p class="lyne-panel__text">
+                Example panel text
+              </p>
+              <lyne-button class="lyne-panel__button" label="Button text" size="small" variant="secondary-negative"></lyne-button>
+            </div>
           </mock:shadow-root>
         </lyne-panel>
       `);
