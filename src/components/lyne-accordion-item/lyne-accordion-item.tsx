49b6858dc2d94238d733dc9d7f82a05f9f644eb6
Yves Tscherry <info@feerglas.com>
2021-08-24 17:31:37 +0200

    feat: add props, slots and basic markup

--- src/components/lyne-accordion-item/lyne-accordion-item.tsx
+++ src/components/lyne-accordion-item/lyne-accordion-item.tsx
@@ -1,7 +1,15 @@
 import {
   Component,
-  h
+  h,
+  Prop
 } from '@stencil/core';
+import chevronIcon from 'lyne-icons/dist/icons/chevron-down-small.svg';
+import { InterfaceAccordionItemAttributes } from './lyne-accordion-item.custom.d';
+
+/**
+ * @slot icon - Pass an svg to display an icon left to the title
+ * @slot content - Pass html-content to show as the content of the accordion
+ */
 
 @Component({
   shadow: true,
@@ -11,11 +19,39 @@ import {
 
 export class LyneAccordionItem {
 
+  /**
+   * Text to show as title for the accordion
+   */
+  @Prop() public heading!: string;
+
+  /** Heading level */
+  @Prop() public headingLevel?: InterfaceAccordionItemAttributes['level'] = '1';
+
   public render(): JSX.Element {
+    const HEADING_TAGNAME = `h${this.headingLevel}`;
 
     return (
-      <div>
-        accordion item
+      <div class='accordion-item'>
+
+        <div class='accordion-item__head'>
+
+          <div class='accordion-item__icon'>
+            <slot name='icon' />
+          </div>
+
+          <HEADING_TAGNAME class='accordion-item__title'>{this.heading}</HEADING_TAGNAME>
+
+          <div
+            class='accordion-item__arrow'
+            innerHTML={chevronIcon}
+          />
+
+        </div>
+
+        <div class='accordion-item__body'>
+          <slot name='content' />
+        </div>
+
       </div>
     );
   }
