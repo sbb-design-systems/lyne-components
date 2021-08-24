1723be3015b6d0f439fe02971a02b1fae6a6fc10
Yves Tscherry <info@feerglas.com>
2021-08-24 13:16:30 +0200

    chore: add custom storybooks config file with viewports for chromatic

---
+++ .storybook/global-config.js
@@ -0,0 +1,11 @@
+import * as DesignTokens from 'lyne-design-tokens/dist/js/tokens.es6';
+
+export const viewports = [
+  DesignTokens.BreakpointZeroMax,
+  DesignTokens.BreakpointMicroMax,
+  DesignTokens.BreakpointSmallMax,
+  DesignTokens.BreakpointMediumMax,
+  DesignTokens.BreakpointLargeMax,
+  DesignTokens.BreakpointWideMax,
+  DesignTokens.BreakpointUltraMax
+];
