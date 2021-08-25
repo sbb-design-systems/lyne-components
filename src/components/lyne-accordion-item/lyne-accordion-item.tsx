4b0c6ff73560865be4ae899f0e82a5b263c4284f
Yves Tscherry <info@feerglas.com>
2021-08-25 21:07:52 +0200

    feat: add functionality to open and close accordion

--- src/components/lyne-accordion-item/lyne-accordion-item.tsx
+++ src/components/lyne-accordion-item/lyne-accordion-item.tsx
@@ -1,3 +1,14 @@
+/**
+ * TODO:
+ *
+ * - hover states
+ * - a11y
+ * - accordion container
+ * - storybook
+ * - Slot description is not rendering on readme
+ * - chevron-down-small has different name in figma. check doku page
+ */
+
 import {
   Component,
   Element,
@@ -8,8 +19,11 @@ import chevronIcon from 'lyne-icons/dist/icons/chevron-down-small.svg';
 import { InterfaceAccordionItemAttributes } from './lyne-accordion-item.custom.d';
 
 /**
- * @slot icon - Pass an svg to display an icon left to the title
- * @slot content - Pass html-content to show as the content of the accordion
+ * @slot icon - Pass an svg to display an icon left to the title.
+ * @slot content - Pass html-content to show as the content of the accordion.
+ * Use flat html: `<p>Some text</p><p>Some other text</p>` is ok.
+ * This instead would not be ok:
+ * <div><p>Some text</p><p>Some other text</p></div>
  */
 
 const iconSlotName = 'icon';
@@ -42,12 +56,27 @@ export class LyneAccordionItem {
    */
   @Prop() public last?: boolean;
 
+  /**
+   * Set to true to open the accordion item. Set to false to close it.
+   */
+  @Prop({
+    reflect: true
+  }) public open?: boolean;
+
   @Element() private _element: HTMLElement;
+
+  private _toggleAccordion(): void {
+    this.open = !this.open;
+  }
 
   public render(): JSX.Element {
     const HEADING_TAGNAME = `h${this.headingLevel}`;
 
     const hasIconInSlot = this._element.querySelector(`svg[slot="${iconSlotName}"`) !== null;
+
+    const iconClass = hasIconInSlot
+      ? ' accordion-item--has-icon'
+      : '';
 
     let firstAndLastClass = '';
 
@@ -57,14 +86,17 @@ export class LyneAccordionItem {
       firstAndLastClass = ' accordion-item--last';
     }
 
-    const iconClass = hasIconInSlot
-      ? ' accordion-item--has-icon'
-      : '';
+    const openClass = this.open
+      ? ' accordion-item--open'
+      : ' accordion-item--closed';
 
     return (
-      <div class={`accordion-item${firstAndLastClass}${iconClass}`}>
+      <div class={`accordion-item${firstAndLastClass}${iconClass}${openClass}`}>
 
-        <HEADING_TAGNAME class='accordion-item__heading'>
+        <HEADING_TAGNAME
+          class='accordion-item__heading'
+          onClick={(): void => this._toggleAccordion()}
+        >
 
           <button class='accordion-item__button'>
 
