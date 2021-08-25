ee66410d6455521ead4fa31aa398f23e1a7080f5
Yves Tscherry <info@feerglas.com>
2021-08-25 20:36:37 +0200

    feat: check if icon slot is filled and set class based on it

--- src/components/lyne-accordion-item/lyne-accordion-item.tsx
+++ src/components/lyne-accordion-item/lyne-accordion-item.tsx
@@ -1,5 +1,6 @@
 import {
   Component,
+  Element,
   h,
   Prop
 } from '@stencil/core';
@@ -10,6 +11,8 @@ import { InterfaceAccordionItemAttributes } from './lyne-accordion-item.custom.d
  * @slot icon - Pass an svg to display an icon left to the title
  * @slot content - Pass html-content to show as the content of the accordion
  */
+
+const iconSlotName = 'icon';
 
 @Component({
   shadow: true,
@@ -39,8 +42,12 @@ export class LyneAccordionItem {
    */
   @Prop() public last?: boolean;
 
+  @Element() private _element: HTMLElement;
+
   public render(): JSX.Element {
     const HEADING_TAGNAME = `h${this.headingLevel}`;
+
+    const hasIconInSlot = this._element.querySelector(`svg[slot="${iconSlotName}"`) !== null;
 
     let firstAndLastClass = '';
 
@@ -50,15 +57,19 @@ export class LyneAccordionItem {
       firstAndLastClass = ' accordion-item--last';
     }
 
+    const iconClass = hasIconInSlot
+      ? ' accordion-item--has-icon'
+      : '';
+
     return (
-      <div class={`accordion-item${firstAndLastClass}`}>
+      <div class={`accordion-item${firstAndLastClass}${iconClass}`}>
 
         <HEADING_TAGNAME class='accordion-item__heading'>
 
           <button class='accordion-item__button'>
 
             <div class='accordion-item__icon'>
-              <slot name='icon' />
+              <slot name={iconSlotName} />
             </div>
 
             <span class='accordion-item__title'>{this.heading}</span>
