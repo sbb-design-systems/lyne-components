8efd152ca8ca84742f1c2d360bda0937d293bd09
Yves Tscherry <info@feerglas.com>
2021-08-26 23:30:48 +0200

    test: use guid mock, fix render test

--- src/components/lyne-accordion-item/lyne-accordion-item.spec.ts
+++ src/components/lyne-accordion-item/lyne-accordion-item.spec.ts
@@ -1,5 +1,18 @@
+import {
+  axe,
+  toHaveNoViolations
+} from 'jest-axe';
+
+jest.mock('../../global/guid');
+
 import { LyneAccordionItem } from './lyne-accordion-item';
 import { newSpecPage } from '@stencil/core/testing';
+
+beforeEach(() => {
+  jest
+    .spyOn(global.Math, 'random')
+    .mockReturnValue(0.123456789);
+});
 
 describe('lyne-accordion-item', () => {
   it('renders', async () => {
@@ -14,6 +27,22 @@ describe('lyne-accordion-item', () => {
       .toEqualHtml(`
         <lyne-accordion-item>
           <mock:shadow-root>
+            <div class="accordion-item accordion-item--closed">
+              <h1 class="accordion-item__heading">
+                <button aria-controls="XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX_body" class="accordion-item__button">
+                  <div class="accordion-item__icon">
+                    <slot name="icon"></slot>
+                  </div>
+                  <span class="accordion-item__title"></span>
+                  <div class="accordion-item__chevron"></div>
+                </button>
+              </h1>
+              <div aria-hidden="true" class="accordion-item__body" id="XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX_body">
+                <div class="accordion-item__body-inner">
+                  <slot name="content"></slot>
+                </div>
+              </div>
+            </div>
           </mock:shadow-root>
         </lyne-accordion-item>
       `);
