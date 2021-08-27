8694e4aa40c8b945f1b30e205734e00ca9a1676d
Yves Tscherry <info@feerglas.com>
2021-08-27 16:24:37 +0200

    feat: finish story

--- src/components/lyne-accordion/lyne-accordion.stories.js
+++ src/components/lyne-accordion/lyne-accordion.stories.js
@@ -1,47 +1,79 @@
+import events from '../lyne-accordion-item/lyne-accordion-item.events.ts';
 import { h } from 'jsx-dom';
 import readme from './readme.md';
 
+const ItemTemplate = (args) => (
+  <lyne-accordion-item {...args}>
+    <p slot='content'>1 Donec sed odio operae, eu vulputate felis rhoncus. Curabitur est gravida et libero vitae dictum. Me non paenitet nullum festiviorem excogitasse ad hoc.</p>
+    <p slot='content'>2 Donec sed odio operae, eu vulputate felis rhoncus. Curabitur est gravida et libero vitae dictum. Me non paenitet nullum festiviorem excogitasse ad hoc.</p>
+    <p slot='content'>3 Quis aute iure reprehenderit in voluptate velit esse. Ab illo tempore, ab est sed immemorabili. Non equidem invideo, lit aliquet. Nihilne te nocturnum praesidium Palati, nihil urbis vigiliae.</p>
+
+    {args.icon &&
+      <svg slot='icon' width='24' height='24' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' clip-rule='evenodd' d='m17.8436,12.1382-3.99-3.99196-.7072.70693,3.1366,3.13823H5v1h11.287l-3.1413,3.1555.7086.7056,3.99-4.008.3519-.3535-.3526-.3528z'></path></svg>
+    }
+  </lyne-accordion-item>
+);
+
 const Template = (args) => (
   <lyne-accordion {...args}>
-    <lyne-accordion-item
-      event-id='id1'
-      first
-      heading='Accordion Item 1'
-      heading-level='2'
-      open={false}
-    >
-      <p slot='content'>1 Donec sed odio operae, eu vulputate felis rhoncus. Curabitur est gravida et libero vitae dictum. Me non paenitet nullum festiviorem excogitasse ad hoc.</p>
-      <p slot='content'>2 Donec sed odio operae, eu vulputate felis rhoncus. Curabitur est gravida et libero vitae dictum. Me non paenitet nullum festiviorem excogitasse ad hoc.</p>
-      <p slot='content'>3 Quis aute iure reprehenderit in voluptate velit esse. Ab illo tempore, ab est sed immemorabili. Non equidem invideo, lit aliquet. Nihilne te nocturnum praesidium Palati, nihil urbis vigiliae.</p>
+    {args.items.map((item, index) => {
+      if (index === 0) {
+        item.first = true;
+      } else if (index === args.items.length - 1) {
+        item.last = true;
+      }
 
-      <svg slot='icon' width='24' height='24' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' clip-rule='evenodd' d='m17.8436,12.1382-3.99-3.99196-.7072.70693,3.1366,3.13823H5v1h11.287l-3.1413,3.1555.7086.7056,3.99-4.008.3519-.3535-.3526-.3528z'></path></svg>
-    </lyne-accordion-item>
-    <lyne-accordion-item
-      event-id='id2'
-      heading='Accordion Item 2'
-      heading-level='2'
-      open={false}
-    >
-      <p slot='content'>1 Donec sed odio operae, eu vulputate felis rhoncus. Curabitur est gravida et libero vitae dictum. Me non paenitet nullum festiviorem excogitasse ad hoc.</p>
-      <p slot='content'>2 Donec sed odio operae, eu vulputate felis rhoncus. Curabitur est gravida et libero vitae dictum. Me non paenitet nullum festiviorem excogitasse ad hoc.</p>
-      <p slot='content'>3 Quis aute iure reprehenderit in voluptate velit esse. Ab illo tempore, ab est sed immemorabili. Non equidem invideo, lit aliquet. Nihilne te nocturnum praesidium Palati, nihil urbis vigiliae.</p>
-    </lyne-accordion-item>
-    <lyne-accordion-item
-      event-id='id3'
-      heading='Accordion Item 3'
-      heading-level='2'
-      last
-      open={true}
-    >
-      <p slot='content'>1 Donec sed odio operae, eu vulputate felis rhoncus. Curabitur est gravida et libero vitae dictum. Me non paenitet nullum festiviorem excogitasse ad hoc.</p>
-      <p slot='content'>2 Donec sed odio operae, eu vulputate felis rhoncus. Curabitur est gravida et libero vitae dictum. Me non paenitet nullum festiviorem excogitasse ad hoc.</p>
-      <p slot='content'>3 Quis aute iure reprehenderit in voluptate velit esse. Ab illo tempore, ab est sed immemorabili. Non equidem invideo, lit aliquet. Nihilne te nocturnum praesidium Palati, nihil urbis vigiliae.</p>
-    </lyne-accordion-item>
+      return (
+        <ItemTemplate {...item}/>
+      );
+    })}
   </lyne-accordion>
 );
 
 export const Default = Template.bind({});
-export const NonWhiteBackground = Template.bind({});
+export const NonWhiteBackground = Template.bind();
+
+const items = [
+  {
+    'event-id': 'id1',
+    'heading': 'Accordion Item 1',
+    'heading-level': '2',
+    'icon': true,
+    'open': false
+  },
+  {
+    'event-id': 'id2',
+    'heading': 'Accordion Item 2',
+    'heading-level': '2',
+    'icon': false,
+    'open': false
+  },
+  {
+    'event-id': 'id3',
+    'heading': 'Accordion Item 3',
+    'heading-level': '2',
+    'icon': true,
+    'open': false
+  }
+];
+
+const table = {
+  disable: true
+};
+
+Default.argTypes = {
+  'items': {
+    table
+  },
+  'non-white-background': {
+    table
+  }
+};
+
+Default.args = {
+  items,
+  'non-white-background': false
+};
 
 Default.decorators = [
   (Story) => (
@@ -51,7 +83,17 @@ Default.decorators = [
   )
 ];
 
+NonWhiteBackground.argTypes = {
+  'items': {
+    table
+  },
+  'non-white-background': {
+    table
+  }
+};
+
 NonWhiteBackground.args = {
+  items,
   'non-white-background': true
 };
 
@@ -65,6 +107,12 @@ NonWhiteBackground.decorators = [
 
 export default {
   parameters: {
+    actions: {
+      handles: [
+        events.didOpen,
+        events.didClose
+      ]
+    },
     docs: {
       extractComponentDescription: () => readme
     }
