a313b60d7b5187ccbc3baf3fd46d12c3c75cc31b
Yves Tscherry <info@feerglas.com>
2021-08-28 07:53:22 +0200

    feat: add events for willOpen and willClose

--- src/components/lyne-accordion-item/lyne-accordion-item.events.ts
+++ src/components/lyne-accordion-item/lyne-accordion-item.events.ts
@@ -1,4 +1,6 @@
 export default {
   didClose: 'lyne-accordion-item_did-close',
-  didOpen: 'lyne-accordion-item_did-open'
+  didOpen: 'lyne-accordion-item_did-open',
+  willClose: 'lyne-accordion-item_will-close',
+  willOpen: 'lyne-accordion-item_will-open'
 };
