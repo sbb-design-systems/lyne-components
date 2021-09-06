b326d8aaa4680fe541bdf9d46e502df6dba23946
Yves Tscherry <info@feerglas.com>
2021-09-06 09:04:11 +0200

    feat: copy and transform stories file

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,30 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const jsx = require('jsx-transform');
+const components = require('../dist/documentation/jsonDocs.json').components;
+
+const copyPatternsStories = [];
+
+components.forEach((comp) => {
+
+  // copy and transform component story file
+  copyPatternsStories.push({
+    from: path.resolve(__dirname, `../src/components/${comp.tag}/${comp.tag}.stories.js`),
+    to: path.resolve(__dirname, `../dist/collection/components/${comp.tag}/${comp.tag}.stories.js`),
+    transform(content, absoluteFrom) {
+      return jsx.fromString(content.toString(), {
+        factory: 'mercury.h'
+      });
+    }
+  });
+
+  // copy component readme file
+  copyPatternsStories.push({
+    from: path.resolve(__dirname, `../src/components/${comp.tag}/readme.md`),
+    to: path.resolve(__dirname, `../dist/collection/components/${comp.tag}/readme.md`)
+  });
+});
+
 module.exports = {
   stories: [
     '../src/**/*.stories.js',
@@ -10,5 +37,14 @@ module.exports = {
   ],
   features: {
     postcss: false
-  }
+  },
+  webpackFinal: async (config, { configType }) => {
+    config.plugins.push(
+      new CopyWebpackPlugin({
+        patterns: copyPatternsStories
+      }),
+    );
+
+    return config;
+  },
 };
