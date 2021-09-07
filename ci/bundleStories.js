7965b09b85af6d9f0e52a3f8b500c395fb17cd33
Yves Tscherry <info@feerglas.com>
2021-09-07 17:13:58 +0200

    fix: remove window mock, define jsx-dom as external

--- ci/bundleStories.js
+++ ci/bundleStories.js
@@ -6,25 +6,9 @@ const buildFiles = (files) => {
   esbuild
     .buildSync({
       bundle: true,
-      define: {
-
-        /**
-         * since we wanna use it in an SSG tool, we most probably won't have
-         * the window context, so we make sure we have no references to window
-         * in the bundled code.
-         */
-
-        window: JSON.stringify({
-          location: {
-            href: '/'
-          },
-          navigator: {
-            userAgent: 'gridsome'
-          }
-        })
-      },
       entryNames: '[name]',
       entryPoints: files,
+      external: ['jsx-dom'],
       format: 'cjs',
       jsxFactory: 'h',
       loader: {
