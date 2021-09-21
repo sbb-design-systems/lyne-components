b5b007787a7c5e78de58014b9ff34af93d0b00c0
Yves Tscherry <info@feerglas.com>
2021-09-21 14:27:52 +0200

    feat: add picture config

--- src/components/lyne-teaser-hero/lyne-teaser-hero.tsx
+++ src/components/lyne-teaser-hero/lyne-teaser-hero.tsx
@@ -10,14 +10,68 @@ import {
 })
 
 export class LyneTeaserHero {
+  private _pictureSizesConfig = {
+    breakpoints: [
+      {
+        image: {
+          height: '675',
+          width: '1200'
+        },
+        mediaQueries: [
+          {
+            conditionFeature: 'min-width',
+            conditionFeatureValue: {
+              lyneDesignToken: true,
+              value: 'BreakpointLargeMin'
+            },
+            conditionOperator: false
+          }
+        ]
+      },
+      {
+        image: {
+          height: '549',
+          width: '976'
+        },
+        mediaQueries: [
+          {
+            conditionFeature: 'min-width',
+            conditionFeatureValue: {
+              lyneDesignToken: true,
+              value: 'BreakpointSmallMin'
+            },
+            conditionOperator: false
+          }
+        ]
+      },
+      {
+        image: {
+          height: '320',
+          width: '320'
+        },
+        mediaQueries: [
+          {
+            conditionFeature: 'max-width',
+            conditionFeatureValue: {
+              lyneDesignToken: true,
+              value: 'BreakpointMicroMax'
+            },
+            conditionOperator: false
+          }
+        ]
+      }
+    ]
+  };
 
   public render(): JSX.Element {
+    console.log(this._pictureSizesConfig);
 
     return (
       <div class='taser-hero'>
         <lyne-image
           class='teaser-hero__image'
           imageSrcExamples='https://cdn.img.sbb.ch/content/dam/internet/lyne/Billetkontrolle.jpg'
+          pictureSizesConfig={JSON.stringify(this._pictureSizesConfig)}
           customFocalPoint={true}
           hideFromScreenreader={true}
           imageSrc=''
