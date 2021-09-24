c7d2cb554ad5b4b2ce42cbe30bdcf004c16fa3fa
Yves Tscherry <info@feerglas.com>
2021-09-24 11:34:11 +0200

    test: fix unit test

--- src/components/lyne-teaser-hero/lyne-teaser-hero.spec.ts
+++ src/components/lyne-teaser-hero/lyne-teaser-hero.spec.ts
@@ -7,16 +7,16 @@ describe('lyne-teaser-hero', () => {
       root
     } = await newSpecPage({
       components: [LyneTeaserHero],
-      html: '<lyne-teaser-hero></lyne-teaser-hero>'
+      html: '<lyne-teaser-hero button-text="Button text" image-src="https://cdn.img.sbb.ch/content/dam/internet/lyne/Billetkontrolle.jpg" text="Panel text"></lyne-teaser-hero>'
     });
 
     expect(root)
       .toEqualHtml(`
-        <lyne-teaser-hero>
+        <lyne-teaser-hero button-text="Button text" image-src="https://cdn.img.sbb.ch/content/dam/internet/lyne/Billetkontrolle.jpg" text="Panel text">
           <mock:shadow-root>
             <div class="taser-hero">
               <lyne-image class="teaser-hero__image" customfocalpoint="" hidefromscreenreader="" imagesrc="https://cdn.img.sbb.ch/content/dam/internet/lyne/Billetkontrolle.jpg" loading="eager" lqip="" performancemark="" picturesizesconfig="{&quot;breakpoints&quot;:[{&quot;image&quot;:{&quot;height&quot;:2579,&quot;width&quot;:2579},&quot;mediaQueries&quot;:[{&quot;conditionFeature&quot;:&quot;min-width&quot;,&quot;conditionFeatureValue&quot;:{&quot;lyneDesignToken&quot;:true,&quot;value&quot;:&quot;breakpoint-ultra-min&quot;},&quot;conditionOperator&quot;:false}]},{&quot;image&quot;:{&quot;height&quot;:1439,&quot;width&quot;:1439},&quot;mediaQueries&quot;:[{&quot;conditionFeature&quot;:&quot;max-width&quot;,&quot;conditionFeatureValue&quot;:{&quot;lyneDesignToken&quot;:true,&quot;value&quot;:&quot;breakpoint-wide-max&quot;},&quot;conditionOperator&quot;:false}]},{&quot;image&quot;:{&quot;height&quot;:1279,&quot;width&quot;:1279},&quot;mediaQueries&quot;:[{&quot;conditionFeature&quot;:&quot;max-width&quot;,&quot;conditionFeatureValue&quot;:{&quot;lyneDesignToken&quot;:true,&quot;value&quot;:&quot;breakpoint-large-max&quot;},&quot;conditionOperator&quot;:false}]},{&quot;image&quot;:{&quot;height&quot;:1023,&quot;width&quot;:1023},&quot;mediaQueries&quot;:[{&quot;conditionFeature&quot;:&quot;max-width&quot;,&quot;conditionFeatureValue&quot;:{&quot;lyneDesignToken&quot;:true,&quot;value&quot;:&quot;breakpoint-medium-max&quot;},&quot;conditionOperator&quot;:false}]},{&quot;image&quot;:{&quot;height&quot;:839,&quot;width&quot;:839},&quot;mediaQueries&quot;:[{&quot;conditionFeature&quot;:&quot;max-width&quot;,&quot;conditionFeatureValue&quot;:{&quot;lyneDesignToken&quot;:true,&quot;value&quot;:&quot;breakpoint-small-max&quot;},&quot;conditionOperator&quot;:false}]},{&quot;image&quot;:{&quot;height&quot;:599,&quot;width&quot;:599},&quot;mediaQueries&quot;:[{&quot;conditionFeature&quot;:&quot;max-width&quot;,&quot;conditionFeatureValue&quot;:{&quot;lyneDesignToken&quot;:true,&quot;value&quot;:&quot;breakpoint-micro-max&quot;},&quot;conditionOperator&quot;:false}]},{&quot;image&quot;:{&quot;height&quot;:359,&quot;width&quot;:359},&quot;mediaQueries&quot;:[{&quot;conditionFeature&quot;:&quot;max-width&quot;,&quot;conditionFeatureValue&quot;:{&quot;lyneDesignToken&quot;:true,&quot;value&quot;:&quot;breakpoint-zero-max&quot;},&quot;conditionOperator&quot;:false}]}]}" variant="teaser-hero"></lyne-image>
-              <lyne-panel buttontext="Sample button text" class="teaser-hero__panel" text="Sample panel text"></lyne-panel>
+              <lyne-panel buttontext="Button text" class="teaser-hero__panel" text="Panel text"></lyne-panel>
             </div>
           </mock:shadow-root>
         </lyne-teaser-hero>
