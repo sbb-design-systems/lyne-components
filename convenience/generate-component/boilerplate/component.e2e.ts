6e09f79c0cf1a9a1590c6334e0eadbdc76e78f50
Yves Tscherry <info@feerglas.com>
2021-10-12 10:39:48 +0200

    feat: add boilerplate e2e file

---
+++ convenience/generate-component/boilerplate/component.e2e.ts
@@ -0,0 +1,16 @@
+import { newE2EPage } from '@stencil/core/testing';
+
+describe('__name__', () => {
+  let element,
+    page;
+
+  it('renders', async () => {
+    page = await newE2EPage();
+    await page.setContent('<__name__></__name__>');
+
+    element = await page.find('__name__');
+    expect(element)
+      .toHaveClass('hydrated');
+  });
+
+});
