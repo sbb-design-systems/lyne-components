839ae704df946f9f5f2da7eb44a18d24ce2f5857
Yves Tscherry <info@feerglas.com>
2021-10-12 10:39:42 +0200

    feat: add boilerplate unit test file

---
+++ convenience/generate-component/boilerplate/component.spec.ts
@@ -0,0 +1,23 @@
+import { __nameUpperCase__ } from './__name__';
+import { newSpecPage } from '@stencil/core/testing';
+
+describe('__name__', () => {
+  it('renders', async () => {
+    const {
+      root
+    } = await newSpecPage({
+      components: [__nameUpperCase__],
+      html: '<__name__ />'
+    });
+
+    expect(root)
+      .toEqualHtml(`
+        <__name__>
+          <mock:shadow-root>
+            <button class="some-class"></button>
+          </mock:shadow-root>
+        </__name__>
+      `);
+  });
+
+});
