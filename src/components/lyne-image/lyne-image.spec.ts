54482e85592de4f6ee510445fbb849382361cfc6
Yves Tscherry <info@feerglas.com>
2021-10-13 17:03:55 +0200

    test: fix unit test

--- src/components/lyne-image/lyne-image.spec.ts
+++ src/components/lyne-image/lyne-image.spec.ts
@@ -15,9 +15,9 @@ describe('lyne-image', () => {
       .toEqualHtml(`
         <lyne-image image-src="${images[0]}">
           <mock:shadow-root>
-            <figure class="lyne-image__figure">
-              <div class="lyne-image__wrapper">
-                <img alt="" class="lyne-image__blur-hash" decoding="auto" height="562" loading="eager" src="${images[0]}?blur=100&amp;w=100&amp;h=56" width="1000">
+            <figure class="image__figure">
+              <div class="image__wrapper">
+                <img alt="" class="image__blur-hash" decoding="auto" height="562" loading="eager" src="${images[0]}?blur=100&amp;w=100&amp;h=56" width="1000">
                 <picture>
                   <source media="(min-width: 64rem)" sizes="1200px" srcset="${images[0]}?&amp;fm=avif&amp;auto&amp;w=1200&amp;h=675 1200w, ${images[0]}?&amp;fm=avif&amp;auto&amp;w=2400&amp;h=1350 2400w" type="image/avif">
                   <source media="(min-width: 64rem)" sizes="1200px" srcset="${images[0]}?auto=format,compress,cs=tinysrgb&amp;w=1200&amp;h=675&amp;q=45 1200w, ${images[0]}?auto=format,compress,cs=tinysrgb&amp;w=2400&amp;h=1350&amp;q=20 2400w">
@@ -25,7 +25,7 @@ describe('lyne-image', () => {
                   <source media="(min-width: 37.5rem)" sizes="976px" srcset="${images[0]}?auto=format,compress,cs=tinysrgb&amp;w=976&amp;h=549&amp;q=45 976w, ${images[0]}?auto=format,compress,cs=tinysrgb&amp;w=1952&amp;h=1098&amp;q=20 1952w">
                   <source media="(max-width: 37.4375rem)" sizes="320px" srcset="${images[0]}?&amp;fm=avif&amp;auto&amp;w=320&amp;h=180 320w, ${images[0]}?&amp;fm=avif&amp;auto&amp;w=640&amp;h=360 640w" type="image/avif">
                   <source media="(max-width: 37.4375rem)" sizes="320px" srcset="${images[0]}?auto=format,compress,cs=tinysrgb&amp;w=320&amp;h=180&amp;q=45 320w, ${images[0]}?auto=format,compress,cs=tinysrgb&amp;w=640&amp;h=360&amp;q=20 640w">
-                  <img class="lyne-image__img" decoding="auto" height="562" loading="eager" src="${images[0]}" width="1000">
+                  <img class="image__img" decoding="auto" height="562" loading="eager" src="${images[0]}" width="1000">
                 </picture>
               </div>
             </figure>
