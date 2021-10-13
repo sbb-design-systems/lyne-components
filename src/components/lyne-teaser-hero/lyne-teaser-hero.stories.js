41a686e89ebcb9b76fa907d281f8b1aa5d71d840
Yves Tscherry <info@feerglas.com>
2021-10-13 16:44:23 +0200

    docs: add additional story to open link in new window

--- src/components/lyne-teaser-hero/lyne-teaser-hero.stories.js
+++ src/components/lyne-teaser-hero/lyne-teaser-hero.stories.js
@@ -2,6 +2,30 @@ import { h } from 'jsx-dom';
 import images from '../../global/images';
 import readme from './readme.md';
 
+// --- Controls
+const imageLoading = {
+  control: {
+    type: 'inline-radio'
+  },
+  options: [
+    'eager',
+    'lazy'
+  ],
+  table: {
+    category: 'Performance'
+  }
+};
+
+const openInNewWindowControl = {
+  control: {
+    type: 'inline-radio'
+  },
+  options: [
+    'true',
+    'false'
+  ]
+};
+
 // --- Component
 
 const Template = (args) => (
@@ -9,10 +33,35 @@ const Template = (args) => (
 );
 
 export const defaultTeaser = Template.bind({});
+export const openInNewWindow = Template.bind({});
+
+defaultTeaser.argTypes = {
+  'image-loading': imageLoading,
+  'open-in-new-window': openInNewWindowControl
+};
 
 defaultTeaser.args = {
   'button-text': 'Button text',
+  'image-loading': imageLoading.options[0],
   'image-src': images[0],
+  'link': 'https://www.sbb.ch',
+  'new-window-info-text': '',
+  'open-in-new-window': openInNewWindowControl.options[1],
+  'text': 'Panel text'
+};
+
+openInNewWindow.argTypes = {
+  'image-loading': imageLoading,
+  'open-in-new-window': openInNewWindowControl
+};
+
+openInNewWindow.args = {
+  'button-text': 'Button text',
+  'image-loading': imageLoading.options[0],
+  'image-src': images[0],
+  'link': 'https://www.sbb.ch',
+  'new-window-info-text': 'Link öffnet in neuem Fenster.',
+  'open-in-new-window': openInNewWindowControl.options[0],
   'text': 'Panel text'
 };
 
