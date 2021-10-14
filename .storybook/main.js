2c37194d2572342b8ae3f7efb1e7b9af5019ac38
Yves Tscherry <info@feerglas.com>
2021-10-14 16:04:06 +0200

    feat: register breakpoints addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,8 @@ module.exports = {
   addons: [
     '@storybook/addon-essentials',
     '@storybook/addon-a11y',
-    '@pxblue/storybook-rtl-addon/register'
+    '@pxblue/storybook-rtl-addon/register',
+    'storybook-addon-breakpoints'
   ],
   features: {
     postcss: false
