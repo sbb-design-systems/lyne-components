52881c3ea043f7413c91612de90ba630d5722380
Marius Bleuer <marius.bleuer@unic.com>
2021-11-02 09:55:21 +0100

    feat: adding first test

--- src/components/lyne-link-list/lyne-link-list.spec.ts
+++ src/components/lyne-link-list/lyne-link-list.spec.ts
@@ -1,3 +1,5 @@
+jest.mock('../../global/guid');
+
 import { LyneLinkList } from './lyne-link-list';
 import { newSpecPage } from '@stencil/core/testing';
 
@@ -7,17 +9,59 @@ describe('lyne-link-list', () => {
       root
     } = await newSpecPage({
       components: [LyneLinkList],
-      html: '<lyne-link-list />'
+      html: `<lyne-link-list title-level="2" title-text="Help &amp; Contact" variant="positive">
+                <li class='link-list__item' slot='link-list__item'>
+                  <lyne-link href-value='https://www.sbb.ch/de/hilfe-und-kontakt/erstattung-entschaedigung/rueckerstattung-von-billetten.html' text='Rückerstattungen' text-size='s' variant='positive'></lyne-link>
+                </li>
+                <li class='link-list__item' slot='link-list__item'>
+                  <lyne-link href-value='https://www.sbb.ch/de/hilfe-und-kontakt/erstattung-entschaedigung/rueckerstattung-von-billetten.html' text='Fundbüro' text-size='s' variant='positive'></lyne-link>
+                </li>
+                <li class='link-list__item' slot='link-list__item'>
+                  <lyne-link href-value='https://www.sbb.ch/de/hilfe-und-kontakt/erstattung-entschaedigung/rueckerstattung-von-billetten.html' text='Beschwerden' text-size='s' variant='positive'></lyne-link>
+                </li>
+                <li class='link-list__item' slot='link-list__item'>
+                  <lyne-link href-value='https://www.sbb.ch/de/hilfe-und-kontakt/erstattung-entschaedigung/rueckerstattung-von-billetten.html' text='Lob aussprechen' text-size='s' variant='positive'></lyne-link>
+                </li>
+                <li class='link-list__item' slot='link-list__item'>
+                  <lyne-link href-value='https://www.sbb.ch/de/hilfe-und-kontakt/erstattung-entschaedigung/rueckerstattung-von-billetten.html' text='Sachbeschädigung melden' text-size='s' variant='positive'></lyne-link>
+                </li>
+            </lyne-link-list>`
     });
 
     expect(root)
       .toEqualHtml(`
-        <lyne-link-list>
+        <lyne-link-list
+            title-level="2"
+            title-text="Help &amp; Contact"
+            variant="positive"
+        >
           <mock:shadow-root>
-            <button class="some-class"></button>
+            <div>
+                <lyne-title id="title-XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX" level="2" text="Help &amp; Contact" variant="positive" visual-level="5"></lyne-title>
+                <ul aria-labelledby="title-XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX" class="link-list" role="list">
+                    <slot name="link-list__item">
+                    </slot>
+                </ul>
+            </div>
           </mock:shadow-root>
+          <li class="link-list__item" slot="link-list__item">
+            <lyne-link href-value="https://www.sbb.ch/de/hilfe-und-kontakt/erstattung-entschaedigung/rueckerstattung-von-billetten.html" text="Rückerstattungen" text-size="s" variant="positive"></lyne-link>
+          </li>
+          <li class="link-list__item" slot="link-list__item">
+            <lyne-link href-value="https://www.sbb.ch/de/hilfe-und-kontakt/erstattung-entschaedigung/rueckerstattung-von-billetten.html" text="Fundbüro" text-size="s" variant="positive"></lyne-link>
+          </li>
+          <li class="link-list__item" slot="link-list__item">
+            <lyne-link href-value="https://www.sbb.ch/de/hilfe-und-kontakt/erstattung-entschaedigung/rueckerstattung-von-billetten.html" text="Beschwerden" text-size="s" variant="positive"></lyne-link>
+          </li>
+          <li class="link-list__item" slot="link-list__item">
+            <lyne-link href-value="https://www.sbb.ch/de/hilfe-und-kontakt/erstattung-entschaedigung/rueckerstattung-von-billetten.html" text="Lob aussprechen" text-size="s" variant="positive"></lyne-link>
+          </li>
+          <li class="link-list__item" slot="link-list__item">
+            <lyne-link href-value="https://www.sbb.ch/de/hilfe-und-kontakt/erstattung-entschaedigung/rueckerstattung-von-billetten.html" text="Sachbeschädigung melden" text-size="s" variant="positive"></lyne-link>
+          </li>
         </lyne-link-list>
       `);
   });
 
 });
+
