d9c127d53060ca3092bcc6ff859eeb5a4b7ff5bc
Marius Bleuer <marius.bleuer@unic.com>
2021-11-26 10:29:47 +0100

    fix: updating to latest lyne-icons version and fixing tests

--- src/components/lyne-button/lyne-button.spec.ts
+++ src/components/lyne-button/lyne-button.spec.ts
@@ -23,7 +23,7 @@ describe('lyne-button', () => {
               <span class="button__label">Label</span>
             </button>
           </mock:shadow-root>
-          <svg height="24" width="24" xmlns="http://www.w3.org/2000/svg">
+          <svg height="24" viewBox="0,0,24,24" width="24" xmlns="http://www.w3.org/2000/svg">
             <path clip-rule="evenodd" d="m17.8436,12.1382-3.99-3.99196-.7072.70693,3.1366,3.13823H5v1h11.287l-3.1413,3.1555.7086.7056,3.99-4.008.3519-.3535-.3526-.3528z" fill-rule="evenodd"></path>
           </svg>
         </lyne-button>
--- src/components/lyne-link-button/lyne-link-button.spec.ts
+++ src/components/lyne-link-button/lyne-link-button.spec.ts
@@ -36,7 +36,7 @@ describe('lyne-link', () => {
             </a>
           </mock:shadow-root>
           <span slot="icon">
-            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+            <svg height="24" viewBox="0,0,24,24" width="24" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="m10.6776,7.74045,3.949,3.90395.3597.3557-.3597.3555-3.95,3.904-.70297-.7112L13.5639,12,9.97459,8.4516l.70301-.71115z">
                 </path>
             </svg>
