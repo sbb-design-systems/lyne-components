a661c086a175cdcb9d217423d880f989ef0d80f3
Yves Tscherry <info@feerglas.com>
2021-12-03 15:27:51 +0100

    feat: integrate subcomponents with correct attributes, elaborate event handling

--- src/components/lyne-autocomplete/lyne-autocomplete.tsx
+++ src/components/lyne-autocomplete/lyne-autocomplete.tsx
@@ -5,7 +5,7 @@ import {
   Prop,
   State
 } from '@stencil/core';
-import itemEvents from '../lyne-text-input/lyne-text-input.events';
+import inputEvents from '../lyne-text-input/lyne-text-input.events';
 import itemsDataHelper from './lyne-autocomplete.helper';
 
 @Component({
@@ -24,45 +24,69 @@ export class LyneAutocomplete {
    * array of objects, containing the `text` key for each object with an
    * appropriate value.
    */
-  @Prop() public items? = '[{"text": "pre ipsum item1 post lorem"},{"text": "pre ipsum item2 post lorem"}]';
+  @Prop() public items? = '[{"text": "pre ipsum item1 post lorem"},{"text": "pre ipsum item2 post lorem"},{"text": "pre ipsum item3 post lorem"}]';
 
   /**
-   * Set if the autocomplete interface should be shown or hidden.
+   * The value to use as default value for the input. The input value or the
+   * selected autocomplete value is reflected to this attribute.
    */
-  @Prop() public visible = true;
+  @Prop({
+    reflect: true
+  }) public value?: string;
 
   @State() private _inputValue: string;
+  @State() private _selectedAutocompleteValue: string;
+  @State() private _isVisible = false;
+
   @Element() private _element: HTMLElement;
 
-  /*
-   * private _handleFocus = (evt): void => {
-   *   console.log('focus', evt.detail.value);
-   * };
-   * private _handleBlur = (evt): void => {
-   *   console.log('blur', evt.detail.value);
-   * };
-   */
+  private _listElement!: HTMLUListElement;
 
-  private _handleInput = (evt): void => {
-    console.log('input', evt.detail.value);
-    this._inputValue = evt.detail.value;
+  private _handleFocus = (evt): void => {
+    this._isVisible = true;
+    console.log('focus', evt);
   };
 
-  public componentWillLoad(): void {
-    // this._element.addEventListener(itemEvents.focus, this._handleFocus);
+  private _handleInput = (evt): void => {
+    this._inputValue = evt.detail.value;
 
-    // this._element.addEventListener(itemEvents.blur, this._handleBlur);
+    this.value = evt.detail.value;
+  };
 
-    this._element.addEventListener(itemEvents.input, this._handleInput);
+  private _handleBlur = (evt): void => {
+    this._isVisible = false;
+    console.log('blur', evt);
+  };
+
+  private _handleListClick = (evt): void => {
+    const {
+      path
+    } = evt;
+
+    let firstElement = null;
+
+    if (path.length > 0) {
+      [firstElement] = path;
+    }
+
+    this._isVisible = false;
+
+    this._selectedAutocompleteValue = firstElement.innerText;
+    this.value = firstElement.innerText;
+  };
+
+  public componentDidLoad(): void {
+    this._element.addEventListener('focus', this._handleFocus);
+    this._element.addEventListener(inputEvents.input, this._handleInput);
+    this._element.addEventListener('blur', this._handleBlur);
+    this._listElement.addEventListener('click', this._handleListClick);
   }
 
   public disconnectCallback(): void {
-
-    // this._element.removeEventListener(itemEvents.focus, this._handleFocus);
-
-    // this._element.removeEventListener(itemEvents.blur, this._handleBlur);
-
-    this._element.removeEventListener(itemEvents.input, this._handleInput);
+    this._element.removeEventListener('focus', this._handleFocus);
+    this._element.removeEventListener(inputEvents.input, this._handleInput);
+    this._element.removeEventListener('blur', this._handleBlur);
+    this._listElement.removeEventListener('click', this._handleListClick);
   }
 
   public render(): JSX.Element {
@@ -70,14 +94,16 @@ export class LyneAutocomplete {
     const dataCheck = itemsDataHelper(this.items);
     let autocompleteClasses = 'autocomplete__list';
 
-    if (this.visible) {
+    if (this._isVisible) {
       autocompleteClasses += ' autocomplete__list--visible';
     }
 
     return (
-      <div>
+      <div class='autocomplete'>
 
         <lyne-text-input
+          class='autocomplete__input'
+          inputAutoCompleteValue='off'
           inputName='textfield'
           inputType='text'
           label='Von'
@@ -86,19 +112,38 @@ export class LyneAutocomplete {
           inputRequired
           eventId='sampleId'
           debounceInputEvent={200}
+          inputAriaExpanded={this._isVisible}
+          inputRole='combobox'
+          inputAriaAutocomplete='list'
+          inputAriaControls='autocomplete-list'
+          inputValue={this.value || this._selectedAutocompleteValue}
         ></lyne-text-input>
 
-        <ul class={autocompleteClasses}>
-          {dataCheck.map((item) => (
-            <div>
-              <lyne-autocomplete-item
-                text={item.text}
-                highlight={this._inputValue}
-              >
-              </lyne-autocomplete-item>
-            </div>
+        <ul
+          class={autocompleteClasses}
+          id='autocomplete-list'
+          role='listbox'
+          ref={(el): void => {
+            this._listElement = el;
+          }}
+        >
+          {dataCheck.map((item, index) => (
+            <lyne-autocomplete-item
+              text={item.text}
+              highlight={this._inputValue}
+              ariaSelected={false}
+              ariaPosinset={index + 1}
+              ariaSetsize={dataCheck.length}
+            />
           ))}
         </ul>
+
+        <p
+          class='autocomplete__accessibility-hint'
+          role='status'
+          aria-live='polite'
+          aria-atomic='true'
+        >10 Ergebnisse sind verfügbar, verwenden Sie nach oben und unten Pfeiltasten um zu navigieren.</p>
 
       </div>
     );
