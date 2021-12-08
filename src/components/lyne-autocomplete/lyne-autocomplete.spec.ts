6a743badce952d0c2c1a8f7bde3e28d8e73db836
Yves Tscherry <info@feerglas.com>
2021-12-08 16:28:02 +0100

    test: adopt tests to refactoring

--- src/components/lyne-autocomplete/lyne-autocomplete.spec.ts
+++ src/components/lyne-autocomplete/lyne-autocomplete.spec.ts
@@ -15,20 +15,8 @@ describe('lyne-autocomplete', () => {
         <lyne-autocomplete>
           <mock:shadow-root>
             <div class="autocomplete">
-              <lyne-text-input borderless="" class="autocomplete__input" debounceinputevent="200" eventid="sampleId" inputariaautocomplete="list" inputariacontrols="autocomplete-list" inputautocompletevalue="off" inputname="textfield" inputplaceholder="z.B. Bern" inputrequired="" inputrole="combobox" inputtype="text" label="Von" labelvisible=""></lyne-text-input>
-              <ul aria-hidden="" class="autocomplete__list" id="autocomplete-list" role="presentation">
-                <lyne-autocomplete-item ariaposinset="1" ariasetsize="10" text="pre ipsum item1 post lorem"></lyne-autocomplete-item>
-                <lyne-autocomplete-item ariaposinset="2" ariasetsize="10" text="pre ipsum item2 post lorem"></lyne-autocomplete-item>
-                <lyne-autocomplete-item ariaposinset="3" ariasetsize="10" text="pre ipsum item3 post lorem"></lyne-autocomplete-item>
-                <lyne-autocomplete-item ariaposinset="4" ariasetsize="10" text="pre ipsum item4 post lorem"></lyne-autocomplete-item>
-                <lyne-autocomplete-item ariaposinset="5" ariasetsize="10" text="pre ipsum item5 post lorem"></lyne-autocomplete-item>
-                <lyne-autocomplete-item ariaposinset="6" ariasetsize="10" text="pre ipsum item6 post lorem"></lyne-autocomplete-item>
-                <lyne-autocomplete-item ariaposinset="7" ariasetsize="10" text="pre ipsum item7 post lorem"></lyne-autocomplete-item>
-                <lyne-autocomplete-item ariaposinset="8" ariasetsize="10" text="pre ipsum item8 post lorem"></lyne-autocomplete-item>
-                <lyne-autocomplete-item ariaposinset="9" ariasetsize="10" text="pre ipsum item9 post lorem"></lyne-autocomplete-item>
-                <lyne-autocomplete-item ariaposinset="10" ariasetsize="10" text="pre ipsum item10 post lorem"></lyne-autocomplete-item>
-              </ul>
-              <p aria-atomic="true" aria-live="polite" class="autocomplete__accessibility-hint" role="status"></p>
+              <lyne-text-input borderless="" debounceinputevent="200" eventid="sampleId" inputariaautocomplete="list" inputariacontrols="autocomplete-list" inputautocompletevalue="off" inputname="textfield" inputplaceholder="z.B. Bern" inputrequired="" inputrole="combobox" inputtype="text" label="Von" labelvisible=""></lyne-text-input>
+              <lyne-autocomplete-list items="[{&quot;text&quot;: &quot;pre ipsum item1 post lorem&quot;},{&quot;text&quot;: &quot;pre ipsum item2 post lorem&quot;},{&quot;text&quot;: &quot;pre ipsum item3 post lorem&quot;},{&quot;text&quot;: &quot;pre ipsum item4 post lorem&quot;},{&quot;text&quot;: &quot;pre ipsum item5 post lorem&quot;},{&quot;text&quot;: &quot;pre ipsum item6 post lorem&quot;},{&quot;text&quot;: &quot;pre ipsum item7 post lorem&quot;},{&quot;text&quot;: &quot;pre ipsum item8 post lorem&quot;},{&quot;text&quot;: &quot;pre ipsum item9 post lorem&quot;},{&quot;text&quot;: &quot;pre ipsum item10 post lorem&quot;}]" listid="autocomplete-list"></lyne-autocomplete-list>
             </div>
           </mock:shadow-root>
         </lyne-autocomplete>
