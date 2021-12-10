8ee8233c7ef7407565ce2b5a5036f33ef26b64c4
Yves Tscherry <info@feerglas.com>
2021-12-10 08:40:28 +0100

    chore: remove empty files for timetable form

--- src/components/lyne-timetable-form-journey/lyne-timetable-form-journey.custom.d.ts
+++
@@ -1,3 +0,0 @@
-export interface InterfaceLyneTimetableFormJourneyAttributes {
-  someInterface: 'opt1' | 'opt2';
-}
--- src/components/lyne-timetable-form-journey/lyne-timetable-form-journey.events.ts
+++
@@ -1,3 +0,0 @@
-export default {
-  click: 'lyne-timetable-form-journey_click'
-};
