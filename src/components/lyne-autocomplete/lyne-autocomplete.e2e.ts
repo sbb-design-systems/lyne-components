0d1bf6c0e0cdb1f42edb42483af2278679a0f77c
Yves Tscherry <info@feerglas.com>
2021-12-12 20:57:55 +0100

    test: adopt test to latest changes

--- src/components/lyne-autocomplete/lyne-autocomplete.e2e.ts
+++ src/components/lyne-autocomplete/lyne-autocomplete.e2e.ts
@@ -1,4 +1,5 @@
 import { newE2EPage } from '@stencil/core/testing';
+import sampleData from './lyne-autocomplete.sample-data';
 
 describe('lyne-autocomplete', () => {
   let element,
@@ -6,7 +7,12 @@ describe('lyne-autocomplete', () => {
 
   it('renders', async () => {
     page = await newE2EPage();
-    await page.setContent('<lyne-autocomplete></lyne-autocomplete>');
+    await page.setContent(`
+      <lyne-autocomplete
+        items='${sampleData}'
+        inputName='test-input'
+        inputLabel='input-label'
+      ></lyne-autocomplete>`);
 
     element = await page.find('lyne-autocomplete');
     expect(element)
