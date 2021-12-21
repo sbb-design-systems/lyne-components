676acddb465779cd4f0b66f1be643f1104ef3979
Marius Bleuer <marius.bleuer@unic.com>
2021-12-21 16:16:27 +0100

    feat: fixing lyne-timetable-transportation-details test

--- src/components/lyne-timetable-transportation-details/lyne-timetable-transportation-details.spec.ts
+++ src/components/lyne-timetable-transportation-details/lyne-timetable-transportation-details.spec.ts
@@ -2,7 +2,7 @@ import { LyneTimetableDetails } from './lyne-timetable-transportation-details';
 import { newSpecPage } from '@stencil/core/testing';
 import sampleData from './lyne-timetable-transportation-details.sample-data';
 
-const config = JSON.stringify(sampleData['bus']);
+const config = JSON.stringify(sampleData[0]);
 
 describe('lyne-timetable-transportation-details', () => {
   it('renders', async () => {
@@ -10,7 +10,7 @@ describe('lyne-timetable-transportation-details', () => {
       root
     } = await newSpecPage({
       components: [LyneTimetableDetails],
-      html: `<lyne-timetable-transportation-details config="{&quot;arrivalTime&quot;:{&quot;time&quot;:&quot;15:34&quot;,&quot;type&quot;:&quot;arrival&quot;},&quot;arrivalWalk&quot;:{&quot;distance&quot;:0,&quot;duration&quot;:0,&quot;type&quot;:&quot;arrival&quot;},&quot;departureTime&quot;:{&quot;time&quot;:&quot;15:14&quot;,&quot;type&quot;:&quot;departure&quot;},&quot;departureWalk&quot;:{&quot;distance&quot;:178,&quot;duration&quot;:2,&quot;type&quot;:&quot;departure&quot;},&quot;pearlChain&quot;:{&quot;legs&quot;:{&quot;legs&quot;:[{&quot;cancellation&quot;:false,&quot;duration&quot;:25},{&quot;cancellation&quot;:false,&quot;duration&quot;:75}]},&quot;status&quot;:&quot;future&quot;},&quot;transportationNumber&quot;:{&quot;direction&quot;:&quot;Direction Romanshorn&quot;,&quot;marketingName&quot;:&quot;&quot;,&quot;meansOfTransport&quot;:{&quot;picto&quot;:&quot;transportation-zug-right&quot;,&quot;text&quot;:&quot;Train&quot;},&quot;product&quot;:{&quot;icon&quot;:&quot;ic-8&quot;,&quot;text&quot;:&quot;IC 8&quot;}}}" role="gridcell">`
+      html: `<lyne-timetable-transportation-details config='${config}' role='gridcell'>`
     });
 
     expect(root)
