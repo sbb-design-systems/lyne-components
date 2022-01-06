bec3bf90cb1f9ecd2fed109428097461b702357c
Yves Tscherry <info@feerglas.com>
2022-01-06 13:15:59 +0100

    test: pass an item to autocomplete

--- src/components/lyne-autocomplete-list/lyne-autocomplete-list.e2e.ts
+++ src/components/lyne-autocomplete-list/lyne-autocomplete-list.e2e.ts
@@ -6,7 +6,7 @@ describe('lyne-autocomplete-list', () => {
 
   it('renders', async () => {
     page = await newE2EPage();
-    await page.setContent('<lyne-autocomplete-list></lyne-autocomplete-list>');
+    await page.setContent('<lyne-autocomplete-list items="[{&quot;text&quot;: &quot;pre ipsum item1 post lorem&quot;}]"></lyne-autocomplete-list>');
 
     element = await page.find('lyne-autocomplete-list');
     expect(element)
