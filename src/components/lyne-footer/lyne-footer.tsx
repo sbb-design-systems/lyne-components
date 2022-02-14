209e7a887092bba9179a8ce78eab768746277ebb
Michael Zumstein <zumi@yokai.ch>
2022-02-14 19:00:35 +0100

    refactor: use lyne-title in lyne-footer

--- src/components/lyne-footer/lyne-footer.tsx
+++ src/components/lyne-footer/lyne-footer.tsx
@@ -56,7 +56,7 @@ export class LyneFooter {
       <Host role='contentinfo'>
         <footer {...attrs}
         >
-          <h1 class='title--visually-hidden'>{this.accessibilityTitle}</h1>
+          <lyne-title level='1' visually-hidden='true' text={this.accessibilityTitle} />
           {this._hasCol1Slot || this._hasCol2Slot || this._hasCol3Slot || this._hasCol4Slot || this._hasClockSlot
             ? <div class='columns'><slot name='col-1'/><slot name='col-2'/><slot name='col-3'/><slot name='col-4'/><slot name='clock'/></div>
             : ''
