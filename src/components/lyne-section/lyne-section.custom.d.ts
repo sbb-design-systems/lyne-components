0558f357dbe7ff340105475e5af047bf7b2b2788
Michael Zumstein <zumi@yokai.ch>
2022-02-24 01:05:30 +0100

    feat: section widths
    - full-bleed--forever
    - full-bleed--until-ultra-plus
    - full-bleed--until-ultra
    - page-spacing

--- src/components/lyne-section/lyne-section.custom.d.ts
+++ src/components/lyne-section/lyne-section.custom.d.ts
@@ -1,4 +1,5 @@
 export interface InterfaceSectionAttributes {
   appearance: 'primary' | 'primary-negative';
+  width: 'full-bleed--forever' | 'full-bleed--until-ultra-plus' | 'full-bleed--until-ultra' | 'page-spacing';
 }
 
--- src/components/lyne-section/lyne-section.tsx
+++ src/components/lyne-section/lyne-section.tsx
@@ -24,9 +24,13 @@ export class LyneSection {
   /** Section title text, visually hidden,  necessary for screenreaders */
   @Prop() public accessibilityTitle!: string;
 
+  /** Section width */
+  @Prop() public width?: InterfaceSectionAttributes['width'] = 'full-bleed--forever';
+
   /** Host element */
   @Element() private _hostElement: HTMLElement;
 
+  private _hasFullWidthSlot: boolean;
   private _hasCol1Slot: boolean;
   private _hasCol2Slot: boolean;
   private _hasCol3Slot: boolean;
@@ -34,6 +38,7 @@ export class LyneSection {
 
   public componentWillLoad(): void {
     // Check slots
+    this._hasFullWidthSlot = Boolean(this._hostElement.querySelector('[slot="full-width"]'));
     this._hasCol1Slot = Boolean(this._hostElement.querySelector('[slot="col-1"]'));
     this._hasCol2Slot = Boolean(this._hostElement.querySelector('[slot="col-2"]'));
     this._hasCol3Slot = Boolean(this._hostElement.querySelector('[slot="col-3"]'));
@@ -42,19 +47,23 @@ export class LyneSection {
 
   public render(): JSX.Element {
 
-    const className = `section section--${this.appearance}`;
+    const className = `section section--${this.appearance} section--${this.width}`;
 
     const attrs = {
       class: className
     };
 
     return (
-      <Host role='contentinfo'>
+      <Host>
         <section {...attrs}
         >
           <lyne-title level='1' visually-hidden='true' text={this.accessibilityTitle} />
           {this._hasCol1Slot || this._hasCol2Slot || this._hasCol3Slot || this._hasCol4Slot
             ? <div class='columns'><slot name='col-1'/><slot name='col-2'/><slot name='col-3'/><slot name='col-4'/><slot name='clock'/></div>
+            : ''
+          }
+          {this._hasFullWidthSlot
+            ? <div class='full-width'><slot name='full-width' /></div>
             : ''
           }
         </section>
