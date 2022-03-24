5f154f35f7dbdcbf8df87b2bcd97da7464dec977
Michael Zumstein <zumi@yokai.ch>
2022-03-24 15:52:53 +0100

    refactor: remove unnecessary accessibility-title attributes for lyne-section

--- src/pages/home/home.stories.js
+++ src/pages/home/home.stories.js
@@ -92,7 +92,7 @@ const Template = (args) => (
     {/* *************************************************
     Header section
     ************************************************* */}
-    <lyne-section width='page-spacing' accessibility-title='Header' role='banner' style='background-color: var(--color-white-default); cursor: pointer;'>
+    <lyne-section width='page-spacing' role='banner' style='background-color: var(--color-white-default); cursor: pointer;'>
       <lyne-stack slot='full-width' stack-width='100%' gap-horizontal='fixed-10x' space-leading='responsive-xxs' space-trailing='responsive-xxs' appearance='horizontal--space-between--centered'>
         <lyne-stack gap-horizontal='responsive-xxs' appearance='horizontal--start--centered'>
           <SlotIconTemplate {...iconBurgerArgs}/>
@@ -109,7 +109,7 @@ const Template = (args) => (
     {/* *************************************************
     Timetable input section
     ************************************************* */}
-    <lyne-section width='full-bleed--until-ultra-plus' accessibility-title='Timetable search' {...args} style='--section-background-color: var(--color-red-default)'>
+    <lyne-section width='full-bleed--until-ultra-plus' {...args} style='--section-background-color: var(--color-red-default)'>
       <lyne-stack slot='full-width' stack-width='100%' space-leading='responsive-xl' gap-vertical='fixed-4x' appearance='vertical--centered' style='margin-inline-start: calc(var(--page-spacing-responsive-left) / var(--typo-scale-default) * 1rem); margin-inline-end: calc(var(--page-spacing-responsive-right) / var(--typo-scale-default) * 1rem);'>
         <lyne-link-button href-value='https://github.com/lyne-design-system/lyne-components' text='Timetable toggle pretender' variant='secondary'></lyne-link-button>
         <lyne-stack gap-vertical='fixed-4x' appearance='vertical--centered' style='background-color: white; border-radius: 16px; max-width: 728px; height: 21vh; width: 100%; margin-bottom: -7vh; box-shadow: calc(var(--shadow-elevation-level-9-shadow-2-offset-x) / var(--typo-scale-default) * 1rem) calc(var(--shadow-elevation-level-9-shadow-2-offset-y) / var(--typo-scale-default) * 1rem) calc(var(--shadow-elevation-level-9-shadow-2-blur) / var(--typo-scale-default) * 1rem) calc(var(--shadow-elevation-level-9-shadow-2-spread) / var(--typo-scale-default) * 1rem) var(--shadow-elevation-level-9-hard-2-color), calc(var(--shadow-elevation-level-9-shadow-1-offset-x) / var(--typo-scale-default) * 1rem) calc(var(--shadow-elevation-level-9-shadow-1-offset-y) / var(--typo-scale-default) * 1rem) calc(var(--shadow-elevation-level-9-shadow-1-blur) / var(--typo-scale-default) * 1rem) calc(var(--shadow-elevation-level-9-shadow-1-spread) / var(--typo-scale-default) * 1rem) var(--shadow-elevation-level-5-hard-1-color)'></lyne-stack>
@@ -122,7 +122,7 @@ const Template = (args) => (
 
     {/* style='margin-block-start:
     calc((var(--spacing-responsive-l)) / var(--typo-scale-default) * 1rem);' */}
-    {/* <lyne-section width='page-spacing' accessibility-title='Experiment' {...args} style='margin-block-start: 7vh;'>
+    {/* <lyne-section width='page-spacing' {...args} style='margin-block-start: 7vh;'>
       <div slot='col-1'>
         <lyne-stack stack-width='100%' is-placeholder='true'>
           <lyne-title level='2' text='I am a teaser.' visual-level='5'></lyne-title>
@@ -145,7 +145,7 @@ const Template = (args) => (
       </div>
     </lyne-section> */}
 
-    <lyne-section width='page-spacing' accessibility-title='Experiment' {...args} style='margin-block-start: 7vh;'>
+    <lyne-section width='page-spacing' {...args} style='margin-block-start: 7vh;'>
       <div slot='col-1'>
         <lyne-stack stack-width='100%' is-placeholder='true'>
           <lyne-card-product appearance='primary' accessibility-label='The text which gets exposed to screen reader users. The text should reflect all the information which gets passed into the components slots and which is visible in the card, either through text or iconography' layout='standard' href-value='https://github.com/lyne-design-system/lyne-components'><div slot='icon'><svg width='36' height='36' viewBox='0,0,36,36' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' clip-rule='evenodd' d='m3.25,6.24951h29.5v6.99999h-.5c-2.2739,0-4,1.7262-4,4,0,2.2739,1.7261,4,4,4h.5v7H3.25V6.24951zm1,1V27.2495h27.5v-5.023c-2.5785-.2395-4.5-2.32-4.5-4.977s1.9215-4.7375,4.5-4.977V7.24951H4.25zm10.9501,8.67479c.3869.2079.8295.3259,1.2999.3259,1.5181,0,2.75-1.2318,2.75-2.75,0-1.5198-1.2321-2.75-2.75-2.75-1.5196,0-2.75,1.2304-2.75,2.75,0,.6876.2524,1.3164.6697,1.7988l-2.6206,3.2764c-.387-.2079-.8293-.3259-1.2991-.3259-1.51986,0-2.75,1.2321-2.75,2.75s1.23014,2.75,2.75,2.75c1.5181,0,2.75-1.2318,2.75-2.75,0-.6877-.2528-1.3167-.6704-1.799l2.6205-3.2762zM14.75,13.5002c0-.9673.7826-1.75,1.75-1.75.9661,0,1.75.7829,1.75,1.75,0,.9659-.7841,1.75-1.75,1.75-.9671,0-1.75-.7839-1.75-1.75zm-4.25,5.7493c-.96714,0-1.75.7839-1.75,1.75s.78286,1.75,1.75,1.75c.9659,0,1.75-.7841,1.75-1.75,0-.9658-.7841-1.75-1.75-1.75zM24.25,7.49951v.4875h1v-.4875h-1zm0,1.4625v.975h1v-.975h-1zm0,1.94999v.975h1v-.975h-1zm0,1.95v.975h1v-.975h-1zm0,1.95v.975h1v-.975h-1zm0,1.95v.975h1v-.975h-1zm0,1.95v.975h1v-.975h-1zm0,1.95v.975h1v-.975h-1zm0,1.95v.975h1v-.975h-1zm0,1.95v.975h1v-.975h-1zM24.25,26.512v.4875h1v-.4875h-1z'></path></svg></div><div slot='title'><lyne-title level='2' text='Tageskarte' visual-level='6'></lyne-title></div><div slot='text'><span>Gültig heute</span></div><div slot='action'><lyne-button label='Kaufen' size='small' variant='secondary' visual-button-only=''></lyne-button></div></lyne-card-product>
@@ -168,7 +168,7 @@ const Template = (args) => (
       </div>
     </lyne-section>
 
-    {/* <lyne-section width='page-spacing' accessibility-title='Experiment' {...args} style='margin-block-start: 7vh;'>
+    {/* <lyne-section width='page-spacing' {...args} style='margin-block-start: 7vh;'>
       <div slot='col-1'>
         <lyne-card-product appearance='primary' accessibility-label='The text which gets exposed to screen reader users. The text should reflect all the information which gets passed into the components slots and which is visible in the card, either through text or iconography' layout='standard' href-value='https://github.com/lyne-design-system/lyne-components'><div slot='icon'><svg width='36' height='36' viewBox='0,0,36,36' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' clip-rule='evenodd' d='m3.25,6.24951h29.5v6.99999h-.5c-2.2739,0-4,1.7262-4,4,0,2.2739,1.7261,4,4,4h.5v7H3.25V6.24951zm1,1V27.2495h27.5v-5.023c-2.5785-.2395-4.5-2.32-4.5-4.977s1.9215-4.7375,4.5-4.977V7.24951H4.25zm10.9501,8.67479c.3869.2079.8295.3259,1.2999.3259,1.5181,0,2.75-1.2318,2.75-2.75,0-1.5198-1.2321-2.75-2.75-2.75-1.5196,0-2.75,1.2304-2.75,2.75,0,.6876.2524,1.3164.6697,1.7988l-2.6206,3.2764c-.387-.2079-.8293-.3259-1.2991-.3259-1.51986,0-2.75,1.2321-2.75,2.75s1.23014,2.75,2.75,2.75c1.5181,0,2.75-1.2318,2.75-2.75,0-.6877-.2528-1.3167-.6704-1.799l2.6205-3.2762zM14.75,13.5002c0-.9673.7826-1.75,1.75-1.75.9661,0,1.75.7829,1.75,1.75,0,.9659-.7841,1.75-1.75,1.75-.9671,0-1.75-.7839-1.75-1.75zm-4.25,5.7493c-.96714,0-1.75.7839-1.75,1.75s.78286,1.75,1.75,1.75c.9659,0,1.75-.7841,1.75-1.75,0-.9658-.7841-1.75-1.75-1.75zM24.25,7.49951v.4875h1v-.4875h-1zm0,1.4625v.975h1v-.975h-1zm0,1.94999v.975h1v-.975h-1zm0,1.95v.975h1v-.975h-1zm0,1.95v.975h1v-.975h-1zm0,1.95v.975h1v-.975h-1zm0,1.95v.975h1v-.975h-1zm0,1.95v.975h1v-.975h-1zm0,1.95v.975h1v-.975h-1zm0,1.95v.975h1v-.975h-1zM24.25,26.512v.4875h1v-.4875h-1z'></path></svg></div><div slot='title'><lyne-title level='2' text='Tageskarte' visual-level='6'></lyne-title></div><div slot='text'><span>Gültig heute</span></div><div slot='action'><lyne-button label='Kaufen' size='small' variant='secondary' visual-button-only=''></lyne-button></div></lyne-card-product>
       </div>
@@ -184,7 +184,7 @@ const Template = (args) => (
     </lyne-section> */}
 
     {/* <lyne-section width='full-bleed--until-ultra-plus'
-      accessibility-title='Experiment' {...args}
+      {...args}
       style='margin-block-start:
       calc((var(--spacing-responsive-l)) / var(--typo-scale-default) * 1rem);'>
       <div slot='col-1'>
@@ -208,7 +208,7 @@ const Template = (args) => (
       </div>
     </lyne-section> */}
 
-    {/* <lyne-section width='full-bleed--until-ultra' accessibility-title='Experiment' {...args} style='margin-block-start: calc((var(--spacing-responsive-l)) / var(--typo-scale-default) * 1rem);'>
+    {/* <lyne-section width='full-bleed--until-ultra' {...args} style='margin-block-start: calc((var(--spacing-responsive-l)) / var(--typo-scale-default) * 1rem);'>
       <div slot='col-1'>
         <lyne-sbb-clock initial-time='now'></lyne-sbb-clock>
       </div>
@@ -226,7 +226,7 @@ const Template = (args) => (
     {/* *************************************************
     Hero Teaser section
     ************************************************* */}
-    <lyne-section width='full-bleed--until-ultra' accessibility-title='Hero Teaser' {...args}>
+    <lyne-section width='full-bleed--until-ultra' {...args}>
       <lyne-teaser-hero slot='full-width' button-text='Mehr erfahren' loading='eager' image-src='https://cdn.img.sbb.ch/content/dam/internet/lyne/Billetkontrolle.jpg' link='https://www.sbb.ch' open-in-new-window='false' text='Rücksichtsvoll mit SBB Green Class.'></lyne-teaser-hero>
     </lyne-section>
 
