920e34a5f4325eed2fe807a7990add4b7a72bdf4
Federico Iseppon <100614752+federicoisepponfincons@users.noreply.github.com>
2022-04-01 16:37:46 +0200

    feat: tab-group component variants

--- src/components/lyne-tab-group/lyne-tab-group.stories.js
+++ src/components/lyne-tab-group/lyne-tab-group.stories.js
@@ -7,14 +7,14 @@ const Template = (args) => (
   <lyne-tab-group selected-index='3' dir={args.dir}>
 
     <lyne-tab>
-      <lyne-tab-label>{args.label} {getMarkupForSvg(args.iconSlot)} </lyne-tab-label>
-      <lyne-tab-amount>{args.amountSlot}</lyne-tab-amount>
+      <lyne-tab-label>{args.labelTab1} {getMarkupForSvg(args.iconSlotTab1)} </lyne-tab-label>
+      <lyne-tab-amount>{args.amountSlotTab1}</lyne-tab-amount>
       Quis aute iure reprehenderit in voluptate velit esse. Ab illo tempore, ab est sed immemorabili. Non equidem invideo,
       lit aliquet. Nihilne te nocturnum praesidium Palati, nihil urbis vigiliae.  Donec sed odio operae, eu vulputate felis rhoncus.
       Curabitur est gravida et libero vitae dictum. Me non paenitet nullum festiviorem excogitasse ad hoc.
     </lyne-tab>
 
-    <lyne-tab label='Tab two' amount='123'>
+    <lyne-tab label={args.labelTab2} amount={args.amountSlotTab2}>
       Donec sed odio operae, eu vulputate felis rhoncus. Curabitur est gravida et libero vitae dictum. Me non paenitet nullum festiviorem
       excogitasse ad hoc. Quis aute iure reprehenderit in voluptate velit esse. Ab illo tempore, ab est sed immemorabili. Non equidem invideo,
       lit aliquet. Nihilne te nocturnum praesidium Palati, nihil urbis vigiliae. Donec sed odio operae, eu vulputate felis rhoncus.
@@ -23,16 +23,16 @@ const Template = (args) => (
       nihil urbis vigiliae.
     </lyne-tab>
 
-    <lyne-tab label='Tab three'>
-      <lyne-tab-label>Lyne tab three {getMarkupForSvg('arrow-right-small')}</lyne-tab-label>
-      <lyne-tab-amount>45</lyne-tab-amount>
+    <lyne-tab>
+      <lyne-tab-label>{args.labelTab3} {getMarkupForSvg(args.iconSlotTab3)}</lyne-tab-label>
+      <lyne-tab-amount>{args.amountSlotTab3}</lyne-tab-amount>
       Donec sed odio operae, eu vulputate felis rhoncus. Curabitur est gravida et libero vitae dictum. Me non paenitet nullum festiviorem
       excogitasse ad hoc. Quis aute iure reprehenderit in voluptate velit esse. Ab illo tempore, ab est sed immemorabili. Non equidem invideo,
       lit aliquet. Nihilne te nocturnum praesidium Palati, nihil urbis vigiliae. Donec sed odio operae, eu vulputate felis rhoncus.
       Curabitur est gravida et libero vitae dictum. Me non paenitet nullum festiviorem excogitasse ad hoc.
     </lyne-tab>
 
-    <lyne-tab label='Tab four' amount='44'>
+    <lyne-tab label={args.labelTab4} amount={args.amountSlotTab4}>
       Ab illo tempore, ab est sed immemorabili. Non equidem invideo,
       lit aliquet. Nihilne te nocturnum praesidium Palati, nihil urbis vigiliae.
     </lyne-tab>
@@ -44,7 +44,21 @@ const Template = (args) => (
   </lyne-tab-group>
 );
 
-const label = {
+const dir = {
+  control: {
+    type: 'select'
+  },
+  options: [
+    'unset',
+    'ltr',
+    'rtl'
+  ],
+  table: {
+    category: 'Tab group'
+  }
+};
+
+const labelTab1 = {
   control: {
     type: 'text'
   },
@@ -52,8 +66,7 @@ const label = {
     category: 'Tab1'
   }
 };
-
-const iconSlot = {
+const iconSlotTab1 = {
   control: {
     type: 'select'
   },
@@ -68,8 +81,7 @@ const iconSlot = {
     category: 'Tab1'
   }
 };
-
-const amountSlot = {
+const amountSlotTab1 = {
   control: {
     type: 'number'
   },
@@ -78,43 +90,143 @@ const amountSlot = {
   }
 };
 
+const labelTab2 = {
+  control: {
+    type: 'text'
+  },
+  table: {
+    category: 'Tab2'
+  }
+};
+const iconSlotTab2 = {
+  control: {
+    type: 'select'
+  },
+  options: [
+    'arrow-right-small',
+    'arrow-down-small',
+    'arrow-compass-small',
+    'pie-small',
+    'qrcode-small'
+  ],
+  table: {
+    category: 'Tab2'
+  }
+};
+const amountSlotTab2 = {
+  control: {
+    type: 'number'
+  },
+  table: {
+    category: 'Tab2'
+  }
+};
+
+const labelTab3 = {
+  control: {
+    type: 'text'
+  },
+  table: {
+    category: 'Tab3'
+  }
+};
+const iconSlotTab3 = {
+  control: {
+    type: 'select'
+  },
+  options: [
+    'arrow-right-small',
+    'arrow-down-small',
+    'arrow-compass-small',
+    'pie-small',
+    'qrcode-small'
+  ],
+  table: {
+    category: 'Tab3'
+  }
+};
+const amountSlotTab3 = {
+  control: {
+    type: 'number'
+  },
+  table: {
+    category: 'Tab3'
+  }
+};
+
+const labelTab4 = {
+  control: {
+    type: 'text'
+  },
+  table: {
+    category: 'Tab4'
+  }
+};
+const iconSlotTab4 = {
+  control: {
+    type: 'select'
+  },
+  options: [
+    'arrow-right-small',
+    'arrow-down-small',
+    'arrow-compass-small',
+    'pie-small',
+    'qrcode-small'
+  ],
+  table: {
+    category: 'Tab4'
+  }
+};
+const amountSlotTab4 = {
+  control: {
+    type: 'number'
+  },
+  table: {
+    category: 'Tab4'
+  }
+};
+
 const disabledArg = {
   control: {
     type: 'boolean'
   },
   table: {
-    category: 'Tab1'
-  }
-};
-
-const dir = {
-  control: {
-    type: 'select'
-  },
-  options: [
-    'unset',
-    'ltr',
-    'rtl'
-  ],
-  table: {
-    category: 'Tab1'
+    category: 'Disabled Tab'
   }
 };
 
 /* eslint-disable sort-keys */
 const basicArgTypes = {
   dir,
-  label,
-  iconSlot,
-  amountSlot,
+  labelTab1,
+  iconSlotTab1,
+  amountSlotTab1,
+  labelTab2,
+  iconSlotTab2,
+  amountSlotTab2,
+  labelTab3,
+  iconSlotTab3,
+  amountSlotTab3,
+  labelTab4,
+  iconSlotTab4,
+  amountSlotTab4,
   disabled: disabledArg
 };
 
 const basicArgs = {
   dir,
-  label: 'Lyne tab one',
-  iconSlot: iconSlot.options[0],
-  amountSlot,
+  labelTab1: 'Lyne tab one',
+  iconSlotTab1,
+  amountSlotTab1,
+  labelTab2: 'Lyne tab two',
+  iconSlotTab2,
+  amountSlotTab2,
+  labelTab3: 'Lyne tab three',
+  iconSlotTab3,
+  amountSlotTab3,
+  labelTab4: 'Lyne tab four',
+  iconSlotTab4,
+  amountSlotTab4,
   disabled: true
 };
 /* eslint-enable sort-keys */
@@ -133,6 +245,27 @@ defaultTabs.args = JSON.parse(JSON.stringify(basicArgs));
 numbers.args = JSON.parse(JSON.stringify(basicArgs));
 icons.args = JSON.parse(JSON.stringify(basicArgs));
 numbersAndIcons.args = JSON.parse(JSON.stringify(basicArgs));
+
+/* VARIANTS */
+numbers.args.amountSlotTab1 = '23';
+numbers.args.amountSlotTab2 = '14';
+numbers.args.amountSlotTab3 = '5';
+numbers.args.amountSlotTab4 = '29';
+
+icons.args.iconSlotTab1 = iconSlotTab1.options[0];
+icons.args.iconSlotTab2 = iconSlotTab1.options[1];
+icons.args.iconSlotTab3 = iconSlotTab1.options[2];
+icons.args.iconSlotTab4 = iconSlotTab1.options[3];
+
+numbersAndIcons.args.amountSlotTab1 = '23';
+numbersAndIcons.args.amountSlotTab2 = '14';
+numbersAndIcons.args.amountSlotTab3 = '5';
+numbersAndIcons.args.amountSlotTab4 = '29';
+numbersAndIcons.args.iconSlotTab1 = iconSlotTab1.options[0];
+numbersAndIcons.args.iconSlotTab2 = iconSlotTab1.options[1];
+numbersAndIcons.args.iconSlotTab3 = iconSlotTab1.options[2];
+numbersAndIcons.args.iconSlotTab4 = iconSlotTab1.options[3];
+/* VARIANTS */
 
 defaultTabs.documentation = {
   title: 'Default Tabs'
