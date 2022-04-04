b578b866f9860b9c2352957589f12e9b08773aed
Michael Zumstein <zumi@yokai.ch>
2022-04-04 22:19:30 +0200

    feat: enhance top product layout add button stack

--- src/pages/home/home.stories.js
+++ src/pages/home/home.stories.js
@@ -121,7 +121,7 @@ const Template = (args) => (
     ************************************************* */}
 
     <lyne-section width='page-spacing' style='margin-block-start: 7vh;'>
-      <lyne-stack gap-vertical='responsive-m' space-leading='responsive-l' space-trailing='responsive-l' slot='full-width'>
+      <lyne-stack slot='full-width' gap-vertical='responsive-m' space-leading='responsive-l' space-trailing='responsive-l'>
         <lyne-title level='2' text='Top Produkte.' variant='positive' visual-level='2'></lyne-title>
         <lyne-grid variant='base--top-products'>
           <div style='height: 100%;' slot='top-product-1'>
@@ -143,6 +143,10 @@ const Template = (args) => (
             <lyne-card-product appearance='primary' accessibility-label='The text which gets exposed to screen reader users. The text should reflect all the information which gets passed into the components slots and which is visible in the card, either through text or iconography' layout='loose' href-value='https://github.com/lyne-design-system/lyne-components'><div slot='title'><lyne-title level='2' text='1/2' visual-level='1'></lyne-title></div><div slot='lead'><lyne-title level='3' text='Mit dem Halbtax zum halben Preis fahren.' visual-level='6'></lyne-title></div><div slot='action'><lyne-button label='Zum halben Preis fahren' variant='secondary' visual-button-only=''></lyne-button></div></lyne-card-product>
           </div>
         </lyne-grid>
+        <lyne-stack appearance='horizontal--start' collapse-horizontal-below='small' gap-horizontal='fixed-4x' gap-vertical='fixed-2x' space-trailing='responsive-l'>
+          <lyne-link-button href-value='https://github.com/lyne-design-system/lyne-components' icon='user-small' icon-placement='start' text='Meine Billette &amp; Abos' variant='primary'><span slot='icon'><svg width='24' height='24' viewBox='0,0,24,24' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' clip-rule='evenodd' d='m9.5,8.99976c0-1.73197,1.1887-3,2.5-3s2.5,1.26803,2.5,3c0,1.73194-1.1887,3.00004-2.5,3.00004s-2.5-1.2681-2.5-3.00004zm2.5-4c-2.00269,0-3.5,1.86596-3.5,4,0,.92922.28389,1.80764.76978,2.50324C6.47282,12.9019,5,16.3669,5,19.5v.5h14v-.5c0-3.1497-1.4746-6.6059-4.2697-7.997.4858-.6957.7697-1.57405.7697-2.50324,0-2.13404-1.4973-4-3.5-4zm2.0156,7.27454c-.5617.4541-1.2519.7255-2.0156.7255-.7639,0-1.4543-.2716-2.01615-.7259C7.61923,13.2905,6.16607,16.1356,6.01337,19H17.9868c-.1522-2.8799-1.6079-5.7167-3.9712-6.7257z'></path></svg></span></lyne-link-button>
+          <lyne-link-button href-value='https://github.com/lyne-design-system/lyne-components' text='Alle Produkte' variant='secondary'></lyne-link-button>
+        </lyne-stack>
       </lyne-stack>
     </lyne-section>
 
