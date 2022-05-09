1b3d5149005b2a2de3c0f08b893fa892988fa536
Davide Mininni <davide.mininni@finconsgroup.com>
2022-05-09 15:28:49 +0200

    fix: single check for createAnimation function

--- src/global/core/components/animations/animation.ts
+++ src/global/core/components/animations/animation.ts
@@ -2,11 +2,13 @@ import { AbstractAnimation } from './animation-abstract';
 import { AnimationWeb } from './animation-web';
 import { AnimationCss } from './animation-css';
 
-const supportsWebAnimations = (): boolean => (typeof (AnimationEffect as any) === 'function' || typeof (window as any).AnimationEffect === 'function') &&
+const supportsWebAnimations: boolean =
+  (typeof (AnimationEffect as any) === 'function' || typeof (window as any).AnimationEffect === 'function') &&
   (typeof (Element as any) === 'function') && (typeof (Element as any).prototype.animate === 'function');
 
-export const createAnimation = (animationId?: string): AbstractAnimation => (
-  supportsWebAnimations()
-    ? new AnimationWeb(animationId)
-    : new AnimationCss(animationId)
-);
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const AnimationVariant = supportsWebAnimations
+  ? AnimationWeb
+  : AnimationCss;
+
+export const createAnimation = (animationId?: string): AbstractAnimation => new AnimationVariant(animationId);
