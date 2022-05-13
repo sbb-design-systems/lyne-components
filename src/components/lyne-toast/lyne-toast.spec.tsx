3e984cde112f5c59a58d19c3660a9134be1b1961
Davide Mininni <davide.mininni@finconsgroup.com>
2022-05-13 16:33:23 +0200

    test: added first unit test

---
+++ src/components/lyne-toast/lyne-toast.spec.tsx
@@ -0,0 +1,33 @@
+import { h } from '@stencil/core';
+import { LyneToast } from './lyne-toast';
+import { newSpecPage } from '@stencil/core/testing';
+
+describe('lyne-toast', () => {
+  it('renders with only message property', async () => {
+    const config = {
+      message: 'Message'
+    };
+    const {
+      root
+    } = await newSpecPage({
+      components: [LyneToast],
+      template: () => (<lyne-toast config={config}></lyne-toast>)
+    });
+
+    expect(root)
+      .toEqualHtml(`
+        <lyne-toast aria-atomic="true" aria-live="polite" class="overlay-hidden" id="lyne-overlay-1" role="status" tabindex="-1">
+           <mock:shadow-root>
+             <div class="toast-wrapper">
+               <div class="toast toast-bottom toast-center">
+                 <span class="toast-text">
+                   Message
+                 </span>
+               </div>
+             </div>
+        </mock:shadow-root>
+      </lyne-toast>
+      `);
+  });
+
+});
