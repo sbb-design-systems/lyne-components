1f25c73737d7e1fe1b49998ea232f8118babedce
Davide Mininni <davide.mininni@finconsgroup.com>
2022-05-17 12:35:41 +0200

    test: added tests

--- src/components/lyne-toast/lyne-toast.spec.tsx
+++ src/components/lyne-toast/lyne-toast.spec.tsx
@@ -208,6 +208,49 @@ describe('lyne-toast', () => {
       `);
   });
 
+  it('renders the toast with the icon as slot and the message', async () => {
+    const config: InterfaceToastConfiguration = {
+      message: 'Message'
+    };
+    const {
+      root
+    } = await newSpecPage({
+      components: [LyneToast],
+      template: () => (
+        <lyne-toast config={config}>
+          <span slot='icon'>
+            <svg height='24' width='24'>
+              <circle cx='50' cy='50' r='24' stroke='black' stroke-width='3' fill='red'/>
+            </svg>
+          </span>
+        </lyne-toast>
+      )
+    });
+
+    expect(root)
+      .toEqualHtml(`
+        <lyne-toast aria-atomic="true" aria-live="polite" class="overlay-hidden" id="lyne-overlay-6" role="status" tabindex="-1">
+          <mock:shadow-root>
+            <div class="toast-wrapper">
+              <div class="toast toast-bottom toast-center">
+                <span class="toast-icon">
+                  <slot name="icon"></slot>
+                </span>
+                <span class="toast-text">
+                  Message
+                </span>
+              </div>
+            </div>
+          </mock:shadow-root>
+          <span slot="icon">
+            <svg height="24" width="24">
+              <circle cx="50" cy="50" fill="red" r="24" stroke="black" stroke-width="3"></circle>
+            </svg>
+          </span>
+        </lyne-toast>
+      `);
+  });
+
   it('renders the toast with the icon and message and the close icon action', async () => {
     const action: InterfaceToastIcon = {
       role: 'cancel',
@@ -227,7 +270,7 @@ describe('lyne-toast', () => {
 
     expect(root)
       .toEqualHtml(`
-        <lyne-toast aria-atomic="true" aria-live="polite" class="overlay-hidden" id="lyne-overlay-6" role="dialog" tabindex="-1">
+        <lyne-toast aria-atomic="true" aria-live="polite" class="overlay-hidden" id="lyne-overlay-7" role="dialog" tabindex="-1">
           <mock:shadow-root>
            <div class="toast-wrapper">
              <div class="toast toast-bottom toast-center">
@@ -272,7 +315,7 @@ describe('lyne-toast', () => {
 
     expect(root)
       .toEqualHtml(`
-         <lyne-toast aria-atomic="true" aria-live="polite" class="overlay-hidden" id="lyne-overlay-7" role="dialog" tabindex="-1">
+         <lyne-toast aria-atomic="true" aria-live="polite" class="overlay-hidden" id="lyne-overlay-8" role="dialog" tabindex="-1">
             <mock:shadow-root>
               <div class="toast-wrapper">
                 <div class="toast toast-bottom toast-center">
@@ -315,7 +358,7 @@ describe('lyne-toast', () => {
 
     expect(root)
       .toEqualHtml(`
-       <lyne-toast aria-atomic="true" aria-live="polite" class="overlay-hidden" id="lyne-overlay-8" role="dialog" tabindex="-1">
+       <lyne-toast aria-atomic="true" aria-live="polite" class="overlay-hidden" id="lyne-overlay-9" role="dialog" tabindex="-1">
           <mock:shadow-root>
             <div class="toast-wrapper">
               <div class="toast toast-bottom toast-center">
@@ -327,6 +370,52 @@ describe('lyne-toast', () => {
                 </span>
                 <span class="toast-action">
                   <button class="lyne-focusable toast-button" part="button" tabindex="0" type="button">
+                    <span>
+                      Button
+                    </span>
+                  </button>
+                </span>
+              </div>
+            </div>
+           </mock:shadow-root>
+       </lyne-toast>
+     `);
+  });
+
+  it('renders the toast with the icon and the message and the button action with custom CSS', async () => {
+    const action: InterfaceToastAction = {
+      cssClass: 'my-css-class',
+      handler: jest.fn(),
+      label: 'Button',
+      role: null,
+      type: 'action'
+    };
+    const config: InterfaceToastConfiguration = {
+      action,
+      icon: '<svg height="24" width="24"><circle cx="50" cy="50" r="24" stroke="black" stroke-width="3" fill="red"/></svg>',
+      message: 'Message'
+    };
+    const {
+      root
+    } = await newSpecPage({
+      components: [LyneToast],
+      template: () => (<lyne-toast config={config}></lyne-toast>)
+    });
+
+    expect(root)
+      .toEqualHtml(`
+       <lyne-toast aria-atomic="true" aria-live="polite" class="overlay-hidden" id="lyne-overlay-10" role="dialog" tabindex="-1">
+          <mock:shadow-root>
+            <div class="toast-wrapper">
+              <div class="toast toast-bottom toast-center">
+                <span class="toast-icon">
+                  <svg height="24" width="24"><circle cx="50" cy="50" r="24" stroke="black" stroke-width="3" fill="red"/></svg>
+                </span>
+                <span class="toast-text">
+                  Message
+                </span>
+                <span class="toast-action">
+                  <button class="lyne-focusable toast-button my-css-class" part="button" tabindex="0" type="button">
                     <span>
                       Button
                     </span>
