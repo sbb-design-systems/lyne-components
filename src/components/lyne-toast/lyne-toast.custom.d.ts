0908bbe6949c789951ee3c5147ee8b2a23c5eec6
Davide Mininni <davide.mininni@finconsgroup.com>
2022-05-23 12:03:05 +0200

    docs: added methods documentation

--- src/components/lyne-toast/lyne-toast.custom.d.ts
+++ src/components/lyne-toast/lyne-toast.custom.d.ts
@@ -8,29 +8,52 @@ export interface InterfaceLyneToastAttributes {
 
 type InterfaceToastType = 'link' | 'action' | 'icon';
 
+/**
+ * Common interface for toast action.
+ */
 interface InterfaceToastCommonAction {
   type: InterfaceToastType;
   role: 'cancel' | string;
   cssClass?: string | string[];
 }
 
+/**
+ * Interface for toast's link.
+ */
 export interface InterfaceToastLink extends InterfaceToastCommonAction {
   type: 'link';
   label: string;
   href: string;
 }
 
+/**
+ * Interface for toast's action.
+ */
 export interface InterfaceToastAction extends InterfaceToastCommonAction {
   type: 'action';
   label: string;
   handler: () => void;
 }
 
+/**
+ * Interface for toast's close icon.
+ */
 export interface InterfaceToastIcon extends InterfaceToastCommonAction {
   type: 'icon';
   role: 'cancel';
 }
 
+/**
+ * Interface for toast's configuration:
+ * - "message": Message to display.
+ * - "timeout": Hide the toast after defined time (in milliseconds).
+ * - "icon": SVG string or reference to a SVG element for icon.
+ * - "iconTemplate": Id of <template> to use for the icon.
+ * - "action": Action configuration.
+ * - "verticalPosition": Where the toast should be displayed vertically.
+ * - "horizontalPosition": Where the toast should be displayed horizontally.
+ * - "politeness": Value for aria-live attribute.
+ */
 export interface InterfaceToastConfiguration {
   message: string;
   timeout?: number;
@@ -45,6 +68,7 @@ export interface InterfaceToastConfiguration {
 export interface InterfaceToastConfigurationController {
   config: InterfaceToastConfiguration;
   id?: string;
+  animated?: boolean;
   keyboardClose?: boolean;
   enterAnimation?: AnimationBuilder;
   leaveAnimation?: AnimationBuilder;
