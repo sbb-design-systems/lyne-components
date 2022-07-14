9efd9da0aae09494c040af5ddcaf9308bdb4f050
simone.sabato <simone.sabato@finconsgroup.com>
2022-07-14 15:29:38 +0200

    fix: fixed test

--- src/components/sbb-form-error/sbb-form-error.spec.ts
+++ src/components/sbb-form-error/sbb-form-error.spec.ts
@@ -13,9 +13,7 @@ describe('sbb-form-error', () => {
           <mock:shadow-root>
             <span class="form-error--error-space-default input-label-error__icon">
               <slot name='icon'>
-                <svg width="24" height="24" viewBox="0,0,24,24" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="m4,12.0002c0-4.47082,3.5301-8,8-8s8,3.52918,8,8c0,4.4699-3.5301,8-8,8-4.46986,0-8-3.5301-8-8zm8-9c-5.0221,0-9,3.97682-9,9,0,5.0221,3.97786,9,9,9,5.0221,0,9-3.9779,9-9,0-5.02318-3.9779-9-9-9zm.98,4.9995v-2h-1v2h1zm0,7.9903h2.0005v1h-5v-1H11.98v-5H10v-1h2.98v6z"></path>
-                </svg>
+                <sbb-icon name="circle-information-small"></sbb-icon>
               </slot>
             </span>
             <span class="input-label-error">
