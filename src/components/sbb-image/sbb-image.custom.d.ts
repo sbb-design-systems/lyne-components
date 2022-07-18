f7943eafc9a961267e0781d1d86445e1ee484e42
Sébastien Closs <sebastien.closs@one-inside.com>
2022-07-18 15:27:09 +0200

    refactor: sbb-image-1245
    - removed hideFromScreenreader property
    - removed the variant property

--- src/components/sbb-image/sbb-image.custom.d.ts
+++ src/components/sbb-image/sbb-image.custom.d.ts
@@ -3,5 +3,4 @@ export interface InterfaceImageAttributes {
   decoding?: 'sync' | 'async' | 'auto';
   importance?: 'auto' | 'high' | 'low';
   loading?: 'eager' | 'lazy';
-  variant?: 'teaser-hero';
 }
