85f395f924c137bdc42b4977da54c03c7bf7ed45
Osman Minaz <osman.minaz@t-systems.com>
2022-07-25 17:34:20 +0200

    feat: add badge slot

--- src/components/sbb-timetable-row/sbb-timetable-row.spec.ts
+++ src/components/sbb-timetable-row/sbb-timetable-row.spec.ts
@@ -74,4 +74,59 @@ describe('sbb-timetable-row', () => {
         `);
     });
   });
+
+  describe('sbb-timetable-row with badge', () => {
+    it('renders', async () => {
+      const { root } = await newSpecPage({
+        components: [SbbTimetableRow],
+        html: `<sbb-timetable-row role="row"><sbb-card-badge slot="badge" text="test"/></sbb-timetable-row>`,
+      });
+
+      expect(root).toEqualHtml(`
+        <sbb-timetable-row role="row">
+          <mock:shadow-root>
+            <div class="timetable__row timetable__row-badge" role="row">
+              <slot name="badge"></slot>
+                <div class="timetable__row-header" role="rowheader">
+                  <slot name="pictogram"></slot>
+                  <slot name="transportNumber">
+                    <span></span>
+                  </slot>
+                  <slot name="direction"></slot>
+                </div>
+              <div class="timetable__row-body" role="gridcell">
+                <slot name="walkTimeBefore"></slot>
+                <slot name="leftTime"></slot>
+                <slot name="pearlChain"></slot>
+                <slot name="rightTime"></slot>
+                <slot name="walkTimeAfter"></slot>
+              </div>
+
+              <div class="timetable__row-footer" role="gridcell">
+                <slot name="plattform"></slot>
+                <ul class="timetable__row-occupancy">
+                  <li>
+                    <slot name="occupancyFirstClass">
+                      1.
+                      <sbb-icon name="walk-small"></sbb-icon>
+                    </slot>
+                  </li>
+                  <li>
+                    <slot name="occupancySecondClass">
+                      2.
+                      <sbb-icon name="walk-small"></sbb-icon>
+                    </slot>
+                  </li>
+                </ul>
+                <slot name="travelHints"></slot>
+                <slot name="duration"></slot>
+                <slot name="warning"></slot>
+              </div>
+            </div>
+          </mock:shadow-root>
+          <sbb-card-badge slot="badge" text="test"></sbb-card-badge>
+        </sbb-timetable-row>
+        `);
+    });
+  });
 });
