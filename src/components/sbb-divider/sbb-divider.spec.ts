023abb911780018d3f64f2eab329209269147655
Konstantin Pachemski <konstantin.pachemski@one-inside.com>
2022-07-26 10:48:34 +0200

    fix: update sbb-divider.spec.ts

--- src/components/sbb-divider/sbb-divider.spec.ts
+++ src/components/sbb-divider/sbb-divider.spec.ts
@@ -11,7 +11,7 @@ describe('sbb-divider', () => {
     expect(root).toEqualHtml(`
         <sbb-divider>
           <mock:shadow-root>
-           <div aria-orientation="horizontal" class="sbb-divider sbb-divider--horizontal sbb-divider--primary" role="separator"></div>
+           <div aria-orientation="horizontal" class="sbb-divider sbb-divider--horizontal" role="separator"></div>
           </mock:shadow-root>
         </sbb-divider>
       `);
