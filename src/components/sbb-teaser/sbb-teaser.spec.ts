5d92acb44779ce088a4de2ff761c0c82906202ae
Jeremias Peier <jeremias.peier@sbb.ch>
2022-07-28 13:14:09 +0200

    fix: tests

--- src/components/sbb-teaser/sbb-teaser.spec.ts
+++ src/components/sbb-teaser/sbb-teaser.spec.ts
@@ -13,12 +13,14 @@ describe('sbb-teaser', () => {
         <sbb-teaser accessibility-label="Sbb teaser" href="https://github.com/lyne-design-system/lyne-components" is-stacked>
           <mock:shadow-root>
             <a aria-label="Sbb teaser" class="teaser  teaser--is-stacked" href="https://github.com/lyne-design-system/lyne-components">
+            <span class="teaser__container">
               <span class='teaser__image-wrapper'><slot name='image'/></span>
                 <span class='teaser__text'>
                   <sbb-title class="teaser__lead" level="5" visuallevel="5">
                     <slot name='title'/>
                   </sbb-title>
                 <span class='teaser__description'><slot name='description'/></span>
+              </span>
             </a>
           </mock:shadow-root>
         </sbb-teaser>
@@ -36,12 +38,14 @@ describe('sbb-teaser', () => {
         <sbb-teaser accessibility-label="Sbb teaser" href="https://github.com/lyne-design-system/lyne-components">
           <mock:shadow-root>
             <a aria-label="Sbb teaser" class="teaser" href="https://github.com/lyne-design-system/lyne-components">
-              <span class='teaser__image-wrapper'><slot name='image'/></span>
-                <span class='teaser__text'>
-                  <sbb-title class="teaser__lead" level="5" visuallevel="5">
-                    <slot name='title'/>
-                  </sbb-title>
-                <span class='teaser__description'><slot name='description'/></span>
+              <span class="teaser__container">
+                <span class='teaser__image-wrapper'><slot name='image'/></span>
+                  <span class='teaser__text'>
+                    <sbb-title class="teaser__lead" level="5" visuallevel="5">
+                      <slot name='title'/>
+                    </sbb-title>
+                  <span class='teaser__description'><slot name='description'/></span>
+                </span>
               </span>
             </a>
           </mock:shadow-root>
