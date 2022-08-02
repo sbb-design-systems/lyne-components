3d19a0e444c9d95d59ac1243cdef20d0e5d5831a
simone.sabato <simone.sabato@finconsgroup.com>
2022-08-02 16:21:02 +0200

    feat: first implementation for sbb-header

--- src/components.d.ts
+++ src/components.d.ts
@@ -325,6 +325,9 @@ export namespace Components {
          */
         "variant"?: InterfaceGridAttributes['variant'];
     }
+    interface SbbHeader {
+        "shadow": boolean;
+    }
     interface SbbIcon {
         /**
           * The aria-hidden property is set to "true" by default, since an icon alone does not convey any useful information for a screen-reader user.
@@ -1140,6 +1143,12 @@ declare global {
         prototype: HTMLSbbGridElement;
         new (): HTMLSbbGridElement;
     };
+    interface HTMLSbbHeaderElement extends Components.SbbHeader, HTMLStencilElement {
+    }
+    var HTMLSbbHeaderElement: {
+        prototype: HTMLSbbHeaderElement;
+        new (): HTMLSbbHeaderElement;
+    };
     interface HTMLSbbIconElement extends Components.SbbIcon, HTMLStencilElement {
     }
     var HTMLSbbIconElement: {
@@ -1380,6 +1389,7 @@ declare global {
         "sbb-divider": HTMLSbbDividerElement;
         "sbb-footer": HTMLSbbFooterElement;
         "sbb-grid": HTMLSbbGridElement;
+        "sbb-header": HTMLSbbHeaderElement;
         "sbb-icon": HTMLSbbIconElement;
         "sbb-image": HTMLSbbImageElement;
         "sbb-input-error": HTMLSbbInputErrorElement;
@@ -1710,6 +1720,9 @@ declare namespace LocalJSX {
           * Grid variant
          */
         "variant"?: InterfaceGridAttributes['variant'];
+    }
+    interface SbbHeader {
+        "shadow"?: boolean;
     }
     interface SbbIcon {
         /**
@@ -2458,6 +2471,7 @@ declare namespace LocalJSX {
         "sbb-divider": SbbDivider;
         "sbb-footer": SbbFooter;
         "sbb-grid": SbbGrid;
+        "sbb-header": SbbHeader;
         "sbb-icon": SbbIcon;
         "sbb-image": SbbImage;
         "sbb-input-error": SbbInputError;
@@ -2513,6 +2527,7 @@ declare module "@stencil/core" {
             "sbb-divider": LocalJSX.SbbDivider & JSXBase.HTMLAttributes<HTMLSbbDividerElement>;
             "sbb-footer": LocalJSX.SbbFooter & JSXBase.HTMLAttributes<HTMLSbbFooterElement>;
             "sbb-grid": LocalJSX.SbbGrid & JSXBase.HTMLAttributes<HTMLSbbGridElement>;
+            "sbb-header": LocalJSX.SbbHeader & JSXBase.HTMLAttributes<HTMLSbbHeaderElement>;
             "sbb-icon": LocalJSX.SbbIcon & JSXBase.HTMLAttributes<HTMLSbbIconElement>;
             "sbb-image": LocalJSX.SbbImage & JSXBase.HTMLAttributes<HTMLSbbImageElement>;
             "sbb-input-error": LocalJSX.SbbInputError & JSXBase.HTMLAttributes<HTMLSbbInputErrorElement>;
