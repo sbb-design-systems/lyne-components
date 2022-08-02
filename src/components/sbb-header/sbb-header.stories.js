524710a0225eeae2b81468003047d3a8fc6419be
simone.sabato <simone.sabato@finconsgroup.com>
2022-08-02 16:21:30 +0200

    feat: first implementation for sbb-header

---
+++ src/components/sbb-header/sbb-header.stories.js
@@ -0,0 +1,36 @@
+import { h } from 'jsx-dom';
+import readme from './readme.md';
+
+const Template = (args) => <sbb-header {...args}></sbb-header>;
+
+export const story1 = Template.bind({});
+
+story1.args = {
+  'some-prop': 'opt1',
+};
+
+story1.documentation = {
+  title: 'Title which will be rendered on documentation platform',
+};
+
+export default {
+  decorators: [
+    (Story) => (
+      <div style={'padding: 2rem'}>
+        <Story />
+      </div>
+    ),
+  ],
+  documentation: {
+    disableArgs: ['someArgToDisableForDocumentationPlatform'],
+  },
+  parameters: {
+    backgrounds: {
+      disable: true,
+    },
+    docs: {
+      extractComponentDescription: () => readme,
+    },
+  },
+  title: 'components/sbb-header',
+};
---
+++ src/components/sbb-header/sbb-header.tsx
@@ -0,0 +1,26 @@
+import { Component, h, Host, Prop } from '@stencil/core';
+
+/**
+ * @slot unnamed - Use this to document a slot.
+ */
+
+@Component({
+  shadow: true,
+  styleUrl: 'sbb-header.scss',
+  tag: 'sbb-header',
+})
+export class SbbHeader {
+
+  @Prop() public shadow: boolean;
+
+  public render(): JSX.Element {
+    return (
+      <Host class={this.shadow ? 'sbb-header-shadow--display' : 'sbb-header-shadow--hide'}>
+        <slot />
+        <slot name='logo'>
+          <sbb-logo></sbb-logo>
+        </slot>
+      </Host>
+    );
+  }
+}
