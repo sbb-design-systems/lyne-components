447451994653effbd7ef9eb1b7759a295cef1031
simone.sabato <simone.sabato@finconsgroup.com>
2022-08-05 15:26:54 +0200

    fix: fixed unit tests

--- src/components/sbb-form-field/sbb-form-field.spec.ts
+++ src/components/sbb-form-field/sbb-form-field.spec.ts
@@ -12,35 +12,35 @@ describe('sbb-form-field', () => {
     });
 
     expect(root).toEqualHtml(`
-    <sbb-form-field class="form-field--error-space-default form-field--size-l" label="Fill input">
-    <mock:shadow-root>
-      <div class="form-field__wrapper">
-        <div class="form-field__prefix form-field__prefix--empty">
-          <slot name="prefix"></slot>
-        </div>
-        <div class="form-field__input-container">
-          <label class="form-field__label">
-            <slot name="label">
-              <span>
-                Fill input
-              </span>
-            </slot>
-          </label>
-          <div class="form-field__input">
-            <slot></slot>
+      <sbb-form-field class="form-field--error-space-default form-field--size-l" label="Fill input">
+        <mock:shadow-root>
+          <div class="form-field__wrapper">
+            <div class="form-field__prefix form-field__prefix--empty">
+              <slot name="prefix"></slot>
+            </div>
+            <div class="form-field__input-container">
+              <label class="form-field__label">
+                <slot name="label">
+                  <span>
+                    Fill input
+                  </span>
+                </slot>
+              </label>
+              <div class="form-field__input">
+                <slot></slot>
+              </div>
+            </div>
+            <div class="form-field__suffix form-field__suffix--empty">
+              <slot name="suffix"></slot>
+            </div>
           </div>
-        </div>
-        <div class="form-field__suffix form-field__suffix--empty">
-          <slot name="suffix"></slot>
-        </div>
-      </div>
-      <div class="form-field__error form-field__error--empty">
-        <slot name="error"></slot>
-      </div>
-    </mock:shadow-root>
-    <input class="input" placeholder="This is an input" slot="input">
-  </sbb-form-field>
-      `);
+          <div class="form-field__error form-field__error--empty">
+            <slot name="error"></slot>
+          </div>
+        </mock:shadow-root>
+        <input class="input" placeholder="This is an input" slot="input">
+      </sbb-form-field>
+    `);
   });
 
   it('renders disabled input', async () => {
