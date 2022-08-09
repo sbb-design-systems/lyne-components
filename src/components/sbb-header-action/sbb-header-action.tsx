b29e9b0dd51aa62da7d8799856af64756abd3fc4
Davide Mininni <davide.mininni@finconsgroup.com>
2022-08-09 18:24:01 +0200

    refactor: reuse logic from sbb-link to create 1st header impl

--- src/components/sbb-header-action/sbb-header-action.tsx
+++ src/components/sbb-header-action/sbb-header-action.tsx
@@ -1,8 +1,16 @@
-import { Component, h, JSX, Prop } from '@stencil/core';
+import { Component, Event, EventEmitter, h, JSX, Prop } from '@stencil/core';
+import {
+  ButtonType,
+  getLinkButtonAttributeList,
+  LinkButtonProperties,
+} from '../../global/interfaces/link-button-properties';
 import { InterfaceSbbHeaderActionAttributes } from './sbb-header-action.custom';
 
+let nextId = 0;
+
 /**
- * @slot unnamed - Use this to document a slot.
+ * @slot icon - Slot used to render the action icon
+ * @slot unnamed - Slot used to render the action label
  */
 
 @Component({
@@ -10,23 +18,102 @@ import { InterfaceSbbHeaderActionAttributes } from './sbb-header-action.custom';
   styleUrl: 'sbb-header-action.scss',
   tag: 'sbb-header-action',
 })
-export class SbbHeaderAction {
-  @Prop() public isAnchorOrButton: boolean;
+export class SbbHeaderAction implements LinkButtonProperties {
+  /**
+   *
+   */
+  @Prop() public actionHeaderId = `sbb-action-header.${++nextId}`;
 
+  /**
+   *
+   */
   @Prop() public expandFrom: InterfaceSbbHeaderActionAttributes['expandFrom'] = 'medium';
 
+  /**
+   *
+   */
   @Prop() public icon?: string;
 
+  /**
+   *
+   */
+  @Prop() public accessibilityDescribedby: string | undefined;
+
+  /**
+   *
+   */
+  @Prop() public accessibilityLabel: string | undefined;
+
+  /**
+   *
+   */
+  @Prop() public accessibilityLabelledby: string | undefined;
+
+  /**
+   *
+   */
+  @Prop() public download: boolean | undefined;
+
+  /**
+   *
+   */
+  @Prop() public href: string | undefined;
+
+  /**
+   *
+   */
+  @Prop() public form: string | undefined;
+
+  /**
+   *
+   */
+  @Prop() public name: string | undefined;
+
+  /**
+   *
+   */
+  @Prop() public type: ButtonType | undefined;
+
+  /**
+   *
+   */
+  @Prop() public disabled: boolean | undefined;
+
+  /**
+   *
+   */
+  @Prop() public eventId: string;
+
+  /**
+   *
+   */
+  @Event({
+    bubbles: true,
+    composed: true,
+    eventName: 'sbb-header-action-button_click',
+  })
+  public click: EventEmitter<any>;
+
+  /**
+   *
+   */
+  public emitButtonClick(): void {
+    if (!this.disabled) {
+      this.click.emit(this.eventId);
+    }
+  }
+
   public render(): JSX.Element {
-    const TAGNAME = this.isAnchorOrButton ? 'a' : 'button';
+    const attributeList = getLinkButtonAttributeList(this.actionHeaderId, '', this);
+    const TAG_NAME: string = this.href ? 'a' : 'button';
 
     return (
-      <TAGNAME href="http://www.sbb.ch" onClick={() => alert('Hello World')}>
+      <TAG_NAME {...attributeList}>
         <slot name="icon">
-          <sbb-icon name={this.icon}></sbb-icon>
+          <sbb-icon name={this.icon} />
         </slot>
         <slot />
-      </TAGNAME>
+      </TAG_NAME>
     );
   }
 }
