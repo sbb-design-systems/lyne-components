7c19a50ac60a48026213195d5848fbf22612b81a
Davide Mininni <davide.mininni@finconsgroup.com>
2022-08-10 11:29:13 +0200

    style: add some css classes

--- src/components/sbb-header-action/sbb-header-action.tsx
+++ src/components/sbb-header-action/sbb-header-action.tsx
@@ -1,4 +1,4 @@
-import { Component, Event, EventEmitter, JSX, h, Host, Prop } from '@stencil/core';
+import { Component, Event, EventEmitter, h, Host, JSX, Prop } from '@stencil/core';
 import {
   ButtonType,
   getButtonAttributeList,
