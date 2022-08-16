056437e7e616d7dd3eddc0f16ea46e44395a30bc
Davide Mininni <davide.mininni@finconsgroup.com>
2022-08-16 15:25:14 +0200

    feat: add shadow management

--- src/components/sbb-header/sbb-header.tsx
+++ src/components/sbb-header/sbb-header.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Host, Prop } from '@stencil/core';
+import { Component, h, Listen, Prop, State } from '@stencil/core';
 
 /**
  * @slot unnamed - Use this to document a slot.
@@ -12,22 +12,28 @@ import { Component, h, Host, Prop } from '@stencil/core';
 export class SbbHeader {
   @Prop() public shadow: boolean;
 
+  /** @internal */
+  @State() private _isScrolled: boolean;
+
+  @Listen('scroll', { target: 'window' })
+  public handleScroll(): void {
+    this._isScrolled = window.scrollY !== 0;
+  }
+
   public render(): JSX.Element {
     return (
-      <Host class={this.shadow ? 'sbb-header--shadow--display' : 'sbb-header--shadow--hide'}>
-        <div class="sbb-header">
-          <div class="sbb-header__wrapper">
-            <div class="sbb-header__left">
-              <slot />
-            </div>
-            <div class="sbb-header__right">
-              <slot name="logo">
-                <sbb-logo protectiveRoom="none" />
-              </slot>
-            </div>
+      <div class={{ 'sbb-header': true, 'sbb-header--shadow': this.shadow && this._isScrolled }}>
+        <div class="sbb-header__wrapper">
+          <div class="sbb-header__left">
+            <slot />
+          </div>
+          <div class="sbb-header__right">
+            <slot name="logo">
+              <sbb-logo protectiveRoom="none" />
+            </slot>
           </div>
         </div>
-      </Host>
+      </div>
     );
   }
 }
