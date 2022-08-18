84eaba60a87f6000bec375217893c20fcbdcd8c0
Davide Mininni <davide.mininni@finconsgroup.com>
2022-08-18 15:22:22 +0200

    fix: minor fixes

--- src/components/sbb-link/sbb-link.spec.ts
+++ src/components/sbb-link/sbb-link.spec.ts
@@ -3,19 +3,14 @@ import { SbbLink } from './sbb-link';
 import { newSpecPage } from '@stencil/core/testing';
 
 describe('sbb-link', () => {
-  it('renders a plain link', async () => {
+  it('renders a plain link with no icon', async () => {
     const { root } = await newSpecPage({
       components: [SbbLink],
       html: `
         <sbb-link
           href="https://github.com/lyne-design-system/lyne-components"
-          icon-placement="end"
-          icon="chevron-small-right-small"
           text-size="m"
           accessibility-label="Travelcards &amp; tickets">
-            <span slot="icon">
-                ${lyneIcons.icons['chevron-small-right-small']}
-            </span>
             Travelcards &amp; tickets.
           </sbb-link>
       `,
@@ -24,8 +19,6 @@ describe('sbb-link', () => {
     expect(root).toEqualHtml(`
         <sbb-link
           href="https://github.com/lyne-design-system/lyne-components"
-          icon-placement="end"
-          icon="chevron-small-right-small"
           text-size="m"
           variant="block"
           accessibility-label="Travelcards &amp; tickets"
@@ -33,34 +26,26 @@ describe('sbb-link', () => {
           <mock:shadow-root>
             <a
               aria-label="Travelcards & tickets. Link target opens in new window."
-              class="sbb-link sbb-link--icon-placement-end sbb-link--text-m"
+              class="sbb-link sbb-link--text-m"
               dir="ltr"
               href="https://github.com/lyne-design-system/lyne-components"
               target="_blank"
               rel="external noopener nofollow"
             >
-              <slot name="icon">
-                <sbb-icon name="chevron-small-right-small"></sbb-icon>
-              </slot>
+              <slot name="icon"></slot>
               <slot></slot>
             </a>
           </mock:shadow-root>
-          <span slot="icon">
-            <svg height="24" viewBox="0,0,24,24" width="24" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" clip-rule="evenodd" d="m10.6776,7.74045,3.949,3.90395.3597.3557-.3597.3555-3.95,3.904-.70297-.7112L13.5639,12,9.97459,8.4516l.70301-.71115z">
-              </path>
-            </svg>
-          </span>
           Travelcards &amp; tickets.
         </sbb-link>
       `);
   });
 
-  it('renders a link as a button', async () => {
+  it('renders a link as a button with provided icon', async () => {
     const { root } = await newSpecPage({
       components: [SbbLink],
       html: `
-        <sbb-link icon-placement="end" icon="chevron-small-right-small" text-size="m">
+        <sbb-link icon-placement="end" text-size="m">
           <span slot="icon">
             ${lyneIcons.icons['chevron-small-right-small']}
           </span>
@@ -71,7 +56,6 @@ describe('sbb-link', () => {
     expect(root).toEqualHtml(`
         <sbb-link
             icon-placement="end"
-            icon="chevron-small-right-small"
             text-size="m"
             variant="block">
           <mock:shadow-root>
@@ -79,9 +63,7 @@ describe('sbb-link', () => {
               class="sbb-link sbb-link--icon-placement-end sbb-link--text-m"
               dir="ltr"
               type="button">
-              <slot name="icon">
-                <sbb-icon name="chevron-small-right-small"></sbb-icon>
-              </slot>
+              <slot name="icon"></slot>
               <slot></slot>
             </button>
           </mock:shadow-root>
@@ -96,12 +78,12 @@ describe('sbb-link', () => {
       `);
   });
 
-  it('renders a link as a span', async () => {
+  it('renders a link as a span with sbb-icon', async () => {
     const { root } = await newSpecPage({
       components: [SbbLink],
       html: `
       <a>
-        <sbb-link icon-placement="end" icon="chevron-small-right-small" text-size="m">
+        <sbb-link icon-placement="end" icon-name="chevron-small-right-small" text-size="m">
           Travelcards &amp; tickets.
         </sbb-link>
       </a>`,
@@ -110,7 +92,7 @@ describe('sbb-link', () => {
     expect(root).toEqualHtml(`
         <sbb-link
             icon-placement="end"
-            icon="chevron-small-right-small"
+            icon-name="chevron-small-right-small"
             text-size="m"
             variant="block">
           <mock:shadow-root>
