e962ba7050c06960e3d71ec943ab3a4087f7cb27
Marco D'Auria <101181211+dauriamarco@users.noreply.github.com>
2022-08-24 09:15:40 +0200

    refactor(sbb-tab-group): improve tabs selection (#1349)

--- src/components/sbb-tab-group/sbb-tab-group.e2e.ts
+++ src/components/sbb-tab-group/sbb-tab-group.e2e.ts
@@ -9,11 +9,11 @@ describe('sbb-tab-group', () => {
     <sbb-tab-group initial-selected-index="1">
       <sbb-tab-title id="sbb-tab-1">Test tab label 1</sbb-tab-title>
       <div>Test tab content 1</div>
-      <sbb-tab-title id="sbb-tab-3">Test tab label 2</sbb-tab-title>
+      <sbb-tab-title id="sbb-tab-2">Test tab label 2</sbb-tab-title>
       <div>Test tab content 2</div>
-      <sbb-tab-title id="sbb-tab-5" disabled>Test tab label 3</sbb-tab-title>
+      <sbb-tab-title id="sbb-tab-3" disabled>Test tab label 3</sbb-tab-title>
       <div>Test tab content 3</div>
-      <sbb-tab-title id="sbb-tab-7">Test tab label 4</sbb-tab-title>
+      <sbb-tab-title id="sbb-tab-4">Test tab label 4</sbb-tab-title>
     </sbb-tab-group>
     `);
     element = await page.find('sbb-tab-group');
@@ -30,14 +30,13 @@ describe('sbb-tab-group', () => {
   });
 
   it('renders no content tab panel', async () => {
-    const content = await page.find('sbb-tab-group > sbb-tab-title#sbb-tab-7 + div');
-    console.log('CONTENUTOOOOO', content.nodeName);
+    const content = await page.find('sbb-tab-group > sbb-tab-title#sbb-tab-4 + div');
 
     expect(content.textContent).toEqual('No content.');
   });
 
   it('renders initial selected index', async () => {
-    const initialSelectedTab = await page.find('sbb-tab-group > sbb-tab-title#sbb-tab-3');
+    const initialSelectedTab = await page.find('sbb-tab-group > sbb-tab-title#sbb-tab-2');
 
     expect(initialSelectedTab).toHaveAttribute('active');
   });
@@ -69,7 +68,7 @@ describe('sbb-tab-group', () => {
     it('selects tab on right arrow key pressed', async () => {
       await page.keyboard.down('Tab');
       await page.keyboard.down('ArrowRight');
-      const tab = await page.find('sbb-tab-group > sbb-tab-title#sbb-tab-7');
+      const tab = await page.find('sbb-tab-group > sbb-tab-title#sbb-tab-4');
 
       expect(tab).toHaveAttribute('active');
     });
