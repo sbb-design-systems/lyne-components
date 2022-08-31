5074fcbb0a686419c2eda1bb87038d6e6709ebe1
Lukas Spirig <lukas.spirig@sbb.ch>
2022-08-31 15:42:52 +0200

    fix: review

--- stencil.config.ts
+++ stencil.config.ts
@@ -20,7 +20,7 @@ export const config: Config = {
       type: 'dist',
     },
     {
-      type: 'dist-custom-elements',
+      type: 'dist-custom-elements-bundle',
     },
     {
       footer: '',
