4227e04eafed4b4e9bc2cff2ec3a150a3e12b9e0
Davide Mininni <davide.mininni@finconsgroup.com>
2022-09-02 09:33:25 +0200

    fix: missing export breaks build

--- react-library/src/components/stencil-generated/index.ts
+++ react-library/src/components/stencil-generated/index.ts
@@ -13,6 +13,7 @@ export const SbbAccordionItem = /*@__PURE__*/createReactComponent<JSX.SbbAccordi
 export const SbbAutocomplete = /*@__PURE__*/createReactComponent<JSX.SbbAutocomplete, HTMLSbbAutocompleteElement>('sbb-autocomplete');
 export const SbbAutocompleteItem = /*@__PURE__*/createReactComponent<JSX.SbbAutocompleteItem, HTMLSbbAutocompleteItemElement>('sbb-autocomplete-item');
 export const SbbButton = /*@__PURE__*/createReactComponent<JSX.SbbButton, HTMLSbbButtonElement>('sbb-button');
+export const SbbCard = /*@__PURE__*/createReactComponent<JSX.SbbCard, HTMLSbbCardElement>('sbb-card');
 export const SbbCardBadge = /*@__PURE__*/createReactComponent<JSX.SbbCardBadge, HTMLSbbCardBadgeElement>('sbb-card-badge');
 export const SbbCardProduct = /*@__PURE__*/createReactComponent<JSX.SbbCardProduct, HTMLSbbCardProductElement>('sbb-card-product');
 export const SbbClock = /*@__PURE__*/createReactComponent<JSX.SbbClock, HTMLSbbClockElement>('sbb-clock');
