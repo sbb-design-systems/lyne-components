1530990d8d1978a5fd452fb514e1f8ae9eec496d
Jeremias Peier <jeremias.peier@sbb.ch>
2022-09-05 17:45:43 +0200

    test: add test dummies

--- src/components/sbb-teaser-hero/sbb-teaser-hero.spec.ts
+++ src/components/sbb-teaser-hero/sbb-teaser-hero.spec.ts
@@ -2,25 +2,134 @@ import { SbbTeaserHero } from './sbb-teaser-hero';
 import { newSpecPage } from '@stencil/core/testing';
 
 describe('sbb-teaser-hero', () => {
-  it('renders', async () => {
+  it('should render all properties', async () => {
     const { root } = await newSpecPage({
       components: [SbbTeaserHero],
-      html: '<sbb-teaser-hero accessibility-title="sbb teaser" link="https://www.sbb.ch" new-window-info-text="Link öffnet in neuem Fenster." open-in-new-window="true"></sbb-teaser-hero>',
+      html: '<sbb-teaser-hero accessibility-label="label" accessibility-describedby="describedby" accessibility-labelledby="labelledby" href="https://www.sbb.ch" rel="external" target="_blank" id-value="id1" link-content="Find out more" image-src="https://cdn.img.sbb.ch/content/dam/internet/sharedimages/personen/frau-im-ferien.jpg?crop=focalpoint&fp-x=0.5053125&fp-y=0.6458333" image-alt="SBB CFF FFS Employee">Break out and explore castles and palaces.</sbb-teaser-hero>',
     });
 
     expect(root).toEqualHtml(`
-      <sbb-teaser-hero accessibility-title="sbb teaser" link="https://www.sbb.ch" new-window-info-text="Link öffnet in neuem Fenster." open-in-new-window="true">
+      <sbb-teaser-hero accessibility-label="label" accessibility-describedby="describedby" accessibility-labelledby="labelledby" href="https://www.sbb.ch" rel="external" target="_blank" id-value="id1" link-content="Find out more" image-src="https://cdn.img.sbb.ch/content/dam/internet/sharedimages/personen/frau-im-ferien.jpg?crop=focalpoint&fp-x=0.5053125&fp-y=0.6458333" image-alt="SBB CFF FFS Employee">
           <mock:shadow-root>
-            <a class="teaser-hero" aria-label="sbb teaser" href="https://www.sbb.ch" rel="external noopener nofollow" target="_blank">
+            <a
+              aria-describedby="describedby"
+              aria-label="label. Link target opens in new window."
+              aria-labelledby="labelledby"
+              class="teaser-hero"
+              dir="ltr"
+              id="id1"
+              href="https://www.sbb.ch"
+              rel="external"
+              target="_blank"
+            >
               <span class="teaser-hero__panel">
-                <slot name="panel"></slot>
+                <span class="teaser-hero__panel-text">
+                  <slot></slot>
+                </span>
+                <sbb-link
+                  class="teaser-hero__panel-link"
+                  icon-name="chevron-small-right-small"
+                  icon-placement="end"
+                  text-size="m"
+                  negative
+                >
+                  <slot name="link-content">Find out more</slot>
+                </sbb-link>
               </span>
-              <slot name="image"></slot>
-              <span class="teaser-hero__link-info-text">
-                Link öffnet in neuem Fenster.
-              </span>
+             <slot name="image">
+               <sbb-image image-src="https://cdn.img.sbb.ch/content/dam/internet/sharedimages/personen/frau-im-ferien.jpg?crop=focalpoint&fp-x=0.5053125&fp-y=0.6458333" image-alt="SBB CFF FFS Employee"></sbb-image>
+             </slot>
             </a>
           </mock:shadow-root>
+          Break out and explore castles and palaces.
+        </sbb-teaser-hero>
+      `);
+  });
+
+  it('should render without link', async () => {
+    const { root } = await newSpecPage({
+      components: [SbbTeaserHero],
+      html: '<sbb-teaser-hero accessibility-label="label" accessibility-describedby="describedby" accessibility-labelledby="labelledby" href="https://www.sbb.ch" rel="external" target="_blank" id-value="id1" link-content="Find out more" image-src="https://cdn.img.sbb.ch/content/dam/internet/sharedimages/personen/frau-im-ferien.jpg?crop=focalpoint&fp-x=0.5053125&fp-y=0.6458333" image-alt="SBB CFF FFS Employee">Break out and explore castles and palaces.</sbb-teaser-hero>',
+    });
+
+    expect(root).toEqualHtml(`
+      <sbb-teaser-hero accessibility-label="label" accessibility-describedby="describedby" accessibility-labelledby="labelledby" href="https://www.sbb.ch" rel="external" target="_blank" id-value="id1" link-content="Find out more" image-src="https://cdn.img.sbb.ch/content/dam/internet/sharedimages/personen/frau-im-ferien.jpg?crop=focalpoint&fp-x=0.5053125&fp-y=0.6458333" image-alt="SBB CFF FFS Employee">
+          <mock:shadow-root>
+            <a
+              aria-describedby="describedby"
+              aria-label="label. Link target opens in new window."
+              aria-labelledby="labelledby"
+              class="teaser-hero"
+              dir="ltr"
+              id="id1"
+              href="https://www.sbb.ch"
+              rel="external"
+              target="_blank"
+            >
+              <span class="teaser-hero__panel">
+                <span class="teaser-hero__panel-text">
+                  <slot></slot>
+                </span>
+                <sbb-link
+                  class="teaser-hero__panel-link"
+                  icon-name="chevron-small-right-small"
+                  icon-placement="end"
+                  text-size="m"
+                  negative
+                >
+                  <slot name="link-content">Find out more</slot>
+                </sbb-link>
+              </span>
+             <slot name="image">
+               <sbb-image image-src="https://cdn.img.sbb.ch/content/dam/internet/sharedimages/personen/frau-im-ferien.jpg?crop=focalpoint&fp-x=0.5053125&fp-y=0.6458333" image-alt="SBB CFF FFS Employee"></sbb-image>
+             </slot>
+            </a>
+          </mock:shadow-root>
+          Break out and explore castles and palaces.
+        </sbb-teaser-hero>
+      `);
+  });
+
+  it('should render with slots', async () => {
+    const { root } = await newSpecPage({
+      components: [SbbTeaserHero],
+      html: '<sbb-teaser-hero accessibility-label="label" accessibility-describedby="describedby" accessibility-labelledby="labelledby" href="https://www.sbb.ch" rel="external" target="_blank" id-value="id1" link-content="Find out more" image-src="https://cdn.img.sbb.ch/content/dam/internet/sharedimages/personen/frau-im-ferien.jpg?crop=focalpoint&fp-x=0.5053125&fp-y=0.6458333" image-alt="SBB CFF FFS Employee">Break out and explore castles and palaces.</sbb-teaser-hero>',
+    });
+
+    expect(root).toEqualHtml(`
+      <sbb-teaser-hero accessibility-label="label" accessibility-describedby="describedby" accessibility-labelledby="labelledby" href="https://www.sbb.ch" rel="external" target="_blank" id-value="id1" link-content="Find out more" image-src="https://cdn.img.sbb.ch/content/dam/internet/sharedimages/personen/frau-im-ferien.jpg?crop=focalpoint&fp-x=0.5053125&fp-y=0.6458333" image-alt="SBB CFF FFS Employee">
+          <mock:shadow-root>
+            <a
+              aria-describedby="describedby"
+              aria-label="label. Link target opens in new window."
+              aria-labelledby="labelledby"
+              class="teaser-hero"
+              dir="ltr"
+              id="id1"
+              href="https://www.sbb.ch"
+              rel="external"
+              target="_blank"
+            >
+              <span class="teaser-hero__panel">
+                <span class="teaser-hero__panel-text">
+                  <slot></slot>
+                </span>
+                <sbb-link
+                  class="teaser-hero__panel-link"
+                  icon-name="chevron-small-right-small"
+                  icon-placement="end"
+                  text-size="m"
+                  negative
+                >
+                  <slot name="link-content">Find out more</slot>
+                </sbb-link>
+              </span>
+             <slot name="image">
+               <sbb-image image-src="https://cdn.img.sbb.ch/content/dam/internet/sharedimages/personen/frau-im-ferien.jpg?crop=focalpoint&fp-x=0.5053125&fp-y=0.6458333" image-alt="SBB CFF FFS Employee"></sbb-image>
+             </slot>
+            </a>
+          </mock:shadow-root>
+          Break out and explore castles and palaces.
         </sbb-teaser-hero>
       `);
   });
