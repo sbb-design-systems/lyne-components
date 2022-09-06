aa04e2f9038fa0e9222a17aabebb97be9f070095
simone.sabato <simone.sabato@finconsgroup.com>
2022-09-06 17:35:30 +0200

    feat: removed span, edited comment

--- src/components/sbb-card/sbb-card.tsx
+++ src/components/sbb-card/sbb-card.tsx
@@ -23,7 +23,7 @@ export class SbbCard {
   /**
    * It is used internally to show the `<sbb-card-badge>`.
    *
-   * @returns True whether size is equal to m, l, xl or xxl. False otherwise.
+   * @returns True whether size is equal to m, l, xl or xxl.
    */
   private _showSBBBadge(): boolean {
     return this.size === 'm' || this.size === 'l' || this.size === 'xl' || this.size === 'xxl';
