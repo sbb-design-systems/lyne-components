2d606b4caa264ea5e0f5f2415e17e29934030b43
Luise <luise.kuschan@sbb.ch>
2022-09-08 13:24:59 +0200

    fix: tests

--- src/components/sbb-checkbox/sbb-checkbox.spec.ts
+++ src/components/sbb-checkbox/sbb-checkbox.spec.ts
@@ -11,13 +11,13 @@ describe('sbb-checkbox', () => {
     expect(root).toEqualHtml(`
         <sbb-checkbox>
           <mock:shadow-root>
-            <label class="checkbox" htmlfor="sbb-checkbox-1">
+            <label class="sbb-checkbox" htmlfor="sbb-checkbox-1">
               <input id="sbb-checkbox-1" type="checkbox"/>
-              <span class="checkbox__inner">
-                <span class="checkbox__selection">
-                  <span class="checkbox__icon"></span>
+              <span class="sbb-checkbox__inner">
+                <span class="sbb-checkbox__selection">
+                  <span class="sbb-checkbox__icon"></span>
                 </span>
-                <span class="checkbox__label">
+                <span class="sbb-checkbox__label">
                   <slot></slot>
                   <sbb-icon></sbb-icon>
                 </span>
@@ -33,18 +33,18 @@ describe('sbb-checkbox', () => {
     it('renders sbb-checkbox with reversed label', async () => {
       const { root } = await newSpecPage({
         components: [SbbCheckbox],
-        html: '<sbb-checkbox label-icon="tickets-class-small" label-position="before" >Label</sbb-checkox>',
+        html: '<sbb-checkbox icon-name="tickets-class-small" icon-placement="start" >Label</sbb-checkox>',
       });
       expect(root).toEqualHtml(`
-          <sbb-checkbox label-icon="tickets-class-small" label-position="before">
+          <sbb-checkbox icon-name="tickets-class-small" icon-placement="start">
             <mock:shadow-root>
-              <label class="checkbox" htmlfor="sbb-checkbox-2">
+              <label class="sbb-checkbox" htmlfor="sbb-checkbox-2">
                 <input id="sbb-checkbox-2" type="checkbox"/>
-                <span class="checkbox__inner">
-                  <span class="checkbox__selection">
-                    <span class="checkbox__icon"></span>
+                <span class="sbb-checkbox__inner">
+                  <span class="sbb-checkbox__selection">
+                    <span class="sbb-checkbox__icon"></span>
                   </span>
-                  <span class="checkbox__label checkbox__label--before">
+                  <span class="sbb-checkbox__label sbb-checkbox__label--start">
                     <slot></slot>
                     <sbb-icon name="tickets-class-small"></sbb-icon>
                   </span>
@@ -62,22 +62,22 @@ describe('sbb-checkbox', () => {
       it('renders sbb-checkbox in checked state', async () => {
         const { root } = await newSpecPage({
           components: [SbbCheckbox],
-          html: '<sbb-checkbox checked>Label</sbb-checkox>',
+          html: '<sbb-checkbox checked>Label</sbb-checkbox>',
         });
         expect(root).toEqualHtml(`
             <sbb-checkbox checked="">
               <mock:shadow-root>
-                <label class="checkbox" htmlfor="sbb-checkbox-3">
+                <label class="sbb-checkbox" htmlfor="sbb-checkbox-3">
                   <input checked="" id="sbb-checkbox-3" type="checkbox"/>
-                  <span class="checkbox__inner">
-                    <span class="checkbox__selection">
-                      <span class="checkbox__icon">
+                  <span class="sbb-checkbox__inner">
+                    <span class="sbb-checkbox__selection">
+                      <span class="sbb-checkbox__icon">
                         <svg fill="none" height="8" viewBox="0 0 10 8" width="10" xmlns="http://www.w3.org/2000/svg">
                           <path d="M1 4.33 3.462 7 9 1" stroke="#EB0000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
                         </svg>
                       </span>
                     </span>
-                    <span class="checkbox__label">
+                    <span class="sbb-checkbox__label">
                       <slot></slot>
                       <sbb-icon></sbb-icon>
                     </span>
@@ -94,37 +94,26 @@ describe('sbb-checkbox', () => {
       it('renders sbb-checkbox in checked state', async () => {
         const { root } = await newSpecPage({
           components: [SbbCheckbox],
-          html: '<sbb-checkbox tristated="true">Label</sbb-checkox>',
+          html: '<sbb-checkbox tristated="true">Label</sbb-checkbox>',
         });
         expect(root).toEqualHtml(`
-            <sbb-checkbox tristated="true">
-              <mock:shadow-root>
-                <label class="checkbox" htmlfor="sbb-checkbox-4">
-                  <input id="sbb-checkbox-4" type="checkbox"/>
-                  <span class="checkbox__inner">
-                    <span class="checkbox__selection">
-                      <span class="checkbox__icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="8" height="2" viewBox="0 0 8 2" fill="none">
-                          <path
-                            d="M1 1H7"
-                            stroke="#EB0000"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                          />
-                        </svg>
-                      </span>
-                    </span>
-                    <span class="checkbox__label">
-                      <slot></slot>
-                      <sbb-icon></sbb-icon>
-                    </span>
-                  </span>
-                </label>
-              </mock:shadow-root>
-              Label
-            </sbb-checkbox>
-          `);
+        <sbb-checkbox tristated="true">
+          <mock:shadow-root>
+            <label class="sbb-checkbox" htmlfor="sbb-checkbox-4">
+              <input id="sbb-checkbox-4" type="checkbox">
+              <span class="sbb-checkbox__inner">
+                <span class="sbb-checkbox__selection">
+                  <span class="sbb-checkbox__icon"></span>
+                </span>
+                <span class="sbb-checkbox__label">
+                  <slot></slot>
+                  <sbb-icon></sbb-icon>
+                </span>
+              </span>
+            </label>
+          </mock:shadow-root>
+          Label
+        </sbb-checkbox>`);
       });
     });
 
@@ -137,14 +126,14 @@ describe('sbb-checkbox', () => {
         expect(root).toEqualHtml(`
             <sbb-checkbox disabled>
               <mock:shadow-root>
-                <label class="checkbox checkbox--disabled" htmlfor="sbb-checkbox-5">
+                <label class="sbb-checkbox sbb-checkbox--disabled" htmlfor="sbb-checkbox-5">
                   <input disabled id="sbb-checkbox-5" type="checkbox"/>
-                  <span class="checkbox__inner">
-                    <span class="checkbox__selection">
-                      <span class="checkbox__icon">
+                  <span class="sbb-checkbox__inner">
+                    <span class="sbb-checkbox__selection">
+                      <span class="sbb-checkbox__icon">
                       </span>
                     </span>
-                    <span class="checkbox__label">
+                    <span class="sbb-checkbox__label">
                       <slot></slot>
                       <sbb-icon></sbb-icon>
                     </span>
