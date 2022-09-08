3d8a91ec90b2236f047c20fcc185a7f4e6b5ca38
Davide Mininni <davide.mininni@finconsgroup.com>
2022-09-08 16:46:33 +0200

    chore: add story

--- src/components/sbb-card/sbb-card.stories.js
+++ src/components/sbb-card/sbb-card.stories.js
@@ -1,13 +1,32 @@
 import { h } from 'jsx-dom';
 import readme from './readme.md';
 
-const Template = (args) => <sbb-card {...args}>SBB card example text.</sbb-card>;
+const Content = () => [
+  <sbb-title level="4">Example text</sbb-title>,
+  <div>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porttitor blandit odio, ut
+    blandit libero cursus vel. Nunc eu congue mauris. Quisque sed facilisis leo. Curabitur
+    malesuada, nibh ac blandit vehicula, urna sem scelerisque magna, sed tincidunt neque arcu ac
+    justo.
+  </div>,
+];
 
-const TemplateWithBadge = (args) => (
-  <sbb-card {...args}>
-    Example text.
+const Template = (args) => <sbb-card {...args}>{Content()}</sbb-card>;
+
+const TemplateWithBadge = (args, style) => (
+  <sbb-card style={style} {...args}>
+    {Content()}
     <sbb-card-badge slot="badge" appearance="primary" is-discount price="19.99" text="from CHF" />
   </sbb-card>
+);
+
+const TemplateMultipleCards = (args) => (
+  <div style="display: flex; justify-content: space-evenly">
+    {TemplateWithBadge(args, 'width: 25vw; margin: 1rem;')}
+    {TemplateWithBadge(args, 'width: 25vw; margin: 1rem;')}
+    {TemplateWithBadge(args, 'width: 25vw; margin: 1rem;')}
+    {TemplateWithBadge(args, 'width: 25vw; margin: 1rem;')}
+  </div>
 );
 
 const sizeArg = {
@@ -26,23 +45,24 @@ const basicArgTypes = {
 };
 
 export const sbbCard = Template.bind({});
-
 sbbCard.argTypes = basicArgTypes;
-
 sbbCard.args = { ...basicArgs };
-
 sbbCard.documentation = {
   title: 'Card.',
 };
 
 export const sbbCardWithSbbBadge = TemplateWithBadge.bind({});
-
 sbbCardWithSbbBadge.argTypes = basicArgTypes;
-
 sbbCardWithSbbBadge.args = { ...basicArgs };
-
 sbbCardWithSbbBadge.documentation = {
   title: 'Card with badge (the slot is hidden whether sizes are below m).',
+};
+
+export const sbbCardMultiple = TemplateMultipleCards.bind({});
+sbbCardMultiple.argTypes = basicArgTypes;
+sbbCardMultiple.args = { ...basicArgs };
+sbbCardMultiple.documentation = {
+  title: 'Multiple cards.',
 };
 
 export default {
