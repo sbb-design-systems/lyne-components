064ed1b7c35956bdb73f2337bda3f6aed37a3ff3
Davide Mininni <davide.mininni@finconsgroup.com>
2022-09-12 17:17:07 +0200

    test: add spec test

--- src/components/sbb-menu/sbb-menu.spec.ts
+++ src/components/sbb-menu/sbb-menu.spec.ts
@@ -2,49 +2,61 @@ import { SbbMenu } from './sbb-menu';
 import { newSpecPage } from '@stencil/core/testing';
 
 describe('sbb-menu', () => {
-  // let component, page, trigger;
+  let page;
 
-  // beforeEach(async () => {
-  //   page = await newSpecPage({
-  //     components: [SbbMenu],
-  //     html: `<sbb-button id="menu-trigger" label="Menu trigger"></sbb-button>
-  //           <sbb-menu trigger="menu-trigger">
-  //             <sbb-link href="https://www.sbb.ch/en" variant="block">Profile</sbb-link>
-  //             <sbb-menu-action icon="tick-small">View</sbb-menu-action>
-  //             <sbb-menu-action icon="pen-small" amount="1" disabled>Edit</sbb-menu-action>
-  //             <sbb-menu-action icon="swisspass-small" amount="2">Details</sbb-menu-action>
-  //             <sbb-divider />
-  //             <sbb-menu-action icon="cross-small">Cancel</sbb-menu-action>
-  //           </sbb-menu>`,
-  //     supportsShadowDom: true,
-  //   });
-  //   component = page.doc.querySelector('sbb-menu');
-  //   trigger = page.doc.querySelector('sbb-button');
-  // });
+  beforeEach(async () => {
+    page = await newSpecPage({
+      components: [SbbMenu],
+      html: `
+        <sbb-button id="menu-trigger" label="Menu trigger"></sbb-button>
+        <sbb-menu trigger="menu-trigger">
+          <sbb-link href="https://www.sbb.ch/en" variant="block">Profile</sbb-link>
+          <sbb-menu-action icon="tick-small">View</sbb-menu-action>
+          <sbb-menu-action icon="pen-small" amount="1" disabled>Edit</sbb-menu-action>
+          <sbb-menu-action icon="swisspass-small" amount="2">Details</sbb-menu-action>
+          <sbb-divider />
+          <sbb-menu-action icon="cross-small">Cancel</sbb-menu-action>
+        </sbb-menu>
+      `,
+    });
+  });
 
   it('renders', async () => {
-    const { root } = await newSpecPage({
-      components: [SbbMenu],
-      html: '<sbb-menu />',
-    });
-
-    expect(root).toEqualHtml(`
-        <sbb-menu>
-          <mock:shadow-root>
-            <dialog class="sbb-menu">
-              <div class="sbb-menu__content">
-                <slot></slot>
-              </div>
-            </dialog>
-          </mock:shadow-root>
-        </sbb-menu>
-      `);
+    expect(page.root).toEqualHtml(`
+      <sbb-menu trigger="menu-trigger">
+        <mock:shadow-root>
+          <dialog class="sbb-menu">
+            <div class="sbb-menu__content">
+              <slot></slot>
+            </div>
+          </dialog>
+        </mock:shadow-root>
+        <sbb-link href="https://www.sbb.ch/en" variant="block">
+          Profile
+        </sbb-link>
+        <sbb-menu-action icon="tick-small">
+          View
+        </sbb-menu-action>
+        <sbb-menu-action amount="1" disabled="" icon="pen-small">
+          Edit
+        </sbb-menu-action>
+        <sbb-menu-action amount="2" icon="swisspass-small">
+          Details
+        </sbb-menu-action>
+        <sbb-divider>
+          <sbb-menu-action icon="cross-small">
+            Cancel
+          </sbb-menu-action>
+        </sbb-divider>
+      </sbb-menu>
+    `);
   });
 
   // it('opens the menu', async () => {
-  //   component.openMenu();
+  //   const component = page.doc.querySelector('sbb-menu');
+  //   const trigger = page.doc.querySelector('sbb-button');
+  //   trigger.click();
   //   await page.waitForChanges();
-
   //   expect(component).toHaveAttribute('open');
   // });
 
