0c5fd9fb7225f877af8cece305b85f129e0468ff
Davide Mininni <davide.mininni@finconsgroup.com>
2022-09-27 12:52:13 +0200

    chore: fix stories and tests

--- src/components/sbb-action-group/sbb-action-group.spec.ts
+++ src/components/sbb-action-group/sbb-action-group.spec.ts
@@ -5,14 +5,37 @@ describe('sbb-action-group', () => {
   it('renders', async () => {
     const { root } = await newSpecPage({
       components: [SbbActionGroup],
-      html: '<sbb-action-group align="start" orientation="horizontal"></sbb-action-group>',
+      html: `
+        <sbb-action-group align="start" orientation="horizontal">
+          <sbb-button variant="secondary" label="Button" />
+          <sbb-link
+            variant="block"
+            text-size="s"
+            icon-name="chevron-small-left-small"
+            icon-placement="start"
+            href="https://github.com/lyne-design-system/lyne-components">
+            Link
+          </sbb-link>
+        </sbb-action-group>
+      `,
     });
 
     expect(root).toEqualHtml(`
-      <sbb-action-group align="start" class="action-group--align-start" orientation="horizontal">
+      <sbb-action-group align="start" orientation="horizontal">
         <mock:shadow-root>
-          <slot></slot>
+          <div class="action-group action-group--align-start">
+            <slot></slot>
+          </div>
         </mock:shadow-root>
+        <sbb-button variant="secondary" label="Button" />
+        <sbb-link
+          variant="block"
+          text-size="s"
+          icon-name="chevron-small-left-small"
+          icon-placement="start"
+          href="https://github.com/lyne-design-system/lyne-components">
+          Link
+        </sbb-link>
       </sbb-action-group>
       `);
   });
