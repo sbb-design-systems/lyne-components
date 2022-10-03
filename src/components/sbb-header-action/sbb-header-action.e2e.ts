b201e6aa36a65873f18e99ca7fa3771cd025d7ba
Davide Mininni <davide.mininni@finconsgroup.com>
2022-10-03 15:49:51 +0200

    test: add click test on header-action

--- src/components/sbb-header-action/sbb-header-action.e2e.ts
+++ src/components/sbb-header-action/sbb-header-action.e2e.ts
@@ -1,13 +1,20 @@
 import { newE2EPage } from '@stencil/core/testing';
+import events from './sbb-header-action.events';
 
 describe('sbb-header-action', () => {
   let element, page;
 
-  it('renders', async () => {
+  it('renders as a button and trigger click', async () => {
     page = await newE2EPage();
     await page.setContent('<sbb-header-action>Action</sbb-header-action>');
 
     element = await page.find('sbb-header-action');
     expect(element).toHaveClass('hydrated');
+
+    const button = await page.find('sbb-header-action >>> button');
+    const clickedSpy = await page.spyOnEvent(events.click);
+
+    await button.click();
+    expect(clickedSpy).toHaveReceivedEventTimes(1);
   });
 });
