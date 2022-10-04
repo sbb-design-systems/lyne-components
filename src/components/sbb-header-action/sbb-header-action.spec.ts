d83425f4898b7d46f3c1a538600049795518fcc4
Davide Mininni <davide.mininni@finconsgroup.com>
2022-10-04 16:12:30 +0200

    test: fix broken test

--- src/components/sbb-header-action/sbb-header-action.spec.ts
+++ src/components/sbb-header-action/sbb-header-action.spec.ts
@@ -2,19 +2,19 @@ import { newSpecPage } from '@stencil/core/testing';
 import { SbbHeaderAction } from './sbb-header-action';
 
 describe('sbb-header-action', () => {
-  it('renders the component as a button', async () => {
+  it('renders the component as a button with icon', async () => {
     const { root } = await newSpecPage({
       components: [SbbHeaderAction],
-      html: `<sbb-header-action name="test" type="reset" value="value">Action</sbb-header-action>`,
+      html: `<sbb-header-action icon='pie-small' name="test" type="reset" value="value">Action</sbb-header-action>`,
     });
 
     expect(root).toEqualHtml(`
-      <sbb-header-action expand-from="medium" name="test" type="reset" value="value">
+      <sbb-header-action icon='pie-small' expand-from="medium" name="test" type="reset" value="value">
         <mock:shadow-root>
           <button class="header-action__button" dir="ltr" id="sbb-header-action-1" name="test" type="reset" value="value">
             <span class="header-action__icon">
               <slot name="icon">
-                <sbb-icon></sbb-icon>
+                <sbb-icon name="pie-small"></sbb-icon>
               </slot>
             </span>
             <span class="header-action__text">
@@ -27,7 +27,7 @@ describe('sbb-header-action', () => {
     `);
   });
 
-  it('renders the component as a link', async () => {
+  it('renders the component as a link without icon', async () => {
     const { root } = await newSpecPage({
       components: [SbbHeaderAction],
       html: '<sbb-header-action expand-from="small" href="https://github.com/lyne-design-system/lyne-components" target="_blank">Action</sbb-header-action>',
@@ -38,9 +38,7 @@ describe('sbb-header-action', () => {
         <mock:shadow-root>
           <a dir="ltr" id="sbb-header-action-2" rel="external noopener nofollow" target="_blank" class="header-action__link" href="https://github.com/lyne-design-system/lyne-components">
             <span class="header-action__icon">
-              <slot name="icon">
-                <sbb-icon></sbb-icon>
-              </slot>
+              <slot name="icon"/>
             </span>
             <span class="header-action__text">
               <slot></slot>
