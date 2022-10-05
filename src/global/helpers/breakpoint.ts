d1c22a30245111780fe68dc104c1299265dc496b
Marco D'Auria <marco.dauria@finconsgroup.com>
2022-10-05 11:18:19 +0200

    feat: improve sbb-menu component

--- src/global/helpers/breakpoint.ts
+++ src/global/helpers/breakpoint.ts
@@ -4,16 +4,19 @@ type Breakpoint = 'zero' | 'micro' | 'small' | 'medium' | 'wide' | 'large' | 'ul
  * Checks whether the document matches a particular media query.
  * It will rely on the global CSS variables to determine the value of the breakpoints.
  *
- * @param from The breakpoint corresponding to the `min-width` value of the media query.
- * @param to The breakpoint corresponding to the `max-width` value of the media query.
+ * @param from The breakpoint corresponding to the `min-width` value of the media query (optional).
+ * @param to The breakpoint corresponding to the `max-width` value of the media query (optional).
  * @returns A boolean indicating whether the window matches the breakpoint.
  */
-export function isBreakpoint(from: Breakpoint, to: Breakpoint): boolean {
+export function isBreakpoint(from?: Breakpoint, to?: Breakpoint): boolean {
   const computedStyle = getComputedStyle(document.documentElement);
 
   const breakpointMin = computedStyle.getPropertyValue(`--sbb-breakpoint-${from}-min`);
   const breakpointMax = computedStyle.getPropertyValue(`--sbb-breakpoint-${to}-max`);
 
-  return window.matchMedia(`(min-width: ${breakpointMin}) and (max-width: ${breakpointMax})`)
-    .matches;
+  const minWidth = breakpointMin && `(min-width: ${breakpointMin})`;
+  const maxWidth = breakpointMax && `(max-width: ${breakpointMax})`;
+  const and = breakpointMin && breakpointMax && ' and ';
+
+  return window.matchMedia(`${minWidth}${and}${maxWidth}`).matches;
 }
