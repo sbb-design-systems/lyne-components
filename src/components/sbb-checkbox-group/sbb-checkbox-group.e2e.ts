c2c889a6e0bcb0bd3ae850e9562d8702478489e9
Davide Mininni <davide.mininni@finconsgroup.com>
2022-11-14 11:28:13 +0100

    test: fix broken tests

--- src/components/sbb-checkbox-group/sbb-checkbox-group.e2e.ts
+++ src/components/sbb-checkbox-group/sbb-checkbox-group.e2e.ts
@@ -17,33 +17,41 @@ describe('sbb-checkbox-group', () => {
     expect(element).toHaveClass('hydrated');
   });
 
-  it('renders disabled children', async () => {
-    page = await newE2EPage();
-    await page.setContent(`
+  describe('disabled status', () => {
+    let element: E2EElement, page: E2EPage;
+
+    it('renders', async () => {
+      page = await newE2EPage();
+      await page.setContent(`
       <sbb-checkbox-group disabled>
         <sbb-checkbox name="checkbox-1" value="checkbox-1">Label 1</sbb-checkbox>
       </sbb-checkbox-group>
     `);
 
-    element = await page.find('sbb-checkbox-group');
-    expect(element).toHaveClass('hydrated');
-    await page.waitForChanges();
-    const check = await page.find('sbb-checkbox');
-    expect(check.getAttribute('disabled')).not.toBeNull();
+      element = await page.find('sbb-checkbox-group');
+      expect(element).toHaveClass('hydrated');
+      await page.waitForChanges();
+      const check = await page.find('sbb-checkbox');
+      expect(check.getAttribute('disabled')).not.toBeNull();
+    });
   });
 
-  it('renders required children', async () => {
-    page = await newE2EPage();
-    await page.setContent(`
+  describe('required status', () => {
+    let element: E2EElement, page: E2EPage;
+
+    it('renders', async () => {
+      page = await newE2EPage();
+      await page.setContent(`
       <sbb-checkbox-group required>
         <sbb-checkbox name="checkbox-1" value="checkbox-1">Label 1</sbb-checkbox>
       </sbb-checkbox-group>
     `);
 
-    element = await page.find('sbb-checkbox-group');
-    expect(element).toHaveClass('hydrated');
-    await page.waitForChanges();
-    const check = await page.find('sbb-checkbox');
-    expect(check.getAttribute('required')).not.toBeNull();
+      element = await page.find('sbb-checkbox-group');
+      expect(element).toHaveClass('hydrated');
+      await page.waitForChanges();
+      const check = await page.find('sbb-checkbox');
+      expect(check.getAttribute('required')).not.toBeNull();
+    });
   });
 });
