423e5715ffb3ab010ba1e5dbd45d1aa399e7d2c4
Davide Mininni <davide.mininni@finconsgroup.com>
2022-11-17 12:38:14 +0100

    test: fix broken e2e tests for required & disabled

--- src/components/sbb-checkbox-group/sbb-checkbox-group.e2e.ts
+++ src/components/sbb-checkbox-group/sbb-checkbox-group.e2e.ts
@@ -3,74 +3,43 @@ import { E2EElement, E2EPage, newE2EPage } from '@stencil/core/testing';
 describe('sbb-checkbox-group', () => {
   let element: E2EElement, page: E2EPage;
 
-  it('renders', async () => {
+  beforeEach(async () => {
     page = await newE2EPage();
     await page.setContent(`
       <sbb-checkbox-group>
-        <sbb-checkbox value="checkbox-1">Label 1</sbb-checkbox>
-        <sbb-checkbox value="checkbox-2">Label 2</sbb-checkbox>
-        <sbb-checkbox value="checkbox-3">Label 3</sbb-checkbox>
+        <sbb-checkbox id="checkbox-1" value="checkbox-1">Label 1</sbb-checkbox>
+        <sbb-checkbox id="checkbox-2" value="checkbox-2">Label 2</sbb-checkbox>
+        <sbb-checkbox id="checkbox-3" value="checkbox-3">Label 3</sbb-checkbox>
       </sbb-checkbox-group>
     `);
-
     element = await page.find('sbb-checkbox-group');
+  });
+
+  it('renders', async () => {
     expect(element).toHaveClass('hydrated');
   });
 
-  // FIXME test is not working: the children checkboxes are not disabled (same for required)
-  // eslint-disable-next-line jest/no-commented-out-tests
-  // it('disabled status', async () => {
-  //   page = await newE2EPage();
-  //   await page.setContent(`
-  //     <sbb-checkbox-group>
-  //       <sbb-checkbox id="checkbox-1" value="checkbox-1">Label 1</sbb-checkbox>
-  //       <sbb-checkbox id="checkbox-2" value="checkbox-2">Label 2</sbb-checkbox>
-  //     </sbb-checkbox-group>
-  //   `);
-  //
-  //   element = await page.find('sbb-checkbox-group');
-  //   expect(element).toEqualHtml(`
-  //     <sbb-checkbox-group orientation="horizontal" class="hydrated">
-  //       <mock:shadow-root>
-  //         <div class="sbb-checkbox-group">
-  //           <slot></slot>
-  //         </div>
-  //       </mock:shadow-root>
-  //       <sbb-checkbox id="checkbox-1" value="checkbox-1" class="hydrated" icon-placement="end" size="s">
-  //           Label 1
-  //       </sbb-checkbox>
-  //       <sbb-checkbox id="checkbox-2" value="checkbox-2" class="hydrated" icon-placement="end" size="s">
-  //           Label 2
-  //       </sbb-checkbox>
-  //     </sbb-checkbox-group>
-  //   `);
-  //
-  //   element.setAttribute('disabled', 'true');
-  //   await page.waitForChanges();
-  //   expect(element).toEqualHtml(`
-  //     <sbb-checkbox-group orientation="horizontal" class="hydrated" disabled="">
-  //       <mock:shadow-root>
-  //         <div class="sbb-checkbox-group">
-  //           <slot></slot>
-  //         </div>
-  //       </mock:shadow-root>
-  //       <sbb-checkbox id="checkbox-1" value="checkbox-1" class="hydrated" icon-placement="end" size="s" disabled="">
-  //           Label 1
-  //       </sbb-checkbox>
-  //       <sbb-checkbox id="checkbox-2" value="checkbox-2" class="hydrated" icon-placement="end" size="s" disabled="">
-  //           Label 2
-  //       </sbb-checkbox>
-  //     </sbb-checkbox-group>
-  //   `);
-  //   element = await page.find('sbb-checkbox-group');
-  //   expect(element).toHaveClass('hydrated');
-  //   expect(element).toEqualAttribute('disabled', '');
-  //   await page.waitForChanges();
-  //   const checkboxOne = await page.find('sbb-checkbox-group > sbb-checkbox#checkbox-1');
-  //   const checkboxTwo = await page.find('sbb-checkbox-group > sbb-checkbox#checkbox-2');
-  //   expect(checkboxOne).not.toBeNull();
-  //   expect(checkboxTwo).not.toBeNull();
-  //   expect(checkboxOne.getAttribute('disabled')).toEqual('true');
-  //   expect(checkboxTwo.getAttribute('disabled')).toEqual('true');
-  // });
+  it('disabled status', async () => {
+    element.setAttribute('disabled', 'true');
+    await page.waitForChanges();
+    expect(element).toEqualAttribute('disabled', 'true');
+    const checkboxOne = await page.find('sbb-checkbox-group > sbb-checkbox#checkbox-1');
+    expect(checkboxOne.getAttribute('data-disabled')).not.toBeNull();
+    const checkboxTwo = await page.find('sbb-checkbox-group > sbb-checkbox#checkbox-2');
+    expect(checkboxTwo.getAttribute('data-disabled')).not.toBeNull();
+    const checkboxThree = await page.find('sbb-checkbox-group > sbb-checkbox#checkbox-3');
+    expect(checkboxThree.getAttribute('data-disabled')).not.toBeNull();
+  });
+
+  it('required status', async () => {
+    element.setAttribute('required', 'true');
+    await page.waitForChanges();
+    expect(element).toEqualAttribute('required', 'true');
+    const checkboxOne = await page.find('sbb-checkbox-group > sbb-checkbox#checkbox-1');
+    expect(checkboxOne.getAttribute('data-required')).not.toBeNull();
+    const checkboxTwo = await page.find('sbb-checkbox-group > sbb-checkbox#checkbox-2');
+    expect(checkboxTwo.getAttribute('data-required')).not.toBeNull();
+    const checkboxThree = await page.find('sbb-checkbox-group > sbb-checkbox#checkbox-3');
+    expect(checkboxThree.getAttribute('data-required')).not.toBeNull();
+  });
 });
