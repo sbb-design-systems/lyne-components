74ab1a5974d98d0f36bda71b75c86fb021fb298e
simone.sabato <simone.sabato@finconsgroup.com>
2022-11-29 15:31:29 +0100

    feat: added new unit tests

--- src/components/sbb-toggle-option/sbb-toggle-option.spec.ts
+++ src/components/sbb-toggle-option/sbb-toggle-option.spec.ts
@@ -5,13 +5,13 @@ describe('sbb-toggle-option', () => {
   it('renders', async () => {
     const { root } = await newSpecPage({
       components: [SbbToggleOption],
-      html: '<sbb-toggle-option />',
+      html: '<sbb-toggle-option checked />',
     });
 
     expect(root).toEqualHtml(`
-    <sbb-toggle-option aria-checked="false">
+        <sbb-toggle-option aria-checked="true" aria-labelledby="sbb-toggle-option-1" checked="">
           <mock:shadow-root>
-            <input aria-hidden="true" id="sbb-toggle-option-1" tabindex="-1" type="radio">
+            <input aria-hidden="true" checked id="sbb-toggle-option-1" tabindex="-1" type="radio">
             <span class="sbb-toggle-option">
             <label htmlfor="sbb-toggle-option-1" id="sbb-toggle-option-1"></label>
             </span>
