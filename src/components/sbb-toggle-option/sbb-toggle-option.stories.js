2f041c4255e110c961ced051e267a2558ac54e0b
simone.sabato <simone.sabato@finconsgroup.com>
2022-12-01 10:06:59 +0100

    feat: added new story on storybook

--- src/components/sbb-toggle-option/sbb-toggle-option.stories.js
+++ src/components/sbb-toggle-option/sbb-toggle-option.stories.js
@@ -37,29 +37,38 @@ const defaultArgTypes = {
   checked,
   disabled,
   size,
-  value,
 };
 
 const defaultArgs = {
   checked: false,
   disabled: false,
   size: 'm',
-  value: 'Option 1',
 };
 
 const DefaultTemplate = (args) => <sbb-toggle-option {...args}>Option 1</sbb-toggle-option>;
 
 export const sbbToggleOption = DefaultTemplate.bind({});
-sbbToggleOption.argTypes = defaultArgTypes;
-sbbToggleOption.args = defaultArgs;
+sbbToggleOption.argTypes = { ...defaultArgTypes, value };
+sbbToggleOption.args = { ...defaultArgs, value: 'Option 1' };
 sbbToggleOption.documentation = {
   title: 'sbb-toggle-option',
 };
 
 export const sbbToggleOptionWithIcon = DefaultTemplate.bind({});
-sbbToggleOptionWithIcon.args = { ...defaultArgs, 'icon-name': 'arrow-right-small' };
 sbbToggleOptionWithIcon.argTypes = { ...defaultArgTypes, 'icon-name': iconName };
+sbbToggleOptionWithIcon.args = { ...defaultArgs, 'icon-name': 'arrow-right-small' };
 sbbToggleOptionWithIcon.documentation = {
+  title: 'sbb-toggle-option with custom icon',
+};
+
+export const sbbToggleOptionWithLabelAndIcon = DefaultTemplate.bind({});
+sbbToggleOptionWithLabelAndIcon.argTypes = { ...defaultArgTypes, 'icon-name': iconName, value };
+sbbToggleOptionWithLabelAndIcon.args = {
+  ...defaultArgs,
+  'icon-name': 'arrow-right-small',
+  value: 'Option 1',
+};
+sbbToggleOptionWithLabelAndIcon.documentation = {
   title: 'sbb-toggle-option with custom icon',
 };
 
@@ -82,5 +91,5 @@ export default {
       extractComponentDescription: () => readme,
     },
   },
-  title: 'components/form elements/sbb-toggle-option',
+  title: 'components/form elements/toggle/sbb-toggle-option',
 };
