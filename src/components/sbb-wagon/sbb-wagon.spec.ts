5bbaf315609c1381ddcc4ceefaf10fa8c770d7fb
Jeremias Peier <jeremias.peier@sbb.ch>
2022-12-01 09:12:01 +0100

    fix: tests

--- src/components/sbb-wagon/sbb-wagon.spec.ts
+++ src/components/sbb-wagon/sbb-wagon.spec.ts
@@ -12,10 +12,10 @@ describe('sbb-wagon', () => {
         <sbb-wagon accessibility-label-class="First class" accessibility-label-occupation="Expected occupancy low" accessibility-label-wagon="Train coach number" label="38" occupancy="none" type="wagon" wagon-class="1">
           <mock:shadow-root>
             <div class="sbb-wagon">
-              <p class="sbb-wagon__label">
-                <span>Train coach number 38. First class. Expected occupancy low.</span>
-                <span aria-hidden="true">38</span>
-              </p>
+              <span class="sbb-wagon__label">
+                <span class="sbb-wagon__label-screenreader">Train coach number 38. First class. Expected occupancy low.</span>
+                <span class="sbb-wagon__label-text" aria-hidden="true">38</span>
+              </span>
               <div class="sbb-wagon__compartment">
                 <sbb-icon name="utilization-none"></sbb-icon>
                 <span class="sbb-wagon__class">
@@ -44,9 +44,10 @@ describe('sbb-wagon', () => {
         <sbb-wagon type="locomotive" accessibility-label-wagon="Locomotive" accessibility-additional-wagon-text="Top of the train">
           <mock:shadow-root>
             <div class="sbb-wagon">
-              <p class="sbb-wagon__label">
-                <span>Locomotive Top of the train</span>
-              </p>
+              <span class="sbb-wagon__label">
+                <span class="sbb-wagon__label-screenreader">Locomotive Top of the train</span>
+                <span class="sbb-wagon__label-text" aria-hidden="true"></span>
+              </span>
               <div class="sbb-wagon__compartment">
                 <svg aria-hidden="true" width="80" height="40" viewBox="0 0 80 40" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M17.7906 4.42719C19.9743 1.93152 23.129 0.5 26.4452 0.5H53.5548C56.871 0.5 60.0257 1.93152 62.2094 4.4272L76.2094 20.4272C82.7157 27.8629 77.4351 39.5 67.5548 39.5H12.4452C2.56489 39.5 -2.71566 27.8629 3.79058 20.4272L17.7906 4.42719Z" stroke="#767676"></path></svg>
               </div>
@@ -65,9 +66,10 @@ describe('sbb-wagon', () => {
         <sbb-wagon type="blocked" accessibility-label-wagon="Passage blocked">
           <mock:shadow-root>
             <div class="sbb-wagon">
-              <p class="sbb-wagon__label">
-                <span>Passage blocked</span>
-              </p>
+              <span class="sbb-wagon__label">
+                <span class="sbb-wagon__label-screenreader">Passage blocked</span>
+                <span class="sbb-wagon__label-text" aria-hidden="true"></span>
+              </span>
               <div class="sbb-wagon__compartment">
                 <span class="sbb-wagon__blocked-icon"></span>
               </div>
