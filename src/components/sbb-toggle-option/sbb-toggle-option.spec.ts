001b0abaa5c4edacf66e764ccbdaa7f68fd96e08
simone.sabato <simone.sabato@finconsgroup.com>
2022-12-01 17:22:21 +0100

    feat: added new unit tests

--- src/components/sbb-toggle-option/sbb-toggle-option.spec.ts
+++ src/components/sbb-toggle-option/sbb-toggle-option.spec.ts
@@ -5,15 +5,56 @@ describe('sbb-toggle-option', () => {
   it('renders', async () => {
     const { root } = await newSpecPage({
       components: [SbbToggleOption],
-      html: '<sbb-toggle-option checked />',
+      html: '<sbb-toggle-option checked value="Option 1" />',
     });
 
     expect(root).toEqualHtml(`
-        <sbb-toggle-option aria-checked="true" aria-labelledby="sbb-toggle-option-1" checked="">
+        <sbb-toggle-option aria-checked="true" aria-labelledby="sbb-toggle-option-1" checked="" value="Option 1">
           <mock:shadow-root>
-            <input aria-hidden="true" checked id="sbb-toggle-option-1" tabindex="-1" type="radio">
+            <input aria-hidden="true" checked id="sbb-toggle-option-1" tabindex="-1" type="radio" value="Option 1">
             <span class="sbb-toggle-option">
-            <label htmlfor="sbb-toggle-option-1" id="sbb-toggle-option-1"></label>
+              <label htmlfor="sbb-toggle-option-1" id="sbb-toggle-option-1">Option 1</label>
+            </span>
+          </mock:shadow-root>
+        </sbb-toggle-option>
+      `);
+  });
+
+  it('renders with sbb-icon', async () => {
+    const { root } = await newSpecPage({
+      components: [SbbToggleOption],
+      html: '<sbb-toggle-option checked icon-name="arrow-right-small" />',
+    });
+
+    expect(root).toEqualHtml(`
+        <sbb-toggle-option aria-checked="true" aria-labelledby="sbb-toggle-option-2" checked="" icon-name="arrow-right-small">
+          <mock:shadow-root>
+            <input aria-hidden="true" checked id="sbb-toggle-option-2" tabindex="-1" type="radio">
+            <span class="sbb-toggle-option">
+              <slot name="icon">
+                <sbb-icon name="arrow-right-small"></sbb-icon>
+              </slot>
+            </span>
+          </mock:shadow-root>
+        </sbb-toggle-option>
+      `);
+  });
+
+  it('renders with sbb-icon and label', async () => {
+    const { root } = await newSpecPage({
+      components: [SbbToggleOption],
+      html: '<sbb-toggle-option checked icon-name="arrow-right-small" value="Option 1" />',
+    });
+
+    expect(root).toEqualHtml(`
+        <sbb-toggle-option aria-checked="true" aria-labelledby="sbb-toggle-option-3" checked="" icon-name="arrow-right-small" value="Option 1">
+          <mock:shadow-root>
+            <input aria-hidden="true" checked id="sbb-toggle-option-3" tabindex="-1" type="radio" value="Option 1">
+            <span class="sbb-toggle-option">
+              <slot name="icon">
+                <sbb-icon name="arrow-right-small"></sbb-icon>
+              </slot>
+              <label htmlfor="sbb-toggle-option-3" id="sbb-toggle-option-3">Option 1</label>
             </span>
           </mock:shadow-root>
         </sbb-toggle-option>
