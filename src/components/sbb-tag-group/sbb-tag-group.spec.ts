5f289798f00384814ef0f2e3f03b4f17ab778db1
Davide Mininni <davide.mininni@finconsgroup.com>
2022-12-07 16:06:41 +0100

    test: add unit test

--- src/components/sbb-tag-group/sbb-tag-group.spec.ts
+++ src/components/sbb-tag-group/sbb-tag-group.spec.ts
@@ -5,15 +5,26 @@ describe('sbb-tag-group', () => {
   it('renders', async () => {
     const { root } = await newSpecPage({
       components: [SbbTagGroup],
-      html: '<sbb-tag-group />',
+      html: `
+        <sbb-tag-group>
+          <sbb-tag value="tag-1">First tag</sbb-tag>
+          <sbb-tag value="tag-2">Second tag</sbb-tag>
+          <sbb-tag value="tag-3">Third tag</sbb-tag>
+        </sbb-tag-group>
+      `,
     });
 
     expect(root).toEqualHtml(`
-        <sbb-tag-group>
-          <mock:shadow-root>
-            <button class="some-class"></button>
-          </mock:shadow-root>
-        </sbb-tag-group>
-      `);
+      <sbb-tag-group>
+        <mock:shadow-root>
+          <div class="sbb-tag-group">
+            <slot />
+          </div>
+        </mock:shadow-root>
+        <sbb-tag value="tag-1">First tag</sbb-tag>
+        <sbb-tag value="tag-2">Second tag</sbb-tag>
+        <sbb-tag value="tag-3">Third tag</sbb-tag>
+      </sbb-tag-group>
+    `);
   });
 });
--- src/components/sbb-tag/sbb-tag.spec.ts
+++ src/components/sbb-tag/sbb-tag.spec.ts
@@ -5,15 +5,86 @@ describe('sbb-tag', () => {
   it('renders', async () => {
     const { root } = await newSpecPage({
       components: [SbbTag],
-      html: '<sbb-tag />',
+      html: '<sbb-tag value="all">All</sbb-tag>',
     });
 
     expect(root).toEqualHtml(`
-        <sbb-tag>
-          <mock:shadow-root>
-            <button class="some-class"></button>
-          </mock:shadow-root>
+      <sbb-tag value="all">
+        <mock:shadow-root>
+          <label class="sbb-tag" htmlFor="sbb-tag-1">
+            <input type="checkbox" value="all" id="sbb-tag-1"/>
+            <span class="sbb-tag__wrapper">
+              <span class="sbb-tag__text">
+                <slot></slot>
+              </span>
+            </span>
+          </label>
+        </mock:shadow-root>
+        All
+      </sbb-tag>
+    `);
+  });
+
+  it('renders required checked with icon', async () => {
+    const { root } = await newSpecPage({
+      components: [SbbTag],
+      html: '<sbb-tag checked="true" required="true" icon-name="circle-information-small">Info</sbb-tag>',
+    });
+
+    expect(root).toEqualHtml(`
+      <sbb-tag checked="" required="true" icon-name="circle-information-small">
+        <mock:shadow-root>
+          <label class="sbb-tag" htmlFor="sbb-tag-2">
+            <input type="checkbox" id="sbb-tag-2" aria-checked="" checked="" required=""/>
+            <span class="sbb-tag__wrapper">
+              <span class="sbb-tag__icon">
+                <slot name="icon">
+                  <sbb-icon name="circle-information-small" /></slot>
+              </span>
+              <span class="sbb-tag__text">
+                <slot></slot>
+              </span>
+            </span>
+          </label>
+        </mock:shadow-root>
+        Info
+      </sbb-tag>
+    `);
+  });
+
+  it('renders disabled with icon and amount', async () => {
+    const { root } = await newSpecPage({
+      components: [SbbTag],
+      html: `
+        <sbb-tag disabled="true" icon-name="circle-information-small">
+          Info
+          <span slot="amount">123</span>
         </sbb-tag>
-      `);
+      `,
+    });
+
+    expect(root).toEqualHtml(`
+      <sbb-tag disabled="" icon-name="circle-information-small">
+        <mock:shadow-root>
+          <label class="sbb-tag" htmlFor="sbb-tag-3">
+            <input type="checkbox" id="sbb-tag-3" aria-disabled="" disabled=""/>
+            <span class="sbb-tag__wrapper">
+              <span class="sbb-tag__icon">
+                <slot name="icon">
+                  <sbb-icon name="circle-information-small" /></slot>
+              </span>
+              <span class="sbb-tag__text">
+                <slot></slot>
+              </span>
+              <span class="sbb-tag__amount">
+                <slot name="amount"></slot>
+              </span>
+            </span>
+          </label>
+        </mock:shadow-root>
+        Info
+        <span slot="amount">123</span>
+      </sbb-tag>
+    `);
   });
 });
