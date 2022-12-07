6fe66416d66c87f25e3bf75ea00498d003842135
Davide Mininni <davide.mininni@finconsgroup.com>
2022-12-07 17:43:42 +0100

    test: add e2e test

--- src/components/sbb-tag-group/sbb-tag-group.e2e.ts
+++ src/components/sbb-tag-group/sbb-tag-group.e2e.ts
@@ -3,11 +3,37 @@ import { E2EElement, E2EPage, newE2EPage } from '@stencil/core/testing';
 describe('sbb-tag-group', () => {
   let element: E2EElement, page: E2EPage;
 
-  it('renders', async () => {
+  beforeEach(async () => {
     page = await newE2EPage();
-    await page.setContent('<sbb-tag-group></sbb-tag-group>');
-
+    await page.setContent(`
+      <sbb-tag-group>
+        <sbb-tag id="sbb-tag-1">Tag 1</sbb-tag>
+        <sbb-tag id="sbb-tag-2">Tag 2</sbb-tag>
+        <sbb-tag id="sbb-tag-3">Tag 3</sbb-tag>
+      </sbb-tag-group>
+    `);
     element = await page.find('sbb-tag-group');
+  });
+
+  it('renders', async () => {
     expect(element).toHaveClass('hydrated');
+  });
+
+  it('navigation with arrow keys', async () => {
+    const tagOne = await page.find('sbb-tag-group > sbb-tag#sbb-tag-1');
+    const tagTwo = await page.find('sbb-tag-group > sbb-tag#sbb-tag-2');
+    const tagThree = await page.find('sbb-tag-group > sbb-tag#sbb-tag-3');
+    expect(tagTwo).not.toHaveAttribute('checked');
+    expect(tagThree).not.toHaveAttribute('checked');
+    await tagOne.focus();
+    await page.keyboard.down('ArrowRight');
+    await page.keyboard.down('ArrowRight');
+    await element.press('Space');
+    await page.waitForChanges();
+    expect(tagThree).toEqualAttribute('checked', '');
+    await page.keyboard.down('ArrowUp');
+    await element.press('Space');
+    await page.waitForChanges();
+    expect(tagTwo).toEqualAttribute('checked', '');
   });
 });
--- src/components/sbb-tag/sbb-tag.e2e.ts
+++ src/components/sbb-tag/sbb-tag.e2e.ts
@@ -1,13 +1,34 @@
-import { E2EElement, E2EPage, newE2EPage } from '@stencil/core/testing';
+import { E2EElement, E2EPage, EventSpy, newE2EPage } from '@stencil/core/testing';
 
 describe('sbb-tag', () => {
   let element: E2EElement, page: E2EPage;
 
-  it('renders', async () => {
+  beforeEach(async () => {
     page = await newE2EPage();
-    await page.setContent('<sbb-tag></sbb-tag>');
-
+    await page.setContent('<sbb-tag>Tag</sbb-tag>');
     element = await page.find('sbb-tag');
+  });
+
+  it('renders', async () => {
     expect(element).toHaveClass('hydrated');
+  });
+
+  it('checked on click', async () => {
+    expect(element).toEqualAttribute('checked', null);
+    const changeSpy: EventSpy = await page.spyOnEvent('change');
+    await element.click();
+    await page.waitForChanges();
+    expect(changeSpy).toHaveReceivedEvent();
+    expect(element).toEqualAttribute('checked', '');
+  });
+
+  it('checked on "Space" keypress', async () => {
+    expect(element).toEqualAttribute('checked', null);
+    const changeSpy: EventSpy = await page.spyOnEvent('change');
+    await element.focus();
+    await element.press('Space');
+    await page.waitForChanges();
+    expect(changeSpy).toHaveReceivedEvent();
+    expect(element).toEqualAttribute('checked', '');
   });
 });
