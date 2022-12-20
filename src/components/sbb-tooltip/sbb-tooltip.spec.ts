c2a26bda728e1bf7a17e73eb23eb1cf381b30f44
Jeri Peier <jeremias.peier@sbb.ch>
2022-12-20 11:57:53 +0100

    fix(sbb-train-formation): fix colors, icon size and translations (#1502)

--- src/components/sbb-tooltip/sbb-tooltip.spec.ts
+++ src/components/sbb-tooltip/sbb-tooltip.spec.ts
@@ -1,5 +1,6 @@
 import { SbbTooltip } from './sbb-tooltip';
 import { newSpecPage } from '@stencil/core/testing';
+import { i18nCloseTooltip } from '../../global/i18n';
 
 describe('sbb-tooltip', () => {
   it('renders', async () => {
@@ -19,7 +20,7 @@ describe('sbb-tooltip', () => {
                 </slot>
               </span>
               <span class="sbb-tooltip__close">
-                <sbb-button accessibility-label="Close tooltip" iconname="cross-small" sbb-tooltip-close="" size="m" type="button" variant="secondary"></sbb-button>
+                <sbb-button accessibility-label="${i18nCloseTooltip.en}" iconname="cross-small" sbb-tooltip-close="" size="m" type="button" variant="secondary"></sbb-button>
               </span>
             </div>
           </dialog>
