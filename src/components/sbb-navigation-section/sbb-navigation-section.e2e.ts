fc68cf78a76fb1ff3fade7ccf39f8aed330914d7
Mario Castigliano <mario.castigliano@finconsgroup.com>
2022-12-22 10:31:44 +0100

    feat: add section tests

--- src/components/sbb-navigation-section/sbb-navigation-section.e2e.ts
+++ src/components/sbb-navigation-section/sbb-navigation-section.e2e.ts
@@ -3,11 +3,48 @@ import { E2EElement, E2EPage, newE2EPage } from '@stencil/core/testing';
 describe('sbb-navigation-section', () => {
   let element: E2EElement, page: E2EPage;
 
-  it('renders', async () => {
+  beforeEach(async () => {
     page = await newE2EPage();
-    await page.setContent('<sbb-navigation-section></sbb-navigation-section>');
+    await page.setContent(`
 
+      <sbb-navigation-section disable-animation>
+        <sbb-navigation-list>
+          <sbb-navigation-action >Tickets & Offers</sbb-navigation-action>
+          <sbb-navigation-action>Vacations & Recreation</sbb-navigation-action>
+          <sbb-navigation-action>Travel information</sbb-navigation-action>
+          <sbb-navigation-action>Help & Contact</sbb-navigation-action>
+        </sbb-navigation-list>
+      </sbb-navigation-section>
+    `);
     element = await page.find('sbb-navigation-section');
+  });
+
+  it('renders', async () => {
     expect(element).toHaveClass('hydrated');
+  });
+
+  it('opens the section', async () => {
+    const dialog = await page.find('sbb-navigation-section >>> dialog');
+
+    await element.callMethod('open');
+    await page.waitForChanges();
+
+    expect(element).toHaveClass('sbb-navigation-section--opened');
+    expect(dialog).toHaveAttribute('open');
+  });
+
+  it('closes the section', async () => {
+    const dialog = await page.find('sbb-navigation-section >>> dialog');
+
+    await element.callMethod('open');
+    await page.waitForChanges();
+
+    expect(dialog).toHaveAttribute('open');
+
+    await element.callMethod('close');
+    await page.waitForChanges();
+
+    expect(element).toHaveClass('sbb-navigation-section--closed');
+    expect(dialog).not.toHaveAttribute('open');
   });
 });
