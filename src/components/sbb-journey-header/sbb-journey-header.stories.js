f8151df39ee62665d1901c5fad9909a5b1f3f87f
Jeremias Peier <jeremias.peier@sbb.ch>
2023-01-23 11:51:57 +0100

    docs: simplify stories

--- src/components/sbb-journey-header/sbb-journey-header.stories.js
+++ src/components/sbb-journey-header/sbb-journey-header.stories.js
@@ -60,99 +60,46 @@ const defaultArgs = {
   origin: 'La Chaux de Fonds',
   destination: 'Loèche-les-Bains',
   'round-trip': false,
-  level: '3',
-  size: 'm',
+  level: level.options[2],
+  size: size.options[0],
   negative: false,
-};
-
-const defaultArgsH1 = {
-  ...defaultArgs,
-  level: '1',
-};
-
-const defaultArgsH6 = {
-  ...defaultArgs,
-  level: '6',
 };
 
 const Template = (args) => <sbb-journey-header {...args} />;
 
-export const DefaultH3 = Template.bind({});
-DefaultH3.argTypes = defaultArgTypes;
-DefaultH3.args = { ...defaultArgs };
+export const SizeM = Template.bind({});
+SizeM.argTypes = defaultArgTypes;
+SizeM.args = { ...defaultArgs };
 
-export const DefaultH3RoundTrip = Template.bind({});
-DefaultH3RoundTrip.argTypes = defaultArgTypes;
-DefaultH3RoundTrip.args = { ...defaultArgs, 'round-trip': true };
+export const SizeMRoundTrip = Template.bind({});
+SizeMRoundTrip.argTypes = defaultArgTypes;
+SizeMRoundTrip.args = { ...defaultArgs, 'round-trip': true };
 
-export const DefaultH3SizeL = Template.bind({});
-DefaultH3SizeL.argTypes = defaultArgTypes;
-DefaultH3SizeL.args = { ...defaultArgs, size: 'l' };
+export const SizeMNegative = Template.bind({});
+SizeMNegative.argTypes = defaultArgTypes;
+SizeMNegative.args = { ...defaultArgs, negative: true };
 
-export const DefaultH3Negative = Template.bind({});
-DefaultH3Negative.argTypes = defaultArgTypes;
-DefaultH3Negative.args = { ...defaultArgs, negative: true };
-
-export const DefaultH3SizeLRoundTripShortText = Template.bind({});
-DefaultH3SizeLRoundTripShortText.argTypes = defaultArgTypes;
-DefaultH3SizeLRoundTripShortText.args = {
+export const SizeMRoundTripShortText = Template.bind({});
+SizeMRoundTripShortText.argTypes = defaultArgTypes;
+SizeMRoundTripShortText.args = {
   ...defaultArgs,
   origin: 'Bern',
   destination: 'Thun',
   'round-trip': true,
-  size: 'l',
 };
 
-export const H1 = Template.bind({});
-H1.argTypes = defaultArgTypes;
-H1.args = { ...defaultArgsH1 };
+export const SizeL = Template.bind({});
+SizeL.argTypes = defaultArgTypes;
+SizeL.args = { ...defaultArgs, size: size.options[1] };
 
-export const H1RoundTrip = Template.bind({});
-H1RoundTrip.argTypes = defaultArgTypes;
-H1RoundTrip.args = { ...defaultArgsH1, 'round-trip': true };
-
-export const H1SizeL = Template.bind({});
-H1SizeL.argTypes = defaultArgTypes;
-H1SizeL.args = { ...defaultArgsH1, size: 'l' };
-
-export const H1Negative = Template.bind({});
-H1Negative.argTypes = defaultArgTypes;
-H1Negative.args = { ...defaultArgsH1, negative: true };
-
-export const H1SizeLRoundTripShortText = Template.bind({});
-H1SizeLRoundTripShortText.argTypes = defaultArgTypes;
-H1SizeLRoundTripShortText.args = {
-  ...defaultArgsH1,
+export const SizeLRoundTripShortText = Template.bind({});
+SizeLRoundTripShortText.argTypes = defaultArgTypes;
+SizeLRoundTripShortText.args = {
+  ...defaultArgs,
   origin: 'Bern',
   destination: 'Thun',
   'round-trip': true,
-  size: 'l',
-};
-
-export const H6 = Template.bind({});
-H6.argTypes = defaultArgTypes;
-H6.args = { ...defaultArgsH6 };
-
-export const H6RoundTrip = Template.bind({});
-H6RoundTrip.argTypes = defaultArgTypes;
-H6RoundTrip.args = { ...defaultArgsH6, 'round-trip': true };
-
-export const H6SizeL = Template.bind({});
-H6SizeL.argTypes = defaultArgTypes;
-H6SizeL.args = { ...defaultArgsH6, size: 'l' };
-
-export const H6Negative = Template.bind({});
-H6Negative.argTypes = defaultArgTypes;
-H6Negative.args = { ...defaultArgsH6, negative: true };
-
-export const H6SizeLRoundTripShortText = Template.bind({});
-H6SizeLRoundTripShortText.argTypes = defaultArgTypes;
-H6SizeLRoundTripShortText.args = {
-  ...defaultArgsH6,
-  origin: 'Bern',
-  destination: 'Thun',
-  'round-trip': true,
-  size: 'l',
+  size: size.options[1],
 };
 
 export default {
