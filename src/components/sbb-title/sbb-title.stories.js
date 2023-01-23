9add9a09203bba101e57348ef7202600f6aa66ee
Jeri Peier <jeremias.peier@sbb.ch>
2023-01-23 13:51:18 +0100

    fix(sbb-title): fix scroll spacing if an id is set (#1552)

--- src/components/sbb-title/sbb-title.stories.js
+++ src/components/sbb-title/sbb-title.stories.js
@@ -11,9 +11,9 @@ const wrapperStyle = (context) => {
 };
 
 // we don't need to pass the args.text to the <sbb-title> tag, but Storybook wants all in it.
-const Template = (args) => <sbb-title {...args}>{args.text}</sbb-title>;
+const Template = ({ text, ...args }) => <sbb-title {...args}>{text}</sbb-title>;
 
-const levels = {
+const level = {
   control: {
     type: 'inline-radio',
   },
@@ -24,97 +24,57 @@ const negative = {
   control: {
     type: 'boolean',
   },
-  options: [true, false],
-  table: {
-    category: 'Styling Variant',
-  },
 };
 
 const defaultArgTypes = {
-  level: levels,
+  level,
   negative,
-  'visual-level': levels,
+  'visual-level': level,
 };
 
 const defaultArgs = {
-  level: 1,
+  level: level.options[0],
   text: 'Data without insights are trivial, and insights without action are pointless',
   negative: false,
-  'visual-level': 1,
+  'visual-level': undefined,
   'visually-hidden': false,
 };
 
 export const h1 = Template.bind({});
 
 h1.argTypes = defaultArgTypes;
-h1.args = JSON.parse(JSON.stringify(defaultArgs));
-h1.args['visual-level'] = null;
-h1.documentation = {
-  title: 'Title Level 1',
-};
+h1.args = { ...defaultArgs };
 
 export const h1Negative = Template.bind({});
-
 h1Negative.argTypes = defaultArgTypes;
 h1Negative.args = {
   ...defaultArgs,
   negative: true,
 };
-h1.args['visual-level'] = null;
-h1Negative.documentation = {
-  title: 'Title Level 1 Negative',
-};
 
 export const h2 = Template.bind({});
-
 h2.argTypes = defaultArgTypes;
-h2.args = JSON.parse(JSON.stringify(defaultArgs));
-h2.args.level = 2;
-h2.args['visual-level'] = null;
-h2.documentation = {
-  title: 'Title Level 2',
-};
+h2.args = { ...defaultArgs, level: level.options[1] };
 
 export const h3 = Template.bind({});
-
 h3.argTypes = defaultArgTypes;
-h3.args = JSON.parse(JSON.stringify(defaultArgs));
-h3.args.level = 3;
-h3.args['visual-level'] = null;
-h3.documentation = {
-  title: 'Title Level 3',
-};
+h3.args = { ...defaultArgs, level: level.options[2] };
 
 export const h4 = Template.bind({});
-
 h4.argTypes = defaultArgTypes;
-h4.args = JSON.parse(JSON.stringify(defaultArgs));
-h4.args.level = 4;
-h4.args['visual-level'] = null;
-h4.documentation = {
-  title: 'Title Level 4',
-};
+h4.args = { ...defaultArgs, level: level.options[3] };
 
 export const h5 = Template.bind({});
-
 h5.argTypes = defaultArgTypes;
-h5.args = JSON.parse(JSON.stringify(defaultArgs));
-h5.args.level = 5;
-h5.args['visual-level'] = null;
-h5.documentation = {
-  title: 'Title Level 5',
-};
+h5.args = { ...defaultArgs, level: level.options[4] };
 
 export const h6 = Template.bind({});
-
 h6.argTypes = defaultArgTypes;
-h6.args = JSON.parse(JSON.stringify(defaultArgs));
-h6.args.level = 6;
-// optional
-h6.args['visual-level'] = null;
-h6.documentation = {
-  title: 'Title Level 6',
-};
+h6.args = { ...defaultArgs, level: level.options[5] };
+
+export const h6VisualLevel = Template.bind({});
+h6VisualLevel.argTypes = defaultArgTypes;
+h6VisualLevel.args = { ...defaultArgs, level: level.options[0], 'visual-level': level.options[5] };
 
 export default {
   decorators: [
