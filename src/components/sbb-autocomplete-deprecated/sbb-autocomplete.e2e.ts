37ea6b6c99ff031987b62ed5ea7afd3f7b792d80
TomMenga <tommaso.menga-94@hotmail.it>
2023-02-03 09:01:18 +0100

    feat(sbb-autocomplete): deprecated sbb-autocomplete

---
+++ src/components/sbb-autocomplete-deprecated/sbb-autocomplete.e2e.ts
@@ -0,0 +1,19 @@
+// import { newE2EPage } from '@stencil/core/testing';
+// import sampleData from './sbb-autocomplete.sample-data';
+
+// describe('sbb-autocomplete', () => {
+//   let element, page;
+
+//   it('renders', async () => {
+//     page = await newE2EPage();
+//     await page.setContent(`
+//       <sbb-autocomplete
+//         items='${sampleData}'
+//         inputName='test-input'
+//         inputLabel='input-label'
+//       ></sbb-autocomplete>`);
+
+//     element = await page.find('sbb-autocomplete');
+//     expect(element).toHaveClass('hydrated');
+//   });
+// });
---
+++ src/components/sbb-autocomplete-item-deprecated/sbb-autocomplete-item.e2e.ts
@@ -0,0 +1,13 @@
+// import { newE2EPage } from '@stencil/core/testing';
+
+// describe('sbb-autocomplete-item', () => {
+//   let element, page;
+
+//   it('renders', async () => {
+//     page = await newE2EPage();
+//     await page.setContent('<sbb-autocomplete-item></sbb-autocomplete-item>');
+
+//     element = await page.find('sbb-autocomplete-item');
+//     expect(element).toHaveClass('hydrated');
+//   });
+// });
