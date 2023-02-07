8121b9ad3d1bc31e415ce01244fb27aea7163c0f
TomMenga <tommaso.menga-94@hotmail.it>
2023-02-07 15:34:35 +0100

    feat(sbb-autocomplete): fix deprecated tests

--- src/components/sbb-autocomplete-deprecated/sbb-autocomplete.e2e.ts
+++ src/components/sbb-autocomplete-deprecated/sbb-autocomplete.e2e.ts
@@ -1,19 +1,19 @@
-// import { newE2EPage } from '@stencil/core/testing';
-// import sampleData from './sbb-autocomplete.sample-data';
+import { newE2EPage } from '@stencil/core/testing';
+import sampleData from './sbb-autocomplete.sample-data';
 
-// describe('sbb-autocomplete', () => {
-//   let element, page;
+describe('sbb-autocomplete', () => {
+  let element, page;
 
-//   it('renders', async () => {
-//     page = await newE2EPage();
-//     await page.setContent(`
-//       <sbb-autocomplete
-//         items='${sampleData}'
-//         inputName='test-input'
-//         inputLabel='input-label'
-//       ></sbb-autocomplete>`);
+  it('renders', async () => {
+    page = await newE2EPage();
+    await page.setContent(`
+      <sbb-autocomplete-deprecated
+        items='${sampleData}'
+        inputName='test-input'
+        inputLabel='input-label'
+      ></sbb-autocomplete-deprecated>`);
 
-//     element = await page.find('sbb-autocomplete');
-//     expect(element).toHaveClass('hydrated');
-//   });
-// });
+    element = await page.find('sbb-autocomplete-deprecated');
+    expect(element).toHaveClass('hydrated');
+  });
+});
