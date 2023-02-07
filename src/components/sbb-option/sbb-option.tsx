03ed40b1e2eacf2471e15164da27c504e28ee31d
TomMenga <tommaso.menga-94@hotmail.it>
2023-02-07 15:42:13 +0100

    feat(sbb-option): prettier run

--- src/components/sbb-option/sbb-option.tsx
+++ src/components/sbb-option/sbb-option.tsx
@@ -1,4 +1,4 @@
-import {Component, ComponentInterface, Element, h, Host, JSX, Prop} from '@stencil/core';
+import { Component, ComponentInterface, Element, h, Host, JSX, Prop } from '@stencil/core';
 
 /**
  * @slot unnamed - Use this to document a slot.
@@ -9,7 +9,6 @@ import {Component, ComponentInterface, Element, h, Host, JSX, Prop} from '@stenc
   tag: 'sbb-option',
 })
 export class SbbOption implements ComponentInterface {
-
   /**
    * The icon name we want to use, choose from the small icon variants
    * from the ui-icons category from here
@@ -29,22 +28,22 @@ export class SbbOption implements ComponentInterface {
   private _hasIconSlot: boolean;
 
   public componentWillLoad(): void {
-      this._hasIconSlot = !!this._element.querySelector('[slot="icon"]');
+    this._hasIconSlot = !!this._element.querySelector('[slot="icon"]');
   }
 
   public render(): JSX.Element {
     return (
       <Host>
-        <div class="sbb-option"
-            role="option"
-            aria-selected={this.selected}>
-          <span 
+        <div class="sbb-option" role="option" aria-selected={this.selected}>
+          <span
             class={{
-              'sbb-option__icon': true, 
-              'sbb-option__icon--hidden': !this.preserveIconSpace && !this._hasIconSlot && !this.iconName
-            }}>
+              'sbb-option__icon': true,
+              'sbb-option__icon--hidden':
+                !this.preserveIconSpace && !this._hasIconSlot && !this.iconName,
+            }}
+          >
             <slot name="icon">
-              {this.iconName && <sbb-icon slot='icon' name={this.iconName} />}
+              {this.iconName && <sbb-icon slot="icon" name={this.iconName} />}
             </slot>
           </span>
           <span>
