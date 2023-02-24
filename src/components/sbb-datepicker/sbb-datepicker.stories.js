37b7535316cc556e065a2f7a23e8b63a716647b4
Davide Mininni <davide.mininni@finconsgroup.com>
2023-02-24 15:50:16 +0100

    chore: add story for picker without form-field

--- src/components/sbb-datepicker/sbb-datepicker.stories.js
+++ src/components/sbb-datepicker/sbb-datepicker.stories.js
@@ -2,6 +2,51 @@ import { h } from 'jsx-dom';
 import readme from './readme.md';
 import events from './sbb-datepicker.events';
 
+const value = {
+  control: {
+    type: 'text',
+  },
+  table: {
+    category: 'Input datepicker attribute',
+  },
+};
+
+const disabled = {
+  control: {
+    type: 'boolean',
+  },
+  table: {
+    category: 'Input datepicker attribute',
+  },
+};
+
+const readonly = {
+  control: {
+    type: 'boolean',
+  },
+  table: {
+    category: 'Input datepicker attribute',
+  },
+};
+
+const required = {
+  control: {
+    type: 'boolean',
+  },
+  table: {
+    category: 'Input datepicker attribute',
+  },
+};
+
+const form = {
+  control: {
+    type: 'text',
+  },
+  table: {
+    category: 'Input datepicker attribute',
+  },
+};
+
 const wide = {
   control: {
     type: 'boolean',
@@ -23,51 +68,6 @@ const min = {
 const max = {
   control: {
     type: 'date',
-  },
-  table: {
-    category: 'Datepicker attribute',
-  },
-};
-
-const value = {
-  control: {
-    type: 'text',
-  },
-  table: {
-    category: 'Datepicker attribute',
-  },
-};
-
-const form = {
-  control: {
-    type: 'text',
-  },
-  table: {
-    category: 'Datepicker attribute',
-  },
-};
-
-const readonly = {
-  control: {
-    type: 'boolean',
-  },
-  table: {
-    category: 'Datepicker attribute',
-  },
-};
-
-const disabled = {
-  control: {
-    type: 'boolean',
-  },
-  table: {
-    category: 'Datepicker attribute',
-  },
-};
-
-const required = {
-  control: {
-    type: 'boolean',
   },
   table: {
     category: 'Datepicker attribute',
@@ -132,24 +132,24 @@ const basicArgTypes = {
   'accessibility-label': accessibilityLabel,
 };
 
+const basicArgs = {
+  value: `15.02.2023`,
+  form: undefined,
+  disabled: false,
+  readonly: false,
+  required: false,
+  min: undefined,
+  max: undefined,
+  wide: false,
+  'accessibility-label': undefined,
+};
+
 const formFieldBasicArgsTypes = {
   ...basicArgTypes,
   label,
   size,
   optional,
   borderless,
-};
-
-const selected = new Date();
-
-const basicArgs = {
-  value: `15.${selected.getMonth() + 1}.${selected.getFullYear()}`,
-  form: undefined,
-  disabled: false,
-  readonly: false,
-  required: false,
-  wide: false,
-  'accessibility-label': undefined,
 };
 
 const formFieldBasicArgs = {
@@ -160,7 +160,7 @@ const formFieldBasicArgs = {
   borderless: false,
 };
 
-const Template = ({ label, optional, borderless, size, min, max, wide, ...args }) => {
+const getDatepickerAttr = (min, max, wide) => {
   let attr = { wide };
   if (min) {
     attr.min = new Date(min);
@@ -168,7 +168,30 @@ const Template = ({ label, optional, borderless, size, min, max, wide, ...args }
   if (max) {
     attr.max = new Date(max);
   }
+  return attr;
+};
 
+const Template = ({ min, max, wide, ...args }) => {
+  return (
+    <div style="display: flex; gap: 0.25rem;">
+      <sbb-datepicker-previous-day date-picker="datepicker" />
+      <sbb-datepicker-toggle date-picker="datepicker" />
+      <input {...args} id="datepicker-input" />
+      <sbb-datepicker
+        id="datepicker"
+        input="datepicker-input"
+        ref={(calendarRef) => {
+          calendarRef.dateFilter = (d) => d?.getDay() !== 6 && d?.getDay() !== 0;
+        }}
+        {...getDatepickerAttr(wide, min, max)}
+        onChange={(event) => changeEventHandler(event)}
+      ></sbb-datepicker>
+      <sbb-datepicker-next-day date-picker="datepicker" />
+    </div>
+  );
+};
+
+const TemplateFormField = ({ label, optional, borderless, size, min, max, wide, ...args }) => {
   return [
     <sbb-form-field
       size={size}
@@ -185,7 +208,7 @@ const Template = ({ label, optional, borderless, size, min, max, wide, ...args }
         ref={(calendarRef) => {
           calendarRef.dateFilter = (d) => d?.getDay() !== 6 && d?.getDay() !== 0;
         }}
-        {...attr}
+        {...getDatepickerAttr(wide, min, max)}
         onChange={(event) => changeEventHandler(event)}
       ></sbb-datepicker>
     </sbb-form-field>,
@@ -200,14 +223,12 @@ const changeEventHandler = async (event) => {
 };
 
 export const Default = Template.bind({});
+Default.argTypes = { ...basicArgTypes };
+Default.args = { ...basicArgs };
 
-Default.argTypes = {
-  ...formFieldBasicArgsTypes,
-};
-
-Default.args = {
-  ...formFieldBasicArgs,
-};
+export const InFormField = TemplateFormField.bind({});
+InFormField.argTypes = { ...formFieldBasicArgsTypes };
+InFormField.args = { ...formFieldBasicArgs };
 
 export default {
   decorators: [
