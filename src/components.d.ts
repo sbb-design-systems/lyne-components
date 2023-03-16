b82bff0e6dfa08dae893af73ee3d8e732eb9afb5
TomMenga <tommaso.menga-94@hotmail.it>
2023-03-16 11:34:56 +0100

    feat(sbb-autocomplete): removed the 'disableHighlight' property

--- src/components.d.ts
+++ src/components.d.ts
@@ -1089,10 +1089,6 @@ export namespace Components {
          */
         "active"?: boolean;
         /**
-          * Disable the highlight of the label.
-         */
-        "disableLabelHighlight": boolean;
-        /**
           * Whether the option is disabled. TBI: missing disabled style, will be implemented with the select component.
          */
         "disabled"?: boolean;
@@ -3430,10 +3426,6 @@ declare namespace LocalJSX {
           * Whether the option is currently active.
          */
         "active"?: boolean;
-        /**
-          * Disable the highlight of the label.
-         */
-        "disableLabelHighlight"?: boolean;
         /**
           * Whether the option is disabled. TBI: missing disabled style, will be implemented with the select component.
          */
