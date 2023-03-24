4576905413f064aab9bc5b5c56d7690cbc21a7da
Davide Mininni <davide.mininni@finconsgroup.com>
2023-03-24 11:07:26 +0100

    feat: changed label detection, add tests

--- src/components/sbb-option-group/sbb-option-group.spec.ts
+++ src/components/sbb-option-group/sbb-option-group.spec.ts
@@ -1,45 +1,62 @@
-// import { SbbOptionGroup } from './sbb-option-group';
-// import { newSpecPage } from '@stencil/core/testing';
+import { SbbOptionGroup } from './sbb-option-group';
+import { newSpecPage } from '@stencil/core/testing';
 
 describe('sbb-option-group', () => {
-  it('renders', async () => {
-    // const { root } = await newSpecPage({
-    //   components: [SbbAutocomplete],
-    //   html: '<sbb-autocomplete />',
-    // });
+  describe('autocomplete', function () {
+    it('renders', async () => {
+      const { root } = await newSpecPage({
+        components: [SbbOptionGroup],
+        html: `
+          <sbb-autocomplete>
+            <sbb-option-group label="Label">
+              <sbb-option value="1">1</sbb-option>
+              <sbb-option value="2">2</sbb-option>
+            </sbb-option-group>
+          </sbb-autocomplete>
+          `,
+      });
 
-    expect(true).toEqual(true);
+      expect(root).toEqualHtml(`
+        <sbb-option-group aria-disabled="false" aria-label="Label" data-variant="autocomplete" label="Label" role="group">
+          <mock:shadow-root>
+            <span aria-hidden="true" class="sbb-option-group__label">Label</span>
+            <slot></slot>
+            <div class="sbb-option-group__divider">
+              <sbb-divider></sbb-divider>
+            </div>
+          </mock:shadow-root>
+          <sbb-option value="1">1</sbb-option>
+          <sbb-option value="2">2</sbb-option>
+        </sbb-option-group>
+      `);
+    });
+
+    it('renders disabled', async () => {
+      const { root } = await newSpecPage({
+        components: [SbbOptionGroup],
+        html: `
+          <sbb-autocomplete>
+            <sbb-option-group label="Label" disabled="true">
+              <sbb-option value="1">1</sbb-option>
+              <sbb-option value="2">2</sbb-option>
+            </sbb-option-group>
+          </sbb-autocomplete>
+        `,
+      });
+
+      expect(root).toEqualHtml(`
+        <sbb-option-group disabled="true" aria-disabled="true" aria-label="Label" data-variant="autocomplete" label="Label" role="group">
+          <mock:shadow-root>
+            <span aria-hidden="true" class="sbb-option-group__label">Label</span>
+            <slot></slot>
+            <div class="sbb-option-group__divider">
+              <sbb-divider></sbb-divider>
+            </div>
+          </mock:shadow-root>
+          <sbb-option value="1">1</sbb-option>
+          <sbb-option value="2">2</sbb-option>
+        </sbb-option-group>
+      `);
+    });
   });
 });
-
-// describe('sbb-option-group', () => {
-//   it('renders', async () => {
-//     const { root } = await newSpecPage({
-//       components: [SbbOptionGroup],
-//       html: `
-//         <sbb-option-group label="Label">
-//           <sbb-option value="1">1</sbb-option>
-//           <sbb-option value="2">2</sbb-option>
-//         </sbb-option-group>
-//       `,
-//     });
-
-//     expect(root).toEqualHtml(`
-//       <sbb-option-group label="Label">
-//         <mock:shadow-root>
-//           <div class="sbb-option-group">
-//             <div class="sbb-option-group__label">Label</div>
-//             <div class="sbb-option-group__options">
-//               <slot></slot>
-//             </div>
-//             <div class="sbb-option-group__divider">
-//               <sbb-divider></sbb-divider>
-//             </div>
-//           </div>
-//         </mock:shadow-root>
-//         <sbb-option value="1">1</sbb-option>
-//         <sbb-option value="2">2</sbb-option>
-//       </sbb-option-group>
-//     `);
-//   });
-// });
