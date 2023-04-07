bf19da59264dea258467bd5381dfee7d3b5f3afa
Federico Iseppon <100614752+federicoisepponfincons@users.noreply.github.com>
2023-04-07 16:59:02 +0200

    feat: component story

--- src/components/sbb-skiplink-list/sbb-skiplink-list.stories.js
+++ src/components/sbb-skiplink-list/sbb-skiplink-list.stories.js
@@ -1,22 +1,74 @@
 import { h } from 'jsx-dom';
 import readme from './readme.md';
 
-const LinkTemplate = (args) => <sbb-link href="https://www.sbb.ch">{args.linkTitle}</sbb-link>;
-
 const Template = (args) => (
-  <sbb-skiplink-list {...args}>
-    {links.map((linkTitle) => (
-      <LinkTemplate {...{ linkTitle }} />
-    ))}
+  <sbb-skiplink-list>
+    <sbb-link href={args.href1}>{args.label1}</sbb-link>
+    <sbb-link href={args.href2}>{args.label2}</sbb-link>
   </sbb-skiplink-list>
 );
 
-const links = ['Content', 'Contact us'];
+const firstLinkLabel = {
+  control: {
+    type: 'text',
+  },
+  table: {
+    category: 'first link',
+  },
+};
 
+const firstLinkHref = {
+  control: {
+    type: 'text',
+  },
+  table: {
+    category: 'first link',
+  },
+};
+
+const secondLinkLabel = {
+  control: {
+    type: 'text',
+  },
+  table: {
+    category: 'second link',
+  },
+};
+
+const secondLinkHref = {
+  control: {
+    type: 'text',
+  },
+  table: {
+    category: 'second link',
+  },
+};
+
+const defaultArgTypes = {
+  label1: firstLinkLabel,
+  href1: firstLinkHref,
+  label2: secondLinkLabel,
+  href2: secondLinkHref,
+};
+
+const defaultArgs = {
+  label1: 'To content',
+  href1: 'http://www.sbb.ch',
+  label2: 'To contact',
+  href2: 'http://www.sbb.ch',
+};
+
+export const skiplinkList = Template.bind({});
+skiplinkList.argTypes = defaultArgTypes;
+skiplinkList.args = {
+  ...defaultArgs,
+};
+
+/*
 export const skiplinkList = Template.bind({});
 
 skiplinkList.args = {};
-
+*/
 export default {
   decorators: [
     (Story) => (
