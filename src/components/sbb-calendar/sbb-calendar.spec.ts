89147c82915a4bb74b7896e86797940af34b988e
Mario Castigliano <mario.castigliano@finconsgroup.com>
2023-04-17 15:04:39 +0200

    fix: improve and stabilize tests

--- src/components/sbb-calendar/sbb-calendar.spec.ts
+++ src/components/sbb-calendar/sbb-calendar.spec.ts
@@ -58,167 +58,167 @@ describe('sbb-calendar', () => {
                 <tr>
                   <td colspan="6" data-day="0 1 2023"></td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 1, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="1 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 1, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="1 1 2023" tabindex="-1" sbb-tooltip-close>
                       1
                     </button>
                   </td>
                 </tr>
                 <tr>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 2, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="2 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 2, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="2 1 2023" tabindex="-1" sbb-tooltip-close>
                       2
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 3, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="3 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 3, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="3 1 2023" tabindex="-1" sbb-tooltip-close>
                       3
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 4, 2023" aria-pressed="false" class="sbb-calendar__day sbb-calendar__day-today" data-day="4 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 4, 2023" aria-pressed="false" class="sbb-calendar__day sbb-calendar__day-today" data-day="4 1 2023" tabindex="-1" sbb-tooltip-close>
                       4
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 5, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="5 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 5, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="5 1 2023" tabindex="-1" sbb-tooltip-close>
                       5
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 6, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="6 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 6, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="6 1 2023" tabindex="-1" sbb-tooltip-close>
                       6
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 7, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="7 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 7, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="7 1 2023" tabindex="-1" sbb-tooltip-close>
                       7
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 8, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="8 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 8, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="8 1 2023" tabindex="-1" sbb-tooltip-close>
                       8
                     </button>
                   </td>
                 </tr>
                 <tr>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 9, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="9 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 9, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="9 1 2023" tabindex="-1" sbb-tooltip-close>
                       9
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 10, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="10 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 10, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="10 1 2023" tabindex="-1" sbb-tooltip-close>
                       10
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 11, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="11 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 11, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="11 1 2023" tabindex="-1" sbb-tooltip-close>
                       11
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 12, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="12 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 12, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="12 1 2023" tabindex="-1" sbb-tooltip-close>
                       12
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 13, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="13 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 13, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="13 1 2023" tabindex="-1" sbb-tooltip-close>
                       13
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 14, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="14 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 14, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="14 1 2023" tabindex="-1" sbb-tooltip-close>
                       14
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 15, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="15 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 15, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="15 1 2023" tabindex="-1" sbb-tooltip-close>
                       15
                     </button>
                   </td>
                 </tr>
                 <tr>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 16, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="16 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 16, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="16 1 2023" tabindex="-1" sbb-tooltip-close>
                       16
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 17, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="17 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 17, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="17 1 2023" tabindex="-1" sbb-tooltip-close>
                       17
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 18, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="18 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 18, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="18 1 2023" tabindex="-1" sbb-tooltip-close>
                       18
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 19, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="19 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 19, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="19 1 2023" tabindex="-1" sbb-tooltip-close>
                       19
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data sbb-calendar__table-data-selected">
-                    <button aria-disabled="false" aria-label="January 20, 2023" aria-pressed="true" class="sbb-calendar__day sbb-calendar__day-selected" data-day="20 1 2023" tabindex="0">
+                    <button aria-disabled="false" aria-label="January 20, 2023" aria-pressed="true" class="sbb-calendar__day sbb-calendar__day-selected" data-day="20 1 2023" tabindex="0" sbb-tooltip-close>
                       20
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 21, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="21 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 21, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="21 1 2023" tabindex="-1" sbb-tooltip-close>
                       21
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 22, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="22 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 22, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="22 1 2023" tabindex="-1" sbb-tooltip-close>
                       22
                     </button>
                   </td>
                 </tr>
                 <tr>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 23, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="23 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 23, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="23 1 2023" tabindex="-1" sbb-tooltip-close>
                       23
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 24, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="24 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 24, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="24 1 2023" tabindex="-1" sbb-tooltip-close>
                       24
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 25, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="25 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 25, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="25 1 2023" tabindex="-1" sbb-tooltip-close>
                       25
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 26, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="26 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 26, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="26 1 2023" tabindex="-1" sbb-tooltip-close>
                       26
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 27, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="27 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 27, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="27 1 2023" tabindex="-1" sbb-tooltip-close>
                       27
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 28, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="28 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 28, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="28 1 2023" tabindex="-1" sbb-tooltip-close>
                       28
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 29, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="29 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 29, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="29 1 2023" tabindex="-1" sbb-tooltip-close>
                       29
                     </button>
                   </td>
                 </tr>
                 <tr>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 30, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="30 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 30, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="30 1 2023" tabindex="-1" sbb-tooltip-close>
                       30
                     </button>
                   </td>
                   <td class="sbb-calendar__table-data">
-                    <button aria-disabled="false" aria-label="January 31, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="31 1 2023" tabindex="-1">
+                    <button aria-disabled="false" aria-label="January 31, 2023" aria-pressed="false" class="sbb-calendar__day" data-day="31 1 2023" tabindex="-1" sbb-tooltip-close>
                       31
                     </button>
                   </td>
