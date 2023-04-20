2da6ea8c025f32a9afc0b535a9a179a1174fb50b
Davide Mininni <davide.mininni@finconsgroup.com>
2023-04-20 12:04:40 +0200

    test: add waitForCondition function

--- src/components/sbb-autocomplete/sbb-autocomplete.e2e.ts
+++ src/components/sbb-autocomplete/sbb-autocomplete.e2e.ts
@@ -1,5 +1,6 @@
 import { E2EElement, E2EPage, newE2EPage } from '@stencil/core/testing';
 import events from './sbb-autocomplete.events';
+import { waitForCondition } from '../../global/helpers/testing/wait-for-condition';
 
 describe('sbb-autocomplete', () => {
   let element: E2EElement, formField: E2EElement, input: E2EElement, page: E2EPage;
@@ -46,43 +47,55 @@ describe('sbb-autocomplete', () => {
 
     await input.focus();
     await page.waitForChanges();
+    await waitForCondition(() => willOpenEventSpy.events.length === 1);
     expect(willOpenEventSpy).toHaveReceivedEventTimes(1);
     await page.waitForChanges();
+    await waitForCondition(() => didOpenEventSpy.events.length === 1);
     expect(didOpenEventSpy).toHaveReceivedEventTimes(1);
     expect(input.getAttribute('aria-expanded')).toEqual('true');
 
     await element.press('Escape');
     await page.waitForChanges();
+    await waitForCondition(() => willCloseEventSpy.events.length === 1);
     expect(willCloseEventSpy).toHaveReceivedEventTimes(1);
     await page.waitForChanges();
+    await waitForCondition(() => didCloseEventSpy.events.length === 1);
     expect(didCloseEventSpy).toHaveReceivedEventTimes(1);
     expect(input.getAttribute('aria-expanded')).toEqual('false');
 
     await element.press('ArrowDown');
     await page.waitForChanges();
+    await waitForCondition(() => willOpenEventSpy.events.length === 2);
     expect(willOpenEventSpy).toHaveReceivedEventTimes(2);
     await page.waitForChanges();
+    await waitForCondition(() => didOpenEventSpy.events.length === 2);
     expect(didOpenEventSpy).toHaveReceivedEventTimes(2);
     expect(input.getAttribute('aria-expanded')).toEqual('true');
 
     await element.press('Tab');
     await page.waitForChanges();
+    await waitForCondition(() => willCloseEventSpy.events.length === 2);
     expect(willCloseEventSpy).toHaveReceivedEventTimes(2);
     await page.waitForChanges();
+    await waitForCondition(() => didCloseEventSpy.events.length === 2);
     expect(didCloseEventSpy).toHaveReceivedEventTimes(2);
     expect(input.getAttribute('aria-expanded')).toEqual('false');
 
     await input.click();
     await page.waitForChanges();
+    await waitForCondition(() => willOpenEventSpy.events.length === 3);
     expect(willOpenEventSpy).toHaveReceivedEventTimes(3);
     await page.waitForChanges();
+    await waitForCondition(() => didOpenEventSpy.events.length === 3);
     expect(didOpenEventSpy).toHaveReceivedEventTimes(3);
     expect(input.getAttribute('aria-expanded')).toEqual('true');
 
     const button = await page.find('button');
     await button.click();
+    await waitForCondition(() => willCloseEventSpy.events.length === 3);
     expect(willCloseEventSpy).toHaveReceivedEventTimes(3);
     await page.waitForChanges();
+    await waitForCondition(() => didCloseEventSpy.events.length === 3);
     expect(didCloseEventSpy).toHaveReceivedEventTimes(3);
     expect(input.getAttribute('aria-expanded')).toEqual('false');
   });
@@ -92,6 +105,7 @@ describe('sbb-autocomplete', () => {
     const didCloseEventSpy = await page.spyOnEvent(events.didClose);
     await input.focus();
     await page.waitForChanges();
+    await waitForCondition(() => didOpenEventSpy.events.length === 1);
     expect(didOpenEventSpy).toHaveReceivedEventTimes(1);
 
     await element.press('ArrowDown');
@@ -108,6 +122,7 @@ describe('sbb-autocomplete', () => {
 
     await element.press('Enter');
     await page.waitForChanges();
+    await waitForCondition(() => didCloseEventSpy.events.length === 1);
     expect(await optTwo.getProperty('active')).toEqual(false);
     expect(await optTwo.getProperty('selected')).toEqual(true);
     expect(didCloseEventSpy).toHaveReceivedEventTimes(1);
