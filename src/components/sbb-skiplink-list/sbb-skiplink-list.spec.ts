5ee66e8999493e139f59e6d315859055c7041675
Davide Mininni <davide.mininni@finconsgroup.com>
2023-05-04 19:12:30 +0200

    test: add unit test

--- src/components/sbb-skiplink-list/sbb-skiplink-list.spec.ts
+++ src/components/sbb-skiplink-list/sbb-skiplink-list.spec.ts
@@ -5,15 +5,24 @@ describe('sbb-skiplink-list', () => {
   it('renders', async () => {
     const { root } = await newSpecPage({
       components: [SbbSkiplinkList],
-      html: '<sbb-skiplink-list />',
+      html: `
+        <sbb-skiplink-list>
+          <sbb-link href='#'>Link</sbb-link>
+        </sbb-skiplink-list>
+      `,
     });
 
     expect(root).toEqualHtml(`
-        <sbb-skiplink-list>
-          <mock:shadow-root>
-            <button class="some-class"></button>
-          </mock:shadow-root>
-        </sbb-skiplink-list>
-      `);
+      <sbb-skiplink-list>
+        <mock:shadow-root>
+          <span class="sbb-skiplink-list">
+            <li>
+              <slot name="link-0"></slot>
+            </li>
+          </span>
+        </mock:shadow-root>
+        <sbb-link href='#' slot='link-0'>Link</sbb-link>
+      </sbb-skiplink-list>
+    `);
   });
 });
