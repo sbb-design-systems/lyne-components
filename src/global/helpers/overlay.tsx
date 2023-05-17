f54c5581048802e0ccbde82c35c7f3e0d5a4e556
TomMenga <tommaso.menga-94@hotmail.it>
2023-05-17 14:25:38 +0200

    docs(overlay-helper): added comment to overlayGapFixCorners

--- src/global/helpers/overlay.tsx
+++ src/global/helpers/overlay.tsx
@@ -2,6 +2,10 @@ import { h, JSX } from '@stencil/core';
 
 export type SbbOverlayState = 'closed' | 'opening' | 'opened' | 'closing';
 
+/**
+ * Used to create the "wrapping" effect around the anchor for overlays (es. autocomplete)
+ * Works in conjuction with the 'overlayGapFixCorners()' function in 'overlay.tsx'
+ */
 export function overlayGapFixCorners(): JSX.Element {
   return [
     <div class="sbb-gap-fix-wrapper">
