608f37e82e4007ccdb7f57cd32079bf4b57a6c14
TomMenga <tommaso.menga-94@hotmail.it>
2023-05-18 14:40:03 +0200

    Revert "fix: fix broken tests (WIP)"
    This reverts commit 8bbb276a835e5cc7849d1f5d5d4934e792d52ef2.

--- src/components/sbb-navigation/sbb-navigation.e2e.ts
+++ src/components/sbb-navigation/sbb-navigation.e2e.ts
@@ -87,7 +87,7 @@ describe('sbb-navigation', () => {
 
     expect(dialog).toHaveAttribute('open');
 
-    closeButton.triggerEvent('click', { bubbles: true, cancelable: true, composed: true });
+    await closeButton.click();
     await page.waitForChanges();
 
     await waitForCondition(() => didCloseEventSpy.events.length === 1);
@@ -139,7 +139,7 @@ describe('sbb-navigation', () => {
     await element.callMethod('open');
     await page.waitForChanges();
 
-    action.triggerEvent('click', { bubbles: true, cancelable: true, composed: true });
+    action.triggerEvent('click');
     await page.waitForChanges();
 
     await waitForCondition(() => didOpenEventSpy.events.length === 1);
@@ -149,7 +149,7 @@ describe('sbb-navigation', () => {
     expect(dialog).toHaveAttribute('open');
     expect(sectionDialog).toHaveAttribute('open');
 
-    closeEl.triggerEvent('click', { bubbles: true, cancelable: true, composed: true });
+    closeEl.triggerEvent('click');
     await page.waitForChanges();
 
     await waitForCondition(() => didCloseEventSpy.events.length === 1);
@@ -178,7 +178,7 @@ describe('sbb-navigation', () => {
     expect(navDialog).toHaveAttribute('open');
     expect(sectionDialog).not.toHaveAttribute('open');
 
-    action.triggerEvent('click', { bubbles: true, cancelable: true, composed: true });
+    action.triggerEvent('click');
     await page.waitForChanges();
 
     expect(navDialog).toHaveAttribute('open');
@@ -204,13 +204,13 @@ describe('sbb-navigation', () => {
     expect(firstSectionDialog).not.toHaveAttribute('open');
     expect(secondSectionDialog).not.toHaveAttribute('open');
 
-    firstAction.triggerEvent('click', { bubbles: true, cancelable: true, composed: true });
+    firstAction.triggerEvent('click');
     await page.waitForChanges();
 
     expect(firstSectionDialog).toHaveAttribute('open');
     expect(secondSectionDialog).not.toHaveAttribute('open');
 
-    secondAction.triggerEvent('click', { bubbles: true, cancelable: true, composed: true });
+    secondAction.triggerEvent('click');
     await page.waitForChanges();
 
     expect(firstSectionDialog).not.toHaveAttribute('open');
@@ -230,7 +230,7 @@ describe('sbb-navigation', () => {
     await element.callMethod('open');
     await page.waitForChanges();
 
-    action.triggerEvent('click', { bubbles: true, cancelable: true, composed: true });
+    action.triggerEvent('click');
     await page.waitForChanges();
 
     await waitForCondition(() => didOpenEventSpy.events.length === 1);
@@ -240,7 +240,7 @@ describe('sbb-navigation', () => {
     expect(dialog).toHaveAttribute('open');
     expect(sectionDialog).toHaveAttribute('open');
 
-    closeButton.triggerEvent('click', { bubbles: true, cancelable: true, composed: true });
+    await closeButton.click();
     await page.waitForChanges();
 
     await waitForCondition(() => didCloseEventSpy.events.length === 1);
@@ -263,7 +263,7 @@ describe('sbb-navigation', () => {
     await element.callMethod('open');
     await page.waitForChanges();
 
-    action.triggerEvent('click', { bubbles: true, cancelable: true, composed: true });
+    action.triggerEvent('click');
     await page.waitForChanges();
 
     await waitForCondition(() => didOpenEventSpy.events.length === 1);
@@ -301,7 +301,7 @@ describe('sbb-navigation', () => {
     await element.callMethod('open');
     await page.waitForChanges();
 
-    action.triggerEvent('click', { bubbles: true, cancelable: true, composed: true });
+    action.triggerEvent('click');
     await page.waitForChanges();
 
     await waitForCondition(() => didOpenEventSpy.events.length === 1);
@@ -311,7 +311,7 @@ describe('sbb-navigation', () => {
     expect(dialog).toHaveAttribute('open');
     expect(sectionDialog).toHaveAttribute('open');
 
-    closeEl.triggerEvent('click', { bubbles: true, cancelable: true, composed: true });
+    await closeEl.click();
     await page.waitForChanges();
 
     expect(dialog).toHaveAttribute('open');
