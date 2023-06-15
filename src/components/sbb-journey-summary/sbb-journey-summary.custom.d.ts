4f9e6fbad13dfa28ab661910aa6ceb5987255279
WalkingOS <osman.minaz@outlook.de>
2023-06-15 12:22:55 +0200

    feat: change interface attribute

--- src/components/sbb-journey-summary/sbb-journey-summary.custom.d.ts
+++ src/components/sbb-journey-summary/sbb-journey-summary.custom.d.ts
@@ -1,7 +1,7 @@
 import { Leg } from '../../global/interfaces/timetable-properties';
 
 export interface InterfaceSbbJourneySummaryAttributes {
-  headerLevel?: '1' | '2' | '3' | '4' | '5' | '6';
+  level?: '1' | '2' | '3' | '4' | '5' | '6';
   legs: Leg[];
   vias?: string[];
   origin: string;
