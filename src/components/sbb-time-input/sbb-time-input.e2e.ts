c2d8214826ee05eaf9bc24cd0d0cc5bff512d294
Mario Castigliano <mario.castigliano@finconsgroup.com>
2023-06-21 09:34:48 +0200

    test: new scenarios in time input

--- src/components/sbb-time-input/sbb-time-input.e2e.ts
+++ src/components/sbb-time-input/sbb-time-input.e2e.ts
@@ -34,6 +34,20 @@ describe('sbb-time-input', () => {
     expect(await input.getProperty('value')).toEqual('11:00');
   });
 
+  it('should watch for value changes and interpret valid values', async () => {
+    element.setProperty('value', ':00');
+    await page.waitForChanges();
+    const input = await page.find('sbb-time-input >>> input');
+    expect(await input.getProperty('value')).toEqual('00:00');
+  });
+
+  it('should watch for value changes and clear invalid values', async () => {
+    element.setProperty('value', ':');
+    await page.waitForChanges();
+    const input = await page.find('sbb-time-input >>> input');
+    expect(await input.getProperty('value')).toEqual('');
+  });
+
   it('should watch for valueAsDate changes', async () => {
     element.setProperty('valueAsDate', '2023-01-01T15:00:00');
     await page.waitForChanges();
