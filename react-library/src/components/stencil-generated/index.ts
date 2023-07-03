ee441c7d97ad1e5fdd58ff8528d5e1a186abcde2
Davide Mininni <davide.mininni@finconsgroup.com>
2023-07-03 09:32:24 +0200

    feat: remove old accordion component

--- react-library/src/components/stencil-generated/index.ts
+++ react-library/src/components/stencil-generated/index.ts
@@ -8,8 +8,6 @@ import type { JSX } from '@sbb-esta/lyne-components';
 import { defineCustomElements } from '@sbb-esta/lyne-components/loader';
 
 defineCustomElements();
-export const SbbAccordion = /*@__PURE__*/createReactComponent<JSX.SbbAccordion, HTMLSbbAccordionElement>('sbb-accordion');
-export const SbbAccordionItem = /*@__PURE__*/createReactComponent<JSX.SbbAccordionItem, HTMLSbbAccordionItemElement>('sbb-accordion-item');
 export const SbbActionGroup = /*@__PURE__*/createReactComponent<JSX.SbbActionGroup, HTMLSbbActionGroupElement>('sbb-action-group');
 export const SbbAlert = /*@__PURE__*/createReactComponent<JSX.SbbAlert, HTMLSbbAlertElement>('sbb-alert');
 export const SbbAlertGroup = /*@__PURE__*/createReactComponent<JSX.SbbAlertGroup, HTMLSbbAlertGroupElement>('sbb-alert-group');
--- src/components/sbb-accordion-item/sbb-accordion-item.events.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  didClose: 'did-close',
-  didOpen: 'did-open',
-  willClose: 'will-close',
-  willOpen: 'will-open',
-};
