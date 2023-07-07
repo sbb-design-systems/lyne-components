9fdee610ea7ceebe7b5c64644f9ea76d638d30f5
Davide Mininni <davide.mininni@finconsgroup.com>
2023-07-07 16:56:50 +0200

    test: add tests

--- src/components/sbb-expansion-panel-header/sbb-expansion-panel-header.e2e.ts
+++ src/components/sbb-expansion-panel-header/sbb-expansion-panel-header.e2e.ts
@@ -3,11 +3,32 @@ import { E2EElement, E2EPage, newE2EPage } from '@stencil/core/testing';
 describe('sbb-expansion-panel-header', () => {
   let element: E2EElement, page: E2EPage;
 
-  it('renders', async () => {
+  beforeEach(async () => {
     page = await newE2EPage();
-    await page.setContent('<sbb-expansion-panel-header></sbb-expansion-panel-header>');
-
+    await page.setContent(`<sbb-expansion-panel-header>Header</sbb-expansion-panel-header>`);
     element = await page.find('sbb-expansion-panel-header');
+  });
+
+  it('renders', async () => {
     expect(element).toHaveClass('hydrated');
+  });
+
+  it('emits event on click', async () => {
+    const spy = await page.spyOnEvent('toggle-expanded');
+    const button = await page.find('sbb-expansion-panel-header >>> button');
+    await button.click();
+    expect(spy).toHaveReceivedEvent();
+  });
+
+  it('does not emits event on click if disabled', async () => {
+    page = await newE2EPage();
+    await page.setContent(
+      `<sbb-expansion-panel-header disabled>Header</sbb-expansion-panel-header>`
+    );
+    element = await page.find('sbb-expansion-panel-header');
+    const spy = await page.spyOnEvent('toggle-expanded');
+    const button = await page.find('sbb-expansion-panel-header >>> button');
+    await button.click();
+    expect(spy).not.toHaveReceivedEvent();
   });
 });
--- src/components/sbb-expansion-panel/sbb-expansion-panel.e2e.ts
+++ src/components/sbb-expansion-panel/sbb-expansion-panel.e2e.ts
@@ -3,11 +3,29 @@ import { E2EElement, E2EPage, newE2EPage } from '@stencil/core/testing';
 describe('sbb-expansion-panel', () => {
   let element: E2EElement, page: E2EPage;
 
-  it('renders', async () => {
+  beforeEach(async () => {
     page = await newE2EPage();
-    await page.setContent('<sbb-expansion-panel></sbb-expansion-panel>');
+    await page.setContent(`
+      <sbb-expansion-panel>
+        <sbb-expansion-panel-header icon-name='dog-medium'>Header</sbb-expansion-panel-header>
+        <sbb-expansion-panel-content>Content</sbb-expansion-panel-content>
+      </sbb-expansion-panel>
+    `);
 
     element = await page.find('sbb-expansion-panel');
+  });
+
+  it('renders', async () => {
     expect(element).toHaveClass('hydrated');
+  });
+
+  it('has slotted elements with the correct properties', async () => {
+    const innerButton = await page.find('sbb-expansion-panel-header >>> button');
+    expect(innerButton).toEqualAttribute('id', 'sbb-expansion-panel-header-1');
+    expect(innerButton).toEqualAttribute('aria-controls', 'sbb-expansion-panel-content-1');
+    const content = await page.find('sbb-expansion-panel-content');
+    expect(content).toEqualAttribute('id', 'sbb-expansion-panel-content-1');
+    expect(content).toEqualAttribute('data-icon-space', '');
+    expect(content).toEqualAttribute('aria-labelledby', `sbb-expansion-panel-header-1`);
   });
 });
