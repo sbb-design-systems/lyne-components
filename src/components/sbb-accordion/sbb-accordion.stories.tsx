3919b9882a574d77a3214d7c73ef9b0bcf468130
Davide Mininni <davide.mininni@finconsgroup.com>
2023-07-10 11:04:41 +0200

    chore: add story

--- src/components/sbb-accordion/sbb-accordion.stories.tsx
+++ src/components/sbb-accordion/sbb-accordion.stories.tsx
@@ -177,6 +177,12 @@ export const WithIcon: StoryObj = {
   args: { ...defaultArgs, iconName: 'swisspass-medium' },
 };
 
+export const Expanded: StoryObj = {
+  render: Template,
+  argTypes: defaultArgTypes,
+  args: { ...defaultArgs, expanded: true },
+};
+
 export const Multi: StoryObj = {
   render: Template,
   argTypes: defaultArgTypes,
