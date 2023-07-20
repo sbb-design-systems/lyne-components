df4644cd312ed41e3caa151af73bf8d7e25a2e9d
Davide Mininni <davide.mininni@finconsgroup.com>
2023-07-20 18:26:08 +0200

    test: add file management test (partial)

--- src/components/sbb-file-selector/sbb-file-selector.e2e.ts
+++ src/components/sbb-file-selector/sbb-file-selector.e2e.ts
@@ -12,4 +12,34 @@ describe('sbb-file-selector', () => {
   it('renders', () => {
     expect(element).toHaveClass('hydrated');
   });
+
+  it('load a file', async () => {
+    await page.evaluate(() => {
+      const dataTransfer = new DataTransfer();
+      dataTransfer.items.add(new File(['Hello world!'], 'hello.txt', { type: 'text/plain' }));
+      const input = document.querySelector('sbb-file-selector').shadowRoot.querySelector('input');
+      input.files = dataTransfer.files;
+      input.dispatchEvent(new Event('change'));
+    });
+    await page.waitForChanges();
+
+    expect(await element.getProperty('files')).not.toBeNull();
+    expect(
+      (await page.find('sbb-file-selector >>> .sbb-file-selector__file-list')).tagName,
+    ).toEqual('UL');
+
+    const listItems: E2EElement[] = await page.findAll('sbb-file-selector >>> li');
+    expect(listItems.length).toEqual(1);
+    expect(listItems[0]).toEqualHtml(`
+      <li class="sbb-file-selector__file">
+        <span class="sbb-file-selector__file-details">
+          <span class="sbb-file-selector__file-name">hello.txt</span>
+          <span class="sbb-file-selector__file-size">12 B</span>
+        </span>
+        <sbb-button aria-label="Remove file" class="hydrated" data-icon-only dir="ltr" icon-name="trash-small"
+                    role="button" size="m" tabindex="0" variant="secondary">
+        </sbb-button>
+      </li>
+    `);
+  });
 });
