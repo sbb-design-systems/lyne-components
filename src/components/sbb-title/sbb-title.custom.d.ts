149605b37a88023b3ba949f6e343b56d2ae51fd7
Davide Mininni <davide.mininni@finconsgroup.com>
2023-07-25 17:07:59 +0200

    fix: review (partial)

--- src/components/sbb-title/sbb-title.custom.d.ts
+++ src/components/sbb-title/sbb-title.custom.d.ts
@@ -1,4 +1,4 @@
-type TitleLevel = '1' | '2' | '3' | '4' | '5' | '6';
+export type TitleLevel = '1' | '2' | '3' | '4' | '5' | '6';
 
 export interface InterfaceTitleAttributes {
   level: TitleLevel;
