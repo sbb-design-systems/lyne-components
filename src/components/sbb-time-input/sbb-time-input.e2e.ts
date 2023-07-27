ec4faa25e0a1f7300ed9530f02d725f9e5f6c403
Jeremias Peier <jeremias.peier@sbb.ch>
2023-07-27 16:48:01 +0200

    test: add additional tests

--- src/components/sbb-time-input/sbb-time-input.e2e.ts
+++ src/components/sbb-time-input/sbb-time-input.e2e.ts
@@ -8,7 +8,6 @@ describe('sbb-time-input', () => {
     await page.setContent(`
       <sbb-time-input input='input-1'></sbb-time-input>
       <input id='input-1' />
-      <button id="trigger-change"></button>
     `);
     await page.waitForChanges();
 
@@ -20,22 +19,105 @@ describe('sbb-time-input', () => {
     expect(element).toHaveClass('hydrated');
   });
 
-  it('should emit event', async () => {
+  it('should emit form events', async () => {
     const changeSpy = await element.spyOnEvent('change');
+    const inputSpy = await element.spyOnEvent('input');
     await input.focus();
     await input.press('1');
-    await page.click('#trigger-change');
+    await input.press('Tab');
     await page.waitForChanges();
     expect(changeSpy).toHaveReceivedEvent();
+    expect(inputSpy).toHaveReceivedEvent();
   });
 
-  it('should watch for value changes and interpret valid values', async () => {
-    await page.evaluate(() => {
-      const input = document.getElementById('input-1') as HTMLInputElement;
-      input.value = ':00';
-      input.dispatchEvent(new Event('change'));
-    });
-    expect(await input.getProperty('value')).toEqual('00:00');
+  it('should emit validation change event', async () => {
+    let validationChangeSpy = await element.spyOnEvent('validationChange');
+
+    // When entering 99
+    await input.focus();
+    await input.type('99');
+    await input.press('Tab');
+
+    // Then validation event should emit with false
+    expect(validationChangeSpy).toHaveFirstReceivedEventDetail({ valid: false });
+    expect(input).toHaveAttribute('data-sbb-invalid');
+
+    // When adding another 9 (999)
+    await input.focus();
+    await input.press('9');
+    await input.press('Tab');
+
+    // Then validation event should not be emitted a second time
+    expect(validationChangeSpy).toHaveReceivedEventTimes(1);
+    expect(input).toHaveAttribute('data-sbb-invalid');
+
+    // Reset event spy
+    validationChangeSpy = await element.spyOnEvent('validationChange');
+
+    // When deleting trailing two nines (convert value to 9 which is valid)
+    await input.focus();
+    await input.press('Backspace');
+    await input.press('Backspace');
+    await input.press('Tab');
+
+    // Then validation event should be emitted with true
+    expect(validationChangeSpy).toHaveFirstReceivedEventDetail({ valid: true });
+    expect(input).not.toHaveAttribute('data-sbb-invalid');
+  });
+
+  it('should interpret valid values', async () => {
+    const testCases = [
+      { value: '0', interpretedAs: '00:00' },
+      { value: '1', interpretedAs: '01:00' },
+      { value: '12', interpretedAs: '12:00' },
+      { value: '123', interpretedAs: '01:23' },
+      { value: '1.12', interpretedAs: '01:12' },
+      { value: '1,12', interpretedAs: '01:12' },
+      { value: '1:12', interpretedAs: '01:12' },
+      { value: '1h12', interpretedAs: '01:12' },
+      { value: '1_12', interpretedAs: '01:12' },
+      { value: '1;12', interpretedAs: '01:12' },
+      { value: '1-12', interpretedAs: '01:12' },
+      { value: '01:12', interpretedAs: '01:12' },
+      { value: ':00', interpretedAs: '00:00' },
+      { value: ':01', interpretedAs: '00:01' },
+      { value: ':1', interpretedAs: '00:01' },
+      { value: 'h', interpretedAs: '00:00' },
+      { value: '.', interpretedAs: '00:00' },
+      { value: ':', interpretedAs: '00:00' },
+    ];
+
+    for (const testCase of testCases) {
+      // Clear input
+      await page.evaluate(
+        () => ((document.getElementById('input-1') as HTMLInputElement).value = ''),
+      );
+
+      await input.type(testCase.value);
+      await input.press('Tab');
+      expect(await input.getProperty('value')).toEqual(testCase.interpretedAs);
+    }
+  });
+
+  it('should not touch invalid values', async () => {
+    const testCases = [
+      { value: '99', interpretedAs: '99' },
+      { value: '24', interpretedAs: '24' },
+      { value: 'hh', interpretedAs: 'hh' },
+      { value: '', interpretedAs: '' },
+      { value: '00:56', interpretedAs: '00:56' },
+    ];
+
+    for (const testCase of testCases) {
+      // Clear input
+      await page.evaluate(
+        () => ((document.getElementById('input-1') as HTMLInputElement).value = ''),
+      );
+
+      await input.type(testCase.value);
+      await input.press('Tab');
+      expect(await input.getProperty('value')).toEqual(testCase.interpretedAs);
+    }
   });
 
   it('should handle deletion', async () => {
