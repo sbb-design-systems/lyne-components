7b5e6c8aeda335bd68f82be75cab8d737ac11606
Marco D'Auria <marco.dauria@finconsgroup.com>
2023-09-01 12:55:45 +0200

    test: implement e2e and unit tests

--- src/components/sbb-form-field-clear/sbb-form-field-clear.e2e.ts
+++ src/components/sbb-form-field-clear/sbb-form-field-clear.e2e.ts
@@ -3,11 +3,29 @@ import { E2EElement, E2EPage, newE2EPage } from '@stencil/core/testing';
 describe('sbb-form-field-clear', () => {
   let element: E2EElement, page: E2EPage;
 
-  it('renders', async () => {
+  beforeEach(async () => {
     page = await newE2EPage();
-    await page.setContent('<sbb-form-field-clear></sbb-form-field-clear>');
+    await page.setContent(`
+      <sbb-form-field label="Label">
+        <input id="input" type="text" placeholder="Input placeholder" value="Input value" />
+        <sbb-form-field-clear />
+      </sbb-form-field>`);
 
     element = await page.find('sbb-form-field-clear');
+  });
+
+  it('renders', async () => {
     expect(element).toHaveClass('hydrated');
+  });
+
+  it('clears the value and sets the focus on the input', async () => {
+    await page.waitForChanges();
+    expect(await page.evaluate(() => document.querySelector('input').value)).toBe('Input value');
+
+    await element.click();
+    await page.waitForChanges();
+
+    expect(await page.evaluate(() => document.querySelector('input').value)).toBeFalsy();
+    expect(await page.evaluate(() => document.activeElement.id)).toBe('input');
   });
 });
