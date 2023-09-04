27f3dbf85bb1e889b3864dd02b73ec6d026e8e4c
Davide Mininni <davide.mininni@finconsgroup.com>
2023-09-04 17:21:56 +0200

    test: add spec tests

--- src/components/sbb-loading-indicator/sbb-loading-indicator.spec.ts
+++ src/components/sbb-loading-indicator/sbb-loading-indicator.spec.ts
@@ -2,18 +2,47 @@ import { SbbLoadingIndicator } from './sbb-loading-indicator';
 import { newSpecPage } from '@stencil/core/testing';
 
 describe('sbb-loading-indicator', () => {
-  it('renders', async () => {
+  it('renders with variant `window`', async () => {
     const { root } = await newSpecPage({
       components: [SbbLoadingIndicator],
-      html: '<sbb-loading-indicator />',
+      html: '<sbb-loading-indicator variant="window"/>',
     });
 
     expect(root).toEqualHtml(`
-        <sbb-loading-indicator>
-          <mock:shadow-root>
-            <button class="some-class"></button>
-          </mock:shadow-root>
-        </sbb-loading-indicator>
-      `);
+      <sbb-loading-indicator variant="window" size="s">
+        <mock:shadow-root>
+          <span class="sbb-loading-indicator">
+            <span class="sbb-loading-indicator__animated-element">
+              <div>
+                <div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+              </div>
+            </span>
+          </span>
+        </mock:shadow-root>
+      </sbb-loading-indicator>
+    `);
+  });
+
+  it('renders with variant `circle`', async () => {
+    const { root } = await newSpecPage({
+      components: [SbbLoadingIndicator],
+      html: '<sbb-loading-indicator variant="circle"/>',
+    });
+
+    expect(root).toEqualHtml(`
+      <sbb-loading-indicator variant="circle" size="s">
+        <mock:shadow-root>
+          <span class="sbb-loading-indicator">
+            <span class="sbb-loading-indicator__animated-element"></span>
+          </span>
+        </mock:shadow-root>
+      </sbb-loading-indicator>
+    `);
   });
 });
