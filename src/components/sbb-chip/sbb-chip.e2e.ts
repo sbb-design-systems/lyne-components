59c1cbdd72055db857f09a856d5da957506385ad
Davide Mininni <davide.mininni@finconsgroup.com>
2023-09-25 17:21:26 +0200

    fix: review

--- src/components/sbb-chip/sbb-chip.e2e.ts
+++ src/components/sbb-chip/sbb-chip.e2e.ts
@@ -4,9 +4,7 @@ import { SbbChip } from './sbb-chip';
 
 describe('sbb-chip', () => {
   it('renders', async () => {
-    await fixture(html`<sbb-chip>Label</sbb-chip>`);
-
-    const element: SbbChip = document.querySelector('sbb-chip');
+    const element: SbbChip = await fixture(html`<sbb-chip>Label</sbb-chip>`);
     assert.instanceOf(element, SbbChip);
   });
 });
