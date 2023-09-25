5cd089c901efbcb1ca835b8ddd71076e3c706c5f
Lukas Spirig <lukas.spirig@sbb.ch>
2023-09-25 12:26:54 +0200

    build: improve typings for .stories.tsx

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,14 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 /// <reference types="vite/client" />
 
 declare module '*?lit&inline' {
   const src: import('lit').CSSResult;
   export default src;
+}
+
+declare namespace JSX {
+  type Element = jsxDom.JSX.Element;
+  interface IntrinsicElements {
+    'sbb-icon': import('./components/sbb-icon/sbb-icon').SbbIcon & jsxDom.JSX.Element;
+  }
 }
