a5ceb8d8ff834dbb66d838f26fc1535d5e76d15b
Jeremias Peier <jeremias.peier@sbb.ch>
2023-10-04 14:31:22 +0200

    docs: improve

--- src/components/sbb-calendar/sbb-calendar.stories.tsx
+++ src/components/sbb-calendar/sbb-calendar.stories.tsx
@@ -30,15 +30,27 @@ const Template = ({ min, max, selectedDate, dateFilter, ...args }): JSX.Element 
 );
 
 const TemplateDynamicWidth = ({ min, max, selectedDate, dateFilter, ...args }): JSX.Element => (
-  <sbb-calendar
-    style={{ width: '900px' }}
-    selectedDate={new Date(selectedDate)}
-    {...getCalendarAttr(min, max)}
-    {...args}
-    ref={(calendarRef) => {
-      calendarRef.dateFilter = dateFilter;
-    }}
-  ></sbb-calendar>
+  <div>
+    <button
+      onClick={() => {
+        const dialog = document.getElementById('dialog') as HTMLSbbDialogElement;
+        return dialog.open();
+      }}
+    >
+      Open
+    </button>
+    <sbb-dialog id="dialog" title-content="test">
+      <sbb-calendar
+        style={{ width: '100%' }}
+        selectedDate={new Date(selectedDate)}
+        {...getCalendarAttr(min, max)}
+        {...args}
+        ref={(calendarRef) => {
+          calendarRef.dateFilter = dateFilter;
+        }}
+      ></sbb-calendar>
+    </sbb-dialog>
+  </div>
 );
 
 const TemplateFilterFunction = ({ dateFilter, ...args }): JSX.Element => (
@@ -186,7 +198,6 @@ export const CalendarWideDynamicWidth: StoryObj = {
 };
 
 const meta: Meta = {
-  excludeStories: /.*DynamicWidth$/,
   decorators: [withActions as Decorator],
   parameters: {
     actions: {
