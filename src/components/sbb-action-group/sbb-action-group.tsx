24610670bd11d2059c5285de30269edf696b5ffb
Marco D'Auria <101181211+dauriamarco@users.noreply.github.com>
2023-10-04 12:35:47 +0200

    refactor(sbb-dialog): lit migration (#2041)
    Co-authored-by: TomMenga <tommaso.menga-94@hotmail.it>

--- src/components/sbb-action-group/sbb-action-group.tsx
+++ src/components/sbb-action-group/sbb-action-group.tsx
@@ -1,73 +1,82 @@
-import { Component, Element, h, JSX, Prop, Watch } from '@stencil/core';
 import { InterfaceButtonAttributes } from '../sbb-button/sbb-button.custom';
 import { InterfaceLinkAttributes } from '../sbb-link/sbb-link.custom';
 import { InterfaceSbbActionGroupAttributes } from './sbb-action-group.custom';
+import { CSSResult, html, LitElement, TemplateResult, PropertyValues } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import Style from './sbb-action-group.scss?lit&inline';
 
 /**
  * @slot unnamed - Slot to render the content inside the container.
  */
 
-@Component({
-  shadow: true,
-  styleUrl: 'sbb-action-group.scss',
-  tag: 'sbb-action-group',
-})
-export class SbbActionGroup {
+@customElement('sbb-action-group')
+export class SbbActionGroup extends LitElement {
+  public static override styles: CSSResult = Style;
+
   /**
    * Set the slotted `<sbb-action-group>` children's alignment.
    */
-  @Prop({ reflect: true }) public alignGroup: InterfaceSbbActionGroupAttributes['alignGroup'] =
-    'start';
+  @property({ attribute: 'align-group', reflect: true })
+  public alignGroup: InterfaceSbbActionGroupAttributes['alignGroup'] = 'start';
 
   /**
    * Overrides the behaviour of `orientation` property.
    */
-  @Prop({ reflect: true })
+  @property({ attribute: 'horizontal-from', reflect: true })
   public horizontalFrom?: InterfaceSbbActionGroupAttributes['horizontalFrom'] = 'medium';
 
   /**
    * Indicates the orientation of the components inside the `<sbb-action-group>`.
    */
-  @Prop({ reflect: true }) public orientation: InterfaceSbbActionGroupAttributes['orientation'] =
-    'horizontal';
+  @property({ reflect: true })
+  public orientation: InterfaceSbbActionGroupAttributes['orientation'] = 'horizontal';
 
   /**
    * Size of the nested sbb-button instances. This will overwrite the size attribute of nested
    * sbb-button instances.
    */
-  @Prop({ reflect: true }) public buttonSize?: InterfaceButtonAttributes['size'] = 'l';
+  @property({ attribute: 'button-size', reflect: true })
+  public buttonSize?: InterfaceButtonAttributes['size'] = 'l';
 
   /**
    * Size of the nested sbb-link instances. This will overwrite the size attribute of nested
    * sbb-link instances.
    */
-  @Prop({ reflect: true }) public linkSize?: InterfaceLinkAttributes['size'] = 'm';
+  @property({ attribute: 'link-size', reflect: true })
+  public linkSize?: InterfaceLinkAttributes['size'] = 'm';
 
-  @Element() private _element!: HTMLElement;
-
-  @Watch('buttonSize')
-  public syncButtons(): void {
-    this._element.querySelectorAll('sbb-button').forEach((b) => (b.size = this.buttonSize));
+  private _syncButtons(): void {
+    this.querySelectorAll('sbb-button').forEach((b) => (b.size = this.buttonSize));
   }
 
-  @Watch('linkSize')
-  public syncLinks(): void {
-    this._element.querySelectorAll('sbb-link').forEach((link) => {
+  public override willUpdate(changedProperties: PropertyValues<this>): void {
+    if (changedProperties.has('buttonSize')) {
+      this._syncButtons();
+    }
+    if (changedProperties.has('linkSize')) {
+      this._syncLinks();
+    }
+  }
+
+  private _syncLinks(): void {
+    this.querySelectorAll('sbb-link').forEach((link) => {
       link.variant = 'block';
       link.size = this.linkSize;
     });
   }
 
-  public render(): JSX.Element {
-    return (
+  protected override render(): TemplateResult {
+    return html`
       <div class="sbb-action-group">
-        <slot
-          onSlotchange={() => {
-            this.syncButtons();
-            this.syncLinks();
-          }}
-        />
+        <slot></slot>
       </div>
-    );
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    'sbb-action-group': SbbActionGroup;
   }
 }
--- src/components/sbb-dialog/sbb-dialog.spec.ts
+++ src/components/sbb-dialog/sbb-dialog.spec.ts
@@ -1,39 +1,36 @@
-import { SbbDialog } from './sbb-dialog';
-import { newSpecPage } from '@stencil/core/testing';
 import { i18nCloseDialog } from '../../global/i18n';
+import { expect, fixture } from '@open-wc/testing';
+import { html } from 'lit/static-html.js';
+import '../sbb-dialog';
 
 describe('sbb-dialog', () => {
   it('renders', async () => {
-    const { root } = await newSpecPage({
-      components: [SbbDialog],
-      html: '<sbb-dialog />',
-    });
+    const root = await fixture(html`<sbb-dialog />`);
 
-    expect(root).toEqualHtml(`
-      <sbb-dialog data-state="closed" data-fullscreen>
-        <mock:shadow-root>
-          <div class="sbb-dialog__container">
-            <dialog class="sbb-dialog" role="group" aria-labelledby="title" id="sbb-dialog-0">
-              <div class="sbb-dialog__wrapper">
-                <div class="sbb-dialog__header">
-                  <sbb-button
-                    aria-label="${i18nCloseDialog.en}"
-                    class="sbb-dialog__close"
-                    icon-name="cross-small"
-                    sbb-dialog-close=""
-                    size="m"
-                    type="button"
-                    variant="secondary">
-                  </sbb-button>
-                </div>
-                <div class="sbb-dialog__content">
-                  <slot></slot>
-                </div>
+    expect(root).dom.to.be.equal(`<sbb-dialog data-state="closed" data-fullscreen></sbb-dialog>`);
+    expect(root).shadowDom.to.be.equal(
+      `
+        <div class="sbb-dialog__container">
+          <dialog class="sbb-dialog" role="group" aria-labelledby="title" id="sbb-dialog-0">
+            <div class="sbb-dialog__wrapper">
+              <div class="sbb-dialog__header">
+                <sbb-button
+                  aria-label="${i18nCloseDialog.en}"
+                  class="sbb-dialog__close"
+                  icon-name="cross-small"
+                  sbb-dialog-close=""
+                  size="m"
+                  type="button"
+                  variant="secondary">
+                </sbb-button>
               </div>
-            </dialog>
-          </div>
-        </mock:shadow-root>
-      </sbb-dialog>
-    `);
+              <div class="sbb-dialog__content">
+                <slot></slot>
+              </div>
+            </div>
+          </dialog>
+        </div>
+      `,
+    );
   });
 });
