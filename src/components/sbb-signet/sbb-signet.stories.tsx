fc09586465716e8e876f8c4eaff3bf3fe4afa85b
Jeremias Peier <jeremias.peier@sbb.ch>
2023-10-05 16:42:02 +0200

    refactor: migrate sbb-signet

--- src/components/sbb-signet/sbb-signet.stories.tsx
+++ src/components/sbb-signet/sbb-signet.stories.tsx
@@ -1,8 +1,9 @@
 /** @jsx h */
 import { h, JSX } from 'jsx-dom';
 import readme from './readme.md';
-import type { Meta, StoryObj, ArgTypes, Args } from '@storybook/html';
+import type { Meta, StoryObj, ArgTypes, Args } from '@storybook/web-components';
 import type { InputType } from '@storybook/types';
+import './sbb-signet';
 
 const Template = (args): JSX.Element => <sbb-signet {...args} />;
 
