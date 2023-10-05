e3039d86e2b45b4bc50753d221cf80345bb32218
Lukas Spirig <lukas.spirig@sbb.ch>
2023-10-05 12:01:24 +0200

    build: fix typo

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -52,7 +52,7 @@ export default {
       // Excluding the dependencies, prevents this from happening at the cost of slightly
       // increased test times.
       optimizeDeps: {
-        disable: true,
+        disabled: true,
       },
     }),
   ],
