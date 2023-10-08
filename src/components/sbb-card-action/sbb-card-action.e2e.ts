a91abc6f186a8f4d51e6f7a4440b95805911575a
Marco D'Auria <marco.dauria@finconsgroup.com>
2023-10-08 22:40:55 +0200

    refactor: card action migration to Lit

--- src/components/sbb-card-action/sbb-card-action.e2e.ts
+++ src/components/sbb-card-action/sbb-card-action.e2e.ts
@@ -1,222 +1,223 @@
-import { E2EElement, E2EPage, newE2EPage } from '@stencil/core/testing';
 import { waitForCondition } from '../../global/testing';
+import { expect, fixture } from '@open-wc/testing';
+import { html } from 'lit/static-html.js';
+import { sendKeys } from '@web/test-runner-commands';
+import { EventSpy } from '../../global/testing/event-spy';
+import { SbbCard } from '../sbb-card/sbb-card';
+import './sbb-card-action';
+import { SbbCardAction } from './sbb-card-action';
 
 // As tests don't work in specs at all (missing :is support in Jest), we moved all tests to e2e.
 
 describe('sbb-card-action', () => {
-  let element: E2EElement, page: E2EPage;
+  let element: SbbCard;
 
   it('should render an sbb-card-action as a link opening in a new window', async () => {
-    page = await newE2EPage();
-    await page.setContent(
-      `
-        <sbb-card>
-          <sbb-card-action href="https://github.com/lyne-design-system/lyne-components" target="_blank">Follow me</sbb-card-action>
-          Content text
-        </sbb-card>`,
+    element = await fixture(
+      html` <sbb-card>
+        <sbb-card-action
+          href="https://github.com/lyne-design-system/lyne-components"
+          target="_blank"
+          >Follow me</sbb-card-action
+        >
+        Content text
+      </sbb-card>`,
     );
-    const card = await page.find('sbb-card');
 
-    await page.waitForChanges();
-
-    expect(card).toHaveAttribute('data-has-action');
-    expect(card).not.toHaveAttribute('data-has-active-action');
-    expect(card).toEqualAttribute('data-action-role', 'link');
-    expect(await card.find('sbb-card-action')).toEqualHtml(`
-      <sbb-card-action href="https://github.com/lyne-design-system/lyne-components" target="_blank" role="link" dir="ltr" tabindex="0" slot="action" class="hydrated">
-        <mock:shadow-root>
-          <a class="sbb-card-action" href="https://github.com/lyne-design-system/lyne-components" target="_blank" rel="external noopener nofollow" role="presentation" tabindex="-1">
-            <span class="sbb-card-action__label">
-              <slot></slot>
-              . Link target opens in new window.
-            </span>
-          </a>
-        </mock:shadow-root>
+    expect(element).to.have.attribute('data-has-action');
+    expect(element).not.to.have.attribute('data-has-active-action');
+    expect(element).to.have.attribute('data-action-role', 'link');
+    expect(element.querySelector('sbb-card-action')).dom.to.be.equal(`
+      <sbb-card-action href="https://github.com/lyne-design-system/lyne-components" target="_blank" role="link" dir="ltr" tabindex="0" slot="action">
         Follow me
       </sbb-card-action>
+    `);
+    expect(element.querySelector('sbb-card-action')).shadowDom.to.be.equal(`
+      <a class="sbb-card-action" href="https://github.com/lyne-design-system/lyne-components" target="_blank" rel="external noopener nofollow" role="presentation" tabindex="-1">
+        <span class="sbb-card-action__label">
+          <slot></slot>
+          . Link target opens in new window.
+        </span>
+      </a>
     `);
   });
 
   it('should render an sbb-card-action as a button which is active', async () => {
-    page = await newE2EPage();
-    await page.setContent(
-      `<sbb-card><sbb-card-action active>Click me</sbb-card-action>Content</sbb-card>`,
+    element = await fixture(
+      html`<sbb-card><sbb-card-action active>Click me</sbb-card-action>Content</sbb-card>`,
     );
-    const card = await page.find('sbb-card');
-    await page.waitForChanges();
 
-    expect(card).toHaveAttribute('data-has-action');
-    expect(card).toHaveAttribute('data-has-active-action');
-    expect(card).toEqualAttribute('data-action-role', 'button');
-    expect(await card.find('sbb-card-action')).toEqualHtml(`
-      <sbb-card-action role="button" dir="ltr" tabindex="0" slot="action" active class="hydrated">
-        <mock:shadow-root>
-          <span class="sbb-card-action">
-            <span class="sbb-card-action__label">
-              <slot></slot>
-            </span>
-          </span>
-        </mock:shadow-root>
+    expect(element).to.have.attribute('data-has-action');
+    expect(element).to.have.attribute('data-has-active-action');
+    expect(element).to.have.attribute('data-action-role', 'button');
+    expect(element.querySelector('sbb-card-action')).dom.to.be.equal(`
+      <sbb-card-action role="button" dir="ltr" tabindex="0" slot="action" active>
         Click me
       </sbb-card-action>
+    `);
+    expect(element.querySelector('sbb-card-action')).shadowDom.to.be.equal(`
+      <span class="sbb-card-action">
+        <span class="sbb-card-action__label">
+          <slot></slot>
+        </span>
+      </span>
     `);
   });
 
   it('should correctly toggle active state', async () => {
-    page = await newE2EPage();
-    await page.setContent(
-      `<sbb-card><sbb-card-action>Click me</sbb-card-action>Content</sbb-card>`,
+    element = await fixture(
+      html`<sbb-card><sbb-card-action>Click me</sbb-card-action>Content</sbb-card>`,
     );
-    const card = await page.find('sbb-card');
-    await page.waitForChanges();
+    expect(element).not.to.have.attribute('data-has-active-action');
 
-    expect(card).not.toHaveAttribute('data-has-active-action');
+    element.querySelector('sbb-card-action').setAttribute('active', '');
+    await element.updateComplete;
 
-    (await card.find('sbb-card-action')).setAttribute('active', '');
-    await page.waitForChanges();
-
-    expect(card).toHaveAttribute('data-has-active-action');
+    expect(element).to.have.attribute('data-has-active-action');
   });
 
   it('should remove data properties from host', async () => {
-    page = await newE2EPage();
-    await page.setContent(
-      `<sbb-card><sbb-card-action active>Click me</sbb-card-action><span><button>Content</button></span></sbb-card>`,
+    element = await fixture(
+      html`<sbb-card
+        ><sbb-card-action active>Click me</sbb-card-action
+        ><span><button>Content</button></span></sbb-card
+      >`,
     );
-    const card = await page.find('sbb-card');
 
-    await page.waitForChanges();
-
-    expect(card).toHaveAttribute('data-has-action');
-    expect(card).toHaveAttribute('data-has-active-action');
-    expect(card).toEqualAttribute('data-action-role', 'button');
+    expect(element).to.have.attribute('data-has-action');
+    expect(element).to.have.attribute('data-has-active-action');
+    expect(element).to.have.attribute('data-action-role', 'button');
 
     // Remove action from DOM
-    await page.evaluate(() => document.querySelector('sbb-card-action').remove());
-    await page.waitForChanges();
+    element.querySelector('sbb-card-action').remove();
+    await element.updateComplete;
 
-    expect(card).not.toHaveAttribute('data-has-action');
-    expect(card).not.toHaveAttribute('data-has-active-action');
-    expect(card).not.toEqualAttribute('data-action-role', 'button');
+    expect(element).not.to.have.attribute('data-has-action');
+    expect(element).not.to.have.attribute('data-has-active-action');
+    expect(element).not.to.have.attribute('data-action-role', 'button');
   });
 
   it('should detect added button in slotted content to update focusable elements', async () => {
-    page = await newE2EPage();
-    await page.setContent(
-      `<sbb-card><sbb-card-action>Click me</sbb-card-action><span id='content'><button>Content</button></span></sbb-card>`,
+    element = await fixture(
+      html`<sbb-card
+        ><sbb-card-action>Click me</sbb-card-action
+        ><span id="content"><button>Content</button></span></sbb-card
+      >`,
     );
-    await page.waitForChanges();
-    expect(await page.find('button')).toHaveAttribute('data-card-focusable');
+    expect(document.querySelector('button')).to.have.attribute('data-card-focusable');
 
     // Add a second button in content
-    await page.evaluate(() =>
-      document
-        .getElementById('content')
-        .insertBefore(document.createElement('button'), document.querySelector('button')),
-    );
+    document
+      .getElementById('content')
+      .insertBefore(document.createElement('button'), document.querySelector('button'));
 
     // Both buttons should be marked as focusable
-    const buttons = await page.findAll('button');
-    expect(buttons.length).toBe(2);
-    expect(buttons.every((el) => el.getAttribute('data-card-focusable') !== null)).toBe(true);
+    await element.updateComplete;
+    const buttons = document.querySelectorAll('button');
+    expect(buttons.length).to.be.equal(2);
+    expect(
+      Array.from(buttons).every((el) => el.getAttribute('data-card-focusable') !== null),
+    ).to.be.equal(true);
 
     // Remove all buttons
-    await page.evaluate(() => document.querySelectorAll('button').forEach((el) => el.remove()));
-    await page.waitForChanges();
+    document.querySelectorAll('button').forEach((el) => el.remove());
+    await element.updateComplete;
 
     // Card should not have marker anymore
-    expect((await page.findAll('button')).length).toBe(0);
+    expect(document.querySelectorAll('button').length).to.be.equal(0);
   });
 
   it('should detect added second element of slot to update focusable elements', async () => {
-    page = await newE2EPage();
-    await page.setContent(
-      `<sbb-card><sbb-card-action>Click me</sbb-card-action><span id='content'></span></sbb-card>`,
+    element = await fixture(
+      html`<sbb-card
+        ><sbb-card-action>Click me</sbb-card-action><span id="content"></span
+      ></sbb-card>`,
     );
-    await page.waitForChanges();
 
     // Add a button to slot
-    await page.evaluate(() =>
-      document
-        .querySelector('sbb-card')
-        .insertBefore(document.createElement('button'), document.getElementById('content')),
-    );
-    await page.waitForChanges();
+    document
+      .querySelector('sbb-card')
+      .insertBefore(document.createElement('button'), document.getElementById('content'));
+    await element.updateComplete;
 
     // Button should be marked as focusable
-    expect(await page.find('button')).toHaveAttribute('data-card-focusable');
+    expect(document.querySelector('button')).to.have.attribute('data-card-focusable');
   });
 
   it('should detect focusable elements when action was added at later point', async () => {
-    page = await newE2EPage();
-    await page.setContent(`<sbb-card><span id='content'><button></button></span></sbb-card>`);
-    await page.waitForChanges();
+    element = await fixture(
+      html`<sbb-card
+        ><span id="content"><button></button></span
+      ></sbb-card>`,
+    );
 
     // Add a sbb-card-action
-    await page.evaluate(() =>
-      document.querySelector('sbb-card').appendChild(document.createElement('sbb-card-action')),
-    );
-    await page.waitForChanges();
+    document.querySelector('sbb-card').appendChild(document.createElement('sbb-card-action'));
+    await element.updateComplete;
 
     // Button should be marked as focusable
-    expect(await page.find('button')).toHaveAttribute('data-card-focusable');
+    expect(document.querySelector('button')).to.have.attribute('data-card-focusable');
   });
 
   describe('events', () => {
-    beforeEach(async () => {
-      page = await newE2EPage();
-      await page.setContent(
-        '<sbb-card><sbb-card-action id="focus-id">Card</sbb-card-action>Content</sbb-card>',
-      );
+    let action: SbbCardAction;
 
-      element = await page.find('sbb-card-action');
+    beforeEach(async () => {
+      element = await fixture(
+        html`<sbb-card><sbb-card-action id="focus-id">Card</sbb-card-action>Content</sbb-card>`,
+      );
+      action = document.querySelector('sbb-card-action');
     });
 
     it('dispatches event on click', async () => {
-      await page.waitForChanges();
-      const changeSpy = await page.spyOnEvent('click');
+      await element.updateComplete;
+      const changeSpy = new EventSpy('click');
 
-      await element.click();
+      action.click();
 
       await waitForCondition(() => changeSpy.events.length === 1);
-      expect(changeSpy).toHaveReceivedEventTimes(1);
+      expect(changeSpy.count).to.be.equal(1);
     });
 
     it('should dispatch click event on pressing Enter', async () => {
-      const changeSpy = await page.spyOnEvent('click');
-      await element.press('Enter');
-      expect(changeSpy).toHaveReceivedEvent();
+      const changeSpy = new EventSpy('click');
+      action.focus();
+      await sendKeys({ press: 'Enter' });
+      expect(changeSpy.count).to.be.greaterThan(0);
     });
 
     it('should dispatch click event on pressing Space', async () => {
-      const changeSpy = await page.spyOnEvent('click');
-      await element.press(' ');
-      expect(changeSpy).toHaveReceivedEvent();
+      const changeSpy = new EventSpy('click');
+      action.focus();
+      await sendKeys({ press: ' ' });
+      expect(changeSpy.count).to.be.greaterThan(0);
     });
 
     it('should dispatch click event on pressing Enter with href', async () => {
       element.setAttribute('href', 'test');
-      await page.waitForChanges();
+      await element.updateComplete;
 
-      const changeSpy = await page.spyOnEvent('click');
-      await element.press('Enter');
-      expect(changeSpy).toHaveReceivedEvent();
+      const changeSpy = new EventSpy('click');
+      action.focus();
+      await sendKeys({ press: 'Enter' });
+      expect(changeSpy.count).to.be.greaterThan(0);
     });
 
     it('should not dispatch click event on pressing Space with href', async () => {
-      element.setAttribute('href', 'test');
-      await page.waitForChanges();
+      action.setAttribute('href', 'test');
+      await element.updateComplete;
 
-      const changeSpy = await page.spyOnEvent('click');
-      await element.press(' ');
-      expect(changeSpy).not.toHaveReceivedEvent();
+      const changeSpy = new EventSpy('click');
+      action.focus();
+      await sendKeys({ press: ' ' });
+      expect(changeSpy.count).not.to.be.greaterThan(0);
     });
 
     it('should receive focus', async () => {
-      await element.focus();
-      await page.waitForChanges();
+      action.focus();
+      await element.updateComplete;
 
-      expect(await page.evaluate(() => document.activeElement.id)).toBe('focus-id');
+      expect(document.activeElement.id).to.be.equal('focus-id');
     });
   });
 });
