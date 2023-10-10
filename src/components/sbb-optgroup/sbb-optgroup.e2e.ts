cefbf0df2699ece6b08f4ff7e9abdac376fb578c
TomMenga <tommaso.menga-94@hotmail.it>
2023-10-10 10:44:56 +0200

    refactor(sbb-optgroup): component migration

--- src/components/sbb-optgroup/sbb-optgroup.e2e.ts
+++ src/components/sbb-optgroup/sbb-optgroup.e2e.ts
@@ -1,61 +1,73 @@
-import { E2EElement, E2EPage, newE2EPage } from '@stencil/core/testing';
+import { assert, expect, fixture } from '@open-wc/testing';
+import { html } from 'lit/static-html.js';
+import { SbbOptGroup } from './sbb-optgroup';
+import { SbbOption } from '../sbb-option';
+import '../sbb-option';
 
 describe('sbb-optgroup', () => {
-  let element: E2EElement, page: E2EPage;
+  let element: SbbOptGroup;
 
   beforeEach(async () => {
-    page = await newE2EPage();
-    await page.setContent(`
+    element = await fixture(html`
       <sbb-optgroup label="Group 1">
         <sbb-option id="option-1" value="option-1">Label 1</sbb-option>
         <sbb-option id="option-2" disabled value="option-2">Label 2</sbb-option>
         <sbb-option id="option-3" value="option-3">Label 3</sbb-option>
       </sbb-optgroup>
     `);
-    element = await page.find('sbb-optgroup');
   });
 
   it('renders', async () => {
-    expect(element).toHaveClass('hydrated');
+    assert.instanceOf(element, SbbOptGroup);
   });
 
   it('disabled status is inherited', async () => {
-    element.setAttribute('disabled', 'true');
-    await page.waitForChanges();
-    expect(element).toEqualAttribute('disabled', 'true');
-    const optionOne = await page.find('sbb-optgroup > sbb-option#option-1');
-    expect(optionOne.getAttribute('data-group-disabled')).not.toBeNull();
-    const optionTwo = await page.find('sbb-optgroup > sbb-option#option-2');
-    expect(optionTwo.getAttribute('data-group-disabled')).not.toBeNull();
-    expect(optionTwo.getAttribute('disabled')).not.toBeNull();
-    const optionThree = await page.find('sbb-optgroup > sbb-option#option-3');
-    expect(optionThree.getAttribute('data-group-disabled')).not.toBeNull();
+    const optionOne = document.querySelector('sbb-optgroup > sbb-option#option-1');
+    const optionTwo = document.querySelector('sbb-optgroup > sbb-option#option-2');
+    const optionThree = document.querySelector('sbb-optgroup > sbb-option#option-3');
+    element.setAttribute('disabled', '');
+    await element.updateComplete;
+
+    expect(element).to.have.attribute('disabled');
+    expect(optionOne).to.have.attribute('data-group-disabled');
+    expect(optionTwo).to.have.attribute('data-group-disabled');
+    expect(optionTwo).to.have.attribute('disabled');
+    expect(optionThree).to.have.attribute('data-group-disabled');
+
     element.removeAttribute('disabled');
-    await page.waitForChanges();
-    expect(optionTwo.getAttribute('data-group-disabled')).toBeNull();
-    expect(optionTwo.getAttribute('disabled')).not.toBeNull();
+    await element.updateComplete;
+    expect(optionTwo).not.to.have.attribute('data-group-disabled');
+    expect(optionTwo).to.have.attribute('disabled');
   });
 
   it('disabled status prevents changes', async () => {
-    const optionOne = await page.find('sbb-optgroup > sbb-option#option-1');
-    const optionTwo = await page.find('sbb-optgroup > sbb-option#option-2');
-    const optionThree = await page.find('sbb-optgroup > sbb-option#option-3');
+    const optionOne: SbbOption = document.querySelector('sbb-optgroup > sbb-option#option-1');
+    const optionTwo: SbbOption = document.querySelector('sbb-optgroup > sbb-option#option-2');
+    const optionThree: SbbOption = document.querySelector('sbb-optgroup > sbb-option#option-3');
     const options = [optionOne, optionTwo, optionThree];
-    options.forEach((opt: E2EElement) => expect(opt).toEqualAttribute('selected', null));
-    element.setAttribute('disabled', 'true');
-    await page.waitForChanges();
-    expect(element).toEqualAttribute('disabled', 'true');
-    for (const check of options) {
-      await check.click();
-      expect(check).toEqualAttribute('selected', null);
+
+    options.forEach((opt) => expect(opt).not.to.have.attribute('selected'));
+
+    element.setAttribute('disabled', '');
+    await element.updateComplete;
+    expect(element).to.have.attribute('disabled');
+
+    // clicks should have no effect since the group is disabled
+    for (const opt of options) {
+      opt.click();
+      await opt.updateComplete;
+      expect(opt).not.to.have.attribute('selected');
     }
+
     element.removeAttribute('disabled');
-    await page.waitForChanges();
-    for (const check of options) {
-      await check.click();
+    await element.updateComplete;
+    for (const opt of options) {
+      opt.click();
+      await opt.updateComplete;
     }
-    expect(optionOne).toEqualAttribute('selected', '');
-    expect(optionTwo).toEqualAttribute('selected', null);
-    expect(optionThree).toEqualAttribute('selected', '');
+
+    expect(optionOne).to.have.attribute('selected');
+    expect(optionTwo).not.to.have.attribute('selected');
+    expect(optionThree).to.have.attribute('selected');
   });
 });
--- src/components/sbb-optgroup/sbb-optgroup.spec.ts
+++ src/components/sbb-optgroup/sbb-optgroup.spec.ts
@@ -1,67 +1,76 @@
-import { SbbOptGroup } from './sbb-optgroup';
-import { newSpecPage } from '@stencil/core/testing';
+import { expect, fixture } from '@open-wc/testing';
+import { html } from 'lit/static-html.js';
+import '../sbb-autocomplete';
+import '../sbb-option';
+import './sbb-optgroup';
+import { isSafari } from '../../global/dom';
 
 describe('sbb-optgroup', () => {
   describe('autocomplete', function () {
     it('renders', async () => {
-      const { root } = await newSpecPage({
-        components: [SbbOptGroup],
-        html: `
-          <sbb-autocomplete>
+      const root = (
+        await fixture(html`
+          <sbb-autocomplete origin="anchor">
             <sbb-optgroup label="Label">
               <sbb-option value="1">1</sbb-option>
               <sbb-option value="2">2</sbb-option>
             </sbb-optgroup>
           </sbb-autocomplete>
-          `,
-      });
+          <div id="anchor"></div>
+        `)
+      ).querySelector('sbb-optgroup');
+      const groupRoleAttr = 'aria-disabled="false" aria-label="Label" role="group"';
 
-      expect(root).toEqualHtml(`
-        <sbb-optgroup aria-disabled="false" aria-label="Label" data-variant="autocomplete" label="Label" role="group">
-          <mock:shadow-root>
-            <div class="sbb-optgroup__divider">
-              <sbb-divider></sbb-divider>
-            </div>
-            <div class="sbb-optgroup__label" aria-hidden="true">
-              <div class="sbb-optgroup__icon-space"></div>
-              <span>Label</span>
-            </div>
-            <slot></slot>
-          </mock:shadow-root>
-          <sbb-option value="1">1</sbb-option>
-          <sbb-option value="2">2</sbb-option>
+      expect(root).dom.to.be.equal(`
+        <sbb-optgroup data-variant="autocomplete" label="Label" ${!isSafari() ? groupRoleAttr : ''}>
+          <sbb-option value="1" aria-disabled="false" aria-selected="false" data-variant="autocomplete" id="sbb-option-1" role="option">1</sbb-option>
+          <sbb-option value="2" aria-disabled="false" aria-selected="false" data-variant="autocomplete" id="sbb-option-2" role="option">2</sbb-option>
         </sbb-optgroup>
+      `);
+      expect(root).shadowDom.to.be.equal(`
+        <div class="sbb-optgroup__divider">
+          <sbb-divider aria-orientation="horizontal" orientation="horizontal" role="separator"></sbb-divider>
+        </div>
+        <div class="sbb-optgroup__label" aria-hidden="true">
+          <div class="sbb-optgroup__icon-space"></div>
+          <span>Label</span>
+        </div>
+        <slot></slot>
       `);
     });
 
     it('renders disabled', async () => {
-      const { root } = await newSpecPage({
-        components: [SbbOptGroup],
-        html: `
-          <sbb-autocomplete>
+      const root = (
+        await fixture(html`
+          <sbb-autocomplete origin="anchor">
             <sbb-optgroup label="Label" disabled="true">
               <sbb-option value="1">1</sbb-option>
               <sbb-option value="2">2</sbb-option>
             </sbb-optgroup>
           </sbb-autocomplete>
-        `,
-      });
+          <div id="anchor"></div>
+        `)
+      ).querySelector('sbb-optgroup');
+      const groupRoleAttr = 'aria-disabled="true" aria-label="Label" role="group"';
 
-      expect(root).toEqualHtml(`
-        <sbb-optgroup disabled="true" aria-disabled="true" aria-label="Label" data-variant="autocomplete" label="Label" role="group">
-          <mock:shadow-root>
-            <div class="sbb-optgroup__divider">
-              <sbb-divider></sbb-divider>
-            </div>
-            <div class="sbb-optgroup__label" aria-hidden="true">
-              <div class="sbb-optgroup__icon-space"></div>
-              <span>Label</span>
-            </div>
-            <slot></slot>
-          </mock:shadow-root>
-          <sbb-option value="1">1</sbb-option>
-          <sbb-option value="2">2</sbb-option>
+      expect(root).dom.to.be.equal(`
+        <sbb-optgroup disabled="true" data-variant="autocomplete" label="Label" ${
+          !isSafari() ? groupRoleAttr : ''
+        }>
+          <sbb-option value="1" data-group-disabled aria-disabled="true" aria-selected="false" data-variant="autocomplete" id="sbb-option-7" role="option">1</sbb-option>
+          <sbb-option value="2" data-group-disabled aria-disabled="true" aria-selected="false" data-variant="autocomplete" id="sbb-option-8" role="option">2</sbb-option>
         </sbb-optgroup>
+      `);
+
+      expect(root).shadowDom.to.be.equal(`
+        <div class="sbb-optgroup__divider">
+          <sbb-divider aria-orientation="horizontal" orientation="horizontal" role="separator"></sbb-divider>
+        </div>
+        <div class="sbb-optgroup__label" aria-hidden="true">
+          <div class="sbb-optgroup__icon-space"></div>
+          <span>Label</span>
+        </div>
+        <slot></slot>
       `);
     });
   });
