2b04e9dbebf5e68c12e6195f89ff7bf2d04b7f46
TomMenga <tommaso.menga-94@hotmail.it>
2023-10-10 10:44:31 +0200

    refactor(sbb-option): component migration

--- src/components/sbb-option/sbb-option.spec.ts
+++ src/components/sbb-option/sbb-option.spec.ts
@@ -1,63 +1,67 @@
-import { SbbOption } from './sbb-option';
-import { newSpecPage } from '@stencil/core/testing';
+import { expect, fixture } from '@open-wc/testing';
+import { html } from 'lit/static-html.js';
+import '../sbb-autocomplete';
+import './sbb-option';
 
 describe('sbb-option', () => {
   describe('autocomplete', () => {
     it('renders selected and active', async () => {
-      const { root } = await newSpecPage({
-        components: [SbbOption],
-        html: `
-          <sbb-autocomplete>
-            <sbb-option value="1" selected="true" active="true">Option 1</sbb-option>
+      const root = (
+        await fixture(html`
+          <sbb-autocomplete origin="anchor">
+            <sbb-option value="1" selected active>Option 1</sbb-option>
           </sbb-autocomplete>
-        `,
-      });
+          <div id="anchor"></div>
+        `)
+      ).querySelector('sbb-option');
 
-      expect(root).toEqualHtml(`
+      expect(root).dom.to.be.equal(`
         <sbb-option selected="" active="" aria-disabled="false" aria-selected="true" data-variant="autocomplete" id="sbb-option-1" role="option" value="1">
-          <mock:shadow-root>
-            <div class="sbb-option__container">
-              <div class="sbb-option">
-                <span class="sbb-option__icon sbb-option__icon--empty">
-                  <slot name="icon"></slot>
-                </span>
-                <span class="sbb-option__label">
-                  <slot></slot>
-                </span>
-              </div>
-            </div>
-          </mock:shadow-root>
           Option 1
         </sbb-option>
+      `);
+      expect(root).shadowDom.to.be.equal(`
+        <div class="sbb-option__container">
+          <div class="sbb-option">
+            <span class="sbb-option__icon sbb-option__icon--empty">
+              <slot name="icon"></slot>
+            </span>
+            <span class="sbb-option__label">
+              <slot></slot>
+              Option 1
+            </span>
+          </div>
+        </div>
       `);
     });
 
     it('renders disabled', async () => {
-      const { root } = await newSpecPage({
-        components: [SbbOption],
-        html: `
-          <sbb-autocomplete>
-            <sbb-option value="1" disabled="true">Option 1</sbb-option>
+      const root = (
+        await fixture(html`
+          <sbb-autocomplete origin="anchor">
+            <sbb-option value="1" disabled>Option 1</sbb-option>
           </sbb-autocomplete>
-        `,
-      });
+          <div id="anchor"></div>
+        `)
+      ).querySelector('sbb-option');
 
-      expect(root).toEqualHtml(`
-        <sbb-option disabled="true" aria-disabled="true" aria-selected="false" data-variant="autocomplete" id="sbb-option-2" role="option" value="1">
-          <mock:shadow-root>
-            <div class="sbb-option__container">
-              <div class="sbb-option">
-                <span class="sbb-option__icon sbb-option__icon--empty">
-                  <slot name="icon"></slot>
-                </span>
-                <span class="sbb-option__label">
-                  <slot></slot>
-                </span>
-              </div>
-            </div>
-          </mock:shadow-root>
+      expect(root).dom.to.be.equal(`
+        <sbb-option disabled aria-disabled="true" aria-selected="false" data-variant="autocomplete" id="sbb-option-4" role="option" value="1">
           Option 1
         </sbb-option>
+      `);
+      expect(root).shadowDom.to.be.equal(`
+        <div class="sbb-option__container">
+          <div class="sbb-option">
+            <span class="sbb-option__icon sbb-option__icon--empty">
+              <slot name="icon"></slot>
+            </span>
+            <span class="sbb-option__label">
+              <slot></slot>
+              Option 1
+            </span>
+          </div>
+        </div>
       `);
     });
   });
