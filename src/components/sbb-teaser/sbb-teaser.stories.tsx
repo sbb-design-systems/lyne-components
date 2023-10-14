1e5cf6b6f148e3d5193381040bb4b414017e92bc
Marco D'Auria <marco.dauria@finconsgroup.com>
2023-10-15 00:45:56 +0200

    refactor: teaser migration to Lit

--- src/components/sbb-teaser/sbb-teaser.stories.tsx
+++ src/components/sbb-teaser/sbb-teaser.stories.tsx
@@ -2,10 +2,10 @@
 import { h, JSX } from 'jsx-dom';
 import readme from './readme.md?raw';
 import { withActions } from '@storybook/addon-actions/decorator';
-
 import placeholderImage from './stories/placeholder.png';
-import type { Meta, StoryObj, ArgTypes, Args, Decorator } from '@storybook/html';
+import type { Meta, StoryObj, ArgTypes, Args, Decorator } from '@storybook/web-components';
 import type { InputType } from '@storybook/types';
+import './sbb-teaser';
 
 /* ************************************************* */
 /* Storybook controls                                */
