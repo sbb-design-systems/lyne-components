c2263015acd0d27ee9dcf2853ab4c795dcf005e7
TomMenga <tommaso.menga-94@hotmail.it>
2023-10-16 09:38:33 +0200

    refactor(sbb-journey-header): component migration

--- src/components/sbb-journey-header/sbb-journey-header.spec.ts
+++ src/components/sbb-journey-header/sbb-journey-header.spec.ts
@@ -1,69 +1,75 @@
-import { SbbJourneyHeader } from './sbb-journey-header';
-import { newSpecPage } from '@stencil/core/testing';
+import { expect, fixture } from '@open-wc/testing';
+import { html } from 'lit/static-html.js';
+import './sbb-journey-header';
 
 describe('sbb-journey-header', () => {
   it('renders', async () => {
-    const { root } = await newSpecPage({
-      components: [SbbJourneyHeader],
-      html: '<sbb-journey-header origin="A" destination="B"></sbb-journey-header>',
-    });
+    const root = await fixture(
+      html`<sbb-journey-header origin="A" destination="B"></sbb-journey-header>`,
+    );
 
-    expect(root).toEqualHtml(`
+    expect(root).dom.to.be.equal(`
       <sbb-journey-header origin="A" destination="B" size="m">
-        <mock:shadow-root>
-          <sbb-title level="3" visual-level="5">
-            <span class="sbb-journey-header" dir="ltr">
-              <span class="sbb-journey-header__origin">
-                <span class="sbb-journey-header__connection--visually-hidden">
-                  Connection from
-                </span>
-                A
-              </span>
-              <sbb-icon name="arrow-long-right-small"></sbb-icon>
-              <span class="sbb-journey-header__destination">
-                <span class="sbb-journey-header__connection--visually-hidden">
-                  to
-                </span>
-                B
-              </span>
-            </span>
-          </sbb-title>
-        </mock:shadow-root>
       </sbb-journey-header>
+    `);
+    expect(root).shadowDom.to.be.equal(`
+      <sbb-title role="heading" level="3" visual-level="5" aria-level="3">
+        <span class="sbb-journey-header" dir="ltr">
+          <span class="sbb-journey-header__origin">
+            <span class="sbb-journey-header__connection--visually-hidden">
+              Connection from
+            </span>
+            A
+          </span>
+          <sbb-icon name="arrow-long-right-small" aria-hidden="true" data-namespace="default" role="img"></sbb-icon>
+          <span class="sbb-journey-header__destination">
+            <span class="sbb-journey-header__connection--visually-hidden">
+              to
+            </span>
+            B
+          </span>
+        </span>
+      </sbb-title>
     `);
   });
 
   it('renders H1 L-sized round-trip negative', async () => {
-    const { root } = await newSpecPage({
-      components: [SbbJourneyHeader],
-      html: '<sbb-journey-header level="1" size="l" round-trip="true" origin="B" destination="C" negative="true"></sbb-journey-header>',
-    });
+    const root = await fixture(
+      html`<sbb-journey-header
+        level="1"
+        size="l"
+        round-trip="true"
+        origin="B"
+        destination="C"
+        negative="true"
+      ></sbb-journey-header>`,
+    );
 
-    expect(root).toEqualHtml(`
-      <sbb-journey-header level="1"size="l" round-trip="true" origin="B" destination="C" negative="">
-        <mock:shadow-root>
-          <sbb-title level="1" visual-level="4" negative="">
-            <span class="sbb-journey-header" dir="ltr">
-              <span class="sbb-journey-header__origin">
-                <span class="sbb-journey-header__connection--visually-hidden">
-                  Connection from
-                </span>
-                B
-              </span>
-              <sbb-icon name="arrows-long-right-left-small"></sbb-icon>
-              <span class="sbb-journey-header__destination">
-                <span class="sbb-journey-header__connection--visually-hidden">
-                  to
-                </span>
-                C
-                <span class="sbb-journey-header__connection--visually-hidden">
-                  and back to B.
-                </span>
-              </span>
-            </span>
-          </sbb-title>
-        </mock:shadow-root>
+    expect(root).dom.to.be.equal(`
+      <sbb-journey-header level="1"size="l" round-trip="true" origin="B" destination="C" negative>
       </sbb-journey-header>
+    `);
+    expect(root).shadowDom.to.be.equal(`
+      <sbb-title level="1" aria-level="1" visual-level="4" role="heading" negative>
+        <span class="sbb-journey-header" dir="ltr">
+          <span class="sbb-journey-header__origin">
+            <span class="sbb-journey-header__connection--visually-hidden">
+              Connection from
+            </span>
+            B
+          </span>
+          <sbb-icon name="arrows-long-right-left-small" aria-hidden="true" data-namespace="default" role="img"></sbb-icon>
+          <span class="sbb-journey-header__destination">
+            <span class="sbb-journey-header__connection--visually-hidden">
+              to
+            </span>
+            C
+            <span class="sbb-journey-header__connection--visually-hidden">
+              and back to B.
+            </span>
+          </span>
+        </span>
+      </sbb-title>
     `);
   });
 });
--- src/components/sbb-journey-header/sbb-journey-header.tsx
+++ src/components/sbb-journey-header/sbb-journey-header.tsx
@@ -1,85 +1,95 @@
-import { Component, Element, h, JSX, Prop, State } from '@stencil/core';
 import { i18nConnectionFrom, i18nConnectionRoundtrip, i18nConnectionTo } from '../../global/i18n';
-import { InterfaceTitleAttributes } from '../sbb-title/sbb-title.custom';
-import { InterfaceJourneyHeaderAttributes } from './sbb-journey-header.custom';
+import { TitleLevel } from '../sbb-title';
 import { getDocumentWritingMode } from '../../global/dom';
 import {
   documentLanguage,
   HandlerRepository,
   languageChangeHandlerAspect,
 } from '../../global/eventing';
+import { CSSResult, html, LitElement, nothing, TemplateResult } from 'lit';
+import { customElement, property, state } from 'lit/decorators.js';
+import Style from './sbb-journey-header.scss?lit&inline';
+import '../sbb-title';
+import '../sbb-icon';
 
-@Component({
-  shadow: true,
-  styleUrl: 'sbb-journey-header.scss',
-  tag: 'sbb-journey-header',
-})
-export class SbbJourneyHeader {
+export type JourneyHeaderSize = 'm' | 'l';
+
+@customElement('sbb-journey-header')
+export class SbbJourneyHeader extends LitElement {
+  public static override styles: CSSResult = Style;
+
   /** Origin location for the journey header. */
-  @Prop() public origin!: string;
+  @property() public origin!: string;
 
   /** Destination location for the journey header. */
-  @Prop() public destination!: string;
+  @property() public destination!: string;
 
   /** Whether the journey is a round trip. If so, the icon changes to a round-trip one. */
-  @Prop() public roundTrip?: boolean;
+  @property({ attribute: 'round-trip', type: Boolean }) public roundTrip?: boolean;
 
   /** Heading level of the journey header element (e.g. h1-h6). */
-  @Prop() public level?: InterfaceTitleAttributes['level'] = '3';
+  @property() public level?: TitleLevel = '3';
 
   /** Negative coloring variant flag. */
-  @Prop({ reflect: true }) public negative = false;
+  @property({ reflect: true, type: Boolean }) public negative = false;
 
   /** Journey header size. */
-  @Prop({ reflect: true }) public size?: InterfaceJourneyHeaderAttributes['size'] = 'm';
+  @property({ reflect: true }) public size?: JourneyHeaderSize = 'm';
 
-  @State() private _currentLanguage = documentLanguage();
-
-  @Element() private _element!: HTMLElement;
+  @state() private _currentLanguage = documentLanguage();
 
   private _handlerRepository = new HandlerRepository(
-    this._element,
+    this,
     languageChangeHandlerAspect((l) => (this._currentLanguage = l)),
   );
 
-  public connectedCallback(): void {
+  public override connectedCallback(): void {
+    super.connectedCallback();
     this._handlerRepository.connect();
   }
 
-  public disconnectedCallback(): void {
+  public override disconnectedCallback(): void {
+    super.disconnectedCallback();
     this._handlerRepository.disconnect();
   }
 
-  public render(): JSX.Element {
+  protected override render(): TemplateResult {
     const iconName = this.roundTrip ? 'arrows-long-right-left-small' : 'arrow-long-right-small';
 
-    return (
+    return html`
       <sbb-title
-        level={this.level}
-        negative={this.negative}
-        visual-level={this.size === 'l' ? '4' : '5'}
+        level=${this.level}
+        ?negative=${this.negative}
+        visual-level=${this.size === 'l' ? '4' : '5'}
       >
-        <span class="sbb-journey-header" dir={getDocumentWritingMode()}>
+        <span class="sbb-journey-header" dir=${getDocumentWritingMode()}>
           <span class="sbb-journey-header__origin">
             <span class="sbb-journey-header__connection--visually-hidden">
-              {i18nConnectionFrom[this._currentLanguage]}
+              ${i18nConnectionFrom[this._currentLanguage]}
             </span>
-            {this.origin}
+            ${this.origin}
           </span>
-          <sbb-icon name={iconName} />
+          <sbb-icon name=${iconName}></sbb-icon>
           <span class="sbb-journey-header__destination">
             <span class="sbb-journey-header__connection--visually-hidden">
-              {i18nConnectionTo[this._currentLanguage]}
+              ${i18nConnectionTo[this._currentLanguage]}
             </span>
-            {this.destination}
-            {this.roundTrip && (
-              <span class="sbb-journey-header__connection--visually-hidden">
-                {i18nConnectionRoundtrip(this.origin)[this._currentLanguage]}
-              </span>
-            )}
+            ${this.destination}
+            ${this.roundTrip
+              ? html` <span class="sbb-journey-header__connection--visually-hidden">
+                  ${i18nConnectionRoundtrip(this.origin)[this._currentLanguage]}
+                </span>`
+              : nothing}
           </span>
         </span>
       </sbb-title>
-    );
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    'sbb-journey-header': SbbJourneyHeader;
   }
 }
