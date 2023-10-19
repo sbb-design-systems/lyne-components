679665241a64bdb512a86c80cc2a7613c228badd
Jeremias Peier <jeremias.peier@sbb.ch>
2023-10-19 13:48:52 +0200

    fix: refactor revert

--- src/components/sbb-navigation/sbb-navigation.tsx
+++ src/components/sbb-navigation/sbb-navigation.tsx
@@ -282,25 +282,24 @@ export class SbbNavigation implements ComponentInterface {
   }
 
   // Set focus on the first focusable element.
-  private async _setNavigationFocus(): Promise<void> {
+  private _setNavigationFocus(): void {
     const firstFocusable = this._element.shadowRoot.querySelector(
       IS_FOCUSABLE_QUERY,
     ) as HTMLElement;
 
-    // Focusing sbb-navigation__wrapper in order to provide a consistent behavior in Safari where else
-    // the focus-visible styles would be incorrectly applied
-    this._navigationContainerElement.tabIndex = 0;
-    this._navigationContainerElement.focus();
-
-    this._navigationContainerElement.addEventListener(
-      'blur',
-      () => this._navigationContainerElement.removeAttribute('tabindex'),
-      { once: true },
-    );
-
     if (sbbInputModalityDetector.mostRecentModality === 'keyboard') {
-      await new Promise((resolve) => setTimeout(resolve, 0));
       firstFocusable.focus();
+    } else {
+      // Focusing sbb-navigation__wrapper in order to provide a consistent behavior in Safari where else
+      // the focus-visible styles would be incorrectly applied
+      this._navigationContainerElement.tabIndex = 0;
+      this._navigationContainerElement.focus();
+
+      this._navigationContainerElement.addEventListener(
+        'blur',
+        () => this._navigationContainerElement.removeAttribute('tabindex'),
+        { once: true },
+      );
     }
   }
 
