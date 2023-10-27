312d888c83c71a0a0bedec5f6a6516024b6e7501
Federico Iseppon <100614752+federicoisepponfincons@users.noreply.github.com>
2023-10-27 16:41:02 +0200

    fix: spec test

--- src/components/sbb-skiplink-list/sbb-skiplink-list.spec.ts
+++ src/components/sbb-skiplink-list/sbb-skiplink-list.spec.ts
@@ -16,10 +16,9 @@ describe('sbb-skiplink-list', () => {
     expect(root).dom.to.be.equal(
       `
       <sbb-skiplink-list>
-        
-        <sbb-link href='#' id="sbb-skiplink-list-link-0" slot='link-0'>Link 1</sbb-link>
-        <sbb-link href='#' id="sbb-skiplink-list-link-1" slot='link-1'>Link 2</sbb-link>
-        <sbb-link href='#' id="sbb-skiplink-list-link-2" slot='link-2'>Link 3</sbb-link>
+        <sbb-link dir="ltr" href='#' id="sbb-skiplink-list-link-0" negative="" role="link" size="m" slot="link-0" tabindex="0" variant="block">Link 1</sbb-link>
+        <sbb-link dir="ltr" href='#' id="sbb-skiplink-list-link-1" negative="" role="link" size="m" slot="link-1" tabindex="0" variant="block">Link 2</sbb-link>
+        <sbb-link dir="ltr" href='#' id="sbb-skiplink-list-link-2" negative="" role="link" size="m" slot="link-2" tabindex="0" variant="block">Link 3</sbb-link>
       </sbb-skiplink-list>
     `,
     );
@@ -57,17 +56,16 @@ describe('sbb-skiplink-list', () => {
     expect(root).dom.to.be.equal(
       `
       <sbb-skiplink-list title-content="Skip to" title-level="3">
-        
-        <sbb-link href='#' id="sbb-skiplink-list-link-0" slot='link-0'>Link 1</sbb-link>
-        <sbb-link href='#' id="sbb-skiplink-list-link-1" slot='link-1'>Link 2</sbb-link>
-        <sbb-link href='#' id="sbb-skiplink-list-link-2" slot='link-2'>Link 3</sbb-link>
+        <sbb-link dir="ltr" href='#' id="sbb-skiplink-list-link-0" negative="" role="link" size="m" slot="link-0" tabindex="0" variant="block">Link 1</sbb-link>
+        <sbb-link dir="ltr" href='#' id="sbb-skiplink-list-link-1" negative="" role="link" size="m" slot="link-1" tabindex="0" variant="block">Link 2</sbb-link>
+        <sbb-link dir="ltr" href='#' id="sbb-skiplink-list-link-2" negative="" role="link" size="m" slot="link-2" tabindex="0" variant="block">Link 3</sbb-link>
       </sbb-skiplink-list>
     `,
     );
     expect(root).shadowDom.to.be.equal(
       `
           <div class="sbb-skiplink-list__wrapper">
-            <sbb-title id="sbb-skiplink-list-title-id" level="3" negative="" visual-level="5" class="sbb-link-list-title" visually-hidden>
+            <sbb-title aria-level="3" id="sbb-skiplink-list-title-id" level="3" negative="" role="heading" visual-level="5" class="sbb-link-list-title" visually-hidden>
               <slot name="title">
                 Skip to
               </slot>
