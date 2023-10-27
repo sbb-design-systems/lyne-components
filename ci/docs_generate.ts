3f854dc37b1cdc3e585a93de52b14a88b6ff5d81
TomMenga <tommaso.menga-94@hotmail.it>
2023-10-27 10:12:59 +0200

    docs(generate): add 'attribute' column to the 'properties' table

--- ci/docs_generate.ts
+++ ci/docs_generate.ts
@@ -8,6 +8,8 @@ import MagicString from 'magic-string';
 const manifestFilePath = './dist/custom-elements.json';
 const tempFolderPath = './dist/docs';
 const componentsFolder = './src/components';
+const inheritedFromColumnIndex = 6;
+const attributeColumnIndex = 2;
 
 // List of components for which the 'toKebabCase' naming convention is not followed
 const componentsNameMapping: { [key: string]: string } = {
@@ -15,10 +17,48 @@ const componentsNameMapping: { [key: string]: string } = {
 };
 
 function toKebabCase(value: string): string {
-  return value
-    .replace(/([a-z])([A-Z])/g, '$1-$2')
-    .replace(/[\s_]+/g, '-')
-    .toLowerCase();
+  return value.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+}
+
+/**
+ * Add the 'attribute' column.
+ * Removes the 'Inherited from' column.
+ * Replace 'Fields' title with 'Properties'
+ */
+function updateFieldsTable(newDocs: MagicString, sections: RegExpMatchArray[]): void {
+  const fieldsSectionIndex = sections.findIndex((sect) => sect.groups!.name === 'Fields');
+  const startIndex = sections[fieldsSectionIndex]?.index;
+  const endIndex = sections[fieldsSectionIndex + 1]?.index;
+  if (fieldsSectionIndex === -1) {
+    return;
+  }
+
+  // Create a matrix 'table[row][column]' structure for the fields table
+  const fieldsSection = newDocs.original.substring(startIndex!, endIndex);
+  const tableRows = Array.from(fieldsSection.matchAll(/^\|.*\|$/gm))
+    .map((match) => match[0])
+    .map((row) => row.split('|'));
+
+  // Remove the 'Inherited from' column
+  tableRows.forEach((row) => row.splice(inheritedFromColumnIndex, 1));
+
+  // Add the 'attribute' column
+  for (let i = 0; i < tableRows.length; i++) {
+    let attributeColumn: string;
+    if (i === 0) {
+      attributeColumn = tableRows[0][1].replace('Name', 'Attribute');
+    } else {
+      attributeColumn = toKebabCase(tableRows[i][1]);
+    }
+    tableRows[i].splice(attributeColumnIndex, 0, attributeColumn);
+  }
+
+  const fieldsTable = tableRows.map((cols) => cols.join('|')).join('\n');
+  newDocs.update(
+    startIndex!,
+    endIndex ?? newDocs.original.length,
+    `## Properties \n\n${fieldsTable}\n\n`,
+  );
 }
 
 function updateComponentReadme(name: string, docs: string): void {
@@ -28,7 +68,14 @@ function updateComponentReadme(name: string, docs: string): void {
     console.error(`Component ${name} has no readme file, please create it following the template`);
   }
   const newReadme = new MagicString(fs.readFileSync(path, 'utf8'));
-  const newDocs = new MagicString(docs);
+  let newDocs = new MagicString(docs);
+
+  // Remove the details section and commit the change
+  const detailsSectionStart = newDocs.original.match(/<details>/)?.index;
+  newDocs.remove(detailsSectionStart!, newDocs.original.length);
+  newDocs = new MagicString(newDocs.toString());
+
+  const sections = Array.from(newDocs.original.matchAll(/## (?<name>.+)/g));
 
   // Change the generated doc here
   newDocs.prepend('<!-- Auto Generated Below --> \n \n');
@@ -36,12 +83,7 @@ function updateComponentReadme(name: string, docs: string): void {
   // Remove the title
   newDocs.replace(/^# class: `.*`\n/m, '');
 
-  // Replace 'Fields' title with 'Properties'
-  newDocs.replace(/## Fields/, '## Properties');
-
-  // Remove the details section
-  const detailsSectionStart = newDocs.original.match(/<details>/)?.index;
-  newDocs.remove(detailsSectionStart!, newDocs.original.length);
+  updateFieldsTable(newDocs, sections);
 
   // Replace the generated doc in the readme
   const generatedStartIndex =
@@ -52,8 +94,8 @@ function updateComponentReadme(name: string, docs: string): void {
 
 const manifest = JSON.parse(fs.readFileSync(manifestFilePath, 'utf-8'));
 const markdown: string = customElementsManifestToMarkdown(manifest, {
-  headingOffset: -1,
-  private: 'details',
+  headingOffset: -1, // Default heading is '###'
+  private: 'details', // We use 'details' cause it's the only way to remove 'protected' members from the tables
   omitDeclarations: ['exports'],
   omitSections: ['super-class', 'css-properties', 'css-parts', 'main-heading', 'static-fields'],
 });
