80dd070dbd62fae8e32f18f9543147cf01e2ecba
Jeremias Peier <jeremias.peier@sbb.ch>
2023-10-31 12:46:19 +0100

    fix: try logging waitForCondition

--- src/global/testing/wait-for-condition.ts
+++ src/global/testing/wait-for-condition.ts
@@ -4,11 +4,21 @@ export async function waitForCondition(
   timeoutInMilliseconds = 2 * 1000,
 ): Promise<boolean> {
   const start = new Date();
-  while (!(await condition()) && start.getTime() + timeoutInMilliseconds >= new Date().getTime()) {
+  let counter = 0;
+  function checkCondition(): boolean | Promise<boolean> {
+    counter++;
+    return condition();
+  }
+  while (
+    !(await checkCondition()) &&
+    start.getTime() + timeoutInMilliseconds >= new Date().getTime()
+  ) {
     await new Promise((resolve) => setTimeout(resolve, interval));
   }
   if (start.getTime() + timeoutInMilliseconds < new Date().getTime()) {
-    return Promise.reject(`waitForCondition timeout: ${condition.toString()}`);
+    return Promise.reject(
+      `waitForCondition timeout: ${condition.toString()}, attempts: ${counter}, start: ${start.getTime()}, end: ${new Date().getTime()}`,
+    );
   }
   return Promise.resolve(true);
 }
