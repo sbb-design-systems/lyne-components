5a83fabc8095309b88ada7143e1a00947305f466
Jeremias Peier <jeremias.peier@sbb.ch>
2023-11-02 16:14:37 +0100

    fix: skip failing tests

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -12,9 +12,9 @@ const globalCss = sass.compile('./src/components/core/styles/global.scss', { loa
 
 const browsers = isCIEnvironment
   ? [
-      playwrightLauncher({ product: 'chromium', concurrency: 1 }),
-      playwrightLauncher({ product: 'firefox', concurrency: 1 }),
-      playwrightLauncher({ product: 'webkit', concurrency: 1 }),
+      playwrightLauncher({ product: 'chromium' }),
+      playwrightLauncher({ product: 'firefox' }),
+      playwrightLauncher({ product: 'webkit' }),
     ]
   : isDebugMode
   ? [puppeteerLauncher({ concurrency: 1, launchOptions: { headless: false, devtools: true } })]
