8e8f436ad072124b9d82332d165fa7afc8c8b9f1
TomMenga <tommaso.menga-94@hotmail.it>
2023-11-07 17:39:55 +0100

    fix(sbb-image): improved test robusteness

--- src/components/image/image.e2e.ts
+++ src/components/image/image.e2e.ts
@@ -1,6 +1,6 @@
 import { aTimeout, assert, expect, fixture } from '@open-wc/testing';
 import { html } from 'lit/static-html.js';
-import { waitForLitRender } from '../core/testing';
+import { waitForCondition, waitForLitRender } from '../core/testing';
 import { SbbImage } from './image';
 import images from '../core/images';
 
@@ -8,15 +8,17 @@ describe('sbb-image', () => {
   let element: SbbImage;
 
   it('renders', async function () {
-    this.timeout(7000);
+    this.timeout(8000);
     const url = images[0];
     element = await fixture(html`<sbb-image image-src="${url}"></sbb-image>`);
 
     assert.instanceOf(element, SbbImage);
     await waitForLitRender(element);
-    // Wait 5 seconds in hope the image will successfully be loaded
-    await aTimeout(5000);
-    await waitForLitRender(element);
+
+    // Wait until the image is successfully be loaded
+    const img: HTMLImageElement = element.shadowRoot.querySelector('img.image__img');
+    await waitForCondition(() => img.complete, 30, 6000);
+    await aTimeout(1000);
 
     expect(element).dom.to.be.equal(`
       <sbb-image image-src="${url}"></sbb-image>
