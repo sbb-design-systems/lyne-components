f3bbd7cc03124f0f2a2fac92a0fabf8361291381
Davide Mininni <davide.mininni@finconsgroup.com>
2023-11-07 09:47:16 +0100

    test: test improvement

--- src/components/timetable-occupancy-icon/timetable-occupancy-icon.e2e.ts
+++ src/components/timetable-occupancy-icon/timetable-occupancy-icon.e2e.ts
@@ -5,45 +5,77 @@ import { SinonStub, stub } from 'sinon';
 import { SbbTimetableOccupancyIcon } from './timetable-occupancy-icon';
 
 describe('sbb-timetable-occupancy-icon', () => {
+  let matchMediaStub: SinonStub<[query: string], MediaQueryList>;
+  const mediaQueryListArgs: MediaQueryList = {
+    matches: false,
+    media: null,
+    onchange: stub(),
+    addListener: stub(), // deprecated
+    removeListener: stub(), // deprecated
+    addEventListener: stub(),
+    removeEventListener: stub(),
+    dispatchEvent: stub(),
+  };
+
+  beforeEach(() => {
+    matchMediaStub = stub(window, 'matchMedia');
+  });
+
+  afterEach(() => {
+    matchMediaStub.restore();
+  });
+
   it('renders', async () => {
+    matchMediaStub.withArgs('(forced-colors: active)').returns({
+      ...mediaQueryListArgs,
+      matches: false,
+      media: '(forced-colors: active)',
+    });
+    matchMediaStub.withArgs('(prefer-color-scheme: dark)').returns({
+      ...mediaQueryListArgs,
+      matches: false,
+      media: '(prefer-color-scheme: dark)',
+    });
     const element: SbbTimetableOccupancyIcon = await fixture(
       html` <sbb-timetable-occupancy-icon occupancy="LOW"></sbb-timetable-occupancy-icon>`,
     );
     assert.instanceOf(element, SbbTimetableOccupancyIcon);
     expect(element.name).to.equal('utilization-low');
   });
-});
-
-describe('high contrast', () => {
-  beforeEach(() => {
-    const matchMediaStub: SinonStub<[query: string], MediaQueryList> = stub(window, 'matchMedia');
-    matchMediaStub.withArgs('(forced-colors: active)').returns({
-      onchange: stub(),
-      matches: true,
-      media: '(forced-colors: active)',
-      addListener: stub(), // deprecated
-      removeListener: stub(), // deprecated
-      addEventListener: stub(),
-      removeEventListener: stub(),
-      dispatchEvent: stub(),
-    });
-    matchMediaStub.withArgs('(prefer-color-scheme: dark)').returns({
-      onchange: stub(),
-      matches: false,
-      media: '(prefer-color-scheme: dark)',
-      addListener: stub(), // deprecated
-      removeListener: stub(), // deprecated
-      addEventListener: stub(),
-      removeEventListener: stub(),
-      dispatchEvent: stub(),
-    });
-  });
 
   it('renders high contrast mode', async () => {
+    matchMediaStub.withArgs('(forced-colors: active)').returns({
+      ...mediaQueryListArgs,
+      matches: true,
+      media: '(forced-colors: active)',
+    });
+    matchMediaStub.withArgs('(prefer-color-scheme: dark)').returns({
+      ...mediaQueryListArgs,
+      matches: false,
+      media: '(prefer-color-scheme: dark)',
+    });
     const element: SbbTimetableOccupancyIcon = await fixture(
       html` <sbb-timetable-occupancy-icon occupancy="MEDIUM"></sbb-timetable-occupancy-icon>`,
     );
     assert.instanceOf(element, SbbTimetableOccupancyIcon);
     expect(element.name).to.equal(`utilization-medium-high-contrast`);
+  });
+
+  it('renders negative', async () => {
+    matchMediaStub.withArgs('(forced-colors: active)').returns({
+      ...mediaQueryListArgs,
+      matches: false,
+      media: '(forced-colors: active)',
+    });
+    matchMediaStub.withArgs('(prefer-color-scheme: dark)').returns({
+      ...mediaQueryListArgs,
+      matches: true,
+      media: '(prefer-color-scheme: dark)',
+    });
+    const element: SbbTimetableOccupancyIcon = await fixture(
+      html` <sbb-timetable-occupancy-icon occupancy="MEDIUM"></sbb-timetable-occupancy-icon>`,
+    );
+    assert.instanceOf(element, SbbTimetableOccupancyIcon);
+    expect(element.name).to.equal(`utilization-medium-negative`);
   });
 });
