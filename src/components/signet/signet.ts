1ec9ecae27c44a63d37d5d041ae322c9b6cb57a2
Lukas Spirig <lukas.spirig@sbb.ch>
2023-11-17 17:27:20 +0100

    fix(sbb-log,sbb-signet): fix svg templating

--- src/components/signet/signet.ts
+++ src/components/signet/signet.ts
@@ -1,4 +1,4 @@
-import { CSSResultGroup, html, LitElement, nothing, TemplateResult } from 'lit';
+import { CSSResultGroup, html, LitElement, nothing, svg, TemplateResult } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
 import { SbbProtectiveRoom } from '../core/interfaces';
@@ -24,7 +24,7 @@ export class SbbSignet extends LitElement {
       <span class="sbb-signet">
         <span class="sbb-signet__svg-container">
           <svg focusable="false" viewBox="0 0 80 40" xmlns="http://www.w3.org/2000/svg">
-            ${this.accessibilityLabel ? html`<title>${this.accessibilityLabel}</title>` : nothing}
+            ${this.accessibilityLabel ? svg`<title>${this.accessibilityLabel}</title>` : nothing}
             <path
               id="sbb-signet__icon"
               d="M20.0265 40H31.6821L16 24.6154H35.3907V40H44.6093V24.6154H64.106L48.4238 40H60.0795L80 20.0531L60.0795 0H48.4238L64.106 15.3846H44.6093V0H35.3907V15.3846H16L31.6821 0H20.0265L0 20.0531L20.0265 40Z"
