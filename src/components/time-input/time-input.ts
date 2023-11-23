b355f2bf2de8ffc7a4b3253565814ac67ebf2876
TomMenga <tommaso.menga-94@hotmail.it>
2023-11-23 12:48:07 +0100

    chore(time-input): add 'todo' comment

--- src/components/time-input/time-input.ts
+++ src/components/time-input/time-input.ts
@@ -94,6 +94,7 @@ export class SbbTimeInput extends LitElement {
   }
 
   /** Gets the input value with the correct date format. */
+  // TODO: refactor this to be a get/set
   public getValueAsDate(): Date | null {
     return this._formatValueAsDate(this._parseInput(this._inputElement?.value));
   }
