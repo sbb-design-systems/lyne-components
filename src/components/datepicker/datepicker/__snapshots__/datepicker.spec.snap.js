abc691566c774d13b7a1cec30df9d42c808cf9a5
Jeremias Peier <jeremias.peier@sbb.ch>
2023-12-06 15:12:22 +0100

    fix: add snapshot

---
+++ src/components/datepicker/datepicker/__snapshots__/datepicker.spec.snap.js
@@ -0,0 +1,12 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["sbb-datepicker renders"] = 
+`<p
+  id="status-container"
+  role="status"
+>
+</p>
+`;
+/* end snapshot sbb-datepicker renders */
+
