b9f5d03af87f92a8e314ed8e339f14d688364e1d
TomMenga <tommaso.menga-94@hotmail.it>
2023-12-18 14:26:56 +0100

    refactor(tab-title): now it uses the new 'NamedSlotStateController'

--- src/components/tabs/tab-title/tab-title.spec.ts
+++ src/components/tabs/tab-title/tab-title.spec.ts
@@ -10,17 +10,7 @@ describe('sbb-tab-title', () => {
     const root = await fixture(html`<sbb-tab-title></sbb-tab-title>`);
 
     expect(root).dom.to.be.equal(`<sbb-tab-title></sbb-tab-title>`);
-    expect(root).shadowDom.to.be.equal(
-      `
-        <div class="sbb-tab-title__wrapper">
-          <h1 class="sbb-tab-title">
-            <span class="sbb-tab-title__text">
-              <slot></slot>
-            </span>
-          </h1>
-        </div>
-      `,
-    );
+    await expect(root).shadowDom.to.be.equalSnapshot();
   });
 
   it('renders correctly an H2 heading tag', async () => {
@@ -31,46 +21,13 @@ describe('sbb-tab-title', () => {
     await waitForLitRender(root);
 
     expect(root).dom.to.be.equal(`<sbb-tab-title level="2" icon-name="pie-small"></sbb-tab-title>`);
-    expect(root).shadowDom.to.be.equal(
-      `
-        <div class="sbb-tab-title__wrapper">
-          <h2 class="sbb-tab-title">
-            <span class="sbb-tab-title__icon">
-              <slot name="icon">
-                <sbb-icon
-                  aria-hidden="true"
-                  data-namespace="default"
-                  name="pie-small"
-                  role="img"
-                ></sbb-icon>
-              </slot>
-            </span>
-            <span class="sbb-tab-title__text">
-              <slot></slot>
-            </span>
-          </h2>
-          </div>
-      `,
-    );
+    await expect(root).shadowDom.to.be.equalSnapshot();
   });
 
   it('renders an H1 heading tag if the provided level is greater than 6', async () => {
     const root = await fixture(html`<sbb-tab-title level="7" amount="78"></sbb-tab-title>`);
 
     expect(root).dom.to.be.equal(`<sbb-tab-title level="7" amount="78"></sbb-tab-title>`);
-    expect(root).shadowDom.to.be.equal(
-      `
-        <div class="sbb-tab-title__wrapper">
-          <h1 class="sbb-tab-title">
-            <span class="sbb-tab-title__text">
-              <slot></slot>
-            </span>
-            <span class="sbb-tab-title__amount">
-              <slot name="amount">78</slot>
-            </span>
-          </h1>
-        </div>
-      `,
-    );
+    await expect(root).shadowDom.to.be.equalSnapshot();
   });
 });
