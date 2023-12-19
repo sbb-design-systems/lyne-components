8a361684bf600d73dd9685325aaf2ac595c7a31d
TomMenga <tommaso.menga-94@hotmail.it>
2023-12-19 12:34:23 +0100

    refactor(skiplink-list): now it uses the new 'NamedSlotStateController'

--- src/components/skiplink-list/skiplink-list.spec.ts
+++ src/components/skiplink-list/skiplink-list.spec.ts
@@ -16,33 +16,14 @@ describe('sbb-skiplink-list', () => {
 
     expect(root).dom.to.be.equal(
       `
-      <sbb-skiplink-list>
+      <sbb-skiplink-list data-slot-names="link-0 link-1 link-2">
         <sbb-link dir="ltr" href='#' id="sbb-skiplink-list-link-0" negative="" role="link" size="m" slot="link-0" tabindex="0" variant="block">Link 1</sbb-link>
         <sbb-link dir="ltr" href='#' id="sbb-skiplink-list-link-1" negative="" role="link" size="m" slot="link-1" tabindex="0" variant="block">Link 2</sbb-link>
         <sbb-link dir="ltr" href='#' id="sbb-skiplink-list-link-2" negative="" role="link" size="m" slot="link-2" tabindex="0" variant="block">Link 3</sbb-link>
       </sbb-skiplink-list>
     `,
     );
-    expect(root).shadowDom.to.be.equal(
-      `
-          <div class="sbb-skiplink-list__wrapper">
-            <ul class="sbb-skiplink-list">
-              <li>
-                <slot name="link-0"></slot>
-              </li>
-              <li>
-                <slot name="link-1"></slot>
-              </li>
-              <li>
-                <slot name="link-2"></slot>
-              </li>
-            </ul>
-            <span hidden>
-              <slot></slot>
-            </span>
-          </div>
-        `,
-    );
+    await expect(root).shadowDom.to.be.equalSnapshot();
   });
 
   it('renders with title', async () => {
@@ -56,38 +37,14 @@ describe('sbb-skiplink-list', () => {
 
     expect(root).dom.to.be.equal(
       `
-      <sbb-skiplink-list title-content="Skip to" title-level="3">
+      <sbb-skiplink-list title-content="Skip to" title-level="3" data-slot-names="link-0 link-1 link-2">
         <sbb-link dir="ltr" href='#' id="sbb-skiplink-list-link-0" negative="" role="link" size="m" slot="link-0" tabindex="0" variant="block">Link 1</sbb-link>
         <sbb-link dir="ltr" href='#' id="sbb-skiplink-list-link-1" negative="" role="link" size="m" slot="link-1" tabindex="0" variant="block">Link 2</sbb-link>
         <sbb-link dir="ltr" href='#' id="sbb-skiplink-list-link-2" negative="" role="link" size="m" slot="link-2" tabindex="0" variant="block">Link 3</sbb-link>
       </sbb-skiplink-list>
     `,
     );
-    expect(root).shadowDom.to.be.equal(
-      `
-          <div class="sbb-skiplink-list__wrapper">
-            <sbb-title aria-level="3" id="sbb-skiplink-list-title-id" level="3" negative="" role="heading" visual-level="5" class="sbb-link-list-title" visually-hidden>
-              <slot name="title">
-                Skip to
-              </slot>
-            </sbb-title>
-            <ul class="sbb-skiplink-list" aria-labelledby="sbb-skiplink-list-title-id">
-              <li>
-                <slot name="link-0"></slot>
-              </li>
-              <li>
-                <slot name="link-1"></slot>
-              </li>
-              <li>
-                <slot name="link-2"></slot>
-              </li>
-            </ul>
-            <span hidden>
-              <slot></slot>
-            </span>
-          </div>
-        `,
-    );
+    await expect(root).shadowDom.to.be.equalSnapshot();
   });
 
   it('should render named slots if data-ssr-child-count attribute is set', async () => {
