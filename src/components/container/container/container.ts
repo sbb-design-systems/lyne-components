520d1993e76aad901f2b005958d747fc130058c9
Mario Castigliano <mario.castigliano@outlook.it>
2024-01-26 17:21:15 +0100

    fix: background color on wider screens

--- src/components/container/container/container.ts
+++ src/components/container/container/container.ts
@@ -45,10 +45,12 @@ export class SbbContainerElement extends LitElement {
 
   protected override render(): TemplateResult {
     return html`
-      <div class="sbb-container">
-        <slot></slot>
+      <div class="sbb-container__wrapper">
+        <div class="sbb-container">
+          <slot></slot>
+        </div>
+        <slot name="sticky-bar"></slot>
       </div>
-      <slot name="sticky-bar"></slot>
     `;
   }
 }
