32a6c090911b0ca1b1c7f9f4be76dc84c599adf7
Jeremias Peier <jeremias.peier@sbb.ch>
2024-01-30 15:13:43 +0100

    feat(sbb-screenreader-only): initial implementation

---
+++ src/components/screenreader-only/__snapshots__/screenreader-only.spec.snap.js
@@ -0,0 +1,17 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["with Light DOM"] = 
+`<sbb-screenreader-only>
+</sbb-screenreader-only>
+`;
+/* end snapshot with Light DOM */
+
+snapshots["with Shadow DOM"] = 
+`<span>
+  <slot>
+  </slot>
+</span>
+`;
+/* end snapshot with Shadow DOM */
+
