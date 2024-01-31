c34fcc657b38944d34b77c95e30d0e0407d22b6b
Mario Castigliano <mario.castigliano@finconsgroup.com>
2024-01-31 15:45:20 +0100

    fix: tests

--- src/components/container/sticky-bar/sticky-bar.spec.ts
+++ src/components/container/sticky-bar/sticky-bar.spec.ts
@@ -6,9 +6,7 @@ describe('sbb-sticky-bar', () => {
   it('renders', async () => {
     const root = await fixture(html`<sbb-sticky-bar></sbb-sticky-bar>`);
 
-    expect(root).dom.to.be.equal(
-      `<sbb-sticky-bar slot="sticky-bar" data-disable-animation=""></sbb-sticky-bar>`,
-    );
+    expect(root).dom.to.be.equal(`<sbb-sticky-bar slot="sticky-bar"></sbb-sticky-bar>`);
 
     expect(root).shadowDom.to.be.equal(`
       <div class="sbb-sticky-bar">
