1d8c3873d47518afb88220b2f4ffb1ac21d79105
Davide Mininni <davide.mininni@finconsgroup.com>
2024-02-21 09:18:43 +0100

    test: missing import

--- src/components/popover/popover/popover.e2e.ts
+++ src/components/popover/popover/popover.e2e.ts
@@ -5,6 +5,7 @@ import { html } from 'lit/static-html.js';
 import type { SbbButtonElement } from '../../button';
 import { waitForCondition, waitForLitRender, EventSpy } from '../../core/testing';
 
+import '../../button';
 import '../../link';
 import { SbbPopoverElement } from './popover';
 
