c3f8f4160b6ee8ebd256d865821a69326c1cdb4d
Mario Castigliano <mario.castigliano@finconsgroup.com>
2024-02-22 11:06:52 +0100

    fix: tests

--- src/components/teaser-paid/teaser-paid.e2e.ts
+++ src/components/teaser-paid/teaser-paid.e2e.ts
@@ -2,7 +2,6 @@ import { assert, expect, fixture } from '@open-wc/testing';
 import { html } from 'lit/static-html.js';
 
 import type { SbbChipElement } from '../chip';
-import type { SbbImageElement } from '../image';
 
 import { SbbTeaserPaidElement } from './teaser-paid';
 
@@ -27,9 +26,7 @@ describe('sbb-teaser-paid', () => {
 
   it('styles slotted components', async () => {
     const chip = element.querySelector<SbbChipElement>('sbb-chip')!;
-    const image = element.querySelector<SbbImageElement>('sbb-image')!;
 
-    expect(image.borderRadius).to.be.equal('none');
     expect(chip).to.have.attribute('color', 'charcoal');
   });
 });
