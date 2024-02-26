b5e2818781a253db504dda20adb54a6235fdc6ee
Mario Castigliano <mario.castigliano@finconsgroup.com>
2024-02-26 16:15:18 +0100

    test: add tests for slotted image

--- src/components/teaser-paid/teaser-paid.e2e.ts
+++ src/components/teaser-paid/teaser-paid.e2e.ts
@@ -2,6 +2,7 @@ import { assert, expect, fixture } from '@open-wc/testing';
 import { html } from 'lit/static-html.js';
 
 import type { SbbChipElement } from '../chip';
+import type { SbbImageElement } from '../image';
 
 import { SbbTeaserPaidElement } from './teaser-paid';
 
@@ -26,7 +27,12 @@ describe('sbb-teaser-paid', () => {
 
   it('styles slotted components', async () => {
     const chip = element.querySelector<SbbChipElement>('sbb-chip')!;
+    const figure = element
+      .querySelector<SbbImageElement>('sbb-image')!
+      .shadowRoot?.querySelector('figure');
 
     expect(chip).to.have.attribute('color', 'charcoal');
+    expect(figure).to.have.class('image__figure--no-radius');
+    expect(figure).to.have.class('image__figure--teaser');
   });
 });
