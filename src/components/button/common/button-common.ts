964ab421bcbfb67061fe13dbe4970bae857e92c7
Jeri Peier <jeremias.peier@sbb.ch>
2024-03-13 07:55:26 +0100

    fix: fix unresolved sass imports (#2483)

--- src/components/button/common/button-common.ts
+++ src/components/button/common/button-common.ts
@@ -16,6 +16,12 @@ import {
 
 import '../../icon';
 
+export { default as buttonCommonStyle } from './button-common.scss?lit&inline';
+export { default as buttonPrimaryStyle } from './primary-button.scss?lit&inline';
+export { default as buttonSecondaryStyle } from './secondary-button.scss?lit&inline';
+export { default as buttonTertiaryStyle } from './tertiary-button.scss?lit&inline';
+export { default as buttonTransparentStyle } from './transparent-button.scss?lit&inline';
+
 export type SbbButtonCommonElement = SbbButtonCommonElementMixinType & SbbActionBaseElement;
 
 export type SbbButtonSize = 'l' | 'm';
