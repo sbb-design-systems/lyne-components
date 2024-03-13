2228a0255cdd205c3891dc6a12278e39584fc793
Jeri Peier <jeremias.peier@sbb.ch>
2024-03-13 10:36:21 +0100

    fix: fix button dts files (#2485)

--- src/components/vite.config.ts
+++ src/components/vite.config.ts
@@ -35,7 +35,20 @@ export default defineConfig((config) =>
             dts({
               entryRoot: '.',
               include: `**/*.ts`,
-              exclude: ['**/*.{stories,spec,e2e}.ts', 'vite.config.ts'],
+              exclude: ['**/*[.-]{stories,spec,e2e,test-utils}.ts', 'vite.config.ts'],
+
+              beforeWriteFile: (filePath, content) => {
+                if (content.includes('.scss?lit&inline') || content.includes('.scss?inline&lit')) {
+                  return {
+                    filePath,
+                    // Remove lines with scss modules
+                    content: content.replace(
+                      /export \{[^}]+\}\s+from\s+'[^']+\.scss\?(lit&inline|inline&lit)';\n?/gm,
+                      '',
+                    ),
+                  };
+                }
+              },
             }),
             customElementsManifest(),
             packageJsonTemplate({
