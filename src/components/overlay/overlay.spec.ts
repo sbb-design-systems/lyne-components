e05de329c002c922059e9b68b27363bf45049640
Marco D'Auria <marco.dauria@finconsgroup.com>
2024-03-25 08:50:22 +0100

    fix: lint

--- src/components/overlay/overlay.spec.ts
+++ src/components/overlay/overlay.spec.ts
@@ -1,8 +1,8 @@
-import { expect, fixture } from '@open-wc/testing';
+import { expect } from '@open-wc/testing';
 import { html } from 'lit/static-html.js';
 
 import { waitForLitRender } from '../core/testing';
-import { testA11yTreeSnapshot } from '../core/testing/a11y-tree-snapshot';
+import { fixture, testA11yTreeSnapshot } from '../core/testing/private';
 
 import type { SbbOverlayElement } from './overlay';
 import './overlay';
