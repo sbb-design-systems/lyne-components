58c6f267b13df2d0270ce78a9d2d8073a7fd021b
Lukas Spirig <lukas.spirig@sbb.ch>
2024-04-05 14:09:09 +0200

    fix: use valid import/export syntax
    Previously we were using the common syntax of importing/exporting files without
    the extension. However, this is not natively supported in browsers and also not in Node.js
    with ESM. This change adds a linting rule that automatically adds the .js extension
    to any relative import.

---
+++ tools/eslint/import-extension-rule.ts
@@ -0,0 +1,71 @@
+/* eslint-disable @typescript-eslint/naming-convention, import/no-unresolved  */
+import { existsSync, statSync } from 'fs';
+import { dirname, resolve } from 'path';
+
+import type { TSESLint, TSESTree } from '@typescript-eslint/utils';
+import { ESLintUtils } from '@typescript-eslint/utils';
+
+const createRule = ESLintUtils.RuleCreator(
+  (name) =>
+    `https://github.com/lyne-design-system/lyne-components/blob/main/tools/eslint/${name}.ts`,
+);
+
+type MessageIds = 'importExtensionRule' | 'importIndexExtensionRule';
+
+export const name = 'import-extension-rule';
+export const rule: TSESLint.RuleModule<MessageIds, never[]> = createRule<never[], MessageIds>({
+  create(context) {
+    function rule(originalNode: TSESTree.Node): void {
+      const node = originalNode as
+        | TSESTree.ExportAllDeclaration
+        | TSESTree.ExportNamedDeclarationWithSource
+        | TSESTree.ImportDeclaration;
+      const value = node.source?.value.replace(/\?.*$/, '');
+      if (!value || !value.startsWith('.') || value.endsWith('.js')) {
+        return;
+      }
+
+      const path = resolve(dirname(context.filename), value);
+      if (!existsSync(path)) {
+        context.report({
+          node,
+          messageId: 'importExtensionRule',
+          data: { import: value },
+          fix: !node.source.value.includes('?')
+            ? (fixer) => fixer.replaceText(node.source, `'${node.source.value}.js'`)
+            : undefined,
+        });
+      } else if (existsSync(path) && statSync(path).isDirectory()) {
+        context.report({
+          node,
+          messageId: 'importExtensionRule',
+          data: { import: value },
+          fix: (fixer) => fixer.replaceText(node.source, `'${node.source.value}/index.js'`),
+        });
+      }
+    }
+
+    return {
+      DeclareExportDeclaration: rule,
+      DeclareExportAllDeclaration: rule,
+      ExportAllDeclaration: rule,
+      ExportNamedDeclaration: rule,
+      ImportDeclaration: rule,
+    };
+  },
+  name,
+  meta: {
+    docs: {
+      description: 'Relative imports and exports must end with .js',
+      recommended: 'recommended',
+    },
+    messages: {
+      importExtensionRule: 'Missing .js extension in import/export path ({{ import }})',
+      importIndexExtensionRule: 'Missing index.js in import/export path ({{ import }})',
+    },
+    fixable: 'code',
+    type: 'problem',
+    schema: [],
+  },
+  defaultOptions: [],
+});
--- tools/eslint/index.ts
+++ tools/eslint/index.ts
@@ -1,20 +1,23 @@
 import * as customElementClassName from './custom-element-class-name-rule';
 import * as customElementDecoratorPosition from './custom-element-decorator-position-rule';
+import * as importExtensionRule from './import-extension-rule';
 import * as missingComponentDocumentation from './missing-component-documentation-rule';
 
 export default {
   rules: {
     [missingComponentDocumentation.name]: missingComponentDocumentation.rule,
     [customElementClassName.name]: customElementClassName.rule,
+    [importExtensionRule.name]: importExtensionRule.rule,
     [customElementDecoratorPosition.name]: customElementDecoratorPosition.rule,
   },
   configs: {
     all: {
       plugins: ['lyne'],
       rules: {
-        [`lyne/${missingComponentDocumentation.name}`]: 'error',
         [`lyne/${customElementClassName.name}`]: 'error',
         [`lyne/${customElementDecoratorPosition.name}`]: 'error',
+        [`lyne/${importExtensionRule.name}`]: 'error',
+        [`lyne/${missingComponentDocumentation.name}`]: 'error',
       },
     },
   },
