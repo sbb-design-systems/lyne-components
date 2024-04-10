d37c7929d2b01e7b890fe836351148a79bd47a7d
Jeri Peier <jeremias.peier@sbb.ch>
2024-04-10 09:20:08 +0200

    fix: review

--- src/components/container/sticky-bar/sticky-bar.ts
+++ src/components/container/sticky-bar/sticky-bar.ts
@@ -14,9 +14,8 @@ import style from './sticky-bar.scss?lit&inline';
  * @cssprop [--sbb-sticky-bar-bottom-overlapping-height=0px] - Define an additional area where
  * the sticky bar overlaps the following content on the bottom.
  * This area becomes visible when the sticky bar transitions from sticky to the normal document flow.
- * @cssprop [--sbb-sticky-bar-z-index=var(--sbb-overlay-default-z-index)] - To specify a custom stack order,
- * the `z-index` can be overridden by defining this CSS variable. The default `z-index` of the
- * component is set to `var(--sbb-overlay-default-z-index)` with a value of `1000`.
+ * @cssprop [--sbb-sticky-bar-z-index] - To specify a custom stack order,
+ * the `z-index` can be overridden by defining this CSS variable.
  */
 @customElement('sbb-sticky-bar')
 @hostAttributes({
