07b2c92c9bc3e7717e03a48126e27816349d9a83
Jeremias Peier <jeremias.peier@sbb.ch>
2024-05-01 17:01:22 +0200

    fix: fix screenshot sizes

--- src/components/core/testing/private/describe-viewports.ts
+++ src/components/core/testing/private/describe-viewports.ts
@@ -1,29 +1,29 @@
 import {
-  SbbBreakpointLargeMax,
-  SbbBreakpointMediumMax,
-  SbbBreakpointMicroMax,
-  SbbBreakpointSmallMax,
-  SbbBreakpointUltraMax,
-  SbbBreakpointWideMax,
-  SbbBreakpointZeroMax,
+  SbbBreakpointLargeMin,
+  SbbBreakpointMediumMin,
+  SbbBreakpointMicroMin,
+  SbbBreakpointSmallMin,
+  SbbBreakpointUltraMin,
+  SbbBreakpointWideMin,
+  SbbBreakpointZeroMin,
 } from '@sbb-esta/lyne-design-tokens';
 import { setViewport } from '@web/test-runner-commands';
 
 const viewportSizes = {
-  zero: SbbBreakpointZeroMax,
-  micro: SbbBreakpointMicroMax,
-  small: SbbBreakpointSmallMax,
-  medium: SbbBreakpointMediumMax,
-  large: SbbBreakpointLargeMax,
-  wide: SbbBreakpointWideMax,
-  ultra: SbbBreakpointUltraMax,
+  zero: SbbBreakpointZeroMin,
+  micro: SbbBreakpointMicroMin,
+  small: SbbBreakpointSmallMin,
+  medium: SbbBreakpointMediumMin,
+  large: SbbBreakpointLargeMin,
+  wide: SbbBreakpointWideMin,
+  ultra: SbbBreakpointUltraMin,
 };
 
-export function describeViewports(fn: (this: Mocha.Suite) => void): void {
+export function describeViewports(fn: (this: Mocha.Suite) => void, viewportHeight = 400): void {
   for (const [size, value] of Object.entries(viewportSizes)) {
     describe(`viewport=${size}`, function () {
       before(async () => {
-        await setViewport({ width: value, height: 400 });
+        await setViewport({ width: value, height: viewportHeight });
       });
 
       fn.call(this);
