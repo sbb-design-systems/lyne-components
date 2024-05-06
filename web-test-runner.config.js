11fbe2a5c37a13758349a6ee9aeea63547115627
Jeremias Peier <jeremias.peier@sbb.ch>
2024-05-06 12:06:23 +0200

    fix: icon handling

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -69,7 +69,6 @@ const testRunnerHtml = (testFramework, _config, group) => `
     <script>
       globalThis.testEnv = '${isDebugMode ? 'debug' : ''}';
       globalThis.testGroup = '${groupNameOverride ?? group?.name ?? 'default'}';
-      globalThis.isVisualRegressionRun = ${visualRegressionRun};
     </script>
   </head>
   <body class="sbb-disable-animation">
