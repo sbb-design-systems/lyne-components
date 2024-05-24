16f9f95b6942bfec5c4621222a4363b31ecbcaed
Osman Minaz <osman.minaz@t-systems.com>
2024-05-24 11:27:04 +0200

    feat: f

--- src/elements-experimental/pearl-chain/pearl-chain.spec.ts
+++ src/elements-experimental/pearl-chain/pearl-chain.spec.ts
@@ -184,8 +184,8 @@ describe(`sbb-pearl-chain`, () => {
       element.legs = [
         {
           __typename: 'PTRideLeg',
-          arrival: { time: '2022-08-16T16:00:00Z' },
-          departure: { time: '2022-08-16T14:00:00Z' },
+          arrival: { time: '2022-08-17T16:00:00Z' },
+          departure: { time: '2022-08-14T14:00:00Z' },
           serviceJourney: {
             serviceAlteration: {
               cancelled: false,
@@ -194,8 +194,8 @@ describe(`sbb-pearl-chain`, () => {
         } as PtRideLeg,
         {
           __typename: 'PTRideLeg',
-          arrival: { time: '2022-08-16T18:00:00Z' },
-          departure: { time: '2022-08-16T17:00:00Z' },
+          arrival: { time: '2022-08-17T20:00:00Z' },
+          departure: { time: '2022-08-17T18:00:00Z' },
           serviceJourney: {
             serviceAlteration: {
               cancelled: false,
@@ -210,15 +210,21 @@ describe(`sbb-pearl-chain`, () => {
       );
       expect(element).shadowDom.to.be.equal(`
         <div class="sbb-pearl-chain">
-          <span class="sbb-pearl-chain__bullet sbb-pearl-chain__bullet--progress"></span>
+          <span class="sbb-pearl-chain__bullet sbb-pearl-chain__bullet--progress">
+          </span>
           <div
-            class="sbb-pearl-chain__leg sbb-pearl-chain__leg--past"
-            style="--sbb-pearl-chain-leg-width:66.66666666666666%;"
+            class="sbb-pearl-chain__leg sbb-pearl-chain__leg--progress"
+            style="--sbb-pearl-chain-leg-width:97.36842105263158%;--sbb-pearl-chain-leg-status:68.91891891891892%;"
           >
+            <span
+              class="sbb-pearl-chain__position"
+              style="--sbb-pearl-chain-status-position:68.91891891891892%;transform:translateX(-100%);"
+            >
+            </span>
           </div>
           <div
             class="sbb-pearl-chain__leg sbb-pearl-chain__leg--future"
-            style="--sbb-pearl-chain-leg-width:33.33333333333333%;"
+            style="--sbb-pearl-chain-leg-width:2.631578947368421%;"
           >
             <span class="sbb-pearl-chain__stop">
             </span>
