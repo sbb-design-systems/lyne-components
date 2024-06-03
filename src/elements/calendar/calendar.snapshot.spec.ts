07bf9990f67ad290993cd0c7b0d557f93625467d
Jeremias Peier <jeremias.peier@sbb.ch>
2024-06-03 22:07:24 +0200

    fix: calendar tests

--- src/elements/calendar/calendar.snapshot.spec.ts
+++ src/elements/calendar/calendar.snapshot.spec.ts
@@ -17,42 +17,6 @@ describe(`sbb-calendar`, () => {
     await expect(root).shadowDom.to.be.equalSnapshot();
   });
 
-  it('renders with min and max', async () => {
-    const page: HTMLElement = await fixture(
-      html`<sbb-calendar
-        selected="2023-01-20T00:00:00"
-        min="2023-01-09T00:00:00"
-        max="2023-01-29T00:00:00"
-      ></sbb-calendar>`,
-    );
-
-    const buttonPrevDay = page.shadowRoot!.querySelector(
-      "sbb-secondary-button[icon-name='chevron-small-left-small']",
-    );
-    expect(buttonPrevDay).to.have.attribute('disabled');
-    const buttonNextDay = page.shadowRoot!.querySelector(
-      "sbb-secondary-button[icon-name='chevron-small-right-small']",
-    );
-    expect(buttonNextDay).to.have.attribute('disabled');
-
-    const emptyCells = page.shadowRoot!.querySelectorAll("[data-day='0 1 2023']");
-    expect(emptyCells.length).to.be.equal(6);
-
-    const lastDisabledMinDate = page.shadowRoot!.querySelector("[data-day='8 1 2023']");
-    expect(lastDisabledMinDate).to.have.attribute('disabled');
-    expect(lastDisabledMinDate).to.have.attribute('aria-disabled', 'true');
-    const firstNotDisabledMinDate = page.shadowRoot!.querySelector("[data-day='9 1 2023']");
-    expect(firstNotDisabledMinDate).not.to.have.attribute('disabled');
-    expect(firstNotDisabledMinDate).to.have.attribute('aria-disabled', 'false');
-
-    const lastNotDisabledMaxDate = page.shadowRoot!.querySelector("[data-day='29 1 2023']");
-    expect(lastNotDisabledMaxDate).not.to.have.attribute('disabled');
-    expect(lastNotDisabledMaxDate).to.have.attribute('aria-disabled', 'false');
-    const firstDisabledMaxDate = page.shadowRoot!.querySelector("[data-day='30 1 2023']");
-    expect(firstDisabledMaxDate).to.have.attribute('disabled');
-    expect(firstDisabledMaxDate).to.have.attribute('aria-disabled', 'true');
-  });
-
   testA11yTreeSnapshot(
     html`<sbb-calendar selected="2023-01-20T00:00:00" data-now="1672790400000"></sbb-calendar>`,
     undefined,
--- src/elements/calendar/calendar.spec.ts
+++ src/elements/calendar/calendar.spec.ts
@@ -7,6 +7,8 @@ import { waitForCondition, waitForLitRender, EventSpy } from '../core/testing.js
 
 import { SbbCalendarElement } from './calendar.js';
 
+import type { SbbSecondaryButtonElement } from '@sbb-esta/lyne-elements/button/secondary-button/secondary-button';
+
 import '../button.js';
 
 describe(`sbb-calendar`, () => {
@@ -206,6 +208,42 @@ describe(`sbb-calendar`, () => {
 
     const dayCells = Array.from(element.shadowRoot!.querySelectorAll('.sbb-calendar__day'));
     expect(dayCells.length).to.be.equal(31);
+  });
+
+  it('renders with min and max', async () => {
+    const page: HTMLElement = await fixture(
+      html`<sbb-calendar
+        selected="2023-01-20T00:00:00"
+        min="2023-01-09T00:00:00"
+        max="2023-01-29T00:00:00"
+      ></sbb-calendar>`,
+    );
+
+    const buttonPrevDay = page.shadowRoot!.querySelector<SbbSecondaryButtonElement>(
+      "sbb-secondary-button[icon-name='chevron-small-left-small']",
+    );
+    expect(buttonPrevDay).to.have.attribute('disabled');
+    const buttonNextDay = page.shadowRoot!.querySelector(
+      "sbb-secondary-button[icon-name='chevron-small-right-small']",
+    );
+    expect(buttonNextDay).to.have.attribute('disabled');
+
+    const emptyCells = page.shadowRoot!.querySelectorAll("[data-day='0 1 2023']");
+    expect(emptyCells.length).to.be.equal(6);
+
+    const lastDisabledMinDate = page.shadowRoot!.querySelector("[data-day='8 1 2023']");
+    expect(lastDisabledMinDate).to.have.attribute('disabled');
+    expect(lastDisabledMinDate).to.have.attribute('aria-disabled', 'true');
+    const firstNotDisabledMinDate = page.shadowRoot!.querySelector("[data-day='9 1 2023']");
+    expect(firstNotDisabledMinDate).not.to.have.attribute('disabled');
+    expect(firstNotDisabledMinDate).to.have.attribute('aria-disabled', 'false');
+
+    const lastNotDisabledMaxDate = page.shadowRoot!.querySelector("[data-day='29 1 2023']");
+    expect(lastNotDisabledMaxDate).not.to.have.attribute('disabled');
+    expect(lastNotDisabledMaxDate).to.have.attribute('aria-disabled', 'false');
+    const firstDisabledMaxDate = page.shadowRoot!.querySelector("[data-day='30 1 2023']");
+    expect(firstDisabledMaxDate).to.have.attribute('disabled');
+    expect(firstDisabledMaxDate).to.have.attribute('aria-disabled', 'true');
   });
 
   describe('navigation', () => {
