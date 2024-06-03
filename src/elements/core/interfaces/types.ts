ddd8c26a2246c7a9194251d0d3ce47ca108312f2
Jeremias Peier <jeremias.peier@sbb.ch>
2024-06-03 13:48:02 +0200

    build: remove SbbTime

--- src/elements/core/interfaces/types.ts
+++ src/elements/core/interfaces/types.ts
@@ -2,14 +2,7 @@ export type SbbLanguage = 'de' | 'en' | 'fr' | 'it';
 
 export type SbbDateLike<T = Date> = T | string | number;
 
-type Digit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-type Hours = `${0 | 1}${Digit}` | `2${0 | 1 | 2 | 3}`;
-type Minutes = `${0 | 1 | 2 | 3 | 4 | 5}${Digit}`;
-type Seconds = Minutes;
-
-export type SbbShortTime = `${Hours}:${Minutes}`;
-
-export type SbbTime = `${Hours}:${Minutes}:${Seconds}`;
+export type SbbTime = `${number}:${number}:${number}`;
 
 export type SbbHorizontalFrom = 'zero' | 'micro' | 'small' | 'medium' | 'large' | 'wide' | 'ultra';
 
