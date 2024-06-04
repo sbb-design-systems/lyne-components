74e498fe1d63364e4777dd035b54e22177c59f43
Lukas Spirig <lukas.spirig@sbb.ch>
2024-06-04 16:55:18 +0200

    fix(sbb-image): render URL correctly with SSR

--- src/elements/image/image.ssr.spec.ts
+++ src/elements/image/image.ssr.spec.ts
@@ -1,4 +1,4 @@
-import { assert } from '@open-wc/testing';
+import { assert, expect } from '@open-wc/testing';
 import { html } from 'lit';
 
 import { fixture } from '../core/testing/private.js';
@@ -7,15 +7,30 @@ import { SbbImageElement } from './image.js';
 
 describe(`sbb-image ${fixture.name}`, () => {
   let root: SbbImageElement;
+  const url = `${location.protocol}//${location.host}/src/elements/clock/assets/sbb_clock_face.svg`;
 
-  beforeEach(async () => {
-    const url = `${location.protocol}//${location.host}/src/elements/clock/assets/sbb_clock_face.svg`;
+  it('renders', async () => {
     root = await fixture(html`<sbb-image image-src=${url}></sbb-image>`, {
       modules: ['./image.js'],
     });
-  });
-
-  it('renders', () => {
     assert.instanceOf(root, SbbImageElement);
   });
+
+  const urls = [
+    { name: 'fully qualified url', url },
+    { name: 'local url', url: 'src/elements/clock/assets/sbb_clock_face.svg' },
+    { name: 'local root url', url: '/src/elements/clock/assets/sbb_clock_face.svg' },
+  ];
+  for (const { name, url } of urls) {
+    it(`should work with ${name}`, async () => {
+      root = await fixture(html`<sbb-image image-src=${url}></sbb-image>`, {
+        modules: ['./image.js'],
+      });
+      const sources = Array.from(root.shadowRoot!.querySelectorAll('source'));
+      expect(sources.length).greaterThan(0);
+      for (const source of sources) {
+        expect(source.srcset.startsWith(url)).to.be.true;
+      }
+    });
+  }
 });
