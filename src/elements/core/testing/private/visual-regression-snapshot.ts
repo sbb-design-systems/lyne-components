3d01beb0dcf06c0d6a6791b383f851f62eb27f16
Lukas Spirig <lukas.spirig@sbb.ch>
2024-06-04 14:45:41 +0200

    refactor: change visual regression testing API

--- src/elements/core/testing/private/visual-regression-snapshot.ts
+++ src/elements/core/testing/private/visual-regression-snapshot.ts
@@ -1,13 +1,15 @@
 import { aTimeout } from '@open-wc/testing';
-import { resetMouse, sendKeys, sendMouse } from '@web/test-runner-commands';
+import { resetMouse, sendKeys, sendMouse, setViewport } from '@web/test-runner-commands';
 import { visualDiff } from '@web/test-runner-visual-regression';
+import type { TemplateResult } from 'lit';
+
+import { visualRegressionFixture } from './fixture.js';
 
 export function imageName(test: Mocha.Runnable): string {
   return test!.fullTitle().replaceAll(', ', '-').replaceAll(' ', '_');
 }
 
-function findElementCenter(snapshotElement: () => HTMLElement): [number, number] {
-  const element = snapshotElement();
+function findElementCenter(element: HTMLElement): [number, number] {
   // Look for the first sbb-* element and get center of the element to
   // move the mouse cursor over it.
   const positionElement = element.localName.startsWith('sbb-')
@@ -20,61 +22,121 @@ function findElementCenter(snapshotElement: () => HTMLElement): [number, number]
   ];
 }
 
-export function testVisualDiff(snapshotElement: () => HTMLElement): void {
-  it('default', async function () {
-    await visualDiff(snapshotElement(), imageName(this.test!));
-  });
+class VisualDiffSetup {
+  private _snapshotElement?: HTMLElement;
+  private _stateElement?: HTMLElement;
+
+  public get snapshotElement(): HTMLElement {
+    return (
+      this._snapshotElement ??
+      (document.getElementById('visual-regression-fixture-wrapper') as HTMLElement)
+    );
+  }
+
+  public get stateElement(): HTMLElement {
+    return this._stateElement ?? this.snapshotElement;
+  }
+
+  public withSnapshotElement(element: HTMLElement): this {
+    this._snapshotElement = element;
+    return this;
+  }
+
+  public withStateElement(element: HTMLElement): this {
+    this._stateElement = element;
+    return this;
+  }
+
+  public async withFixture(
+    template: TemplateResult,
+    wrapperStyles?: Parameters<typeof visualRegressionFixture>[1],
+  ): Promise<this> {
+    this._snapshotElement = await visualRegressionFixture(template, wrapperStyles);
+    return this;
+  }
 }
 
-export function testVisualDiffFocus(snapshotElement: () => HTMLElement): void {
-  it('focus', async function () {
-    snapshotElement().focus();
-    await sendKeys({ press: 'Tab' });
-    await visualDiff(snapshotElement(), imageName(this.test!));
-  });
+const applyViewportIfDefined = async (
+  viewport: { width: number; height: number } | undefined,
+): Promise<void> => {
+  if (viewport) {
+    await setViewport(viewport);
+  }
+};
+
+export interface VisualDiffState {
+  name: string;
+  with: (setup: (setup: VisualDiffSetup) => void | Promise<void>) => Mocha.Func;
 }
 
-export function testVisualDiffHover(
-  snapshotElement: () => HTMLElement,
-  stateElement?: (() => HTMLElement) | undefined,
-): void {
-  it('hover', async function () {
-    const position = findElementCenter(stateElement ?? snapshotElement);
+export const visualDiffDefault: VisualDiffState = {
+  name: 'default',
+  with(setup: (setup: VisualDiffSetup) => void | Promise<void>): Mocha.Func {
+    return async function (this: Mocha.Context) {
+      const context = new VisualDiffSetup();
+      await setup(context);
+      await applyViewportIfDefined(this.ctx['requestViewport']);
+      await visualDiff(context.snapshotElement, imageName(this.test!));
+    };
+  },
+};
 
-    try {
-      await sendMouse({ type: 'move', position });
-      await aTimeout(5);
-      await visualDiff(snapshotElement(), imageName(this.test!));
-    } finally {
-      await resetMouse();
-    }
-  });
-}
+export const visualDiffFocus: VisualDiffState = {
+  name: 'default',
+  with(setup: (setup: VisualDiffSetup) => void | Promise<void>): Mocha.Func {
+    return async function (this: Mocha.Context) {
+      const context = new VisualDiffSetup();
+      await setup(context);
+      await applyViewportIfDefined(this.ctx['requestViewport']);
+      context.snapshotElement.focus();
+      await sendKeys({ press: 'Tab' });
+      await visualDiff(context.snapshotElement, imageName(this.test!));
+    };
+  },
+};
 
-export function testVisualDiffActive(
-  snapshotElement: () => HTMLElement,
-  stateElement?: (() => HTMLElement) | undefined,
-): void {
-  it('active', async function () {
-    const position = findElementCenter(stateElement ?? snapshotElement);
+export const visualDiffHover: VisualDiffState = {
+  name: 'default',
+  with(setup: (setup: VisualDiffSetup) => void | Promise<void>): Mocha.Func {
+    return async function (this: Mocha.Context) {
+      const context = new VisualDiffSetup();
+      await setup(context);
+      await applyViewportIfDefined(this.ctx['requestViewport']);
+      const position = findElementCenter(context.stateElement);
+      try {
+        await sendMouse({ type: 'move', position });
+        await aTimeout(5);
+        await visualDiff(context.snapshotElement, imageName(this.test!));
+      } finally {
+        await resetMouse();
+      }
+    };
+  },
+};
 
-    try {
-      await sendMouse({ type: 'move', position });
-      await sendMouse({ type: 'down' });
-      await aTimeout(5);
-      await visualDiff(snapshotElement(), imageName(this.test!));
-    } finally {
-      await resetMouse();
-    }
-  });
-}
+export const visualDiffActive: VisualDiffState = {
+  name: 'default',
+  with(setup: (setup: VisualDiffSetup) => void | Promise<void>): Mocha.Func {
+    return async function (this: Mocha.Context) {
+      const context = new VisualDiffSetup();
+      await setup(context);
+      await applyViewportIfDefined(this.ctx['requestViewport']);
+      const position = findElementCenter(context.stateElement);
+      try {
+        await sendMouse({ type: 'move', position });
+        await sendMouse({ type: 'down' });
+        await aTimeout(5);
+        await visualDiff(context.snapshotElement, imageName(this.test!));
+      } finally {
+        await resetMouse();
+      }
+    };
+  },
+};
 
-export function visualRegressionSnapshot(
-  snapshotElement: () => HTMLElement,
-  stateElement?: () => HTMLElement,
-): void {
-  testVisualDiff(snapshotElement);
-  testVisualDiffFocus(snapshotElement);
-  testVisualDiffHover(snapshotElement, stateElement);
-  testVisualDiffActive(snapshotElement, stateElement);
-}
+export const visualDiffStandardStates = [
+  visualDiffDefault,
+  visualDiffFocus,
+  visualDiffHover,
+  visualDiffActive,
+] as const;
--- src/elements/lead-container/lead-container.snapshot.spec.ts
+++ src/elements/lead-container/lead-container.snapshot.spec.ts
@@ -1,17 +1,7 @@
-import { aTimeout, expect } from '@open-wc/testing';
-import type { TemplateResult } from 'lit';
+import { expect } from '@open-wc/testing';
 import { html } from 'lit/static-html.js';
 
-import {
-  describeViewports,
-  fixture,
-  isVisualRegressionRun,
-  loadAssetAsBase64,
-  testA11yTreeSnapshot,
-  testVisualDiff,
-  visualRegressionFixture,
-} from '../core/testing/private.js';
-import { waitForCondition } from '../core/testing/wait-for-condition.js';
+import { fixture, testA11yTreeSnapshot } from '../core/testing/private.js';
 
 import type { SbbLeadContainerElement } from './lead-container.js';
 
@@ -21,115 +11,24 @@ import '../link/block-link/block-link.js';
 import '../title.js';
 import './lead-container.js';
 
-const leadImageUrl = import.meta.resolve('./assets/lucerne.png');
-const leadImageBase64 = await loadAssetAsBase64(leadImageUrl);
-
 describe(`sbb-lead-container`, () => {
-  if (!isVisualRegressionRun) {
-    let element: SbbLeadContainerElement;
+  let element: SbbLeadContainerElement;
 
-    beforeEach(async () => {
-      element = await fixture(
-        html`<sbb-lead-container>
-          <sbb-image slot="image"></sbb-image>
-        </sbb-lead-container>`,
-      );
-    });
+  beforeEach(async () => {
+    element = await fixture(
+      html`<sbb-lead-container>
+        <sbb-image slot="image"></sbb-image>
+      </sbb-lead-container>`,
+    );
+  });
 
-    it('DOM', async () => {
-      await expect(element).dom.to.be.equalSnapshot();
-    });
+  it('DOM', async () => {
+    await expect(element).dom.to.be.equalSnapshot();
+  });
 
-    it('Shadow DOM', async () => {
-      await expect(element).shadowDom.to.be.equalSnapshot();
-    });
+  it('Shadow DOM', async () => {
+    await expect(element).shadowDom.to.be.equalSnapshot();
+  });
 
-    testA11yTreeSnapshot();
-  } else {
-    describe('visual-regression', () => {
-      let root: HTMLElement;
-
-      const wrapperStyles = { backgroundColor: `var(--sbb-color-milk)`, padding: '0' };
-
-      const leadContainerTemplate = (image: TemplateResult): TemplateResult => html`
-        <sbb-lead-container>
-          <style>
-            p.other-content {
-              margin-block-end: 0;
-            }
-          </style>
-          ${image}
-          <sbb-breadcrumb-group class="sbb-lead-container-spacing">
-            <sbb-breadcrumb href="#" icon-name="house-small" id="breadcrumb-0"></sbb-breadcrumb>
-            <sbb-breadcrumb href="#" id="breadcrumb-1">Level 1</sbb-breadcrumb>
-          </sbb-breadcrumb-group>
-          <sbb-block-link
-            icon-placement="start"
-            icon-name="chevron-small-left-small"
-            size="xs"
-            href="https://www.sbb.ch"
-            class="sbb-lead-container-spacing"
-          >
-            Link
-          </sbb-block-link>
-          <sbb-title class="sbb-lead-container-spacing">Title</sbb-title>
-          <p class="sbb-text-xl sbb-lead-container-lead-text">
-            Lead text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer enim elit,
-            ultricies in tincidunt quis, mattis eu quam. Nulla sit amet lorem fermentum, molestie
-            nunc ut, hendrerit risus.
-          </p>
-          <p class="sbb-text-m other-content">
-            Other content. Vestibulum rutrum elit et lacus sollicitudin, quis malesuada lorem
-            vehicula. Suspendisse at augue quis tellus vulputate tempor. Vivamus urna velit, varius
-            nec est ac, mollis efficitur lorem. Quisque non nisl eget massa interdum tempus.
-            Praesent vel feugiat metus.
-          </p>
-        </sbb-lead-container>
-      `;
-
-      describeViewports(() => {
-        describe('with sbb-image', () => {
-          beforeEach(async function () {
-            root = await visualRegressionFixture(
-              leadContainerTemplate(
-                html`<sbb-image
-                  slot="image"
-                  image-src=${leadImageUrl}
-                  alt="Station of Lucerne from outside"
-                ></sbb-image>`,
-              ),
-              this,
-              wrapperStyles,
-            );
-            await waitForCondition(() =>
-              root.querySelector('sbb-image')!.hasAttribute('data-loaded'),
-            );
-            await aTimeout(20);
-          });
-
-          testVisualDiff(() => root);
-        });
-
-        describe('with img tag', async () => {
-          beforeEach(async function () {
-            root = await visualRegressionFixture(
-              leadContainerTemplate(
-                html`<img
-                  slot="image"
-                  src=${leadImageBase64}
-                  alt="Station of Lucerne from outside"
-                />`,
-              ),
-              this,
-              wrapperStyles,
-            );
-          });
-
-          await waitForCondition(() => root.querySelector('img')!.complete);
-
-          testVisualDiff(() => root);
-        });
-      });
-    });
-  }
+  testA11yTreeSnapshot();
 });
