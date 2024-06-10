c325151105b51d01f0745f9e08cca2c687265e5a
Davide Mininni <davide.mininni@finconsgroup.com>
2024-06-10 09:27:02 +0200

    chore: reduce lorem-ipsum text to fix chromatic snapshot error on dimensions

--- src/elements/stepper/stepper/stepper.stories.ts
+++ src/elements/stepper/stepper/stepper.stories.ts
@@ -20,10 +20,8 @@ import '../../form-error.js';
 import '../../card.js';
 
 const loremIpsum = `
-  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-  eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
-  eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-  sanctus est Lorem ipsum dolor sit amet.
+  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+  magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
 `;
 
 const linear: InputType = {
