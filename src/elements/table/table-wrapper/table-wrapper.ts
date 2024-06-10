f986e9ca202f0091e93cad59f006f021d2bd8d7b
Tommmaso Menga <tommaso.menga-94@hotmail.it>
2024-06-10 14:23:19 +0200

    feat(sbb-table-wrapper): handle 'always visible' scrollbar

--- src/elements/table/table-wrapper/table-wrapper.ts
+++ src/elements/table/table-wrapper/table-wrapper.ts
@@ -1,5 +1,13 @@
-import { type CSSResultGroup, html, LitElement, type TemplateResult } from 'lit';
+import {
+  type CSSResultGroup,
+  html,
+  LitElement,
+  type PropertyValues,
+  type TemplateResult,
+} from 'lit';
 import { customElement } from 'lit/decorators.js';
+
+import { SbbConnectedAbortController } from '../../core/controllers/connected-abort-controller.js';
 
 import style from './table-wrapper.scss?lit&inline';
 
@@ -11,6 +19,30 @@ import style from './table-wrapper.scss?lit&inline';
 @customElement('sbb-table-wrapper')
 export class SbbTableWrapperElement extends LitElement {
   public static override styles: CSSResultGroup = style;
+
+  private _abort = new SbbConnectedAbortController(this);
+
+  public override connectedCallback(): void {
+    super.connectedCallback();
+
+    window.addEventListener('resize', () => this._checkHorizontalScrollbar(), {
+      passive: true,
+      signal: this._abort.signal,
+    });
+  }
+
+  protected override firstUpdated(_changedProperties: PropertyValues): void {
+    super.firstUpdated(_changedProperties);
+    this._checkHorizontalScrollbar();
+  }
+
+  private _checkHorizontalScrollbar(): void {
+    const wrapper: HTMLElement = this.shadowRoot!.querySelector('.sbb-table-wrapper')!;
+    this.toggleAttribute(
+      'data-has-horizontal-scrollbar',
+      wrapper.scrollWidth > wrapper.offsetWidth,
+    );
+  }
 
   protected override render(): TemplateResult {
     return html`<div class="sbb-table-wrapper">
