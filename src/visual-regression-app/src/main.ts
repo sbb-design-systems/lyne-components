0299762c21cbf7bde6c77673047b12761f08f735
Jeri Peier <jeremias.peier@sbb.ch>
2024-06-12 09:13:04 +0200

    build: enable visual regression app for Firefox and Safari (#2740)

--- src/visual-regression-app/src/main.ts
+++ src/visual-regression-app/src/main.ts
@@ -4,6 +4,12 @@ import { customElement } from 'lit/decorators.js';
 
 import '@sbb-esta/lyne-elements/core/styles/standard-theme.scss';
 
+// Lit Router uses URLPattern which is not defined so far in Firefox and Safari.
+// TODO: Remove as soon as possible
+if (!('URLPattern' in globalThis)) {
+  await import('urlpattern-polyfill');
+}
+
 /**
  * Main app containing the router outlet.
  */
