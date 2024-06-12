1ab16024ceae2624ff00dc5952c06256a9643e87
Tommmaso Menga <tommaso.menga-94@hotmail.it>
2024-06-12 10:31:40 +0200

    test(sbb-expansion-panel): add visual spec

---
+++ src/elements/expansion-panel/expansion-panel-header/expansion-panel-header.visual.spec.ts
@@ -0,0 +1,48 @@
+import { html, nothing } from 'lit';
+
+import {
+  describeEach,
+  describeViewports,
+  visualDiffStandardStates,
+  visualRegressionFixture,
+} from '../../core/testing/private.js';
+
+import './expansion-panel-header.js';
+import '../../icon.js';
+
+describe(`sbb-button`, () => {
+  let root: HTMLElement;
+
+  const cases = {
+    disabled: [false, true],
+    state: [
+      { icon: undefined, slotted: false },
+      { icon: 'arrow-right-small', slotted: true },
+      { icon: 'arrow-right-small', slotted: false },
+    ],
+  };
+
+  describeViewports({ viewports: ['zero', 'medium'] }, () => {
+    describeEach(cases, ({ disabled, state }) => {
+      beforeEach(async function () {
+        root = await visualRegressionFixture(html`
+          <sbb-expansion-panel-header
+            ?disabled=${disabled}
+            .iconName=${!state.slotted ? state.icon : undefined}
+          >
+            ${state.slotted ? html`<sbb-icon name=${state.icon!}></sbb-icon>` : nothing} Label
+          </sbb-expansion-panel-header>
+        `);
+      });
+
+      for (const state of visualDiffStandardStates) {
+        it(
+          state.name,
+          state.with((setup) => {
+            setup.withSnapshotElement(root);
+          }),
+        );
+      }
+    });
+  });
+});
---
+++ src/elements/expansion-panel/expansion-panel/expansion-panel.visual.spec.ts
@@ -0,0 +1,102 @@
+import { html } from 'lit';
+
+import {
+  describeEach,
+  describeViewports,
+  visualDiffDefault,
+  visualDiffStandardStates,
+  visualRegressionFixture,
+} from '../../core/testing/private.js';
+
+import './expansion-panel.js';
+import '../expansion-panel-header.js';
+import '../expansion-panel-content.js';
+import '../../icon.js';
+
+describe(`sbb-button`, () => {
+  let root: HTMLElement;
+
+  const cases = {
+    borderless: [false, true],
+    disabled: [false, true],
+    expanded: [false, true],
+    color: ['white', 'milk'],
+  };
+
+  const sizeCases = {
+    size: ['s', 'l'],
+    expanded: [false, true],
+  };
+
+  const titleLevelCases = {
+    titleLevel: ['1', '4'],
+  };
+
+  describeViewports({ viewports: ['zero', 'medium'] }, () => {
+    // Main test cases
+    describeEach(cases, ({ borderless, disabled, expanded, color }) => {
+      beforeEach(async function () {
+        root = await visualRegressionFixture(html`
+          <sbb-expansion-panel
+            ?borderless=${borderless}
+            ?disabled=${disabled}
+            ?expanded=${expanded}
+            color=${color}
+          >
+            <sbb-expansion-panel-header icon-name="arrow-right-small">
+              Header
+            </sbb-expansion-panel-header>
+            <sbb-expansion-panel-content> Content </sbb-expansion-panel-content>
+          </sbb-expansion-panel>
+        `);
+      });
+
+      for (const state of visualDiffStandardStates) {
+        it(
+          state.name,
+          state.with((setup) => {
+            setup.withSnapshotElement(root);
+          }),
+        );
+      }
+    });
+
+    // Size test cases
+    describeEach(sizeCases, ({ expanded, size }) => {
+      beforeEach(async function () {
+        root = await visualRegressionFixture(html`
+          <sbb-expansion-panel ?expanded=${expanded} size=${size}>
+            <sbb-expansion-panel-header icon-name="arrow-right-small">
+              Header
+            </sbb-expansion-panel-header>
+            <sbb-expansion-panel-content> Content </sbb-expansion-panel-content>
+          </sbb-expansion-panel>
+        `);
+      });
+
+      it(
+        visualDiffDefault.name,
+        visualDiffDefault.with((setup) => {
+          setup.withSnapshotElement(root);
+        }),
+      );
+    });
+
+    // Title level test
+    for (const titleLevel of titleLevelCases.titleLevel) {
+      it(
+        `title-level=${titleLevel} ${visualDiffDefault.name}`,
+        visualDiffDefault.with(async (setup) => {
+          await setup.withFixture(html`
+            <sbb-expansion-panel title-level=${titleLevel}>
+              <sbb-expansion-panel-header icon-name="arrow-right-small">
+                Header
+              </sbb-expansion-panel-header>
+              <sbb-expansion-panel-content> Content </sbb-expansion-panel-content>
+            </sbb-expansion-panel>
+          `);
+        }),
+      );
+    }
+  });
+});
