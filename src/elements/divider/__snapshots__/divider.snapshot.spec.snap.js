9b2df8d161c1d25e77a466eb4fac86128978b60a
Tommmaso Menga <tommaso.menga-94@hotmail.it>
2024-06-20 15:25:43 +0200

    test(sbb-divider): add visual spec

--- src/elements/divider/__snapshots__/divider.snapshot.spec.snap.js
+++ src/elements/divider/__snapshots__/divider.snapshot.spec.snap.js
@@ -21,3 +21,71 @@ snapshots["sbb-divider A11y tree Firefox"] =
 `;
 /* end snapshot sbb-divider A11y tree Firefox */
 
+snapshots["sbb-divider should render with default values DOM"] = 
+`<sbb-divider
+  aria-orientation="horizontal"
+  orientation="horizontal"
+  role="separator"
+>
+</sbb-divider>
+`;
+/* end snapshot sbb-divider should render with default values DOM */
+
+snapshots["sbb-divider should render with default values Shadow DOM"] = 
+`<div class="sbb-divider">
+</div>
+`;
+/* end snapshot sbb-divider should render with default values Shadow DOM */
+
+snapshots["sbb-divider should render with default values A11y tree Chrome"] = 
+`<p>
+  {
+  "role": "WebArea",
+  "name": ""
+}
+</p>
+`;
+/* end snapshot sbb-divider should render with default values A11y tree Chrome */
+
+snapshots["sbb-divider should render with orientation horizontal DOM"] = 
+`<sbb-divider
+  aria-orientation="horizontal"
+  orientation="horizontal"
+  role="separator"
+>
+</sbb-divider>
+`;
+/* end snapshot sbb-divider should render with orientation horizontal DOM */
+
+snapshots["sbb-divider should render with orientation horizontal Shadow DOM"] = 
+`<div class="sbb-divider">
+</div>
+`;
+/* end snapshot sbb-divider should render with orientation horizontal Shadow DOM */
+
+snapshots["sbb-divider should render with orientation vertical DOM"] = 
+`<sbb-divider
+  aria-orientation="vertical"
+  orientation="vertical"
+  role="separator"
+>
+</sbb-divider>
+`;
+/* end snapshot sbb-divider should render with orientation vertical DOM */
+
+snapshots["sbb-divider should render with orientation vertical Shadow DOM"] = 
+`<div class="sbb-divider">
+</div>
+`;
+/* end snapshot sbb-divider should render with orientation vertical Shadow DOM */
+
+snapshots["sbb-divider should render with default values A11y tree Firefox"] = 
+`<p>
+  {
+  "role": "document",
+  "name": ""
+}
+</p>
+`;
+/* end snapshot sbb-divider should render with default values A11y tree Firefox */
+
--- src/elements/divider/divider.snapshot.spec.ts
+++ src/elements/divider/divider.snapshot.spec.ts
@@ -2,48 +2,58 @@ import { expect } from '@open-wc/testing';
 import { html } from 'lit/static-html.js';
 
 import { fixture, testA11yTreeSnapshot } from '../core/testing/private.js';
-import { waitForLitRender } from '../core/testing.js';
 
 import type { SbbDividerElement } from './divider.js';
-
 import './divider.js';
 
 describe(`sbb-divider`, () => {
-  it('should render with default values', async () => {
-    const element: SbbDividerElement = await fixture(html`<sbb-divider></sbb-divider>`);
-    expect(element).dom.to.be.equal(
-      `<sbb-divider orientation='horizontal' aria-orientation='horizontal' role='separator'></sbb-divider>`,
-    );
-    expect(element).shadowDom.to.be.equal(`<div class='sbb-divider'></div>`);
+  describe('should render with default values', () => {
+    let element: SbbDividerElement;
+
+    beforeEach(async () => {
+      element = await fixture(html`<sbb-divider></sbb-divider>`);
+    });
+
+    it('DOM', async () => {
+      await expect(element).dom.to.be.equalSnapshot();
+    });
+
+    it('Shadow DOM', async () => {
+      await expect(element).shadowDom.to.be.equalSnapshot();
+    });
+
+    testA11yTreeSnapshot();
   });
 
-  it('should render with orientation horizontal', async () => {
-    const element: SbbDividerElement = await fixture(
-      html`<sbb-divider orientation="horizontal"></sbb-divider>`,
-    );
-    expect(element).dom.to.be.equal(
-      `<sbb-divider orientation='horizontal' aria-orientation='horizontal' role='separator'></sbb-divider>`,
-    );
-    expect(element).shadowDom.to.be.equal(`<div class='sbb-divider'></div>`);
+  describe('should render with orientation horizontal', () => {
+    let element: SbbDividerElement;
+
+    beforeEach(async () => {
+      element = await fixture(html`<sbb-divider orientation="horizontal"></sbb-divider>`);
+    });
+
+    it('DOM', async () => {
+      await expect(element).dom.to.be.equalSnapshot();
+    });
+
+    it('Shadow DOM', async () => {
+      await expect(element).shadowDom.to.be.equalSnapshot();
+    });
   });
 
-  it('should render with orientation vertical', async () => {
-    const element: SbbDividerElement = await fixture(
-      html`<sbb-divider orientation="vertical"></sbb-divider>`,
-    );
-    expect(element).dom.to.be.equal(
-      `<sbb-divider orientation='vertical' aria-orientation='vertical' role='separator'></sbb-divider>`,
-    );
-    expect(element).shadowDom.to.be.equal(`<div class='sbb-divider'></div>`);
+  describe('should render with orientation vertical', () => {
+    let element: SbbDividerElement;
+
+    beforeEach(async () => {
+      element = await fixture(html`<sbb-divider orientation="vertical"></sbb-divider>`);
+    });
+
+    it('DOM', async () => {
+      await expect(element).dom.to.be.equalSnapshot();
+    });
+
+    it('Shadow DOM', async () => {
+      await expect(element).shadowDom.to.be.equalSnapshot();
+    });
   });
-
-  it('should react to change of orientation', async () => {
-    const element: SbbDividerElement = await fixture(html`<sbb-divider></sbb-divider>`);
-
-    element.orientation = 'vertical';
-    await waitForLitRender(element);
-    expect(element).to.have.attribute('aria-orientation', 'vertical');
-  });
-
-  testA11yTreeSnapshot(html`<sbb-divider></sbb-divider>`);
 });
