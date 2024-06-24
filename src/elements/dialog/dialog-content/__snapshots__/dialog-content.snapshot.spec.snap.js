d2fdb7ebf3491d5d792521036b04b6ac1cfd8557
Tommmaso Menga <tommaso.menga-94@hotmail.it>
2024-06-24 14:20:45 +0200

    chore(sbb-dialog): update snapshots

---
+++ src/elements/dialog/dialog-content/__snapshots__/dialog-content.snapshot.spec.snap.js
@@ -0,0 +1,50 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["sbb-dialog-content renders DOM"] = 
+`<sbb-dialog-content>
+  Content
+</sbb-dialog-content>
+`;
+/* end snapshot sbb-dialog-content renders DOM */
+
+snapshots["sbb-dialog-content renders Shadow DOM"] = 
+`<div class="sbb-dialog-content">
+  <slot>
+  </slot>
+</div>
+`;
+/* end snapshot sbb-dialog-content renders Shadow DOM */
+
+snapshots["sbb-dialog-content renders A11y tree Chrome"] = 
+`<p>
+  {
+  "role": "WebArea",
+  "name": "",
+  "children": [
+    {
+      "role": "text",
+      "name": "Content"
+    }
+  ]
+}
+</p>
+`;
+/* end snapshot sbb-dialog-content renders A11y tree Chrome */
+
+snapshots["sbb-dialog-content renders A11y tree Firefox"] = 
+`<p>
+  {
+  "role": "document",
+  "name": "",
+  "children": [
+    {
+      "role": "text leaf",
+      "name": "Content"
+    }
+  ]
+}
+</p>
+`;
+/* end snapshot sbb-dialog-content renders A11y tree Firefox */
+
