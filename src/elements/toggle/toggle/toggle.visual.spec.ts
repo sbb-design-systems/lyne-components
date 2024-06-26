e7666dec85f477f534042f871a266302444e3d3c
Mario Castigliano <mario.castigliano@finconsgroup.com>
2024-06-26 11:15:29 +0200

    test: fix toggle

--- src/elements/toggle/toggle/toggle.visual.spec.ts
+++ src/elements/toggle/toggle/toggle.visual.spec.ts
@@ -52,54 +52,44 @@ describe(`sbb-toggle`, () => {
       }
     });
 
-    describeEach(sizeCases, ({ size }) => {
-      beforeEach(async function () {
-        root = await visualRegressionFixture(html`
-          <sbb-toggle disabled size=${size as 's' | 'm'}> ${options(true)} </sbb-toggle>
-        `);
-      });
-
+    for (const size of ['s', 'm']) {
       it(
-        `disabled ${visualDiffDefault.name}`,
-        visualDiffDefault.with((setup) => {
-          setup.withSnapshotElement(root);
-        }),
-      );
-    });
-
-    describeEach(iconCases, ({ size, label }) => {
-      beforeEach(async function () {
-        root = await visualRegressionFixture(html`
-          <sbb-toggle size=${size as 's' | 'm'}> ${options(label, 'app-icon-small')} </sbb-toggle>
-        `);
-      });
-
-      it(
-        `icon ${visualDiffDefault.name}`,
-        visualDiffDefault.with((setup) => {
-          setup.withSnapshotElement(root);
-        }),
-      );
-    });
-
-    describe('slotted icon', () => {
-      it(
-        visualDiffDefault.name,
+        `disabled size ${size}`,
         visualDiffDefault.with(async (setup) => {
           await setup.withFixture(html`
-            <sbb-toggle>
-              <sbb-toggle-option value="Value 1">
-                Bern
-                <sbb-icon slot="icon" name="app-icon-small"></sbb-icon>
-              </sbb-toggle-option>
-              <sbb-toggle-option value="Value 2">
-                Zürich
-                <sbb-icon slot="icon" name="arrows-right-left-small"></sbb-icon>
-              </sbb-toggle-option>
-            </sbb-toggle>
+            <sbb-toggle disabled size=${size as 's' | 'm'}> ${options(true)} </sbb-toggle>
+          `);
+        }),
+      );
+    }
+
+    describeEach(iconCases, ({ size, label }) => {
+      it(
+        `icon ${visualDiffDefault.name}`,
+        visualDiffDefault.with(async (setup) => {
+          await setup.withFixture(html`
+            <sbb-toggle size=${size as 's' | 'm'}> ${options(label, 'app-icon-small')} </sbb-toggle>
           `);
         }),
       );
     });
+
+    it(
+      'slotted icon',
+      visualDiffDefault.with(async (setup) => {
+        await setup.withFixture(html`
+          <sbb-toggle>
+            <sbb-toggle-option value="Value 1">
+              Bern
+              <sbb-icon slot="icon" name="app-icon-small"></sbb-icon>
+            </sbb-toggle-option>
+            <sbb-toggle-option value="Value 2">
+              Zürich
+              <sbb-icon slot="icon" name="arrows-right-left-small"></sbb-icon>
+            </sbb-toggle-option>
+          </sbb-toggle>
+        `);
+      }),
+    );
   });
 });
