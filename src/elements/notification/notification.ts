136c4af7e56ea2b8cf329b1b8708aadcee87f1cd
Davide Mininni <davide.mininni@finconsgroup.com>
2024-06-28 14:28:17 +0200

    test: add visual test (see fixme)

--- src/elements/notification/notification.ts
+++ src/elements/notification/notification.ts
@@ -47,19 +47,13 @@ export class SbbNotificationElement extends LitElement {
     didClose: 'didClose',
   } as const;
 
-  /**
-   * The type of the notification.
-   */
+  /** The type of the notification. */
   @property({ reflect: true }) public type: 'info' | 'success' | 'warn' | 'error' = 'info';
 
-  /**
-   * Content of title.
-   */
+  /** Content of title. */
   @property({ attribute: 'title-content', reflect: true }) public titleContent?: string;
 
-  /**
-   * Level of title, it will be rendered as heading tag (e.g. h3). Defaults to level 3.
-   */
+  /** Level of title, it will be rendered as heading tag (e.g. h3). Defaults to level 3. */
   @property({ attribute: 'title-level' }) public titleLevel: SbbTitleLevel = '3';
 
   /**
@@ -71,14 +65,10 @@ export class SbbNotificationElement extends LitElement {
   /** Size variant, either s or m. */
   @property({ reflect: true }) public size: 'm' | 's' = 'm';
 
-  /**
-   * The enabled animations.
-   */
+  /** The enabled animations. */
   @property({ reflect: true }) public animation: 'open' | 'close' | 'all' | 'none' = 'all';
 
-  /**
-   * The state of the notification.
-   */
+  /** The state of the notification. */
   private set _state(state: SbbOpenedClosedState) {
     this.setAttribute('data-state', state);
   }
