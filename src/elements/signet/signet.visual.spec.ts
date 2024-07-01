04d4110f9a3759c112f53fdfbfd19d5fce026ef8
Jeremias Peier <jeremias.peier@sbb.ch>
2024-07-01 21:31:09 +0200

    fix: review

--- src/elements/signet/signet.visual.spec.ts
+++ src/elements/signet/signet.visual.spec.ts
@@ -7,14 +7,14 @@ import './signet.js';
 describe(`sbb-signet`, () => {
   const protectiveRooms = ['none', 'minimal', 'ideal'];
 
-  describeViewports({ viewports: ['medium'] }, () => {
+  describeViewports({ viewports: ['zero'] }, () => {
     for (const protectiveRoom of protectiveRooms) {
       it(
         `protectiveRoom=${protectiveRoom}`,
         visualDiffDefault.with(async (setup) => {
           await setup.withFixture(
             html`<sbb-signet protective-room=${protectiveRoom}></sbb-signet>`,
-            { padding: '0' },
+            { padding: '0', maxWidth: '300px' },
           );
         }),
       );
