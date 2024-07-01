a07b84340dc945fc6684d8a60232abd85479413d
Jeremias Peier <jeremias.peier@sbb.ch>
2024-07-01 17:40:58 +0200

    test(sbb-signet): migrate visual regression tests

---
+++ src/elements/signet/signet.visual.spec.ts
@@ -0,0 +1,23 @@
+import { html } from 'lit';
+
+import { describeViewports, visualDiffDefault } from '../core/testing/private.js';
+
+import './signet.js';
+
+describe(`sbb-signet`, () => {
+  const protectiveRooms = ['none', 'minimal', 'ideal'];
+
+  describeViewports({ viewports: ['medium'] }, () => {
+    for (const protectiveRoom of protectiveRooms) {
+      it(
+        `protectiveRoom=${protectiveRoom}`,
+        visualDiffDefault.with(async (setup) => {
+          await setup.withFixture(
+            html`<sbb-signet protective-room=${protectiveRoom}></sbb-signet>`,
+            { padding: '0' },
+          );
+        }),
+      );
+    }
+  });
+});
