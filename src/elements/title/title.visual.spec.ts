ad4fe3174252b64dff2508ffd8eedd34c2edb838
Mario Castigliano <mario.castigliano@finconsgroup.com>
2024-07-02 10:06:41 +0200

    fix: review comments

--- src/elements/title/title.visual.spec.ts
+++ src/elements/title/title.visual.spec.ts
@@ -5,15 +5,21 @@ import { describeViewports, visualDiffDefault } from '../core/testing/private.js
 import '../title.js';
 
 describe(`sbb-title`, () => {
-  describeViewports({ viewports: ['zero', 'small', 'large', 'ultra'] }, () => {
+  describeViewports({ viewports: ['zero', 'small', 'medium', 'large', 'ultra'] }, () => {
     for (const level of ['1', '2', '3', '4', '5', '6']) {
       it(
-        `visual level ${level}`,
+        `level=${level}`,
         visualDiffDefault.with(async (setup) => {
           await setup.withFixture(
             html`<sbb-title level=${level}>
-              Data without insights are trivial, and insights without action are pointless
-            </sbb-title>`,
+                Data without insights are trivial, and insights without action are pointless
+              </sbb-title>
+              <article>
+                Diam maecenas ultricies mi eget mauris pharetra et ultrices neque ornare aenean
+                euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl
+                rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui sapien eget mi
+                proin sed libero enim sed faucibus turpis in eu mi bibendum neque egestas congue.
+              </article> `,
           );
         }),
       );
@@ -23,11 +29,12 @@ describe(`sbb-title`, () => {
       'negative',
       visualDiffDefault.with(async (setup) => {
         await setup.withFixture(
-          html` <div style="padding: 1rem; background-color: var(--sbb-color-charcoal);">
+          html`
             <sbb-title negative>
               Data without insights are trivial, and insights without action are pointless
             </sbb-title>
-          </div>`,
+          `,
+          { backgroundColor: 'var(--sbb-color-charcoal)' },
         );
       }),
     );
