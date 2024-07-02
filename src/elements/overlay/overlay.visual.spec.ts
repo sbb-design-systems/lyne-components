d19b4729603e67f216c98e74263cff5b70b216d8
Davide Mininni <davide.mininni@finconsgroup.com>
2024-07-02 10:52:20 +0200

    fix: review

--- src/elements/overlay/overlay.visual.spec.ts
+++ src/elements/overlay/overlay.visual.spec.ts
@@ -1,4 +1,5 @@
 import { html, type TemplateResult } from 'lit';
+import { repeat } from 'lit/directives/repeat.js';
 
 import { describeViewports, visualDiffDefault } from '../core/testing/private.js';
 
@@ -10,17 +11,27 @@ describe(`sbb-overlay`, () => {
     expanded: false,
     backButton: false,
     negative: false,
+    numberOfBlocks: 1,
   };
 
-  const template = ({ expanded, backButton, negative }: typeof defaultArgs): TemplateResult => html`
+  const template = ({
+    expanded,
+    backButton,
+    negative,
+    numberOfBlocks,
+  }: typeof defaultArgs): TemplateResult => html`
     <sbb-overlay ?negative=${negative} ?expanded=${expanded} ?back-button=${backButton}>
-      <div class="overlay-content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </div>
+      ${repeat(
+        new Array(numberOfBlocks),
+        () => html`
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum. <br />
+        `,
+      )}
     </sbb-overlay>
   `;
 
@@ -33,7 +44,7 @@ describe(`sbb-overlay`, () => {
   describeViewports({ viewportHeight: 600 }, () => {
     for (const negative of [false, true]) {
       it(
-        `negative=${negative} ${visualDiffDefault.name}`,
+        `negative=${negative}`,
         visualDiffDefault.with(async (setup) => {
           await setup.withFixture(template({ ...defaultArgs, negative }));
           openOverlay(setup);
@@ -41,7 +52,7 @@ describe(`sbb-overlay`, () => {
       );
 
       it(
-        `negative=${negative} backButton ${visualDiffDefault.name}`,
+        `negative=${negative} backButton`,
         visualDiffDefault.with(async (setup) => {
           await setup.withFixture(template({ ...defaultArgs, negative, backButton: true }));
           openOverlay(setup);
@@ -49,9 +60,17 @@ describe(`sbb-overlay`, () => {
       );
 
       it(
-        `negative=${negative} expanded ${visualDiffDefault.name}`,
+        `negative=${negative} expanded`,
         visualDiffDefault.with(async (setup) => {
           await setup.withFixture(template({ ...defaultArgs, negative, expanded: true }));
+          openOverlay(setup);
+        }),
+      );
+
+      it(
+        `negative=${negative} long content`,
+        visualDiffDefault.with(async (setup) => {
+          await setup.withFixture(template({ ...defaultArgs, negative, numberOfBlocks: 7 }));
           openOverlay(setup);
         }),
       );
