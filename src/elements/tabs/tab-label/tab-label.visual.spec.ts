30f55e55ae326fcbbff209a3552541cc9864011e
Mario Castigliano <mario.castigliano@finconsgroup.com>
2024-07-03 10:14:31 +0200

    fix: review comments

--- src/elements/tabs/tab-label/tab-label.visual.spec.ts
+++ src/elements/tabs/tab-label/tab-label.visual.spec.ts
@@ -1,24 +1,26 @@
-import { html } from 'lit';
+import { html, nothing } from 'lit';
 
 import {
   describeEach,
   describeViewports,
+  visualDiffActive,
   visualDiffDefault,
-  visualDiffStandardStates,
+  visualDiffHover,
 } from '../../core/testing/private.js';
 
 import '../tab-group.js';
 import '../tab-label.js';
 import '../tab.js';
+import '../../icon.js';
 
 const cases = {
-  amount: ['', 16],
-  iconName: ['', 'app-icon-small'],
+  amount: [false, true],
+  icon: [false, true],
 };
 
 describe(`sbb-tab-label`, () => {
   describeViewports({ viewports: ['zero', 'medium'] }, () => {
-    for (const state of visualDiffStandardStates) {
+    for (const state of [visualDiffDefault, visualDiffHover, visualDiffActive]) {
       it(
         state.name,
         state.with(async (setup) => {
@@ -27,12 +29,16 @@ describe(`sbb-tab-label`, () => {
       );
     }
 
-    describeEach(cases, ({ amount, iconName }) => {
+    describeEach(cases, ({ amount, icon }) => {
       it(
         visualDiffDefault.name,
         visualDiffDefault.with(async (setup) => {
           await setup.withFixture(
-            html`<sbb-tab-label amount=${amount} icon-name=${iconName}>Tab title</sbb-tab-label>`,
+            html`<sbb-tab-label
+              amount=${amount ? 16 : nothing}
+              icon-name=${icon ? 'app-icon-small' : nothing}
+              >Tab title</sbb-tab-label
+            >`,
           );
         }),
       );
@@ -56,6 +62,30 @@ describe(`sbb-tab-label`, () => {
           html` <sbb-tab-label amount="16" icon-name="app-icon-small" disabled
             >Tab title</sbb-tab-label
           >`,
+        );
+      }),
+    );
+
+    it(
+      'slotted icon',
+      visualDiffDefault.with(async (setup) => {
+        await setup.withFixture(
+          html` <sbb-tab-label amount="16"
+            >Tab title
+            <sbb-icon name="app-icon-small" slot="icon"></sbb-icon>
+          </sbb-tab-label>`,
+        );
+      }),
+    );
+
+    it(
+      'slotted amount',
+      visualDiffDefault.with(async (setup) => {
+        await setup.withFixture(
+          html` <sbb-tab-label icon-name="app-icon-small"
+            >Tab title
+            <span slot="amount">16</span>
+          </sbb-tab-label>`,
         );
       }),
     );
