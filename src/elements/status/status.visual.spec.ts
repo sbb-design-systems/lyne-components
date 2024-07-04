d2f3b304562f79a09c8f73f864a4fcd984959b6b
Jeremias Peier <jeremias.peier@sbb.ch>
2024-07-04 08:27:00 +0200

    test: add more cases

--- src/elements/status/status.visual.spec.ts
+++ src/elements/status/status.visual.spec.ts
@@ -21,36 +21,68 @@ describe(`sbb-status`, () => {
     describeEach(cases, ({ type, titleContent }) => {
       beforeEach(async function () {
         root = await visualRegressionFixture(html`
-          <sbb-status type=${type} title-content=${titleContent || nothing}
-            >Status text.</sbb-status
-          >
+          <sbb-status type=${type} title-content=${titleContent || nothing}>
+            Status text.
+          </sbb-status>
         `);
       });
 
       it(
-       '',
+        '',
         visualDiffDefault.with((setup) => {
           setup.withSnapshotElement(root);
         }),
       );
     });
 
-    it(
-      'custom icon',
-      visualDiffDefault.with(async (setup) => {
-        await setup.withFixture(html`
-          <sbb-status icon-name="pie-small">Status text.</sbb-status>
-        `);
-      }),
-    );
+    for (const titleContent of [undefined, 'Title']) {
+      describe(`title=${titleContent}`, () => {
+        it(
+          'custom icon',
+          visualDiffDefault.with(async (setup) => {
+            await setup.withFixture(html`
+              <sbb-status
+                icon-name="pie-small"
+                title-content=${titleContent || nothing}
+                type="success"
+              >
+                Status text.
+              </sbb-status>
+            `);
+          }),
+        );
 
-    it(
-      'custom slotted icon',
-      visualDiffDefault.with(async (setup) => {
-        await setup.withFixture(html`
-          <sbb-status><sbb-icon slot="icon" name="pie-small"></sbb-icon>Status text.</sbb-status>
-        `);
-      }),
-    );
+        it(
+          'custom slotted icon',
+          visualDiffDefault.with(async (setup) => {
+            await setup.withFixture(html`
+              <sbb-status title-content=${titleContent || nothing} type="success">
+                <sbb-icon slot="icon" name="pie-small"></sbb-icon>
+                Status text.
+              </sbb-status>
+            `);
+          }),
+        );
+
+        it(
+          'long content',
+          visualDiffDefault.with(async (setup) => {
+            await setup.withFixture(html`
+              <sbb-status
+                title-content=${titleContent
+                  ? 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor'
+                  : nothing || nothing}
+                type="success"
+              >
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
+                eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+                takimata sanctus est Lorem ipsum dolor sit amet.
+              </sbb-status>
+            `);
+          }),
+        );
+      });
+    }
   });
 });
