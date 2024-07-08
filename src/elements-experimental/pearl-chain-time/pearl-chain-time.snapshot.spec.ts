8d87e6d85f48e99d90eaf02d46987d8cbc22d11a
Tommmaso Menga <tommaso.menga-94@hotmail.it>
2024-07-08 17:08:41 +0200

    test(sbb-pearl-chain-time): add visual spec

--- src/elements-experimental/pearl-chain-time/pearl-chain-time.snapshot.spec.ts
+++ src/elements-experimental/pearl-chain-time/pearl-chain-time.snapshot.spec.ts
@@ -5,233 +5,118 @@ import { html } from 'lit/static-html.js';
 import type { PtRideLeg } from '../core/timetable.js';
 
 import type { SbbPearlChainTimeElement } from './pearl-chain-time.js';
-
 import './pearl-chain-time.js';
 
 const now = '2022-08-16T15:00:00Z';
 
 describe(`sbb-pearl-chain-time`, () => {
-  it('should render component with time', async () => {
-    const element = await fixture<SbbPearlChainTimeElement>(html`
-      <sbb-pearl-chain-time
-        departure-time="2022-08-16T12:00:00"
-        arrival-time="2022-08-16T15:00:00"
-        .now=${now}
-        .legs=${[
-          {
-            __typename: 'PTRideLeg',
-          } as PtRideLeg,
-        ]}
-      >
-      </sbb-pearl-chain-time>
-    `);
+  let element: SbbPearlChainTimeElement;
 
-    expect(element).dom.to.be.equal(`
-      <sbb-pearl-chain-time arrival-time="2022-08-16T15:00:00" departure-time="2022-08-16T12:00:00">
-      </sbb-pearl-chain-time>
-    `);
-    expect(element).shadowDom.to.be.equal(`
-      <div class="sbb-pearl-chain__time">
-        <time class="sbb-pearl-chain__time-time" datetime="2022-08-16T12:00:00">
-          <span class="sbb-screen-reader-only">
-            Departure:
-          </span>
-          12:00
-        </time>
-        <sbb-pearl-chain class="sbb-pearl-chain__time-chain"></sbb-pearl-chain>
-        <time class="sbb-pearl-chain__time-time" datetime="2022-08-16T15:00:00">
-          <span class="sbb-screen-reader-only">
-            Arrival:
-          </span>
-          15:00
-        </time>
-      </div>
-    `);
+  describe('renders with time', () => {
+    beforeEach(async () => {
+      element = await fixture(html`
+        <sbb-pearl-chain-time
+          departure-time="2022-08-16T12:00:00"
+          arrival-time="2022-08-16T15:00:00"
+          .now=${now}
+          .legs=${[
+            {
+              __typename: 'PTRideLeg',
+            } as PtRideLeg,
+          ]}
+        >
+        </sbb-pearl-chain-time>
+      `);
+    });
+
+    it('DOM', async () => {
+      await expect(element).dom.to.be.equalSnapshot();
+    });
+
+    it('Shadow DOM', async () => {
+      await expect(element).shadowDom.to.be.equalSnapshot();
+    });
   });
 
-  it('should render component with departure walk', async () => {
-    const element = await fixture<SbbPearlChainTimeElement>(html`
-      <sbb-pearl-chain-time
-        departure-time="2022-08-16T12:00:00"
-        arrival-time="2022-08-16T15:00:00"
-        departure-walk="10"
-        .now=${now}
-        .legs=${[
-          {
-            __typename: 'PTRideLeg',
-          } as PtRideLeg,
-        ]}
-      >
-      </sbb-pearl-chain-time>
-    `);
+  describe('renders with departure walk', () => {
+    beforeEach(async () => {
+      element = await fixture(html`
+        <sbb-pearl-chain-time
+          departure-time="2022-08-16T12:00:00"
+          arrival-time="2022-08-16T15:00:00"
+          departure-walk="10"
+          .now=${now}
+          .legs=${[
+            {
+              __typename: 'PTRideLeg',
+            } as PtRideLeg,
+          ]}
+        >
+        </sbb-pearl-chain-time>
+      `);
+    });
 
-    expect(element).dom.to.be.equal(`
-      <sbb-pearl-chain-time departure-time='2022-08-16T12:00:00' arrival-time='2022-08-16T15:00:00' departure-walk="10">
-      </sbb-pearl-chain-time>
-    `);
-    expect(element).shadowDom.to.be.equal(`
-      <div class="sbb-pearl-chain__time">
-        <span class="sbb-pearl-chain__time-walktime sbb-pearl-chain__time-walktime--left">
-          <sbb-icon
-            aria-hidden="true"
-            data-namespace="default"
-            name="walk-small"
-            role="img"
-          ></sbb-icon>
-          <time datetime="10M">
-            <span class="sbb-screen-reader-only">
-              minutes of walking time before departure:
-            </span>
-            10
-            <span class="sbb-pearl-chain__time-walktime-prime-symbol" aria-hidden="true">
-              '
-            </span>
-          </time>
-        </span>
-        <time class="sbb-pearl-chain__time-time" datetime="2022-08-16T12:00:00">
-          <span class="sbb-screen-reader-only">
-            Departure:
-          </span>
-          12:00
-        </time>
-        <sbb-pearl-chain class="sbb-pearl-chain__time-chain"></sbb-pearl-chain>
-        <time class="sbb-pearl-chain__time-time" datetime="2022-08-16T15:00:00">
-          <span class="sbb-screen-reader-only">
-            Arrival:
-          </span>
-          15:00
-        </time>
-      </div>
-    `);
+    it('DOM', async () => {
+      await expect(element).dom.to.be.equalSnapshot();
+    });
+
+    it('Shadow DOM', async () => {
+      await expect(element).shadowDom.to.be.equalSnapshot();
+    });
   });
 
-  it('should render component with arrival walk', async () => {
-    const element = await fixture<SbbPearlChainTimeElement>(html`
-      <sbb-pearl-chain-time
-        departure-time="2022-08-16T12:00:00"
-        arrival-time="2022-08-16T15:00:00"
-        arrival-walk="10"
-        .now=${now}
-        .legs=${[
-          {
-            __typename: 'PTRideLeg',
-          } as PtRideLeg,
-        ]}
-      >
-      </sbb-pearl-chain-time>
-    `);
+  describe('renders with arrival walk', () => {
+    beforeEach(async () => {
+      element = await fixture(html`
+        <sbb-pearl-chain-time
+          departure-time="2022-08-16T12:00:00"
+          arrival-time="2022-08-16T15:00:00"
+          arrival-walk="10"
+          .now=${now}
+          .legs=${[
+            {
+              __typename: 'PTRideLeg',
+            } as PtRideLeg,
+          ]}
+        >
+        </sbb-pearl-chain-time>
+      `);
+    });
 
-    expect(element).dom.to.be.equal(`
-      <sbb-pearl-chain-time arrival-time="2022-08-16T15:00:00" departure-time="2022-08-16T12:00:00" arrival-walk="10">
-      </sbb-pearl-chain-time>
-    `);
-    expect(element).shadowDom.to.be.equal(`
-      <div class="sbb-pearl-chain__time">
-        <time class="sbb-pearl-chain__time-time" datetime="2022-08-16T12:00:00">
-          <span class="sbb-screen-reader-only">
-            Departure:
-          </span>
-          12:00
-        </time>
-        <sbb-pearl-chain class="sbb-pearl-chain__time-chain"></sbb-pearl-chain>
-        <time class="sbb-pearl-chain__time-time" datetime="2022-08-16T15:00:00">
-          <span class="sbb-screen-reader-only">
-            Arrival:
-          </span>
-          15:00
-        </time>
-        <span class="sbb-pearl-chain__time-walktime sbb-pearl-chain__time-walktime--right">
-          <sbb-icon
-            aria-hidden="true"
-            data-namespace="default"
-            name="walk-small"
-            role="img"
-          ></sbb-icon>
-          <time datetime="10M">
-            <span class="sbb-screen-reader-only">
-              minutes of walking time after arrival:
-            </span>
-            10
-            <span class="sbb-pearl-chain__time-walktime-prime-symbol" aria-hidden="true">
-              '
-            </span>
-          </time>
-        </span>
-      </div>
-    `);
+    it('DOM', async () => {
+      await expect(element).dom.to.be.equalSnapshot();
+    });
+
+    it('Shadow DOM', async () => {
+      await expect(element).shadowDom.to.be.equalSnapshot();
+    });
   });
 
-  it('should render component with departure and arrival walk', async () => {
-    const element = await fixture<SbbPearlChainTimeElement>(html`
-      <sbb-pearl-chain-time
-        departure-time="2022-08-16T12:00:00"
-        arrival-time="2022-08-16T15:00:00"
-        departure-walk="20"
-        arrival-walk="10"
-        .now=${now}
-        .legs=${[
-          {
-            __typename: 'PTRideLeg',
-          } as PtRideLeg,
-        ]}
-      >
-      </sbb-pearl-chain-time>
-    `);
+  describe('renders with departure and arrival walk', () => {
+    beforeEach(async () => {
+      element = await fixture(html`
+        <sbb-pearl-chain-time
+          departure-time="2022-08-16T12:00:00"
+          arrival-time="2022-08-16T15:00:00"
+          departure-walk="20"
+          arrival-walk="10"
+          .now=${now}
+          .legs=${[
+            {
+              __typename: 'PTRideLeg',
+            } as PtRideLeg,
+          ]}
+        >
+        </sbb-pearl-chain-time>
+      `);
+    });
 
-    expect(element).dom.to.be.equal(`
-      <sbb-pearl-chain-time arrival-time="2022-08-16T15:00:00" departure-time="2022-08-16T12:00:00" departure-walk="20" arrival-walk="10">
-      </sbb-pearl-chain-time>
-    `);
-    expect(element).shadowDom.to.be.equal(`
-      <div class="sbb-pearl-chain__time">
-        <span class="sbb-pearl-chain__time-walktime sbb-pearl-chain__time-walktime--left">
-          <sbb-icon
-            aria-hidden="true"
-            data-namespace="default"
-            name="walk-small"
-            role="img"
-          ></sbb-icon>
-          <time datetime="20M">
-            <span class="sbb-screen-reader-only">
-              minutes of walking time before departure:
-            </span>
-            20
-            <span class="sbb-pearl-chain__time-walktime-prime-symbol" aria-hidden="true">
-              '
-            </span>
-          </time>
-        </span>
-        <time class="sbb-pearl-chain__time-time" datetime="2022-08-16T12:00:00">
-          <span class="sbb-screen-reader-only">
-            Departure:
-          </span>
-          12:00
-        </time>
-        <sbb-pearl-chain class="sbb-pearl-chain__time-chain"></sbb-pearl-chain>
-        <time class="sbb-pearl-chain__time-time" datetime="2022-08-16T15:00:00">
-          <span class="sbb-screen-reader-only">
-            Arrival:
-          </span>
-          15:00
-        </time>
-        <span class="sbb-pearl-chain__time-walktime sbb-pearl-chain__time-walktime--right">
-          <sbb-icon
-            aria-hidden="true"
-            data-namespace="default"
-            name="walk-small"
-            role="img"
-          ></sbb-icon>
-          <time datetime="10M">
-            <span class="sbb-screen-reader-only">
-              minutes of walking time after arrival:
-            </span>
-            10
-            <span class="sbb-pearl-chain__time-walktime-prime-symbol" aria-hidden="true">
-              '
-            </span>
-          </time>
-        </span>
-      </div>
-    `);
+    it('DOM', async () => {
+      await expect(element).dom.to.be.equalSnapshot();
+    });
+
+    it('Shadow DOM', async () => {
+      await expect(element).shadowDom.to.be.equalSnapshot();
+    });
   });
 });
