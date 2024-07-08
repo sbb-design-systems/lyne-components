f1ca281c9806458d51663f603bd67b866f86a60c
Tommmaso Menga <tommaso.menga-94@hotmail.it>
2024-07-08 15:58:51 +0200

    test(sbb-pearl-chain): refactor snapshots

---
+++ src/elements-experimental/pearl-chain/__snapshots__/pearl-chain.snapshot.spec.snap.js
@@ -0,0 +1,169 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["sbb-pearl-chain renders with one leg DOM"] = 
+`<sbb-pearl-chain>
+</sbb-pearl-chain>
+`;
+/* end snapshot sbb-pearl-chain renders with one leg DOM */
+
+snapshots["sbb-pearl-chain renders with one leg Shadow DOM"] = 
+`<div class="sbb-pearl-chain">
+  <span class="sbb-pearl-chain__bullet sbb-pearl-chain__bullet--past">
+  </span>
+  <div
+    class="sbb-pearl-chain__leg sbb-pearl-chain__leg--past"
+    style="--sbb-pearl-chain-leg-width:100%;"
+  >
+  </div>
+  <span class="sbb-pearl-chain__bullet sbb-pearl-chain__bullet--past">
+  </span>
+</div>
+`;
+/* end snapshot sbb-pearl-chain renders with one leg Shadow DOM */
+
+snapshots["sbb-pearl-chain renders with two legs DOM"] = 
+`<sbb-pearl-chain>
+</sbb-pearl-chain>
+`;
+/* end snapshot sbb-pearl-chain renders with two legs DOM */
+
+snapshots["sbb-pearl-chain renders with two legs Shadow DOM"] = 
+`<div class="sbb-pearl-chain">
+  <span class="sbb-pearl-chain__bullet sbb-pearl-chain__bullet--past">
+  </span>
+  <div
+    class="sbb-pearl-chain__leg sbb-pearl-chain__leg--past"
+    style="--sbb-pearl-chain-leg-width:8.333333333333332%;"
+  >
+  </div>
+  <div
+    class="sbb-pearl-chain__leg sbb-pearl-chain__leg--past"
+    style="--sbb-pearl-chain-leg-width:91.66666666666666%;"
+  >
+    <span class="sbb-pearl-chain__stop">
+    </span>
+  </div>
+  <span class="sbb-pearl-chain__bullet sbb-pearl-chain__bullet--past">
+  </span>
+</div>
+`;
+/* end snapshot sbb-pearl-chain renders with two legs Shadow DOM */
+
+snapshots["sbb-pearl-chain renders with departure stop skipped DOM"] = 
+`<sbb-pearl-chain>
+</sbb-pearl-chain>
+`;
+/* end snapshot sbb-pearl-chain renders with departure stop skipped DOM */
+
+snapshots["sbb-pearl-chain renders with departure stop skipped Shadow DOM"] = 
+`<div class="sbb-pearl-chain">
+  <span class="sbb-pearl-chain__bullet sbb-pearl-chain__bullet--past">
+  </span>
+  <div
+    class="sbb-pearl-chain__leg sbb-pearl-chain__leg--past"
+    style="--sbb-pearl-chain-leg-width:8.333333333333332%;"
+  >
+  </div>
+  <div
+    class="sbb-pearl-chain__leg sbb-pearl-chain__leg--skipped"
+    style="--sbb-pearl-chain-leg-width:91.66666666666666%;"
+  >
+    <span class="sbb-pearl-chain__stop sbb-pearl-chain__stop--departure-skipped">
+    </span>
+  </div>
+  <span class="sbb-pearl-chain__bullet sbb-pearl-chain__bullet--past">
+  </span>
+</div>
+`;
+/* end snapshot sbb-pearl-chain renders with departure stop skipped Shadow DOM */
+
+snapshots["sbb-pearl-chain renders with arrival stop skipped DOM"] = 
+`<sbb-pearl-chain>
+</sbb-pearl-chain>
+`;
+/* end snapshot sbb-pearl-chain renders with arrival stop skipped DOM */
+
+snapshots["sbb-pearl-chain renders with arrival stop skipped Shadow DOM"] = 
+`<div class="sbb-pearl-chain">
+  <span class="sbb-pearl-chain__bullet sbb-pearl-chain__bullet--past">
+  </span>
+  <div
+    class="sbb-pearl-chain__leg sbb-pearl-chain__leg--past"
+    style="--sbb-pearl-chain-leg-width:8.333333333333332%;"
+  >
+  </div>
+  <div
+    class="sbb-pearl-chain__leg sbb-pearl-chain__leg--skipped"
+    style="--sbb-pearl-chain-leg-width:91.66666666666666%;"
+  >
+    <span class="sbb-pearl-chain__stop">
+    </span>
+  </div>
+  <span class="sbb-pearl-chain--arrival-skipped sbb-pearl-chain__bullet sbb-pearl-chain__bullet--past">
+  </span>
+</div>
+`;
+/* end snapshot sbb-pearl-chain renders with arrival stop skipped Shadow DOM */
+
+snapshots["sbb-pearl-chain renders with progress leg DOM"] = 
+`<sbb-pearl-chain>
+</sbb-pearl-chain>
+`;
+/* end snapshot sbb-pearl-chain renders with progress leg DOM */
+
+snapshots["sbb-pearl-chain renders with progress leg Shadow DOM"] = 
+`<div class="sbb-pearl-chain">
+  <span class="sbb-pearl-chain__bullet sbb-pearl-chain__bullet--progress">
+  </span>
+  <div
+    class="sbb-pearl-chain__leg sbb-pearl-chain__leg--progress"
+    style="--sbb-pearl-chain-leg-width:97.36842105263158%;--sbb-pearl-chain-leg-status:66.21621621621621%;"
+  >
+    <span
+      class="sbb-pearl-chain__position"
+      style="--sbb-pearl-chain-status-position:66.21621621621621%;transform:translateX(-100%);"
+    >
+    </span>
+  </div>
+  <div
+    class="sbb-pearl-chain__leg sbb-pearl-chain__leg--future"
+    style="--sbb-pearl-chain-leg-width:2.631578947368421%;"
+  >
+    <span class="sbb-pearl-chain__stop">
+    </span>
+  </div>
+  <span class="sbb-pearl-chain__bullet sbb-pearl-chain__bullet--future">
+  </span>
+</div>
+`;
+/* end snapshot sbb-pearl-chain renders with progress leg Shadow DOM */
+
+snapshots["sbb-pearl-chain renders with cancelled instead of progress leg DOM"] = 
+`<sbb-pearl-chain>
+</sbb-pearl-chain>
+`;
+/* end snapshot sbb-pearl-chain renders with cancelled instead of progress leg DOM */
+
+snapshots["sbb-pearl-chain renders with cancelled instead of progress leg Shadow DOM"] = 
+`<div class="sbb-pearl-chain">
+  <span class="sbb-pearl-chain--departure-skipped sbb-pearl-chain__bullet sbb-pearl-chain__bullet--progress">
+  </span>
+  <div
+    class="sbb-pearl-chain__leg sbb-pearl-chain__leg--skipped"
+    style="--sbb-pearl-chain-leg-width:66.66666666666666%;"
+  >
+  </div>
+  <div
+    class="sbb-pearl-chain__leg sbb-pearl-chain__leg--future"
+    style="--sbb-pearl-chain-leg-width:33.33333333333333%;"
+  >
+    <span class="sbb-pearl-chain__stop">
+    </span>
+  </div>
+  <span class="sbb-pearl-chain__bullet sbb-pearl-chain__bullet--future">
+  </span>
+</div>
+`;
+/* end snapshot sbb-pearl-chain renders with cancelled instead of progress leg Shadow DOM */
+
