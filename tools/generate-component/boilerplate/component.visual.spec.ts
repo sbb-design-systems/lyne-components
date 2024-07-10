1d1e9a5080cf0385ac15af5f28fcdca5999cb21b
Davide Mininni <101575400+DavideMininni-Fincons@users.noreply.github.com>
2024-07-10 09:25:57 +0200

    chore: add visual.spec.ts to boilerplate code (#2910)

---
+++ tools/generate-component/boilerplate/component.visual.spec.ts
@@ -0,0 +1,57 @@
+import { html } from 'lit';
+
+import {
+  describeViewports,
+  describeEach,
+  visualDiffDefault,
+  visualDiffStandardStates,
+} from '../core/testing/private.js';
+
+import './__noPrefixName__.js';
+
+describe('__name__', () => {
+  /**
+   * Add the `viewports` param to test only specific viewport;
+   * add the `viewportHeight` param to set a fixed height for the browser.
+   */
+  describeViewports(() => {
+    // Create visual tests considering the implemented states (default, hover, active, focus)
+    for (const state of visualDiffStandardStates) {
+      it(
+        `${state.name}`,
+        state.with(async (setup) => {
+          await setup.withFixture(html`<__name__></__name__>`);
+        }),
+      );
+    }
+
+    /**
+     * Create visual tests combining the values of the provided object;
+     * useful when testing combinations of disabled, negative, visual variants, etc.
+     * eg.
+     *  1. one=true two={ name: 'A', value: 1 }
+     *  2. one=true two={ name: 'B', value: 2 }
+     *  3. one=false two={ name: 'A', value: 1 }
+     *  4. one=false two={ name: 'B', value: 2 }
+     */
+    const example = {
+      one: [true, false],
+      two: [
+        { name: 'A', value: 1 },
+        { name: 'B', value: 2 },
+      ],
+    };
+    describeEach(example, ({ one, two }) => {
+      it(
+        visualDiffDefault.name,
+        visualDiffDefault.with(async (setup) => {
+          await setup.withFixture(html`
+            <__name__ ?one=${one} two=${two.value}>
+              ${two.name}
+            </__name__>
+          `);
+        }),
+      );
+    });
+  });
+});
