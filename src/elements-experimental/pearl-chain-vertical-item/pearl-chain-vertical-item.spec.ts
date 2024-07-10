cbca3253a2644213888ed541e8fe3707d7a00818
Davide Mininni <101575400+DavideMininni-Fincons@users.noreply.github.com>
2024-07-10 17:29:09 +0200

    test(sbb-pearl-chain-vertical): add visual tests (#2915)

---
+++ src/elements-experimental/pearl-chain-vertical-item/pearl-chain-vertical-item.spec.ts
@@ -0,0 +1,14 @@
+import { assert } from '@open-wc/testing';
+import { fixture } from '@sbb-esta/lyne-elements/core/testing/private.js';
+import { html } from 'lit/static-html.js';
+
+import { SbbPearlChainVerticalItemElement } from './pearl-chain-vertical-item.js';
+
+describe(`sbb-pearl-chain`, () => {
+  let element: SbbPearlChainVerticalItemElement;
+
+  it('renders', async () => {
+    element = await fixture(html`<sbb-pearl-chain-vertical-item></sbb-pearl-chain-vertical-item>`);
+    assert.instanceOf(element, SbbPearlChainVerticalItemElement);
+  });
+});
