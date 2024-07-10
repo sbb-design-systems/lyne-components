5e90bc9de94f65ef604ecb1f56c148c5669b3e91
Tommaso Menga <tommaso.menga-94@hotmail.it>
2024-07-10 09:34:01 +0200

    test(sbb-teaser-hero, sbb-teaser-paid): add visual spec (#2914)

--- src/elements-experimental/teaser-paid/teaser-paid.snapshot.spec.ts
+++ src/elements-experimental/teaser-paid/teaser-paid.snapshot.spec.ts
@@ -9,24 +9,26 @@ import type { SbbTeaserPaidElement } from './teaser-paid.js';
 describe(`sbb-teaser-paid`, () => {
   let element: SbbTeaserPaidElement;
 
-  beforeEach(async () => {
-    element = await fixture(
-      html`<sbb-teaser-paid
-        accessibility-label="label"
-        href="https://www.sbb.ch"
-        rel="external"
-        target="_blank"
-      ></sbb-teaser-paid>`,
-    );
-  });
+  describe('renders', () => {
+    beforeEach(async () => {
+      element = await fixture(
+        html`<sbb-teaser-paid
+          accessibility-label="label"
+          href="https://www.sbb.ch"
+          rel="external"
+          target="_blank"
+        ></sbb-teaser-paid>`,
+      );
+    });
 
-  it('DOM', async () => {
-    await expect(element).dom.to.be.equalSnapshot();
-  });
+    it('DOM', async () => {
+      await expect(element).dom.to.be.equalSnapshot();
+    });
 
-  it('Shadow DOM', async () => {
-    await expect(element).shadowDom.to.be.equalSnapshot();
-  });
+    it('Shadow DOM', async () => {
+      await expect(element).shadowDom.to.be.equalSnapshot();
+    });
 
-  testA11yTreeSnapshot();
+    testA11yTreeSnapshot();
+  });
 });
