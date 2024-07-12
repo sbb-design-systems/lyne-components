d44d8de6398a246aca74399401ea2aca99000ed5
Davide Mininni <davide.mininni@finconsgroup.com>
2024-07-12 10:11:37 +0200

    chore: add visual tests for size

--- src/elements/visual-checkbox/visual-checkbox.visual.spec.ts
+++ src/elements/visual-checkbox/visual-checkbox.visual.spec.ts
@@ -10,26 +10,29 @@ import {
 import './visual-checkbox.js';
 
 describe(`sbb-visual-checkbox`, () => {
-  const cases = {
-    state: [
-      { indeterminate: undefined, checked: true },
-      { indeterminate: undefined, checked: false },
-      { indeterminate: true, checked: true },
-    ],
+  const states = [
+    { indeterminate: undefined, checked: true },
+    { indeterminate: undefined, checked: false },
+    { indeterminate: true, checked: true },
+  ];
+
+  const visualStates = {
     disabled: [false, true],
     negative: [false, true],
     forcedColors: [false, true],
   };
 
+  const size = ['xs', 's', 'm'];
+
   describeViewports({ viewports: ['zero'] }, () => {
     let root: HTMLElement;
 
-    describeEach(cases, ({ state, disabled, negative, forcedColors }) => {
+    describeEach({ states, ...visualStates }, ({ states, disabled, negative, forcedColors }) => {
       beforeEach(async function () {
         root = await visualRegressionFixture(
           html`<sbb-visual-checkbox
-            ?indeterminate=${state.indeterminate}
-            ?checked=${state.checked}
+            ?indeterminate=${states.indeterminate}
+            ?checked=${states.checked}
             ?disabled=${disabled}
             ?negative=${negative}
           ></sbb-visual-checkbox>`,
@@ -37,6 +40,25 @@ describe(`sbb-visual-checkbox`, () => {
             forcedColors,
             backgroundColor: negative ? 'var(--sbb-color-black)' : undefined,
           },
+        );
+      });
+
+      it(
+        visualDiffDefault.name,
+        visualDiffDefault.with((setup) => {
+          setup.withSnapshotElement(root);
+        }),
+      );
+    });
+
+    describeEach({ states, size }, ({ states, size }) => {
+      beforeEach(async function () {
+        root = await visualRegressionFixture(
+          html`<sbb-visual-checkbox
+            ?indeterminate=${states.indeterminate}
+            ?checked=${states.checked}
+            size=${size}
+          ></sbb-visual-checkbox>`,
         );
       });
 
