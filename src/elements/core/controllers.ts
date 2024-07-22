cbdc4f61c5f5118e89c114dc298031272de950d0
Lukas Spirig <lukas.spirig@sbb.ch>
2024-07-22 08:45:45 +0200

    feat(sbb-tooltip): initial implementation

--- src/elements/core/controllers.ts
+++ src/elements/core/controllers.ts
@@ -1,3 +1,4 @@
 export * from './controllers/connected-abort-controller.js';
 export * from './controllers/language-controller.js';
+export * from './controllers/overlay-controller.js';
 export * from './controllers/slot-state-controller.js';
