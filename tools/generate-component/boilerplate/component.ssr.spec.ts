cb4eecff3f79c83c96f850e9d8a9bed7250c0cfb
Tommaso Menga <tommaso.menga-94@hotmail.it>
2024-07-31 11:25:43 +0200

    build(boilerplate): fix ssr and snapshot spec (#2977)

--- tools/generate-component/boilerplate/component.ssr.spec.ts
+++ tools/generate-component/boilerplate/component.ssr.spec.ts
@@ -6,17 +6,15 @@ import { ssrHydratedFixture } from '../core/testing/private.js';
 import { __nameUpperCase__ } from './__noPrefixName__.js';
 
 describe(`__name__ ssr`, () => {
+  let root: __nameUpperCase__;
+
+  beforeEach(async () => {
+    root = await ssrHydratedFixture(html`<__name__ my-prop="Label"></__name__>`, {
+      modules: ['./__noPrefixName__.js'],
+    });
+  });
+
   it('renders', () => {
-    let root: __nameUpperCase__;
-
-    beforeEach(async () => {
-      root = await ssrHydratedFixture(html`<__name__ my-prop="Label"></__name__>`, {
-        modules: ['./__noPrefixName__.js'],
-      });
-    });
-
-    it('renders', () => {
-      assert.instanceOf(root, __nameUpperCase__);
-    });
+    assert.instanceOf(root, __nameUpperCase__);
   });
 });
