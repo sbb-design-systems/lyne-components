e84ed8b4f3108920da617f1b83123c79eca68970
Davide Mininni <davide.mininni@finconsgroup.com>
2024-09-04 11:52:25 +0200

    fix: review Jeri pt.4

--- src/elements/paginator/paginator.visual.spec.ts
+++ src/elements/paginator/paginator.visual.spec.ts
@@ -13,38 +13,15 @@ import './paginator.js';
 describe('sbb-paginator', () => {
   describeViewports({ viewports: ['zero', 'medium'] }, () => {
     for (const negative of [false, true]) {
-      const wrapperStyle = {
-        minHeight: '300px',
-        backgroundColor: negative ? 'var(--sbb-color-black)' : undefined,
-        focusOutlineDark: negative,
-      };
+      describe(`negative=${negative}`, () => {
+        const wrapperStyle = {
+          backgroundColor: negative ? 'var(--sbb-color-black)' : undefined,
+          focusOutlineDark: negative,
+        };
 
-      for (const state of [visualDiffDefault, visualDiffFocus]) {
-        it(
-          `negative=${negative} state=${state.name}`,
-          state.with(async (setup) => {
-            await setup.withFixture(
-              html` <sbb-paginator
-                ?negative=${negative || nothing}
-                length="50"
-                page-size="10"
-                pager-position="end"
-              ></sbb-paginator>`,
-              wrapperStyle,
-            );
-            setup.withStateElement(
-              setup.snapshotElement
-                .querySelector('sbb-paginator')!
-                .shadowRoot!.querySelector(`.sbb-paginator__page--number-item[data-index="0"]`)!,
-            );
-          }),
-        );
-      }
-
-      for (const state of [visualDiffActive, visualDiffHover]) {
-        for (const selected of [false, true]) {
+        for (const state of [visualDiffDefault, visualDiffFocus]) {
           it(
-            `negative=${negative} state=${state.name} selected=${selected}`,
+            `state=${state.name}`,
             state.with(async (setup) => {
               await setup.withFixture(
                 html` <sbb-paginator
@@ -58,49 +35,75 @@ describe('sbb-paginator', () => {
               setup.withStateElement(
                 setup.snapshotElement
                   .querySelector('sbb-paginator')!
-                  .shadowRoot!.querySelector(
-                    `.sbb-paginator__page--number-item[data-index="${selected ? 0 : 2}"]`,
-                  )!,
+                  .shadowRoot!.querySelector(`.sbb-paginator__page--number-item[data-index="0"]`)!,
               );
             }),
           );
         }
-      }
 
-      for (const pageIndex of [0, 2, 5, 7, 9]) {
-        it(
-          `negative=${negative} pageIndex=${pageIndex}`,
-          visualDiffDefault.with(async (setup) => {
-            await setup.withFixture(
-              html` <sbb-paginator
-                length="100"
-                page-index="${pageIndex}"
-                ?negative=${negative || nothing}
-              ></sbb-paginator>`,
-              wrapperStyle,
-            );
-          }),
-        );
-      }
+        for (const selected of [false, true]) {
+          describe(`selected=${selected}`, () => {
+            for (const state of [visualDiffActive, visualDiffHover]) {
+              it(
+                `state=${state.name}`,
+                state.with(async (setup) => {
+                  await setup.withFixture(
+                    html` <sbb-paginator
+                      ?negative=${negative || nothing}
+                      length="50"
+                      page-size="10"
+                      pager-position="end"
+                    ></sbb-paginator>`,
+                    wrapperStyle,
+                  );
+                  setup.withStateElement(
+                    setup.snapshotElement
+                      .querySelector('sbb-paginator')!
+                      .shadowRoot!.querySelector(
+                        `.sbb-paginator__page--number-item[data-index="${selected ? 0 : 2}"]`,
+                      )!,
+                  );
+                }),
+              );
+            }
+          });
+        }
 
-      for (const size of ['s', 'm']) {
-        it(
-          `negative=${negative} size=${size} pageSizeOptions`,
-          visualDiffDefault.with(async (setup) => {
-            const pageSizeOptions = [10, 20, 50];
-            await setup.withFixture(
-              html` <sbb-paginator
-                length="50"
-                page-size="4"
-                size=${size}
-                .pageSizeOptions="${pageSizeOptions}"
-                ?negative=${negative || nothing}
-              ></sbb-paginator>`,
-              wrapperStyle,
-            );
-          }),
-        );
-      }
+        for (const pageIndex of [0, 2, 5, 7, 9]) {
+          it(
+            `pageIndex=${pageIndex}`,
+            visualDiffDefault.with(async (setup) => {
+              await setup.withFixture(
+                html` <sbb-paginator
+                  length="100"
+                  page-index="${pageIndex}"
+                  ?negative=${negative || nothing}
+                ></sbb-paginator>`,
+                wrapperStyle,
+              );
+            }),
+          );
+        }
+
+        for (const size of ['s', 'm']) {
+          it(
+            `size=${size} pageSizeOptions`,
+            visualDiffDefault.with(async (setup) => {
+              const pageSizeOptions = [10, 20, 50];
+              await setup.withFixture(
+                html` <sbb-paginator
+                  length="50"
+                  page-size="4"
+                  size=${size}
+                  .pageSizeOptions="${pageSizeOptions}"
+                  ?negative=${negative || nothing}
+                ></sbb-paginator>`,
+                wrapperStyle,
+              );
+            }),
+          );
+        }
+      });
     }
   });
 });
