22ec235b5bef2a94dacb55da39664eab7ad4bc09
Jeremias Peier <jeremias.peier@sbb.ch>
2024-09-18 11:28:08 +0200

    feat: open close mechanism without tests

--- src/elements/container/sticky-bar/sticky-bar.ts
+++ src/elements/container/sticky-bar/sticky-bar.ts
@@ -1,13 +1,8 @@
 import { IntersectionController } from '@lit-labs/observers/intersection-controller.js';
-import {
-  type CSSResultGroup,
-  html,
-  LitElement,
-  type PropertyValues,
-  type TemplateResult,
-} from 'lit';
+import { type CSSResultGroup, html, type PropertyValues, type TemplateResult } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
+import { SbbOpenCloseBaseElement } from '../../core/base-elements.js';
 import { hostAttributes } from '../../core/decorators.js';
 
 import style from './sticky-bar.scss?lit&inline';
@@ -16,6 +11,10 @@ import style from './sticky-bar.scss?lit&inline';
  * A container that sticks to the bottom of the page if slotted into `sbb-container`.
  *
  * @slot - Use the unnamed slot to add content to the sticky bar.
+ * @event {CustomEvent<void>} willOpen - Emits when the opening animation starts. Can be canceled.
+ * @event {CustomEvent<void>} didOpen - Emits when the opening animation ends.
+ * @event {CustomEvent<void>} willClose - Emits when the closing animation starts. Can be canceled.
+ * @event {CustomEvent<void>} didClose - Emits when the closing animation ends.
  * @cssprop [--sbb-sticky-bar-padding-block=var(--sbb-spacing-responsive-xs)] - Block padding of the sticky bar.
  * @cssprop [--sbb-sticky-bar-bottom-overlapping-height=0px] - Define an additional area where
  * the sticky bar overlaps the following content on the bottom.
@@ -27,7 +26,7 @@ import style from './sticky-bar.scss?lit&inline';
 @hostAttributes({
   slot: 'sticky-bar',
 })
-export class SbbStickyBarElement extends LitElement {
+export class SbbStickyBarElement extends SbbOpenCloseBaseElement {
   public static override styles: CSSResultGroup = style;
 
   /** Color of the container, like transparent, white etc. */
@@ -43,6 +42,7 @@ export class SbbStickyBarElement extends LitElement {
 
   public override connectedCallback(): void {
     super.connectedCallback();
+    this.state = 'opened';
 
     const container = this.closest('sbb-container');
     if (container) {
@@ -87,9 +87,58 @@ export class SbbStickyBarElement extends LitElement {
     this.toggleAttribute('data-sticking', isSticky);
   }
 
+  /**
+   * Animates from content flow position to `position: sticky`.
+   */
+  public override open(): void {
+    if (!this.willOpen.emit() || this.state !== 'closed') {
+      return;
+    }
+
+    this.willOpen.emit();
+    this.state = 'opening';
+    if (!this.hasAttribute('data-sticking')) {
+      this._openedCallback();
+    }
+  }
+
+  /**
+   * Animates `position: sticky` to normal content flow position.
+   */
+  public override close(): void {
+    if (!this.willClose.emit() || this.state !== 'opened') {
+      return;
+    }
+
+    this.willClose.emit();
+    this.state = 'closing';
+
+    if (!this.hasAttribute('data-sticking')) {
+      this._closedCallback();
+    }
+  }
+
+  private _openedCallback(): void {
+    this.state = 'opened';
+    this.didOpen.emit();
+  }
+
+  private _closedCallback(): void {
+    this.didClose.emit();
+    this.state = 'closed';
+  }
+
+  private _onAnimationEnd(event: AnimationEvent): void {
+    if (this.state === 'opening' && event.animationName === 'open') {
+      this._openedCallback();
+    } else if (this.state === 'closing' && event.animationName === 'close') {
+      this._closedCallback();
+    }
+  }
+
   protected override render(): TemplateResult {
     return html`
-      <div class="sbb-sticky-bar__wrapper">
+      <div class="sbb-sticky-bar__wrapper" @animationend=${this._onAnimationEnd}>
         <div class="sbb-sticky-bar">
           <slot></slot>
         </div>
