0fc5f91e4c36c18ff35cb71526a64846341e984d
Jeremias Peier <jeremias.peier@sbb.ch>
2024-09-30 09:30:17 +0200

    refactor: button refactoring

--- src/elements/button/common/button-common.ts
+++ src/elements/button/common/button-common.ts
@@ -47,9 +47,7 @@ export const SbbButtonCommonElementMixin = <T extends AbstractConstructor<SbbAct
     protected override renderTemplate(): TemplateResult {
       return html`
         ${super.renderIconSlot()}
-        <span class="sbb-button__label">
-          <slot></slot>
-        </span>
+        <slot></slot>
       `;
     }
   }
