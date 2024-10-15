56d8ef9c302f5eca891f981638d00e0adda4df09
Mario Castigliano <mario.castigliano@finconsgroup.com>
2024-10-15 02:31:26 +0200

    refactor: first commit

--- src/elements-experimental/pearl-chain/pearl-chain.snapshot.spec.ts
+++ src/elements-experimental/pearl-chain/pearl-chain.snapshot.spec.ts
@@ -2,8 +2,6 @@ import { expect } from '@open-wc/testing';
 import { fixture } from '@sbb-esta/lyne-elements/core/testing/private.js';
 import { html } from 'lit/static-html.js';
 
-import type { PtRideLeg } from '../core/timetable.js';
-
 import type { SbbPearlChainElement } from './pearl-chain.js';
 import './pearl-chain.js';
 
@@ -15,15 +13,12 @@ describe(`sbb-pearl-chain`, () => {
   describe('renders with one leg', () => {
     beforeEach(async () => {
       element = await fixture(html`
-        <sbb-pearl-chain
-          .legs=${[
-            {
-              __typename: 'PTRideLeg',
-              arrival: { time: '2022-08-18T05:00' },
-              departure: { time: '2022-08-18T04:00' },
-            },
-          ]}
-        ></sbb-pearl-chain>
+        <sbb-pearl-chain>
+          <sbb-pearl-chain-leg
+            departure="2022-08-18T04:00"
+            arrival="2022-08-18T05:00"
+          ></sbb-pearl-chain-leg>
+        </sbb-pearl-chain>
       `);
     });
 
@@ -39,30 +34,16 @@ describe(`sbb-pearl-chain`, () => {
   describe('renders with two legs', () => {
     beforeEach(async () => {
       element = await fixture(html`
-        <sbb-pearl-chain
-          .legs=${[
-            {
-              __typename: 'PTRideLeg',
-              arrival: { time: '2022-08-18T05:00' },
-              departure: { time: '2022-08-18T04:00' },
-              serviceJourney: {
-                serviceAlteration: {
-                  cancelled: false,
-                },
-              },
-            } as PtRideLeg,
-            {
-              __typename: 'PTRideLeg',
-              arrival: { time: '2022-08-18T16:00' },
-              departure: { time: '2022-08-18T05:00' },
-              serviceJourney: {
-                serviceAlteration: {
-                  cancelled: false,
-                },
-              },
-            } as PtRideLeg,
-          ]}
-        ></sbb-pearl-chain>
+        <sbb-pearl-chain>
+          <sbb-pearl-chain-leg
+            departure="2022-08-18T04:00"
+            arrival="2022-08-18T05:00"
+          ></sbb-pearl-chain-leg>
+          <sbb-pearl-chain-leg
+            departure="2022-08-18T05:00"
+            arrival="2022-08-18T16:00"
+          ></sbb-pearl-chain-leg>
+        </sbb-pearl-chain>
       `);
     });
 
@@ -78,38 +59,17 @@ describe(`sbb-pearl-chain`, () => {
   describe('renders with departure stop skipped', () => {
     beforeEach(async () => {
       element = await fixture(html`
-        <sbb-pearl-chain
-          .legs=${[
-            {
-              __typename: 'PTRideLeg',
-              arrival: { time: '2022-08-18T05:00' },
-              departure: { time: '2022-08-18T04:00' },
-              serviceJourney: {
-                serviceAlteration: {
-                  cancelled: false,
-                },
-              },
-            } as PtRideLeg,
-            {
-              __typename: 'PTRideLeg',
-              arrival: { time: '2022-08-18T16:00' },
-              departure: { time: '2022-08-18T05:00' },
-              serviceJourney: {
-                serviceAlteration: {
-                  cancelled: false,
-                },
-                stopPoints: [
-                  {
-                    stopStatus: 'NOT_SERVICED',
-                  },
-                  {
-                    stopStatus: 'PLANNED',
-                  },
-                ],
-              },
-            } as PtRideLeg,
-          ]}
-        ></sbb-pearl-chain>
+        <sbb-pearl-chain>
+          <sbb-pearl-chain-leg
+            departure="2022-08-18T04:00"
+            arrival="2022-08-18T05:00"
+          ></sbb-pearl-chain-leg>
+          <sbb-pearl-chain-leg
+            departure="2022-08-18T05:00"
+            arrival="2022-08-18T16:00"
+            departure-skipped
+          ></sbb-pearl-chain-leg>
+        </sbb-pearl-chain>
       `);
     });
 
@@ -125,38 +85,17 @@ describe(`sbb-pearl-chain`, () => {
   describe('renders with arrival stop skipped', () => {
     beforeEach(async () => {
       element = await fixture(html`
-        <sbb-pearl-chain
-          .legs=${[
-            {
-              __typename: 'PTRideLeg',
-              arrival: { time: '2022-08-18T05:00' },
-              departure: { time: '2022-08-18T04:00' },
-              serviceJourney: {
-                serviceAlteration: {
-                  cancelled: false,
-                },
-              },
-            } as PtRideLeg,
-            {
-              __typename: 'PTRideLeg',
-              arrival: { time: '2022-08-18T16:00' },
-              departure: { time: '2022-08-18T05:00' },
-              serviceJourney: {
-                serviceAlteration: {
-                  cancelled: false,
-                },
-                stopPoints: [
-                  {
-                    stopStatus: 'PLANNED',
-                  },
-                  {
-                    stopStatus: 'NOT_SERVICED',
-                  },
-                ],
-              },
-            } as PtRideLeg,
-          ]}
-        ></sbb-pearl-chain>
+        <sbb-pearl-chain>
+          <sbb-pearl-chain-leg
+            departure="2022-08-18T04:00"
+            arrival="2022-08-18T05:00"
+          ></sbb-pearl-chain-leg>
+          <sbb-pearl-chain-leg
+            departure="2022-08-18T05:00"
+            arrival="2022-08-18T16:00"
+            arrival-skipped
+          ></sbb-pearl-chain-leg>
+        </sbb-pearl-chain>
       `);
     });
 
@@ -172,31 +111,17 @@ describe(`sbb-pearl-chain`, () => {
   describe('renders with progress leg', () => {
     beforeEach(async () => {
       element = await fixture(html`
-        <sbb-pearl-chain
-          .now=${now}
-          .legs=${[
-            {
-              __typename: 'PTRideLeg',
-              arrival: { time: '2022-08-17T16:00:00' },
-              departure: { time: '2022-08-14T14:00:00' },
-              serviceJourney: {
-                serviceAlteration: {
-                  cancelled: false,
-                },
-              },
-            } as PtRideLeg,
-            {
-              __typename: 'PTRideLeg',
-              arrival: { time: '2022-08-17T20:00:00' },
-              departure: { time: '2022-08-17T18:00:00' },
-              serviceJourney: {
-                serviceAlteration: {
-                  cancelled: false,
-                },
-              },
-            } as PtRideLeg,
-          ]}
-        ></sbb-pearl-chain>
+        <sbb-pearl-chain .now=${now}>
+          <sbb-pearl-chain-leg
+            departure="2022-08-14T14:00:00"
+            arrival="2022-08-17T16:00:00"
+          ></sbb-pearl-chain-leg>
+          <sbb-pearl-chain-leg
+            departure="2022-08-17T18:00:00"
+            arrival="2022-08-17T20:00:00"
+            departure-skipped
+          ></sbb-pearl-chain-leg>
+        </sbb-pearl-chain>
       `);
     });
 
@@ -212,39 +137,17 @@ describe(`sbb-pearl-chain`, () => {
   describe('renders with cancelled instead of progress leg', () => {
     beforeEach(async () => {
       element = await fixture(html`
-        <sbb-pearl-chain
-          .now=${now}
-          .legs=${[
-            {
-              __typename: 'PTRideLeg',
-              arrival: { time: '2022-08-16T16:00:00' },
-              departure: { time: '2022-08-16T14:00:00' },
-              serviceJourney: {
-                serviceAlteration: {
-                  cancelled: false,
-                },
-                stopPoints: [
-                  {
-                    stopStatus: 'NOT_SERVICED',
-                  },
-                  {
-                    stopStatus: 'PLANNED',
-                  },
-                ],
-              },
-            } as PtRideLeg,
-            {
-              __typename: 'PTRideLeg',
-              arrival: { time: '2022-08-16T18:00:00' },
-              departure: { time: '2022-08-16T17:00:00' },
-              serviceJourney: {
-                serviceAlteration: {
-                  cancelled: false,
-                },
-              },
-            } as PtRideLeg,
-          ]}
-        ></sbb-pearl-chain>
+        <sbb-pearl-chain .now=${now}>
+          <sbb-pearl-chain-leg
+            departure="2022-08-14T14:00:00"
+            arrival="2022-08-17T16:00:00"
+            disruption
+          ></sbb-pearl-chain-leg>
+          <sbb-pearl-chain-leg
+            departure="2022-08-17T18:00:00"
+            arrival="2022-08-17T20:00:00"
+          ></sbb-pearl-chain-leg>
+        </sbb-pearl-chain>
       `);
     });
 
