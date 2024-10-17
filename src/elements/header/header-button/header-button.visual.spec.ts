e9257dcf6856f78a3e8324027d3d06bc7f20d5d3
Jeremias Peier <jeremias.peier@sbb.ch>
2024-10-17 11:12:45 +0200

    fix: test forced colors

--- src/elements/header/header-button/header-button.visual.spec.ts
+++ src/elements/header/header-button/header-button.visual.spec.ts
@@ -6,22 +6,29 @@ import './header-button.js';
 
 describe(`sbb-header-button`, () => {
   describeViewports({ viewports: ['zero', 'medium'] }, () => {
-    for (const active of [false, true]) {
-      describe(`active=${active}`, () => {
-        for (const state of visualDiffStandardStates) {
-          it(
-            state.name,
-            state.with(async (setup) => {
-              await setup.withFixture(html`
-                <sbb-header-button
-                  icon-name="hamburger-menu-small"
-                  class=${active ? 'sbb-active' : ''}
-                >
-                  Menu
-                </sbb-header-button>
-              `);
-            }),
-          );
+    for (const forcedColors of [false, true]) {
+      describe(`forcedColors=${forcedColors}`, () => {
+        for (const active of [false, true]) {
+          describe(`active=${active}`, () => {
+            for (const state of visualDiffStandardStates) {
+              it(
+                state.name,
+                state.with(async (setup) => {
+                  await setup.withFixture(
+                    html`
+                      <sbb-header-button
+                        icon-name="hamburger-menu-small"
+                        class=${active ? 'sbb-active' : ''}
+                      >
+                        Menu
+                      </sbb-header-button>
+                    `,
+                    { forcedColors },
+                  );
+                }),
+              );
+            }
+          });
         }
       });
     }
