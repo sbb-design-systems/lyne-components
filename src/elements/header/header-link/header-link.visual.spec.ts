f6b4f38cc51fcd4f66cf01043e248e32251d7517
Jeremias Peier <jeremias.peier@sbb.ch>
2024-10-17 10:13:02 +0200

    feat(sbb-header): introduce active state

--- src/elements/header/header-link/header-link.visual.spec.ts
+++ src/elements/header/header-link/header-link.visual.spec.ts
@@ -6,15 +6,25 @@ import './header-link.js';
 
 describe(`sbb-header-link`, () => {
   describeViewports({ viewports: ['zero', 'medium'] }, () => {
-    for (const state of visualDiffStandardStates) {
-      it(
-        state.name,
-        state.with(async (setup) => {
-          await setup.withFixture(html`
-            <sbb-header-link icon-name="hamburger-menu-small" href="#">Menu</sbb-header-link>
-          `);
-        }),
-      );
+    for (const active of [false, true]) {
+      describe(`active=${active}`, () => {
+        for (const state of visualDiffStandardStates) {
+          it(
+            state.name,
+            state.with(async (setup) => {
+              await setup.withFixture(html`
+                <sbb-header-link
+                  icon-name="hamburger-menu-small"
+                  class=${active ? 'sbb-active' : ''}
+                  href="#"
+                >
+                  Menu
+                </sbb-header-link>
+              `);
+            }),
+          );
+        }
+      });
     }
   });
 });
