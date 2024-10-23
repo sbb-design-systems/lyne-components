842ae678cfbaf7a83eccf399cbb84384be56fab2
Mario Castigliano <mario.castigliano@finconsgroup.com>
2024-10-23 13:07:09 +0200

    refactor: leg stories

--- src/elements-experimental/pearl-chain-leg/pearl-chain-leg.stories.ts
+++ src/elements-experimental/pearl-chain-leg/pearl-chain-leg.stories.ts
@@ -1,13 +1,6 @@
 import { withActions } from '@storybook/addon-actions/decorator';
 import type { InputType } from '@storybook/types';
-import type {
-  Args,
-  ArgTypes,
-  Decorator,
-  Meta,
-  StoryContext,
-  StoryObj,
-} from '@storybook/web-components';
+import type { Args, ArgTypes, Decorator, Meta, StoryObj } from '@storybook/web-components';
 import type { TemplateResult } from 'lit';
 import { html } from 'lit';
 
@@ -16,22 +9,73 @@ import { sbbSpread } from '../../storybook/helpers/spread.js';
 import readme from './readme.md?raw';
 import './pearl-chain-leg.js';
 
-const myProp: InputType = {
+const departure: InputType = {
   control: {
-    type: 'text',
+    type: 'date',
+  },
+};
+
+const arrival: InputType = {
+  control: {
+    type: 'date',
+  },
+};
+
+const past: InputType = {
+  control: {
+    type: 'boolean',
+  },
+};
+const arrivalSkipped: InputType = {
+  control: {
+    type: 'boolean',
+  },
+};
+
+const departureSkipped: InputType = {
+  control: {
+    type: 'boolean',
+  },
+};
+
+const disruption: InputType = {
+  control: {
+    type: 'boolean',
+  },
+};
+
+const progress: InputType = {
+  control: {
+    type: 'boolean',
   },
 };
 
 const defaultArgTypes: ArgTypes = {
-  'my-prop': myProp,
+  departure,
+  arrival,
+  past,
+  'arrival-skipped': arrivalSkipped,
+  'departure-skipped': departureSkipped,
+  disruption,
+  progress,
 };
 
 const defaultArgs: Args = {
-  'my-prop': 'Label',
+  departure: new Date('2024-12-05T12:11:00'),
+  arrival: new Date('2024-12-05T15:11:00'),
+  past: false,
+  'arrival-skipped': false,
+  'departure-skipped': false,
+  disruption: false,
+  progress: false,
 };
 
-const Template = (args: Args): TemplateResult =>
-  html`<sbb-pearl-chain-leg ${sbbSpread(args)}></sbb-pearl-chain-leg>`;
+const Template = ({ progress, ...args }: Args): TemplateResult =>
+  html`<sbb-pearl-chain-leg
+    ?data-progress=${progress}
+    ${sbbSpread(args)}
+    style="--sbb-pearl-chain-status-position: 28%"
+  ></sbb-pearl-chain-leg>`;
 
 export const Default: StoryObj = {
   render: Template,
@@ -39,11 +83,33 @@ export const Default: StoryObj = {
   args: { ...defaultArgs },
 };
 
+export const Past: StoryObj = {
+  render: Template,
+  argTypes: defaultArgTypes,
+  args: { ...defaultArgs, past: true },
+};
+
+export const DepartureSkipped: StoryObj = {
+  render: Template,
+  argTypes: defaultArgTypes,
+  args: { ...defaultArgs, 'departure-skipped': true },
+};
+
+export const Disruption: StoryObj = {
+  render: Template,
+  argTypes: defaultArgTypes,
+  args: { ...defaultArgs, disruption: true },
+};
+
+export const InProgress: StoryObj = {
+  render: Template,
+  argTypes: defaultArgTypes,
+  args: { ...defaultArgs, progress: true },
+};
+
 const meta: Meta = {
   decorators: [withActions as Decorator],
   parameters: {
-    backgroundColor: (context: StoryContext) =>
-      context.args.negative ? 'var(--sbb-color-black)' : 'var(--sbb-color-white)',
     docs: {
       extractComponentDescription: () => readme,
     },
