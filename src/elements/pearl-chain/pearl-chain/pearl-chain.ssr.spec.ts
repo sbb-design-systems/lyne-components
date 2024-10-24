416f2809575bf095a37693e2c2efb756ca339bec
Mario Castigliano <mario.castigliano@finconsgroup.com>
2024-10-24 17:43:33 +0200

    fix: tests

--- src/elements/pearl-chain/pearl-chain/pearl-chain.ssr.spec.ts
+++ src/elements/pearl-chain/pearl-chain/pearl-chain.ssr.spec.ts
@@ -1,9 +1,9 @@
 import { assert } from '@open-wc/testing';
 import { html } from 'lit';
 
-import { SbbPearlChainElement } from './pearl-chain.js';
+import { ssrHydratedFixture } from '../../core/testing/private.js';
 
-import { ssrHydratedFixture } from '@sbb-esta/lyne-elements/core/testing/private.js';
+import { SbbPearlChainElement } from './pearl-chain.js';
 
 describe(`sbb-pearl-chain ssr`, () => {
   let root: SbbPearlChainElement;
