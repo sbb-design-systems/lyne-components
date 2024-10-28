fe36735dddef8d19970efcfa36eba058102bc6eb
Jeremias Peier <jeremias.peier@sbb.ch>
2024-10-28 08:14:37 +0100

    fix: review

--- scripts/manual-diff-check.ts
+++ scripts/manual-diff-check.ts
@@ -19,52 +19,63 @@ const { values: cliArgs } = parseArgs({
   },
 });
 
-async function downloadArtifact(prNumber: string): Promise<void> {
-  const prDetailsResponse = await fetch(
-    `https://api.github.com/repos/sbb-design-systems/lyne-components/pulls/${prNumber}`,
-  );
-
-  const prDetails = (await prDetailsResponse.json()) as any;
-  const branch = prDetails.head.ref;
-  const commitSha = prDetails.head.sha;
-
-  const workflowRunsResponse = await fetch(
-    `https://api.github.com/repos/sbb-design-systems/lyne-components/actions/runs?branch=${branch}&event=pull_request`,
-  );
-  const prWorkflowRuns = (await workflowRunsResponse.json()) as any;
-
-  const lastContinuousRun = prWorkflowRuns.workflow_runs.find(
-    (run: any) => run.head_sha === commitSha && run.name === 'Continuous Integration',
-  );
-  const runId = lastContinuousRun.id;
-
-  const artifactsResponse = await fetch(
-    `https://api.github.com/repos/sbb-design-systems/lyne-components/actions/runs/${runId}/artifacts`,
-  );
-  const artifacts = (await artifactsResponse.json()) as any;
-
-  const artifact = artifacts.artifacts.find(
-    (artifact: any) => artifact.name === 'visual-regression-screenshots',
-  );
-  const artifactDownloadUrl = artifact.archive_download_url;
-
-  const zipResponse = await fetch(artifactDownloadUrl, {
-    headers: {
-      Authorization: `Bearer ${cliArgs['github-token']}`,
-    },
-  });
-
-  const screenshotDir = new URL(`./screenshots/`, distDir);
-  if (!existsSync(screenshotDir)) {
-    mkdirSync(screenshotDir, { recursive: true });
-  }
-
-  const stream = createWriteStream(new URL('screenshots.zip', screenshotDir));
-  await finished(Readable.fromWeb(zipResponse.body!).pipe(stream));
-}
-
-if (cliArgs.pr && cliArgs['github-token']) {
-  downloadArtifact(String(cliArgs.pr!)).then(() => console.log('screenshots.zip downloaded'));
-} else {
+if (!(cliArgs.pr && cliArgs['github-token'])) {
   throw Error('PR number (--pr=) or github-token missing (--github-token=)');
 }
+
+const prDetailsResponse = await fetch(
+  `https://api.github.com/repos/sbb-design-systems/lyne-components/pulls/${cliArgs.pr}`,
+);
+
+const prDetails = (await prDetailsResponse.json()) as { head: { ref: string; sha: string } };
+const branch = prDetails.head.ref;
+const commitSha = prDetails.head.sha;
+
+const workflowRunsResponse = await fetch(
+  `https://api.github.com/repos/sbb-design-systems/lyne-components/actions/runs?branch=${branch}&event=pull_request`,
+);
+const prWorkflowRuns = (await workflowRunsResponse.json()) as {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  workflow_runs: { head_sha: string; name: string; id: string }[];
+};
+
+const lastContinuousRun = prWorkflowRuns.workflow_runs.find(
+  (run) => run.head_sha === commitSha && run.name === 'Continuous Integration',
+);
+if (!lastContinuousRun) {
+  throw Error('no workflow run found');
+}
+const runId = lastContinuousRun.id;
+
+const artifactsResponse = await fetch(
+  `https://api.github.com/repos/sbb-design-systems/lyne-components/actions/runs/${runId}/artifacts`,
+);
+const artifacts = (await artifactsResponse.json()) as {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  artifacts: { name: string; archive_download_url: string }[];
+};
+
+const artifact = artifacts.artifacts.find(
+  (artifact) => artifact.name === 'visual-regression-screenshots',
+);
+
+if (!artifact) {
+  throw Error('no artifact found');
+}
+const artifactDownloadUrl = artifact.archive_download_url;
+
+const zipResponse = await fetch(artifactDownloadUrl, {
+  headers: {
+    Authorization: `Bearer ${cliArgs['github-token']}`,
+  },
+});
+
+const screenshotDir = new URL(`./screenshots/`, distDir);
+if (!existsSync(screenshotDir)) {
+  mkdirSync(screenshotDir, { recursive: true });
+}
+
+const stream = createWriteStream(new URL('screenshots.zip', screenshotDir));
+await finished(Readable.fromWeb(zipResponse.body!).pipe(stream));
+
+console.log('screenshots.zip downloaded');
