5a00eef9519304e0e90c96fa869475690d5beaf0
Jeremias Peier <jeremias.peier@sbb.ch>
2024-11-20 10:50:31 +0100

    fix: cleanup deprecated core functionality
    BREAKING CHANGE: Several deprecated core functionality has been removed.
    - dom.js `getDocumentWritingMode()` removed
    - dom.js `getLocalName()` removed
    - eventing.js `formElementHandlerAspect()` removed
    - eventing.js `HandlerRepository`, `HandlerAspectParams` and `HandlerAspect` removed
    - mixins.js `SbbFocusableDisabledActionMixin` removed
    - observers.js completely removed (containing `NodeIntersectionObserver`,
    `AgnosticIntersectionObserver`, `NodeMutationObserver`, `AgnosticMutationObserver`,
    `NodeResizeObserver` and `AgnosticResizeObserver`)

--- src/elements/core/eventing.ts
+++ src/elements/core/eventing.ts
@@ -3,6 +3,5 @@ export * from './eventing/event-emitter.js';
 export * from './eventing/event-target.js';
 export * from './eventing/form-element-handlers.js';
 export * from './eventing/forward-event.js';
-export * from './eventing/handler-repository.js';
 export * from './eventing/is-event-prevented.js';
 export * from './eventing/throttle.js';
