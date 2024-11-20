4b129c4f2f581d6fa9b9ff7a96a4969f92838f86
Jeri Peier <jeremias.peier@sbb.ch>
2024-11-20 13:14:18 +0100

    fix: cleanup deprecated core functionality (#3219)
    BREAKING CHANGE: Several deprecated core functionality has been removed.
    - dom.js `getDocumentWritingMode()` removed
    - dom.js `getLocalName()` removed
    - eventing.js `formElementHandlerAspect()` removed
    - eventing.js `HandlerRepository`, `HandlerAspectParams` and
    `HandlerAspect` removed
    - mixins.js `SbbFocusableDisabledActionMixin` removed
    - observers.js completely removed (containing
    `NodeIntersectionObserver`, `AgnosticIntersectionObserver`,
    `NodeMutationObserver`, `AgnosticMutationObserver`, `NodeResizeObserver`
    and `AgnosticResizeObserver`)

--- src/elements/core/eventing/form-element-handlers.ts
+++ src/elements/core/eventing/form-element-handlers.ts
@@ -1,5 +1,3 @@
-import type { HandlerAspect } from './handler-repository.js';
-
 /**
  * Prevents scrolling from pressing Space
  * @param event The origin event.
@@ -9,12 +7,3 @@ export function preventScrollOnSpacebarPress(event: KeyboardEvent): void {
     event.preventDefault();
   }
 }
-
-/**
- * Handler to prevent scrolling on space bar click.
- *
- * @deprecated TODO: remove once radio button became form element
- */
-export const formElementHandlerAspect: HandlerAspect = ({ host, signal }) => {
-  host.addEventListener('keydown', preventScrollOnSpacebarPress, { signal });
-};
