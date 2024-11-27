35a56da61d7095c6c0cfced12f3ad2c7a9a95097
Davide Mininni <davide.mininni@finconsgroup.com>
2024-11-27 09:54:08 +0100

    chore: fix on method doc

--- src/elements/core/dom/lean-context.ts
+++ src/elements/core/dom/lean-context.ts
@@ -1,7 +1,7 @@
 import { isServer } from 'lit';
 
 /**
- * Checks if the body has the `sbb-lean` class.
+ * Checks if the document has the `sbb-lean` class.
  */
 export function isLean(): boolean {
   return !isServer && document.documentElement.classList.contains('sbb-lean');
