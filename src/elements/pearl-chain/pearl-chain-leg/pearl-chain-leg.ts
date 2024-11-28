77f0d3c3f400a3937248b64fd94c66b10b2a3c27
Mario Castigliano <mario.castigliano@finconsgroup.com>
2024-11-28 16:48:55 +0100

    fix: review part one

--- src/elements/pearl-chain/pearl-chain-leg/pearl-chain-leg.ts
+++ src/elements/pearl-chain/pearl-chain-leg/pearl-chain-leg.ts
@@ -1,9 +1,8 @@
 import type { CSSResultGroup, PropertyValues, TemplateResult } from 'lit';
-import { html, LitElement, nothing } from 'lit';
+import { html, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
 import { forceType } from '../../core/decorators.js';
-import type { SbbDateLike } from '../../core/interfaces.js';
 
 import style from './pearl-chain-leg.scss?lit&inline';
 
@@ -17,23 +16,23 @@ class SbbPearlChainLegElement extends LitElement {
 
   /** Departure time of the leg. */
   @property()
-  public set departure(value: SbbDateLike | null) {
+  public set departure(value: Date | null) {
     this._departure = value;
   }
-  public get departure(): SbbDateLike | null {
+  public get departure(): Date | null {
     return this._departure;
   }
-  private _departure: SbbDateLike | null = null;
+  private _departure: Date | null = null;
 
   /** Arrival time of the leg. */
   @property()
-  public set arrival(value: SbbDateLike | null) {
+  public set arrival(value: Date | null) {
     this._arrival = value;
   }
-  public get arrival(): SbbDateLike | null {
+  public get arrival(): Date | null {
     return this._arrival;
   }
-  private _arrival: SbbDateLike | null = null;
+  private _arrival: Date | null = null;
 
   /** Whether the leg is disrupted. */
   @forceType()
@@ -65,26 +64,22 @@ class SbbPearlChainLegElement extends LitElement {
   @property({ type: Number, attribute: 'arrival-delay' })
   public accessor arrivalDelay: number = 0;
 
-  private _displayStop(): boolean {
-    return !this.hasAttribute('data-first-leg');
-  }
+  protected override willUpdate(changedProperties: PropertyValues): void {
+    super.willUpdate(changedProperties);
 
-  protected override willUpdate(_changedProperties: PropertyValues): void {
-    super.willUpdate(_changedProperties);
-
-    //We need to update parent pearl-chain so that following leg can be styled properly.
-    if (_changedProperties.has('arrivalSkipped')) {
+    // We need to update parent pearl-chain so that following leg can be styled properly.
+    if (changedProperties.has('arrivalSkipped')) {
       const parentPearlChain = this.closest?.('sbb-pearl-chain');
       if (!parentPearlChain) {
         return;
       }
-      parentPearlChain.requestUpdate();
+      parentPearlChain?.requestUpdate();
     }
   }
 
   protected override render(): TemplateResult {
     return html` <div class="sbb-pearl-chain__leg">
-      ${this._displayStop() ? html` <span class="sbb-pearl-chain__stop"></span>` : nothing}
+      <span class="sbb-pearl-chain__stop"></span>
     </div>`;
   }
 }
