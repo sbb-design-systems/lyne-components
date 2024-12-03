8e0be99268bd52e884ec70c11c81e2e79cbd6eb7
Lukas Spirig <lukas.spirig@sbb.ch>
2024-12-03 10:12:45 +0100

    fix: provide correct react typings (#3269)
    This removes the SSR extensions, which have not been in use by now. This
    fixes an issue with the React typings.

--- tools/manifest/custom-elements-manifest.config.js
+++ tools/manifest/custom-elements-manifest.config.js
@@ -20,13 +20,6 @@ export function createManifestConfig(library = '') {
     plugins: [
       {
         analyzePhase({ ts, node, moduleDoc }) {
-          function setSsrSlotState(classNode) {
-            const classDoc = moduleDoc.declarations.find(
-              (declaration) => declaration.name === classNode.name.getText(),
-            );
-            classDoc['_ssrslotstate'] = true;
-          }
-
           function replace(typeObj, typeName, typeValue) {
             if (typeObj && typeObj.text) {
               typeObj.text = typeObj.text.replace(typeName, typeValue);
@@ -102,28 +95,7 @@ export function createManifestConfig(library = '') {
             });
           }
 
-          if (ts.isNewExpression(node) && node.expression.getText() === 'SbbSlotStateController') {
-            let classNode = node;
-            while (classNode) {
-              if (ts.isClassDeclaration(classNode)) {
-                setSsrSlotState(classNode);
-              }
-              classNode = classNode.parent;
-            }
-          }
-
           if (ts.isClassDeclaration(node)) {
-            /**
-             * The usage of the `slotState` decorator breaks the logic in the previous block of code,
-             * since the decorated class has no explicit usage of the `SbbSlotStateController` constructor any more.
-             */
-            const decorators = ts.getDecorators(node);
-            if (decorators && decorators.length > 0) {
-              if (decorators.find((e) => e.getText() === '@slotState()')) {
-                setSsrSlotState(node);
-              }
-            }
-
             /**
              * When a generic T type is used in a superclass declaration, it overrides the type defined in derived class
              * during the doc generation (as the `value` property in the `SbbFormAssociatedMixinType`).
