0d04586fe127f877ccc4325fdb1291a6b530c06a
Jeremias Peier <jeremias.peier@sbb.ch>
2024-12-05 10:30:17 +0100

    test: add tests

--- src/elements/notification/notification.spec.ts
+++ src/elements/notification/notification.spec.ts
@@ -13,21 +13,7 @@ describe(`sbb-notification`, () => {
   let element: SbbNotificationElement;
   let didOpenEventSpy: EventSpy<Event>;
 
-  beforeEach(async () => {
-    didOpenEventSpy = new EventSpy(SbbNotificationElement.events.didOpen);
-    element = await fixture(html`
-      <sbb-notification id="notification">
-        The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.
-        <sbb-link href="/">Link one</sbb-link>
-      </sbb-notification>
-    `);
-  });
-
-  it('renders', async () => {
-    assert.instanceOf(element, SbbNotificationElement);
-  });
-
-  it('closes the notification and removes it from the DOM', async () => {
+  async function openAndClose(): Promise<void> {
     const parent = element.parentElement!;
     const willCloseEventSpy = new EventSpy(SbbNotificationElement.events.willClose);
     const didCloseEventSpy = new EventSpy(SbbNotificationElement.events.didClose);
@@ -51,34 +37,68 @@ describe(`sbb-notification`, () => {
     await aTimeout(0);
     element = parent.querySelector<SbbNotificationElement>('sbb-notification')!;
     expect(element).to.be.null;
+  }
+
+  describe('with zero animation duration', () => {
+    beforeEach(async () => {
+      didOpenEventSpy = new EventSpy(SbbNotificationElement.events.didOpen);
+      element = await fixture(html`
+        <sbb-notification id="notification">
+          The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.
+          <sbb-link href="/">Link one</sbb-link>
+        </sbb-notification>
+      `);
+    });
+
+    it('renders', async () => {
+      assert.instanceOf(element, SbbNotificationElement);
+    });
+
+    it('closes the notification and removes it from the DOM', async () => {
+      await openAndClose();
+    });
+
+    it('closes the notification and removes it from the DOM on close button click', async () => {
+      const parent = element.parentElement!;
+      const willCloseEventSpy = new EventSpy(SbbNotificationElement.events.willClose);
+      const didCloseEventSpy = new EventSpy(SbbNotificationElement.events.didClose);
+      const closeButton = element.shadowRoot!.querySelector(
+        '.sbb-notification__close',
+      ) as SbbSecondaryButtonElement;
+
+      await didOpenEventSpy.calledOnce();
+      expect(element).to.have.attribute('data-state', 'opened');
+
+      closeButton.click();
+      await waitForLitRender(element);
+
+      await willCloseEventSpy.calledOnce();
+      expect(willCloseEventSpy.count).to.be.equal(1);
+      await waitForLitRender(element);
+
+      await didCloseEventSpy.calledOnce();
+      expect(didCloseEventSpy.count).to.be.equal(1);
+      await waitForLitRender(element);
+
+      expect(element).to.have.attribute('data-state', 'closed');
+
+      await aTimeout(0);
+      element = parent.querySelector<SbbNotificationElement>('sbb-notification')!;
+      expect(element).to.be.null;
+    });
   });
 
-  it('closes the notification and removes it from the DOM on close button click', async () => {
-    const parent = element.parentElement!;
-    const willCloseEventSpy = new EventSpy(SbbNotificationElement.events.willClose);
-    const didCloseEventSpy = new EventSpy(SbbNotificationElement.events.didClose);
-    const closeButton = element.shadowRoot!.querySelector(
-      '.sbb-notification__close',
-    ) as SbbSecondaryButtonElement;
+  describe('with non-zero animation duration', () => {
+    it('closes the notification and removes it from the DOM with animationend event', async () => {
+      didOpenEventSpy = new EventSpy(SbbNotificationElement.events.didOpen);
+      element = await fixture(html`
+        <sbb-notification id="notification" style="--sbb-notification-animation-duration: 1ms">
+          The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.
+          <sbb-link href="/">Link one</sbb-link>
+        </sbb-notification>
+      `);
 
-    await didOpenEventSpy.calledOnce();
-    expect(element).to.have.attribute('data-state', 'opened');
-
-    closeButton.click();
-    await waitForLitRender(element);
-
-    await willCloseEventSpy.calledOnce();
-    expect(willCloseEventSpy.count).to.be.equal(1);
-    await waitForLitRender(element);
-
-    await didCloseEventSpy.calledOnce();
-    expect(didCloseEventSpy.count).to.be.equal(1);
-    await waitForLitRender(element);
-
-    expect(element).to.have.attribute('data-state', 'closed');
-
-    await aTimeout(0);
-    element = parent.querySelector<SbbNotificationElement>('sbb-notification')!;
-    expect(element).to.be.null;
+      await openAndClose();
+    });
   });
 });
