13bdde071043a86f2113e52f27d485ca8d0f8495
Davide Mininni <davide.mininni@finconsgroup.com>
2024-12-10 09:56:59 +0100

    fix: lint

--- src/angular/autocomplete-grid/autocomplete-grid-option/autocomplete-grid-option.ts
+++ src/angular/autocomplete-grid/autocomplete-grid-option/autocomplete-grid-option.ts
@@ -1,5 +1,5 @@
 import { Directive, ElementRef, inject, Output } from '@angular/core';
-import type { SbbAutocompleteGridOptionElement } from '@sbb-esta/lyne-elements/autocomplete-grid.js';
+import type { SbbAutocompleteGridOptionElement } from '@sbb-esta/lyne-elements/autocomplete-grid/autocomplete-grid-option.js';
 import { fromEvent, type Observable } from 'rxjs';
 import '@sbb-esta/lyne-elements/autocomplete-grid/autocomplete-grid-option.js';
 
--- src/angular/expansion-panel/expansion-panel-header/expansion-panel-header.ts
+++ src/angular/expansion-panel/expansion-panel-header/expansion-panel-header.ts
@@ -1,6 +1,7 @@
 import { Directive, ElementRef, inject, Output } from '@angular/core';
-import '@sbb-esta/lyne-elements/expansion-panel/expansion-panel-header.js';
+import type { SbbExpansionPanelHeaderElement } from '@sbb-esta/lyne-elements/expansion-panel/expansion-panel-header.js';
 import { fromEvent, type Observable } from 'rxjs';
+import '@sbb-esta/lyne-elements/expansion-panel/expansion-panel-header.js';
 
 import { SbbButtonBaseElement, SbbDisabledTabIndexActionMixin } from '@sbb-esta/lyne-angular/core';
 import { SbbIconNameMixin } from '@sbb-esta/lyne-angular/icon/icon-name-mixin.js';
@@ -12,7 +13,7 @@ import { SbbIconNameMixin } from '@sbb-esta/lyne-angular/icon/icon-name-mixin.js
 export class SbbExpansionPanelHeader extends SbbDisabledTabIndexActionMixin(
   SbbIconNameMixin(SbbButtonBaseElement),
 ) {
-  #element = inject(ElementRef<SbbExpansionPanelHeader>);
+  #element = inject(ElementRef<SbbExpansionPanelHeaderElement>);
 
   @Output() public toggleExpanded: Observable<undefined> = fromEvent(
     this.#element.nativeElement,
