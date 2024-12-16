ca15fd3a5d03616b7ae4ab1b48f0c7d0e6585135
Davide Mininni <davide.mininni@finconsgroup.com>
2024-12-16 16:03:29 +0100

    fix: review Jeri pt.1

--- src/elements/slider/slider.ts
+++ src/elements/slider/slider.ts
@@ -57,7 +57,7 @@ class SbbSliderElement extends SbbDisabledMixin(SbbFormAssociatedMixin(LitElemen
   /** Numeric value for the inner HTMLInputElement. */
   @property({ attribute: 'value-as-number', type: Number })
   public set valueAsNumber(value: number | null) {
-    this.value = value?.toString() || null;
+    this.value = value?.toString() ?? null;
   }
   public get valueAsNumber(): number | null {
     return Number(this.value);
