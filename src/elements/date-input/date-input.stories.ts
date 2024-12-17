3d7b99f6cfbdcf07ba9cb73967ca8c13cf3f48dd
Jeremias Peier <jeremias.peier@sbb.ch>
2024-12-17 15:22:10 +0100

    docs: remove events from storybook

--- src/elements/date-input/date-input.stories.ts
+++ src/elements/date-input/date-input.stories.ts
@@ -114,7 +114,7 @@ const meta: Meta = {
   decorators: [withActions as Decorator],
   parameters: {
     actions: {
-      handles: ['input', 'change', 'keydown', 'keyup', 'keypress', 'focus', 'blur'],
+      handles: ['input', 'change'],
     },
     backgroundColor: (context: StoryContext) =>
       context.args.negative ? 'var(--sbb-color-black)' : 'var(--sbb-color-white)',
