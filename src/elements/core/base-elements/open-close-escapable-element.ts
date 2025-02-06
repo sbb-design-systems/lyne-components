f2fbceeb428c5e0c4f6bc281f6a2cc7ef2aecbd2
Davide Mininni <davide.mininni@finconsgroup.com>
2025-02-06 15:10:26 +0100

    docs: add doc

--- src/elements/core/base-elements/open-close-escapable-element.ts
+++ src/elements/core/base-elements/open-close-escapable-element.ts
@@ -2,6 +2,12 @@ import { isServer } from 'lit';
 
 import { SbbOpenCloseBaseElement } from './open-close-base-element.js';
 
+/**
+ * The registry is mapped as an array of WeakRef: it has been noticed that changing page
+ * nor the `disconnectedCallback` nor other `unload` hooks are triggered,
+ * so if the change is done with an opened dialog in the view, its instance is not removed.
+ * This way the reference should be removed from the array when the garbage collector acts.
+ */
 const overlayStack = new Array<WeakRef<SbbOpenCloseEscapableElement>>();
 const registry = new FinalizationRegistry((element: WeakRef<SbbOpenCloseEscapableElement>) => {
   overlayStack.splice(
