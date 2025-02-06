56fd8369296dbe588b88b4b7ec825ab8e321d485
Davide Mininni <davide.mininni@finconsgroup.com>
2025-02-06 10:13:27 +0100

    feat: add weakref (to be completed)

--- src/elements/core/base-elements/open-close-escapable-element.ts
+++ src/elements/core/base-elements/open-close-escapable-element.ts
@@ -1,46 +1,49 @@
+import { isServer } from 'lit';
+
 import { SbbOpenCloseBaseElement } from './open-close-base-element.js';
 
-const overlayStack = new Array<SbbOpenCloseEscapableElement>();
+const overlayStack = new Array<WeakRef<SbbOpenCloseEscapableElement>>();
+const registry = new FinalizationRegistry((element: WeakRef<SbbOpenCloseEscapableElement>) => {
+  overlayStack.splice(
+    overlayStack.findIndex((e) => e === element),
+    1,
+  );
+});
+
+if (!isServer) {
+  window.addEventListener('keydown', (event: KeyboardEvent) => {
+    if (event.key === 'Escape' && !event.defaultPrevented) {
+      event.preventDefault();
+      overlayStack.at(-1)?.deref()?.close();
+    }
+  });
+}
 
 /**
  * Class for overlay components which extends SbbOpenCloseBaseElement
  * and adds the functionality to close the overlay pressing the `Escape` button.
  */
 export abstract class SbbOpenCloseEscapableElement extends SbbOpenCloseBaseElement {
-  private _abortController: AbortController = new AbortController();
-
   /** Opens the component. */
   public open(): void {
     this._cleanStack();
-    this._abortController = new AbortController();
-    if (!overlayStack.length) {
-      window.addEventListener(
-        'keydown',
-        (event: KeyboardEvent) => {
-          if (event.key === 'Escape' && !event.defaultPrevented) {
-            event.preventDefault();
-            overlayStack.at(-1)?.close();
-          }
-        },
-        { signal: this._abortController.signal },
-      );
-    }
-    overlayStack.push(this);
+    const componentWeakRef = new WeakRef(this);
+    overlayStack.push(componentWeakRef);
+    registry.register(this, componentWeakRef);
   }
 
   /** Closes the component. */
   public close(): void {
     if (overlayStack.length) {
       overlayStack.pop();
-      this._abortController.abort();
     }
   }
 
   private _cleanStack(): void {
     overlayStack
-      .map((overlay: SbbOpenCloseEscapableElement) => overlay.id)
-      .forEach((overlayId: string, index: number) => {
-        if (!document.getElementById(overlayId)) {
+      .map((overlay: WeakRef<SbbOpenCloseEscapableElement>) => overlay.deref()?.id)
+      .forEach((overlayId: string | undefined, index: number) => {
+        if (overlayId && !document.getElementById(overlayId)) {
           overlayStack.splice(index, 1);
         }
       });
