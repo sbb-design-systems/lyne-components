948cd3b987631d0819a802a48cc30fcadf6a4a32
reneboms <r_boms@web.de>
2025-02-21 09:51:32 +0100

    feat(sbb-seat-reservation): add first mock data and interfaces + types for seat-reservation

---
+++ src/elements-experimental/seat-reservation/seat-reservation-sample-data.ts
@@ -0,0 +1,490 @@
+import type { SeatReservationLayout } from './seat-reservation.js';
+
+export const MOCK_SEAT_RESERVATION_LAYOUT_0: SeatReservationLayout = {
+  coachItems: [
+    {
+      id: 'coach-layout-0',
+      number: '1',
+      dimension: {
+        w: 64,
+        h: 10,
+      },
+      places: [
+        {
+          icon: 'string',
+          number: '1',
+          propertyIds: ['WINDOW'],
+          state: 'FREE',
+          travelClass: 'ANY_CLASS',
+          remarkId: 'Hint for seat',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 10,
+            y: 0,
+            z: 0,
+          },
+        },
+        {
+          icon: 'string',
+          number: '2',
+          direction: 'LEFT',
+          propertyIds: ['WINDOW'],
+          state: 'FREE',
+          travelClass: 'ANY_CLASS',
+          remarkId: 'Hint for seat',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 14,
+            y: 0,
+            z: 0,
+          },
+        },
+        {
+          icon: 'string',
+          number: '3',
+          propertyIds: ['WINDOW'],
+          state: 'FREE',
+          travelClass: 'ANY_CLASS',
+          remarkId: 'Hint for seat',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 18,
+            y: 0,
+            z: 0,
+          },
+        },
+        {
+          icon: 'string',
+          number: '4',
+          direction: 'LEFT',
+          propertyIds: ['WINDOW'],
+          state: 'ALLOCATED',
+          travelClass: 'ANY_CLASS',
+          remarkId: 'Hint for seat',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 22,
+            y: 0,
+            z: 0,
+          },
+        },
+        {
+          icon: 'string',
+          number: '5',
+          propertyIds: ['WINDOW', 'POWER'],
+          state: 'FREE',
+          travelClass: 'FIRST',
+          remarkId: 'First class seat',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 10,
+            y: 8,
+            z: 0,
+          },
+        },
+        {
+          icon: 'string',
+          number: '6',
+          direction: 'LEFT',
+          propertyIds: ['WINDOW', 'POWER'],
+          state: 'FREE',
+          travelClass: 'FIRST',
+          remarkId: 'First class seat',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 14,
+            y: 8,
+            z: 0,
+          },
+        },
+        {
+          icon: 'string',
+          number: '7',
+          propertyIds: ['WINDOW'],
+          state: 'FREE',
+          travelClass: 'SECOND',
+          remarkId: 'Hint for seat',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 18,
+            y: 8,
+            z: 0,
+          },
+        },
+        {
+          icon: 'string',
+          number: '8',
+          direction: 'LEFT',
+          propertyIds: ['WINDOW'],
+          state: 'FREE',
+          travelClass: 'SECOND',
+          remarkId: 'Hint for seat',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 22,
+            y: 8,
+            z: 0,
+          },
+        },
+      ],
+      signs: [
+        {
+          icon: 'WHEELCHAIR_ICON',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 36,
+            y: 0,
+            z: 0,
+          },
+        },
+        {
+          icon: 'RESTAURANT_ICON',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 32,
+            y: 3,
+            z: 0,
+          },
+        },
+        {
+          icon: 'RESTAURANT_ICON',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 20,
+            y: 3,
+            z: 0,
+          },
+        },
+      ],
+      internals: [
+        {
+          icon: 'LUGGAGE_AREA',
+          dimension: {
+            w: 2,
+            h: 4,
+          },
+          position: {
+            x: 36,
+            y: 0,
+            z: 0,
+          },
+        },
+      ],
+      directedInternals: [
+        {
+          icon: 'DRIVER_AREA',
+          dimension: {
+            w: 13,
+            h: 8,
+          },
+          position: {
+            x: 0,
+            y: 1,
+            z: 0,
+          },
+        },
+      ],
+      compartmentNumbers: [
+        {
+          number: '2',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 10,
+            y: 4,
+            z: 0,
+          },
+        },
+        {
+          number: '1',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 40,
+            y: 4,
+            z: 0,
+          },
+        },
+      ],
+    },
+    {
+      id: 'coach-layout-1',
+      number: '2',
+      dimension: {
+        w: 64,
+        h: 10,
+      },
+      places: [
+        {
+          icon: 'string',
+          number: '1',
+          propertyIds: ['WINDOW'],
+          state: 'FREE',
+          travelClass: 'ANY_CLASS',
+          remarkId: 'Hint for seat',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 10,
+            y: 0,
+            z: 0,
+          },
+        },
+        {
+          icon: 'string',
+          number: '2',
+          direction: 'LEFT',
+          propertyIds: ['WINDOW'],
+          state: 'FREE',
+          travelClass: 'ANY_CLASS',
+          remarkId: 'Hint for seat',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 14,
+            y: 0,
+            z: 0,
+          },
+        },
+        {
+          icon: 'string',
+          number: '3',
+          propertyIds: ['WINDOW'],
+          state: 'FREE',
+          travelClass: 'ANY_CLASS',
+          remarkId: 'Hint for seat',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 18,
+            y: 0,
+            z: 0,
+          },
+        },
+        {
+          icon: 'string',
+          number: '4',
+          direction: 'LEFT',
+          propertyIds: ['WINDOW'],
+          state: 'ALLOCATED',
+          travelClass: 'ANY_CLASS',
+          remarkId: 'Hint for seat',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 22,
+            y: 0,
+            z: 0,
+          },
+        },
+        {
+          icon: 'string',
+          number: '5',
+          propertyIds: ['WINDOW', 'POWER'],
+          state: 'FREE',
+          travelClass: 'FIRST',
+          remarkId: 'First class seat',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 10,
+            y: 8,
+            z: 0,
+          },
+        },
+        {
+          icon: 'string',
+          number: '6',
+          direction: 'LEFT',
+          propertyIds: ['WINDOW', 'POWER'],
+          state: 'FREE',
+          travelClass: 'FIRST',
+          remarkId: 'First class seat',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 14,
+            y: 8,
+            z: 0,
+          },
+        },
+        {
+          icon: 'string',
+          number: '7',
+          propertyIds: ['WINDOW'],
+          state: 'FREE',
+          travelClass: 'SECOND',
+          remarkId: 'Hint for seat',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 18,
+            y: 8,
+            z: 0,
+          },
+        },
+        {
+          icon: 'string',
+          number: '8',
+          direction: 'LEFT',
+          propertyIds: ['WINDOW'],
+          state: 'FREE',
+          travelClass: 'SECOND',
+          remarkId: 'Hint for seat',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 22,
+            y: 8,
+            z: 0,
+          },
+        },
+      ],
+      signs: [
+        {
+          icon: 'WHEELCHAIR_ICON',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 36,
+            y: 0,
+            z: 0,
+          },
+        },
+        {
+          icon: 'RESTAURANT_ICON',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 32,
+            y: 3,
+            z: 0,
+          },
+        },
+        {
+          icon: 'RESTAURANT_ICON',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 20,
+            y: 3,
+            z: 0,
+          },
+        },
+      ],
+      internals: [
+        {
+          icon: 'LUGGAGE_AREA',
+          dimension: {
+            w: 2,
+            h: 4,
+          },
+          position: {
+            x: 36,
+            y: 0,
+            z: 0,
+          },
+        },
+      ],
+      directedInternals: [
+        {
+          icon: 'DRIVER_AREA',
+          dimension: {
+            w: 13,
+            h: 8,
+          },
+          position: {
+            x: 0,
+            y: 1,
+            z: 0,
+          },
+        },
+      ],
+      compartmentNumbers: [
+        {
+          number: '2',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 10,
+            y: 4,
+            z: 0,
+          },
+        },
+        {
+          number: '1',
+          dimension: {
+            w: 2,
+            h: 2,
+          },
+          position: {
+            x: 40,
+            y: 4,
+            z: 0,
+          },
+        },
+      ],
+    },
+  ],
+};
