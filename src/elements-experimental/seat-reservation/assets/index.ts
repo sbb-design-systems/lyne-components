23069e1b2134004ea94539044ec5c9be6a54456e
Ronny Kreutel <ronny.kreutel@sbb.ch>
2025-02-24 14:46:53 +0100

    feat(sbb-seat-reservation): seat-reservation-graphic component

---
+++ src/elements-experimental/seat-reservation/assets/index.ts
@@ -0,0 +1,81 @@
+import chassisDriverBus from './chassis-driver-bus.svg?raw';
+import chassisDriverTrain from './chassis-driver-train.svg?raw';
+import chassisPassageCompartmentLeftTop from './chassis-passage-compartment-left-top.svg?raw';
+import chassisPassageCompartmentMiddle from './chassis-passage-compartment-middle.svg?raw';
+import chassisPassageCompartmentRightBottom from './chassis-passage-compartment-right-bottom.svg?raw';
+import chassisPassageWaggonBottomRight from './chassis-passage-waggon-bottom-right.svg?raw';
+import chassisPassageWaggonTopLeft from './chassis-passage-waggon-top-left.svg?raw';
+import chassisSeparator from './chassis-separator.svg?raw';
+import chassisTmpRowEmpty from './chassis-tmp-row-empty.svg?raw';
+import helperTmpNoteDefault from './helper-tmp-note-default.svg?raw';
+import helperTmpNotePostIt from './helper-tmp-note-post-it.svg?raw';
+import helperTmpPlaceholder from './helper-tmp-placeholder.svg?raw';
+import interiorPlaceBikeDefault from './interior-place-bike-default.svg?raw';
+import interiorPlaceBikeSelected from './interior-place-bike-selected.svg?raw';
+import interiorPlaceBikeUnavailableNotBookable from './interior-place-bike-unavailable-not-bookable.svg?raw';
+import interiorPlaceSeatDefault from './interior-place-seat-default.svg?raw';
+import interiorPlaceSeatNotBookable from './interior-place-seat-not-bookable.svg?raw';
+import interiorPlaceSeatSelected from './interior-place-seat-selected.svg?raw';
+import interiorPlaceSeatUnavailable from './interior-place-seat-unavailable.svg?raw';
+import interiorTableTest from './interior-table.svg?raw';
+import interiorTmpTable from './interior-tmp-table.svg?raw';
+import layoutEntrance from './layout-entrance.svg?raw';
+import layoutSki from './layout-ski.svg?raw';
+import layoutStair from './layout-stair.svg?raw';
+import layoutTmpGenericSpace from './layout-tmp-generic-space.svg?raw';
+import layoutWardrobe from './layout-wardrobe.svg?raw';
+import serviceBistro from './service-bistro.svg?raw';
+import serviceBusiness from './service-business.svg?raw';
+import serviceFamily from './service-family.svg?raw';
+import serviceLuggage from './service-luggage.svg?raw';
+import serviceMultifunction from './service-multifunction.svg?raw';
+import servicePram from './service-pram.svg?raw';
+import servicePrm from './service-prm.svg?raw';
+import serviceRestaurant from './service-restaurant.svg?raw';
+import serviceSilence from './service-silence.svg?raw';
+import serviceToiletPrm from './service-toilet-prm.svg?raw';
+import serviceToilet from './service-toilet.svg?raw';
+import serviceWheelchair from './service-wheelchair.svg?raw';
+import serviceWifi from './service-wifi.svg?raw';
+
+export {
+  chassisDriverBus,
+  chassisDriverTrain,
+  chassisPassageCompartmentLeftTop,
+  chassisPassageCompartmentMiddle,
+  chassisPassageCompartmentRightBottom,
+  chassisPassageWaggonBottomRight,
+  chassisPassageWaggonTopLeft,
+  chassisSeparator,
+  chassisTmpRowEmpty,
+  helperTmpNoteDefault,
+  helperTmpNotePostIt,
+  helperTmpPlaceholder,
+  interiorPlaceBikeDefault,
+  interiorPlaceBikeSelected,
+  interiorPlaceBikeUnavailableNotBookable,
+  interiorPlaceSeatDefault,
+  interiorPlaceSeatNotBookable,
+  interiorPlaceSeatSelected,
+  interiorPlaceSeatUnavailable,
+  interiorTableTest,
+  interiorTmpTable,
+  layoutEntrance,
+  layoutSki,
+  layoutStair,
+  layoutTmpGenericSpace,
+  layoutWardrobe,
+  serviceBistro,
+  serviceBusiness,
+  serviceFamily,
+  serviceLuggage,
+  serviceMultifunction,
+  servicePram,
+  servicePrm,
+  serviceRestaurant,
+  serviceSilence,
+  serviceToiletPrm,
+  serviceToilet,
+  serviceWheelchair,
+  serviceWifi,
+};
