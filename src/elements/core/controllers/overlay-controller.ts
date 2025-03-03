ffb177d53f993041c65d6bd0f8f5555b4d60e185
Davide Mininni <davide.mininni@finconsgroup.com>
2025-03-03 17:02:03 +0100

    feat: refactor implementation

--- src/elements/core/controllers/overlay-controller.ts
+++ src/elements/core/controllers/overlay-controller.ts
@@ -2,25 +2,13 @@ import { isServer, type ReactiveController, type ReactiveControllerHost } from '
 
 import type { SbbOpenCloseBaseElement } from '../base-elements/open-close-base-element.js';
 
-/**
- * The registry is mapped as an array of WeakRef: it has been noticed that changing page
- * nor the `disconnectedCallback` nor other `unload` hooks are triggered,
- * so if the change is done with an opened dialog in the view, its instance is not removed.
- * This way the reference should be removed from the array when the garbage collector acts.
- */
-const overlayStack = new Array<WeakRef<SbbOpenCloseBaseElement>>();
-const registry = new FinalizationRegistry((element: WeakRef<SbbOpenCloseBaseElement>) => {
-  overlayStack.splice(
-    overlayStack.findIndex((e) => e === element),
-    1,
-  );
-});
+const overlayStack = new Array<SbbOpenCloseBaseElement>();
 
 if (!isServer) {
   window.addEventListener('keydown', (event: KeyboardEvent) => {
     if (event.key === 'Escape' && !event.defaultPrevented) {
       event.preventDefault();
-      overlayStack.at(-1)?.deref()?.close();
+      overlayStack.at(-1)?.close();
     }
   });
 }
@@ -30,32 +18,22 @@ export class SbbOverlayController implements ReactiveController {
     this._host.addController?.(this);
   }
 
-  public hostConnected(): void {
-    // nothing
+  public hostDisconnected(): void {
+    this.disconnect();
   }
 
-  // This is meant to be called when the overlay is opened
+  // This must be called when the overlay is opened
   public connect(): void {
-    this._cleanStack();
-    const componentWeakRef = new WeakRef(this._host);
-    overlayStack.push(componentWeakRef);
-    registry.register(this, componentWeakRef);
+    overlayStack.push(this._host);
   }
 
-  // This is meant to be called when the overlay is closed
+  // This must be called when the overlay is closed
   public disconnect(): void {
     if (overlayStack.length) {
-      overlayStack.pop();
+      const findElIndex = overlayStack.findIndex((e) => e === this._host);
+      if (findElIndex !== -1) {
+        overlayStack.splice(findElIndex, 1);
+      }
     }
-  }
-
-  private _cleanStack(): void {
-    overlayStack
-      .map((overlay: WeakRef<SbbOpenCloseBaseElement>) => overlay.deref()?.id)
-      .forEach((overlayId: string | undefined, index: number) => {
-        if (overlayId && !document.getElementById(overlayId)) {
-          overlayStack.splice(index, 1);
-        }
-      });
   }
 }
