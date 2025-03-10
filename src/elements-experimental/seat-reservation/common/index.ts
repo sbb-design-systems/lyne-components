21de2c31840b04988c526061070260dcc7dc6329
thm-sbb <thomas.hamm@sbb.ch>
2025-03-10 09:46:19 +0100

    feat(sbb-seat-reservation-navigation): initial version for seat-reservation navigation (#3436)
    ## Preflight Checklist
    
    <!-- Please ensure you've completed the following steps by replacing [ ]
    with [x]-->
    
    - [x] I have read the [Contributing
    Guidelines](https://github.com/sbb-design-systems/lyne-components/blob/main/docs/CONTRIBUTING.md)
    for this project.
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/sbb-design-systems/lyne-components/blob/main/docs/CODE_OF_CONDUCT.md)
    that this project adheres to.
    - [x] I have searched the [pull request
    tracker](https://github.com/sbb-design-systems/lyne-components/pulls)
    for a Pull Request (PR) that matches the one I want to submit, without
    success.
    
    ## Issue
    
    This PR Closes #
    
    ## Pull request checklist
    
    Please check if your PR fulfills the following requirements:
    
    - [ ] Tests for the changes have been added (for bug fixes / features)
    - [ ] Docs have been reviewed and added / updated if needed (for bug
    fixes / features)
    
    See [Review Guidelines](../docs/REVIEW.md) for more information on what
    is checked during the review process.
    
    ## Changes
    
    Changes in this pull request:
    
    -
    
    ## Browsers
    
    I tested the build on the following browsers:
    
    - [ ] Firefox Desktop
    - [ ] Chrome Desktop
    - [ ] Edge Desktop
    - [ ] Safari Desktop
    - [ ] Chrome Mobile
    - [ ] Safari Mobile
    
    ## Screen readers
    
    I tested the build on the following browsers:
    
    - [ ] JAWS Firefox Desktop
    - [ ] JAWS Chrome Desktop
    - [ ] NVDA Firefox Desktop
    - [ ] NVDA Chrome Desktop
    - [ ] VoiceOver Safari Desktop
    - [ ] VoiceOver Chrome Desktop
    - [ ] VoiceOver Safari Mobile
    - [ ] Android Accessibility Suite Chrome Mobile
    
    ## Pull request type
    
    <!-- Please do not submit updates to dependencies unless it fixes an
    issue. -->
    
    <!-- Please try to limit your pull request to one type, submit multiple
    pull requests if needed. -->
    
    Please check the type of change your PR introduces:
    
    - [ ] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, renaming)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] Documentation content changes
    - [ ] Other (please describe):
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [ ] No
    
    <!-- If this introduces a breaking change, please describe the impact
    and migration path for existing applications below. -->
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as
    screenshots of how the component looks before and after the change. -->
    
    ---------
    
    Co-authored-by: reneboms <r_boms@web.de>

--- src/elements-experimental/seat-reservation/common/index.ts
+++ src/elements-experimental/seat-reservation/common/index.ts
@@ -1,2 +1,3 @@
 export * from './mapper.js';
 export * from './aria-label.js';
+export * from './navigation-icon-mapper.js';
--- src/elements-experimental/seat-reservation/seat-reservation-navigation/seat-reservation-navigation.ts
+++ src/elements-experimental/seat-reservation/seat-reservation-navigation/seat-reservation-navigation.ts
@@ -1,17 +1,19 @@
 import { forceType } from '@sbb-esta/lyne-elements/core/decorators.js';
 import { EventEmitter } from '@sbb-esta/lyne-elements/core/eventing.js';
-import type { CSSResultGroup, TemplateResult } from 'lit';
-import { html, LitElement } from 'lit';
+import { type CSSResultGroup, type TemplateResult, html, LitElement, nothing } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
+import { mapNavigationIconToSvg } from '../common.js';
 import type { CoachItem, SeatReservation } from '../seat-reservation.js';
 
 import style from './seat-reservation-navigation.scss?lit&inline';
 
+import '../seat-reservation-graphic.js';
+import '@sbb-esta/lyne-elements/icon/icon.js';
+
 /**
- * Describe the purpose of the component with a single short sentence.
+ * It will display the navigation for Seat reservation.
  *
- * @slot - Use the unnamed slot to add elements.
  * @event {CustomEvent<number>} selectCoach - Emits when select a coach navigation element and returns the clicked coach nav index
  */
 export
@@ -22,50 +24,104 @@ class SbbSeatReservationNavigationElement extends LitElement {
     selectCoach: 'selectCoach',
   } as const;
 
+  /* seat-reservation property */
   @forceType()
-  @property({ attribute: 'seat-reservatio', type: Object })
+  @property({ attribute: 'seat-reservation', type: Object })
   public accessor seatReservation: SeatReservation = null!;
 
+  /* pre-selected Coach index property */
   @forceType()
   @property({ attribute: 'selected-coach-index', type: Number })
   public accessor selectedCoachIndex: number = 0;
 
-  @forceType()
-  @property({ attribute: 'align-vertical', type: Boolean })
-  public accessor alignVertical: boolean = false;
-
-  /** Emits when an place was selected by user. */
+  /** Emits when a coach within the navigation was selected */
   protected selectNavCoach: EventEmitter = new EventEmitter(
     this,
     SbbSeatReservationNavigationElement.events.selectCoach,
   );
 
   protected override render(): TemplateResult {
-    const classAlignVertical = this.alignVertical
-      ? 'sbb-seat-reservation-navigation__wrapper sbb-seat-reservation-navigation__wrapper--vertical'
-      : 'sbb-seat-reservation-navigation__wrapper';
-    return html` <div class="${classAlignVertical}">
+    return html`
       <ul class="sbb-seat-reservation-navigation__list-coaches">
         ${this._getRenderedNavCoachs(this.seatReservation.coachItems)}
       </ul>
-    </div>`;
+    `;
   }
 
   private _getRenderedNavCoachs(coaches: CoachItem[]): TemplateResult[] {
     return coaches.map((coachItem: CoachItem, index: number) => {
       const coachSelectedClass =
         this.selectedCoachIndex === index
-          ? 'sbb-seat-reservation-navigation__item-coache--selected'
+          ? 'sbb-seat-reservation-navigation__item-coach--selected'
           : '';
       return html`
-        <li class="sbb-seat-reservation-navigation__item-coache ${coachSelectedClass}">
-          <button
-            class="sbb-seat-reservation-place-control__button"
-            @click=${() => this._selectNavCoach(index)}
-          >
-            ${coachItem.number}
-          </button>
+        <li class="sbb-seat-reservation-navigation__item-coach ${coachSelectedClass}">
+          ${this._getNavigationButton(index, coachItem)}
+          <div class="sbb-seat-reservation-navigation__signs">
+            ${this._getRenderedNavCoachSigns(coachItem.propertyIds)}
+          </div>
         </li>
+      `;
+    });
+  }
+
+  private _getNavigationButton(index: number, coachItem: CoachItem): TemplateResult | null {
+    if (!coachItem) {
+      return null;
+    }
+
+    return html`
+      <button
+        class="sbb-seat-reservation-navigation-control__button"
+        @click=${() => this._selectNavCoach(index)}
+      >
+        ${this._getAdditionalCoachInformation(coachItem)}
+      </button>
+    `;
+  }
+
+  private _getAdditionalCoachInformation(coachItem: CoachItem): TemplateResult | null {
+    if (!coachItem) {
+      return null;
+    }
+
+    return html`
+      ${coachItem.travelClass.includes('FIRST')
+        ? html`<span class="sbb-seat-reservation-navigation--first-class"></span>`
+        : nothing}
+      <span class="sbb-seat-reservation-navigation__additional-information">
+        <span class="sbb-seat-reservation-navigation__item-coach-number">${coachItem.number}</span>
+        <span class="sbb-seat-reservation-navigation__item-coach-travelclass">
+          ${coachItem.travelClass.includes('FIRST')
+            ? 1
+            : coachItem.travelClass.includes('SECOND')
+              ? 2
+              : nothing}
+        </span>
+      </span>
+    `;
+  }
+
+  private _getRenderedNavCoachSigns(signs?: string[]): (null | TemplateResult<1>)[] | null {
+    if (!signs) {
+      return null;
+    }
+
+    return signs?.map((sign: string) => {
+      if (!sign) {
+        return null;
+      }
+
+      const svgName = mapNavigationIconToSvg[sign];
+
+      return html`
+        ${svgName
+          ? html`<sbb-icon
+              name="${svgName}"
+              aria-hidden="false"
+              aria-label="todo: Coach desc"
+            ></sbb-icon>`
+          : nothing}
       `;
     });
   }
