af6c0f287ce07c1344e100cd59c295038581ea8e
Jeremias Peier <jeremias.peier@sbb.ch>
2025-03-18 16:55:27 +0100

    feat(buttons): introduce loading state

--- src/elements/button/common/button-common.ts
+++ src/elements/button/common/button-common.ts
@@ -1,9 +1,9 @@
-import type { TemplateResult } from 'lit';
+import { nothing, type PropertyValues, type TemplateResult } from 'lit';
 import { property } from 'lit/decorators.js';
 import { html } from 'lit/static-html.js';
 
 import type { SbbActionBaseElement } from '../../core/base-elements.js';
-import { hostAttributes, slotState } from '../../core/decorators.js';
+import { forceType, hostAttributes, slotState } from '../../core/decorators.js';
 import { isLean } from '../../core/dom.js';
 import type {
   AbstractConstructor,
@@ -21,6 +21,7 @@ export declare class SbbButtonCommonElementMixinType
   implements SbbNegativeMixinType, Partial<SbbDisabledMixinType>, Partial<SbbIconNameMixinType>
 {
   public accessor size: SbbButtonSize;
+  public accessor loading: boolean;
   public accessor disabled: boolean;
   public accessor iconName: string;
   public accessor negative: boolean;
@@ -44,12 +45,38 @@ export const SbbButtonCommonElementMixin = <T extends AbstractConstructor<SbbAct
      */
     @property({ reflect: true }) public accessor size: SbbButtonSize = isLean() ? 's' : 'l';
 
+    /**
+     * Whether the button indicates a loading state.
+     */
+    @forceType()
+    @property({ reflect: true, type: Boolean })
+    public accessor loading: boolean = false;
+
+    protected override willUpdate(changedProperties: PropertyValues<this>): void {
+      super.willUpdate(changedProperties);
+
+      if (changedProperties.has('loading')) {
+        if (this.loading) {
+          this.setAttribute('aria-busy', 'true');
+          this.setAttribute('aria-disabled', 'true');
+        } else {
+          this.removeAttribute('aria-busy');
+          this.removeAttribute('aria-disabled');
+        }
+      }
+    }
+
     protected override renderTemplate(): TemplateResult {
       return html`
         ${super.renderIconSlot()}
         <span class="sbb-button__label">
           <slot></slot>
         </span>
+        ${this.loading && !this.maybeDisabled
+          ? html`<div class="sbb-button-loading-border">
+              <div class="sbb-button-loading-trail"></div>
+            </div>`
+          : nothing}
       `;
     }
   }
