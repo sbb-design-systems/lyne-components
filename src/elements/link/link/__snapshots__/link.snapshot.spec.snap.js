be5a2103c31d4cac5f7cf4432cf3e90083204b89
Jeremias Peier <jeremias.peier@sbb.ch>
2025-03-18 13:58:29 +0100

    fix(links): reflect link properties to allow focusing

--- src/elements/link/link/__snapshots__/link.snapshot.spec.snap.js
+++ src/elements/link/link/__snapshots__/link.snapshot.spec.snap.js
@@ -65,3 +65,70 @@ snapshots["sbb-link renders A11y tree Firefox"] =
 `;
 /* end snapshot sbb-link renders A11y tree Firefox */
 
+snapshots["sbb-link reflects properties DOM"] = 
+`<sbb-link
+  data-action=""
+  data-link=""
+  data-sbb-link=""
+  data-slot-names="unnamed"
+  download=""
+  href="#"
+  rel="nofollow"
+  size="m"
+  target="_blank"
+>
+  Travelcards & tickets.
+</sbb-link>
+`;
+/* end snapshot sbb-link reflects properties DOM */
+
+snapshots["sbb-link reflects properties Shadow DOM"] = 
+`<a
+  class="sbb-action-base sbb-link"
+  download=""
+  href="#"
+  rel="nofollow"
+  target="_blank"
+>
+  <slot>
+  </slot>
+  <sbb-screen-reader-only>
+    . Link target opens in a new window.
+  </sbb-screen-reader-only>
+</a>
+`;
+/* end snapshot sbb-link reflects properties Shadow DOM */
+
+snapshots["sbb-link reflects properties A11y tree Chrome"] = 
+`<p>
+  {
+  "role": "WebArea",
+  "name": "",
+  "children": [
+    {
+      "role": "link",
+      "name": "Travelcards & tickets. . Link target opens in a new window."
+    }
+  ]
+}
+</p>
+`;
+/* end snapshot sbb-link reflects properties A11y tree Chrome */
+
+snapshots["sbb-link reflects properties A11y tree Firefox"] = 
+`<p>
+  {
+  "role": "document",
+  "name": "",
+  "children": [
+    {
+      "role": "link",
+      "name": "Travelcards & tickets. . Link target opens in a new window.",
+      "value": "http://localhost:8000/?wtr-session-id=QOniSBgMK9pPYiLSIOJQR#"
+    }
+  ]
+}
+</p>
+`;
+/* end snapshot sbb-link reflects properties A11y tree Firefox */
+
