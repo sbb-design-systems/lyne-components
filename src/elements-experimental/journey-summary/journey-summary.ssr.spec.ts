23ebc6955d253a8fb4da01eca540ecf77dfc0fe8
Lukas Spirig <lukas.spirig@sbb.ch>
2025-03-25 15:43:49 +0100

    fix: review

--- src/elements-experimental/journey-summary/journey-summary.ssr.spec.ts
+++ src/elements-experimental/journey-summary/journey-summary.ssr.spec.ts
@@ -7,16 +7,16 @@ import { SbbJourneySummaryElement } from './journey-summary.js';
 describe(`sbb-journey-summary ssr`, () => {
   let root: SbbJourneySummaryElement;
 
-  beforeEach(async () => {
+  beforeEach(async function () {
+    // This test seems flakey for unknown reason, so we extend the timeout for this
+    // specific test.
+    this.timeout(20000);
     root = await ssrHydratedFixture(html`<sbb-journey-summary></sbb-journey-summary>`, {
       modules: ['./journey-summary.js'],
     });
   });
 
-  it('renders', function () {
-    // This test seems flakey for unknown reason, so we extend the timeout for this
-    // specific test.
-    this.timeout(20000);
+  it('renders', () => {
     assert.instanceOf(root, SbbJourneySummaryElement);
   });
 });
