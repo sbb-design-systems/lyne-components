ce75d582f65cc574634684ca8c0eccfc87b5f97d
vkennke <kennke@mailbox.org>
2025-03-27 08:25:30 +0100

    fix: make seat reservation web component usable (#3501)
    ## Preflight Checklist
    
    <!-- Please ensure you've completed the following steps by replacing [ ]
    with [x]-->
    
    - [ ] I have read the [Contributing
    Guidelines](https://github.com/sbb-design-systems/lyne-components/blob/main/docs/CONTRIBUTING.md)
    for this project.
    - [ ] I agree to follow the [Code of
    Conduct](https://github.com/sbb-design-systems/lyne-components/blob/main/docs/CODE_OF_CONDUCT.md)
    that this project adheres to.
    - [ ] I have searched the [pull request
    tracker](https://github.com/sbb-design-systems/lyne-components/pulls)
    for a Pull Request (PR) that matches the one I want to submit, without
    success.
    
    ## Issue
    
    This PR Closes #
    
    ## Pull request checklist
    
    Please check if your PR fulfills the following requirements:
    
    - [ ] Tests for the changes have been added (for bug fixes / features)
    - [ ] Docs have been reviewed and added / updated if needed (for bug
    fixes / features)
    
    See [Review Guidelines](../docs/REVIEW.md) for more information on what
    is checked during the review process.
    
    ## Changes
    
    Changes in this pull request:
    
    -
    
    ## Browsers
    
    I tested the build on the following browsers:
    
    - [ ] Firefox Desktop
    - [ ] Chrome Desktop
    - [ ] Edge Desktop
    - [ ] Safari Desktop
    - [ ] Chrome Mobile
    - [ ] Safari Mobile
    
    ## Screen readers
    
    I tested the build on the following browsers:
    
    - [ ] JAWS Firefox Desktop
    - [ ] JAWS Chrome Desktop
    - [ ] NVDA Firefox Desktop
    - [ ] NVDA Chrome Desktop
    - [ ] VoiceOver Safari Desktop
    - [ ] VoiceOver Chrome Desktop
    - [ ] VoiceOver Safari Mobile
    - [ ] Android Accessibility Suite Chrome Mobile
    
    ## Pull request type
    
    <!-- Please do not submit updates to dependencies unless it fixes an
    issue. -->
    
    <!-- Please try to limit your pull request to one type, submit multiple
    pull requests if needed. -->
    
    Please check the type of change your PR introduces:
    
    - [ ] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, renaming)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] Documentation content changes
    - [ ] Other (please describe):
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [ ] No
    
    <!-- If this introduces a breaking change, please describe the impact
    and migration path for existing applications below. -->
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as
    screenshots of how the component looks before and after the change. -->
    
    ---------
    
    Co-authored-by: Valentin Kennke <valentin.kennke@sbb.ch>

---
+++ src/elements-experimental/seat-reservation/common/svgs.ts
@@ -0,0 +1,85 @@
+import chassisDriverBus from './svgs/chassis-driver-bus.svg?raw';
+import chassisDriverTrainFull from './svgs/chassis-driver-train-full.svg?raw';
+import chassisDriverTrain from './svgs/chassis-driver-train.svg?raw';
+import chassisPassageCompartmentLeftTop from './svgs/chassis-passage-compartment-left-top.svg?raw';
+import chassisPassageCompartmentMiddle from './svgs/chassis-passage-compartment-middle.svg?raw';
+import chassisPassageCompartmentRightBottom from './svgs/chassis-passage-compartment-right-bottom.svg?raw';
+import chassisPassageWaggonBottomRight from './svgs/chassis-passage-waggon-bottom-right.svg?raw';
+import chassisPassageWaggonTopLeft from './svgs/chassis-passage-waggon-top-left.svg?raw';
+import chassisRowMiddle from './svgs/chassis-row-middle.svg?raw';
+import chassisRowOuter from './svgs/chassis-row-outer.svg?raw';
+import chassisSeparator from './svgs/chassis-separator.svg?raw';
+import chassisTmpRowEmpty from './svgs/chassis-tmp-row-empty.svg?raw';
+import interiorPlaceBikeDefault from './svgs/interior-place-bike-default.svg?raw';
+import interiorPlaceBikeNotBookable from './svgs/interior-place-bike-not-bookable.svg?raw';
+import interiorPlaceBikeSelected from './svgs/interior-place-bike-selected.svg?raw';
+import interiorPlaceBikeUnavailable from './svgs/interior-place-bike-unavailable.svg?raw';
+import interiorPlaceSeatDefault from './svgs/interior-place-seat-default.svg?raw';
+import interiorPlaceSeatNotBookable from './svgs/interior-place-seat-not-bookable.svg?raw';
+import interiorPlaceSeatSelected from './svgs/interior-place-seat-selected.svg?raw';
+import interiorPlaceSeatUnavailable from './svgs/interior-place-seat-unavailable.svg?raw';
+import interiorTableTest from './svgs/interior-table.svg?raw';
+import interiorTmpTable from './svgs/interior-tmp-table.svg?raw';
+import layoutEntrance from './svgs/layout-entrance.svg?raw';
+import layoutSki from './svgs/layout-ski.svg?raw';
+import layoutStair from './svgs/layout-stair.svg?raw';
+import layoutTmpGenericSpace from './svgs/layout-tmp-generic-space.svg?raw';
+import layoutWardrobe from './svgs/layout-wardrobe.svg?raw';
+import serviceBistro from './svgs/service-bistro.svg?raw';
+import serviceBusiness from './svgs/service-business.svg?raw';
+import serviceFamily from './svgs/service-family.svg?raw';
+import serviceLuggage from './svgs/service-luggage.svg?raw';
+import serviceMultifunction from './svgs/service-multifunction.svg?raw';
+import servicePram from './svgs/service-pram.svg?raw';
+import servicePrm from './svgs/service-prm.svg?raw';
+import serviceRestaurant from './svgs/service-restaurant.svg?raw';
+import serviceSilence from './svgs/service-silence.svg?raw';
+import serviceToiletPrm from './svgs/service-toilet-prm.svg?raw';
+import serviceToilet from './svgs/service-toilet.svg?raw';
+import serviceWheelchair from './svgs/service-wheelchair.svg?raw';
+import serviceWifi from './svgs/service-wifi.svg?raw';
+
+const svgs = {
+  chassisDriverBus,
+  chassisDriverTrain,
+  chassisDriverTrainFull,
+  chassisPassageCompartmentLeftTop,
+  chassisPassageCompartmentMiddle,
+  chassisPassageCompartmentRightBottom,
+  chassisPassageWaggonBottomRight,
+  chassisPassageWaggonTopLeft,
+  chassisRowOuter,
+  chassisRowMiddle,
+  chassisSeparator,
+  chassisTmpRowEmpty,
+  interiorPlaceBikeDefault,
+  interiorPlaceBikeNotBookable,
+  interiorPlaceBikeSelected,
+  interiorPlaceBikeUnavailable,
+  interiorPlaceSeatDefault,
+  interiorPlaceSeatNotBookable,
+  interiorPlaceSeatSelected,
+  interiorPlaceSeatUnavailable,
+  interiorTableTest,
+  interiorTmpTable,
+  layoutEntrance,
+  layoutSki,
+  layoutStair,
+  layoutTmpGenericSpace,
+  layoutWardrobe,
+  serviceBistro,
+  serviceBusiness,
+  serviceFamily,
+  serviceLuggage,
+  serviceMultifunction,
+  servicePram,
+  servicePrm,
+  serviceRestaurant,
+  serviceSilence,
+  serviceToiletPrm,
+  serviceToilet,
+  serviceWheelchair,
+  serviceWifi,
+};
+
+export { svgs };
