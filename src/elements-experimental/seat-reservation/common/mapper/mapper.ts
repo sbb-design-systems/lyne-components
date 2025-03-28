f11119508945aa799e3002a1816904b621d36194
thm-sbb <thomas.hamm@sbb.ch>
2025-03-28 16:38:57 +0100

    feature/thm/seat-reservation-icons-refactoring (#3503)
    ## Preflight Checklist
    
    <!-- Please ensure you've completed the following steps by replacing [ ]
    with [x]-->
    
    - [ ] I have read the [Contributing
    Guidelines](https://github.com/sbb-design-systems/lyne-components/blob/main/docs/CONTRIBUTING.md)
    for this project.
    - [ ] I agree to follow the [Code of
    Conduct](https://github.com/sbb-design-systems/lyne-components/blob/main/docs/CODE_OF_CONDUCT.md)
    that this project adheres to.
    - [ ] I have searched the [pull request
    tracker](https://github.com/sbb-design-systems/lyne-components/pulls)
    for a Pull Request (PR) that matches the one I want to submit, without
    success.
    
    ## Issue
    
    This PR Closes #
    
    ## Pull request checklist
    
    Please check if your PR fulfills the following requirements:
    
    - [ ] Tests for the changes have been added (for bug fixes / features)
    - [ ] Docs have been reviewed and added / updated if needed (for bug
    fixes / features)
    
    See [Review Guidelines](../docs/REVIEW.md) for more information on what
    is checked during the review process.
    
    ## Changes
    
    Changes in this pull request:
    
    -
    
    ## Browsers
    
    I tested the build on the following browsers:
    
    - [ ] Firefox Desktop
    - [ ] Chrome Desktop
    - [ ] Edge Desktop
    - [ ] Safari Desktop
    - [ ] Chrome Mobile
    - [ ] Safari Mobile
    
    ## Screen readers
    
    I tested the build on the following browsers:
    
    - [ ] JAWS Firefox Desktop
    - [ ] JAWS Chrome Desktop
    - [ ] NVDA Firefox Desktop
    - [ ] NVDA Chrome Desktop
    - [ ] VoiceOver Safari Desktop
    - [ ] VoiceOver Chrome Desktop
    - [ ] VoiceOver Safari Mobile
    - [ ] Android Accessibility Suite Chrome Mobile
    
    ## Pull request type
    
    <!-- Please do not submit updates to dependencies unless it fixes an
    issue. -->
    
    <!-- Please try to limit your pull request to one type, submit multiple
    pull requests if needed. -->
    
    Please check the type of change your PR introduces:
    
    - [ ] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, renaming)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] Documentation content changes
    - [ ] Other (please describe):
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [ ] No
    
    <!-- If this introduces a breaking change, please describe the impact
    and migration path for existing applications below. -->
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as
    screenshots of how the component looks before and after the change. -->

---
+++ src/elements-experimental/seat-reservation/common/mapper/mapper.ts
@@ -0,0 +1,96 @@
+import type {
+  CoachItem,
+  PlaceTravelClass,
+  SeatReservation,
+  VehicleType,
+} from '../../seat-reservation.js';
+import { MOCK_COACHES_RAW_0, MOCK_COACHES_RAW_1 } from '../seat-reservation-sample-data.js';
+
+/**
+ * Map function that converts the RAW OSDM mock data into SeatReservation
+ */
+export const mapRawDataToSeatReservation = (vehicleType: VehicleType): SeatReservation => {
+  const MOCK_DATA = vehicleType === 'TRAIN' ? MOCK_COACHES_RAW_0 : MOCK_COACHES_RAW_1;
+  const coachsArr = MOCK_DATA.map((coachDeckLayout) => {
+    const choachLayout = coachDeckLayout?.coachDeckLayout;
+    const coachTravelClasses: PlaceTravelClass[] = [];
+    const coachPropertyIds: string[] = [];
+    const places = choachLayout.placeGroups
+      .map((placeGroup: any) => {
+        //Collect unique travel classes for coach
+        if (coachTravelClasses.indexOf(placeGroup.travelClass) === -1) {
+          coachTravelClasses.push(placeGroup.travelClass);
+        }
+
+        //Collect unique properties for coach
+        if (coachPropertyIds.indexOf(placeGroup.accommodationSubType) === -1) {
+          coachPropertyIds.push(placeGroup.accommodationSubType);
+        }
+
+        return placeGroup.places?.map((place: any) => {
+          return {
+            number: place?.number,
+            state: place?.state,
+            type: placeGroup.accommodationSubType.indexOf('BICYCLE') === -1 ? 'SEAT' : 'BICYCLE',
+            dimension: { w: place.rectangle.dimension.width, h: place.rectangle.dimension.height },
+            position: {
+              x: place.rectangle.position.x,
+              y: place.rectangle.position.y,
+              z: place.rectangle.position.z,
+            },
+            rotation: place?.orientation || 0,
+            travelClass: placeGroup?.travelClass,
+            remarkId: '',
+            propertyIds: place.placeProperties || [],
+            selected: false,
+          };
+        });
+      })
+      .flat();
+
+    const signs = choachLayout.serviceIcons.map((serviceIcon) => {
+      return {
+        icon: serviceIcon.type,
+        position: {
+          x: serviceIcon.rectangle.position.x,
+          y: serviceIcon.rectangle.position.y,
+          z: serviceIcon.rectangle.position.z,
+        },
+        dimension: {
+          w: serviceIcon.rectangle.dimension.width,
+          h: serviceIcon.rectangle.dimension.height,
+        },
+      };
+    });
+
+    const graphicalElements = choachLayout.graphicElements.map((element) => {
+      return {
+        icon: element?.type,
+        position: {
+          x: element.rectangle.position.x,
+          y: element.rectangle.position.y,
+          z: element.rectangle.position.z,
+        },
+        dimension: { w: element.rectangle.dimension.width, h: element.rectangle.dimension.height },
+        rotation: element.orientation,
+      };
+    });
+
+    return {
+      id: choachLayout?.id,
+      number: choachLayout?.name,
+      dimension: { w: choachLayout.dimension?.width, h: choachLayout.dimension?.height },
+      places: places,
+      serviceElements: signs,
+      graphicElements: graphicalElements,
+      travelClass: coachTravelClasses,
+      propertyIds: coachPropertyIds,
+    } as CoachItem;
+  });
+
+  return {
+    vehicleType: vehicleType,
+    deckCoachIndex: 0,
+    coachItems: coachsArr,
+  };
+};
