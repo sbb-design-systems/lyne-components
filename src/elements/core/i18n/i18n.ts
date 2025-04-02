fcb7bb7c1ed18fee79887b1a8529d02aaed4095a
Tommmaso Menga <tommaso.menga-94@hotmail.it>
2025-04-02 10:10:18 +0200

    chore: update i18n

--- src/elements/core/i18n/i18n.ts
+++ src/elements/core/i18n/i18n.ts
@@ -770,7 +770,7 @@ export const i18nDateMax = (max: string): Record<string, string> => ({
 });
 
 export const i18nChipDelete: Record<string, string> = {
-  de: ', Drücken Sie die Löschen Taste, um den Chip zu entfernen',
+  de: ', Drücken Sie die Entfernen-Taste um den Chip zu löschen',
   en: ', Press the Delete button to remove the chip',
   fr: ', Appuyez sur la touche Del pour supprimer cette puce',
   it: ', Premi il tasto Canc per rimuovere questa chip',
