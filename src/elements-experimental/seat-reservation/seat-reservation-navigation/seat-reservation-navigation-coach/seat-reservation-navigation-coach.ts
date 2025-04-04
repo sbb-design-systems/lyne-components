f65078f8effedbc101cf1e64171e48ad5b87e7eb
thm-sbb <thomas.hamm@sbb.ch>
2025-04-04 14:34:47 +0200

    feat(sbb-seat-reservation-refact-navigation): refactor nav; divide into smaller components (#3517)
    ## Preflight Checklist
    
    <!-- Please ensure you've completed the following steps by replacing [ ]
    with [x]-->
    
    - [ ] I have read the [Contributing
    Guidelines](https://github.com/sbb-design-systems/lyne-components/blob/main/docs/CONTRIBUTING.md)
    for this project.
    - [ ] I agree to follow the [Code of
    Conduct](https://github.com/sbb-design-systems/lyne-components/blob/main/docs/CODE_OF_CONDUCT.md)
    that this project adheres to.
    - [ ] I have searched the [pull request
    tracker](https://github.com/sbb-design-systems/lyne-components/pulls)
    for a Pull Request (PR) that matches the one I want to submit, without
    success.
    
    ## Issue
    
    This PR Closes #
    
    ## Pull request checklist
    
    Please check if your PR fulfills the following requirements:
    
    - [ ] Tests for the changes have been added (for bug fixes / features)
    - [ ] Docs have been reviewed and added / updated if needed (for bug
    fixes / features)
    
    See [Review Guidelines](../docs/REVIEW.md) for more information on what
    is checked during the review process.
    
    ## Changes
    
    Changes in this pull request:
    
    -
    
    ## Browsers
    
    I tested the build on the following browsers:
    
    - [ ] Firefox Desktop
    - [ ] Chrome Desktop
    - [ ] Edge Desktop
    - [ ] Safari Desktop
    - [ ] Chrome Mobile
    - [ ] Safari Mobile
    
    ## Screen readers
    
    I tested the build on the following browsers:
    
    - [ ] JAWS Firefox Desktop
    - [ ] JAWS Chrome Desktop
    - [ ] NVDA Firefox Desktop
    - [ ] NVDA Chrome Desktop
    - [ ] VoiceOver Safari Desktop
    - [ ] VoiceOver Chrome Desktop
    - [ ] VoiceOver Safari Mobile
    - [ ] Android Accessibility Suite Chrome Mobile
    
    ## Pull request type
    
    <!-- Please do not submit updates to dependencies unless it fixes an
    issue. -->
    
    <!-- Please try to limit your pull request to one type, submit multiple
    pull requests if needed. -->
    
    Please check the type of change your PR introduces:
    
    - [ ] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, renaming)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] Documentation content changes
    - [ ] Other (please describe):
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [ ] No
    
    <!-- If this introduces a breaking change, please describe the impact
    and migration path for existing applications below. -->
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as
    screenshots of how the component looks before and after the change. -->

---
+++ src/elements-experimental/seat-reservation/seat-reservation-navigation/seat-reservation-navigation-coach/seat-reservation-navigation-coach.ts
@@ -0,0 +1,151 @@
+import { SbbLanguageController } from '@sbb-esta/lyne-elements/core/controllers/language-controller';
+import { forceType } from '@sbb-esta/lyne-elements/core/decorators/force-type';
+import { EventEmitter } from '@sbb-esta/lyne-elements/core/eventing/event-emitter';
+import { type CSSResultGroup, nothing, type TemplateResult } from 'lit';
+import { html, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+
+import style from './seat-reservation-navigation-coach.scss?lit&inline';
+
+import { getI18nSeatReservation } from '@sbb-esta/lyne-elements-experimental/seat-reservation/common/translations/i18n';
+import '../../seat-reservation-graphic/seat-reservation-graphic.js';
+import '../seat-reservation-navigation-services/seat-reservation-navigation-services.js';
+import type { PlaceTravelClass } from '@sbb-esta/lyne-elements-experimental/seat-reservation/seat-reservation';
+
+/**
+ * This component will display the navigation coach item for Seat reservation.
+ *
+ * @event {CustomEvent<T>} selectCoach - Emits when a coach within the navigation was selected and returns the clicked coach nav index
+ */
+export
+@customElement('sbb-seat-reservation-navigation-coach')
+class SbbSeatReservationNavigationCoachElement extends LitElement {
+  public static override styles: CSSResultGroup = style;
+  public static readonly events = {
+    selectCoach: 'selectCoach',
+  } as const;
+
+  @forceType()
+  @property({ attribute: 'coach-id', type: String })
+  public accessor coachId: string = '';
+
+  @property({ attribute: 'property-ids', type: Array })
+  public accessor propertyIds: string[] = [];
+
+  /* pre-selected Coach index property */
+  @forceType()
+  @property({ type: Boolean })
+  public accessor selected: boolean = false;
+
+  @forceType()
+  @property({ type: Number })
+  public accessor index: number = 0;
+
+  @property({ attribute: 'travel-class', type: Array })
+  public accessor travelClass: PlaceTravelClass[] = ['ANY_CLASS'];
+
+  @forceType()
+  @property({ attribute: 'driver-area', type: Boolean })
+  public accessor driverArea: boolean = false;
+
+  @forceType()
+  @property({ type: Boolean })
+  public accessor first: boolean = false;
+
+  @forceType()
+  @property({ type: Boolean })
+  public accessor last: boolean = false;
+
+  private _language = new SbbLanguageController(this);
+
+  /** Emits when a coach within the navigation was selected */
+  protected selectNavCoach: EventEmitter<number> = new EventEmitter(
+    this,
+    SbbSeatReservationNavigationCoachElement.events.selectCoach,
+  );
+
+  protected override render(): TemplateResult {
+    const coachSelectedClass = this.selected
+      ? 'sbb-seat-reservation-navigation__item-coach--selected'
+      : '';
+
+    const lastCoachInLayout = this.last ? 'last-coach' : '';
+    const firstCoachInLayout = this.first ? 'first-coach' : '';
+
+    return html`
+      <div
+        class="sbb-seat-reservation-navigation__item-coach ${coachSelectedClass} ${lastCoachInLayout} ${firstCoachInLayout}"
+      >
+        ${this._getNavigationButton()}
+        ${!this.driverArea && this.propertyIds?.length
+          ? html`<sbb-seat-reservation-navigation-services
+              .propertyIds="${this.propertyIds}"
+            ></sbb-seat-reservation-navigation-services>`
+          : nothing}
+      </div>
+    `;
+  }
+
+  private _getNavigationButton(): TemplateResult | null {
+    return html`
+      ${!this.driverArea
+        ? html` <button
+            type="button"
+            class="sbb-seat-reservation-navigation__control-button"
+            title="${getI18nSeatReservation('NAVIGATE_TO_COACH', this._language.current, [
+              this.id,
+            ])}"
+            @click=${() => this._selectNavCoach(this.index)}
+            @keydown="${(evt: KeyboardEvent) => this._handleKeyboardEvent(evt)}"
+          >
+            ${this._getBtnInformation()}
+          </button>`
+        : html`<div class="sbb-seat-reservation-navigation-driver-area"></div>`}
+    `;
+  }
+
+  private _getBtnInformation(): TemplateResult | null {
+    return html`
+      ${this.travelClass?.includes('FIRST')
+        ? html`<span class="sbb-seat-reservation-navigation--first-class"></span>`
+        : nothing}
+      ${this.travelClass?.length > 0 || this.coachId
+        ? html`<div class="sbb-seat-reservation-navigation__additional-information">
+            ${this.coachId
+              ? html`<div class="sbb-seat-reservation-navigation__item-coach-number">
+                  ${this.coachId}
+                </div>`
+              : nothing}
+            ${this.travelClass?.includes('FIRST')
+              ? html`<div class="sbb-seat-reservation-navigation__item-coach-travelclass">1</div>`
+              : this.travelClass?.includes('SECOND')
+                ? html`<div class="sbb-seat-reservation-navigation__item-coach-travelclass">2</div>`
+                : nothing}
+          </div>`
+        : nothing}
+    `;
+  }
+
+  // handle navigation done with enter.
+  private _handleKeyboardEvent(evt: KeyboardEvent): void {
+    if (evt.code === 'Enter') {
+      this._selectNavCoach(this.index);
+    }
+  }
+
+  /**
+   * emits the index of the coach array for the main navigation.
+   * @param coachIndex
+   * @private
+   */
+  private _selectNavCoach(coachIndex: number): void {
+    this.selectNavCoach.emit(coachIndex);
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    'sbb-seat-reservation-navigation-coach': SbbSeatReservationNavigationCoachElement;
+  }
+}
