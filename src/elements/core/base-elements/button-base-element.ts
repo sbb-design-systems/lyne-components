3d4bb07f37352bb6c736cf9d6389dc057b94120b
Lukas Spirig <lukas.spirig@sbb.ch>
2025-04-08 15:11:10 +0200

    fix: use correct id resolution for form (#3552)
    Currently we use `this.ownerDocument`, which is basically the same as
    `document`. We however want to resolve to the given id in the current
    document fragment, for which we should use `this.getRootNode()`.

--- src/elements/core/base-elements/button-base-element.ts
+++ src/elements/core/base-elements/button-base-element.ts
@@ -45,7 +45,9 @@ abstract class SbbButtonBaseElement extends SbbFormAssociatedMixin(SbbActionBase
     // Use querySelector with form and id selector, as the form property must
     // reference a valid <form> element
     return this._formId
-      ? ((this.ownerDocument?.querySelector?.(`form#${this._formId}`) as HTMLFormElement) ?? null)
+      ? (((this.getRootNode?.() as DocumentFragment | null)?.getElementById?.(
+          this._formId,
+        ) as HTMLFormElement) ?? null)
       : this.internals.form;
   }
   private _formId: string = '';
