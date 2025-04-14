2380326f40c66d3fb628d8ab9e838e49d67f819b
Boms <r_boms@web.de>
2025-04-14 10:24:52 +0200

    Seat resvervation improve tab navigation (#3566)
    ## Preflight Checklist
    
    <!-- Please ensure you've completed the following steps by replacing [ ]
    with [x]-->
    
    - [ ] I have read the [Contributing
    Guidelines](https://github.com/sbb-design-systems/lyne-components/blob/main/docs/CONTRIBUTING.md)
    for this project.
    - [ ] I agree to follow the [Code of
    Conduct](https://github.com/sbb-design-systems/lyne-components/blob/main/docs/CODE_OF_CONDUCT.md)
    that this project adheres to.
    - [ ] I have searched the [pull request
    tracker](https://github.com/sbb-design-systems/lyne-components/pulls)
    for a Pull Request (PR) that matches the one I want to submit, without
    success.
    
    ## Issue
    
    This PR Closes #
    
    ## Pull request checklist
    
    Please check if your PR fulfills the following requirements:
    
    - [ ] Tests for the changes have been added (for bug fixes / features)
    - [ ] Docs have been reviewed and added / updated if needed (for bug
    fixes / features)
    
    See [Review Guidelines](../docs/REVIEW.md) for more information on what
    is checked during the review process.
    
    ## Changes
    
    Changes in this pull request:
    
    -
    
    ## Browsers
    
    I tested the build on the following browsers:
    
    - [ ] Firefox Desktop
    - [ ] Chrome Desktop
    - [ ] Edge Desktop
    - [ ] Safari Desktop
    - [ ] Chrome Mobile
    - [ ] Safari Mobile
    
    ## Screen readers
    
    I tested the build on the following browsers:
    
    - [ ] JAWS Firefox Desktop
    - [ ] JAWS Chrome Desktop
    - [ ] NVDA Firefox Desktop
    - [ ] NVDA Chrome Desktop
    - [ ] VoiceOver Safari Desktop
    - [ ] VoiceOver Chrome Desktop
    - [ ] VoiceOver Safari Mobile
    - [ ] Android Accessibility Suite Chrome Mobile
    
    ## Pull request type
    
    <!-- Please do not submit updates to dependencies unless it fixes an
    issue. -->
    
    <!-- Please try to limit your pull request to one type, submit multiple
    pull requests if needed. -->
    
    Please check the type of change your PR introduces:
    
    - [ ] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, renaming)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] Documentation content changes
    - [ ] Other (please describe):
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [ ] No
    
    <!-- If this introduces a breaking change, please describe the impact
    and migration path for existing applications below. -->
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as
    screenshots of how the component looks before and after the change. -->

--- src/elements-experimental/seat-reservation/seat-reservation/seat-reservation.ts
+++ src/elements-experimental/seat-reservation/seat-reservation/seat-reservation.ts
@@ -15,7 +15,7 @@ import type {
   SeatReservation,
 } from '../seat-reservation.js';
 
-import { SeatReservationService } from './seat-reservation-service.js';
+import { SeatReservationBaseElement } from './seat-reservation-base-element.js';
 import style from './seat-reservation.scss?lit&inline';
 
 import '../seat-reservation-area.js';
@@ -30,12 +30,17 @@ import '../seat-reservation-navigation/seat-reservation-navigation-coach/seat-re
  */
 export
 @customElement('sbb-seat-reservation')
-class SbbSeatReservationElement extends SeatReservationService {
+class SbbSeatReservationElement extends SeatReservationBaseElement {
   public static override styles: CSSResultGroup = style;
 
   /** seat reservation*/
   @property({ attribute: 'seat-reservation', type: Object })
   public override accessor seatReservation: SeatReservation = null!;
+
+  /** The seat resvervation navigation can be toggled by this property*/
+  @forceType()
+  @property({ attribute: 'has-navigation', type: Boolean })
+  public override accessor hasNavigation: boolean = true;
 
   /** align-vertical controls the visual represention of seat reservation in a horizonal or vertical alignment*/
   @forceType()
@@ -57,7 +62,6 @@ class SbbSeatReservationElement extends SeatReservationService {
   public accessor disable: boolean = false;
 
   private _language = new SbbLanguageController(this);
-
   private _coachesHtmlTemplate?: TemplateResult;
   private _notAreaElements = [
     'DRIVER_AREA_FULL',
@@ -68,6 +72,22 @@ class SbbSeatReservationElement extends SeatReservationService {
     'COMPARTMENT_PASSAGE_LOW',
     'COACH_BORDER_OUTER',
   ];
+
+  protected override willUpdate(changedProperties: PropertyValues<this>): void {
+    super.willUpdate(changedProperties);
+
+    if (changedProperties.has('hasNavigation')) {
+      if (this.hasNavigation) {
+        this.shadowRoot
+          ?.querySelectorAll('table')
+          .forEach((table) => table.removeAttribute('tabindex'));
+      } else {
+        this.shadowRoot
+          ?.querySelectorAll('table')
+          .forEach((table) => table.setAttribute('tabindex', '0'));
+      }
+    }
+  }
 
   protected override firstUpdated(changedProperties: PropertyValues<this>): void {
     super.firstUpdated(changedProperties);
@@ -87,55 +107,87 @@ class SbbSeatReservationElement extends SeatReservationService {
     const coachItems = JSON.parse(JSON.stringify(this.seatReservation?.coachItems));
     const classAlignVertical = this.alignVertical ? 'sbb-seat-reservation__wrapper--vertical' : '';
 
-    //TODO TAB empty jump fields <input id="first-tab-element" type="input" value="firstlement">
-    //TODO TAB empty jump fields <input id="last-tab-element" type="input" value="lastelement">
     this._coachesHtmlTemplate = html`
       <style>
         .sbb-seat-reservation__list-coaches {
           transform: scale(${this.scale});
         }
       </style>
+
       <div class="sbb-seat-reservation">
+        <sbb-screen-reader-only>
+          <input
+            id="first-tab-element"
+            type="text"
+            aria-label="Beginn der Grafische Sitzplatzreservierung"
+            role="contentinfo"
+            readonly
+          />
+        </sbb-screen-reader-only>
+
         <div
           class="sbb-seat-reservation__wrapper ${classAlignVertical}"
           @keydown=${(evt: KeyboardEvent) => this.handleKeyboardEvent(evt)}
         >
-          <nav>
-            <ul
-              class="sbb-seat-reservation-navigation__list-coaches"
-              aria-label="${getI18nSeatReservation(
-                'SEAT_RESERVATION_NAVIGATION',
-                this._language.current,
-              )}"
-            >
-              ${this.seatReservation?.coachItems.map((coachItem: CoachItem, index: number) => {
-                return html`<li>
-                  <sbb-seat-reservation-navigation-coach
-                    @selectCoach=${(event: CustomEvent) => this._onSelectNavCoach(event)}
-                    @focusCoach=${() => this._onFocusNavCoach()}
-                    index="${index}"
-                    coach-id="${coachItem.id}"
-                    .selected=${this.selectedCoachIndex === index}
-                    .focused=${this.focusedCoachIndex === index}
-                    .propertyIds="${coachItem.propertyIds}"
-                    .travelClass="${coachItem.travelClass}"
-                    ?driver-area="${!coachItem.places?.length}"
-                    ?first="${index === 0}"
-                    ?last="${index === this.seatReservation?.coachItems.length - 1}"
-                  >
-                  </sbb-seat-reservation-navigation-coach>
-                </li>`;
-              })}
-            </ul>
-          </nav>
-
-          <div id="sbb-seat-reservation__parent-area" class="sbb-seat-reservation__parent">
+          ${this._renderNavigation()}
+          <div
+            id="sbb-seat-reservation__parent-area"
+            class="sbb-seat-reservation__parent"
+            tabindex="-1"
+          >
             <ul class="sbb-seat-reservation__list-coaches" role="presentation">
               ${this._renderCoaches(coachItems)}
             </ul>
           </div>
         </div>
+
+        <sbb-screen-reader-only>
+          <input
+            id="last-tab-element"
+            type="input"
+            aria-label="Verlassen der Grafische Sitzplatzreservierung"
+            role="contentinfo"
+            readonly
+          />
+        </sbb-screen-reader-only>
       </div>
+    `;
+  }
+
+  private _renderNavigation(): TemplateResult | null {
+    if (!this.hasNavigation) {
+      return null;
+    }
+
+    return html`
+      <nav>
+        <ul
+          class="sbb-seat-reservation-navigation__list-coaches"
+          aria-label="${getI18nSeatReservation(
+            'SEAT_RESERVATION_NAVIGATION',
+            this._language.current,
+          )}"
+        >
+          ${this.seatReservation?.coachItems.map((coachItem: CoachItem, index: number) => {
+            return html`<li>
+              <sbb-seat-reservation-navigation-coach
+                @selectCoach=${(event: CustomEvent) => this._onSelectNavCoach(event)}
+                @focusCoach=${() => this._onFocusNavCoach()}
+                index="${index}"
+                coach-id="${coachItem.id}"
+                .selected=${this.selectedCoachIndex === index}
+                .focused=${this.focusedCoachIndex === index}
+                .propertyIds="${coachItem.propertyIds}"
+                .travelClass="${coachItem.travelClass}"
+                ?driver-area="${!coachItem.places?.length}"
+                ?first="${index === 0}"
+                ?last="${index === this.seatReservation?.coachItems.length - 1}"
+              >
+              </sbb-seat-reservation-navigation-coach>
+            </li>`;
+          })}
+        </ul>
+      </nav>
     `;
   }
   /**
@@ -172,8 +224,13 @@ class SbbSeatReservationElement extends SeatReservationService {
         ${this._getRenderedCoachBorders(coachItem, index)}
         ${this._getRenderedGraphicalElements(coachItem.graphicElements || [], coachItem.dimension)}
         ${this._getRenderedServiceElements(coachItem.serviceElements)}
-        <table class="coach-wrapper__table" id="seat-reservation-coach-${index}" role="grid">
-          ${this._getRenderedPlaces(coachItem, index)}
+        <table
+          @focus=${() => this.onFocusTableCoachAndPreselectPlace(index)}
+          class="coach-wrapper__table"
+          id="seat-reservation-coach-${index}"
+          role="grid"
+        >
+          ${this._getRenderedRowPlaces(coachItem, index)}
         </table>
       </div>
     `;
@@ -214,7 +271,7 @@ class SbbSeatReservationElement extends SeatReservationService {
     `;
   }
 
-  private _getRenderedPlaces(coach: CoachItem, coachIndex: number): TemplateResult[] | null {
+  private _getRenderedRowPlaces(coach: CoachItem, coachIndex: number): TemplateResult[] | null {
     if (!coach.places) {
       return null;
     }
@@ -232,23 +289,15 @@ class SbbSeatReservationElement extends SeatReservationService {
     return Object.values(tableRowPlaces)
       .map((rowPlaces: Place[], index) => {
         return html`
-          <tr
-            id="row-${coachIndex}-${rowPlaces[0].position.y}"
-            data-row-y=${rowPlaces[0].position.y}
-            data-row-index=${index}
-          >
-            ${this._getRenderedPlace(rowPlaces, coachIndex, index)}
+          <tr id="row-${coachIndex}-${rowPlaces[0].position.y}" data-row-index=${index}>
+            ${this._getRenderedColumnPlaces(rowPlaces, coachIndex)}
           </tr>
         `;
       })
       .flatMap((rowTemplate) => rowTemplate);
   }
 
-  private _getRenderedPlace(
-    places: Place[],
-    coachIndex: number,
-    rowIndex: number,
-  ): TemplateResult[] | null {
+  private _getRenderedColumnPlaces(places: Place[], coachIndex: number): TemplateResult[] | null {
     //Sorts each place by its ascending x coordinate
     places.sort(
       (placeA: Place, placeB: Place) => Number(placeA.position.x) - Number(placeB.position.x),
@@ -277,11 +326,9 @@ class SbbSeatReservationElement extends SeatReservationService {
         >
           <sbb-seat-reservation-place-control
             @selectPlace=${(selectPlaceEvent: CustomEvent) => this._onSelectPlace(selectPlaceEvent)}
-            @focusPlace=${() => this._onFocusPlace()}
             id="seat-reservation__place-button-${coachIndex}-${place.number}"
             class="seat-reservation-place-control"
             data-cell-id="${coachIndex}-${place.position.y}-${index}"
-            data-row-y=${rowIndex}
             text=${place.number}
             type=${place.type}
             state=${place.state}
@@ -475,51 +522,39 @@ class SbbSeatReservationElement extends SeatReservationService {
     });
   }
 
-  private _onFocusPlace(): void {
-    //TODO - If place got focus by TAB and no coach is selected, we have to set the focus to the last input element jump out of seat reservation by TAB
-    //Place got a tab focus, so we jumps out from the seatreservation and set the focus to the fake input field, so the user does not tab throught all seats until the last tabable place is reached.
-    //if(this._selectedCoachIndex === -1){
-    //  this.preventCoachScrollByPlaceClick = true;
-    //this.currSelectedCoachIndex = 4;
-    //this._focusedCoachIndex = 4;
-    //console.log("SEAT RESERVATION -> focus LAST TAB Input Element",this.currSelectedCoachIndex)
-    //this.shadowRoot?.getElementById('last-tab-element')?.focus();
-    //}
-  }
-
-  private _onSelectNavCoach(event: CustomEvent): void {
-    const selectedNavCoachIndex = event.detail as number;
-    if (selectedNavCoachIndex !== null && selectedNavCoachIndex !== this.currSelectedCoachIndex) {
-      this.currSelectedPlace = null;
-      this.unfocusPlaceElement();
-      this.scrollToSelectedNavCoach(selectedNavCoachIndex);
-    }
-  }
-
   /**
    * Manages the selected place event triggered from the place
    * Each selection emits an array of all selected places
    * @param selectPlaceEvent
    */
   private _onSelectPlace(selectPlaceEvent: CustomEvent): void {
-    const currSelectedPlace = selectPlaceEvent.detail as PlaceSelection;
+    const selectedPlace = selectPlaceEvent.detail as PlaceSelection;
+
     if (this.focusedCoachIndex === -1 || this.focusedCoachIndex === this.currSelectedCoachIndex) {
       // preventCoachScrollByPlaceClick tur used to prevent auto scroll We prevent
       this.preventCoachScrollByPlaceClick = true;
       if (!this.disable) {
-        const place = this.seatReservation.coachItems[currSelectedPlace.coachIndex].places?.find(
-          (place) => place.number == currSelectedPlace.number,
+        const place = this.seatReservation.coachItems[selectedPlace.coachIndex].places?.find(
+          (place) => place.number == selectedPlace.number,
         );
 
         //Add place to place collection
-        this.updateSelectedSeatReservationPlaces(currSelectedPlace);
+        this.updateSelectedSeatReservationPlaces(selectedPlace);
 
         if (place) {
-          this.selectedCoachIndex = currSelectedPlace.coachIndex;
-          this.currSelectedCoachIndex = currSelectedPlace.coachIndex;
+          this.selectedCoachIndex = selectedPlace.coachIndex;
+          this.currSelectedCoachIndex = selectedPlace.coachIndex;
           this.currSelectedPlace = place;
         }
       }
+    }
+  }
+
+  private _onSelectNavCoach(event: CustomEvent): void {
+    const selectedNavCoachIndex = event.detail as number;
+    if (selectedNavCoachIndex !== null && selectedNavCoachIndex !== this.currSelectedCoachIndex) {
+      this.unfocusPlaceElement();
+      this.scrollToSelectedNavCoach(selectedNavCoachIndex);
     }
   }
 
