7da06a2834c75c6bfb03561a4d568216467d421e
thm-sbb <thomas.hamm@sbb.ch>
2025-04-15 10:51:51 +0200

    feat(sbb-seat-reservation-refactor-scoped-parts): only one scoped element (#3580)
    ## Preflight Checklist
    
    <!-- Please ensure you've completed the following steps by replacing [ ]
    with [x]-->
    
    - [ ] I have read the [Contributing
    Guidelines](https://github.com/sbb-design-systems/lyne-components/blob/main/docs/CONTRIBUTING.md)
    for this project.
    - [ ] I agree to follow the [Code of
    Conduct](https://github.com/sbb-design-systems/lyne-components/blob/main/docs/CODE_OF_CONDUCT.md)
    that this project adheres to.
    - [ ] I have searched the [pull request
    tracker](https://github.com/sbb-design-systems/lyne-components/pulls)
    for a Pull Request (PR) that matches the one I want to submit, without
    success.
    
    ## Issue
    
    This PR Closes #
    
    ## Pull request checklist
    
    Please check if your PR fulfills the following requirements:
    
    - [ ] Tests for the changes have been added (for bug fixes / features)
    - [ ] Docs have been reviewed and added / updated if needed (for bug
    fixes / features)
    
    See [Review Guidelines](../docs/REVIEW.md) for more information on what
    is checked during the review process.
    
    ## Changes
    
    Changes in this pull request:
    
    -
    
    ## Browsers
    
    I tested the build on the following browsers:
    
    - [ ] Firefox Desktop
    - [ ] Chrome Desktop
    - [ ] Edge Desktop
    - [ ] Safari Desktop
    - [ ] Chrome Mobile
    - [ ] Safari Mobile
    
    ## Screen readers
    
    I tested the build on the following browsers:
    
    - [ ] JAWS Firefox Desktop
    - [ ] JAWS Chrome Desktop
    - [ ] NVDA Firefox Desktop
    - [ ] NVDA Chrome Desktop
    - [ ] VoiceOver Safari Desktop
    - [ ] VoiceOver Chrome Desktop
    - [ ] VoiceOver Safari Mobile
    - [ ] Android Accessibility Suite Chrome Mobile
    
    ## Pull request type
    
    <!-- Please do not submit updates to dependencies unless it fixes an
    issue. -->
    
    <!-- Please try to limit your pull request to one type, submit multiple
    pull requests if needed. -->
    
    Please check the type of change your PR introduces:
    
    - [ ] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, renaming)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] Documentation content changes
    - [ ] Other (please describe):
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [ ] No
    
    <!-- If this introduces a breaking change, please describe the impact
    and migration path for existing applications below. -->
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as
    screenshots of how the component looks before and after the change. -->
    
    ---------
    
    Co-authored-by: Boms <r_boms@web.de>

--- src/elements-experimental/seat-reservation/seat-reservation/scoped-components.ts
+++ src/elements-experimental/seat-reservation/seat-reservation/scoped-components.ts
@@ -1,4 +1,1 @@
-export * from './scoped-components/coach-border.js';
-export * from './scoped-components/coach-wrapper.js';
-export * from './scoped-components/table-cell.js';
-export * from './scoped-components/seat-reservation-graphical-element.js';
+export * from './scoped-components/scoped-element.js';
