33d31739bd6f499d767aff3ccb23f7127608474a
Jeri Peier <jeremias.peier@sbb.ch>
2025-04-17 12:20:26 +0200

    chore: fix filename of id reference controller (#3601)

--- src/elements/core/controllers.ts
+++ src/elements/core/controllers.ts
@@ -3,7 +3,7 @@
  */
 export * from './controllers/connected-abort-controller.js';
 export * from './controllers/escapable-overlay-controller.js';
-export * from './controllers/id-observer-controller.js';
+export * from './controllers/id-reference-controller.js';
 export * from './controllers/inert-controller.js';
 export * from './controllers/language-controller.js';
 export * from './controllers/media-matchers-controller.js';
