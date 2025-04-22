485f507258e0c799610a1b554e2120db570ab2c6
thm-sbb <thomas.hamm@sbb.ch>
2025-04-22 12:49:54 +0200

    feature/thm/vertical-navi (#3616)
    ## Preflight Checklist
    
    <!-- Please ensure you've completed the following steps by replacing [ ]
    with [x]-->
    
    - [ ] I have read the [Contributing
    Guidelines](https://github.com/sbb-design-systems/lyne-components/blob/main/docs/CONTRIBUTING.md)
    for this project.
    - [ ] I agree to follow the [Code of
    Conduct](https://github.com/sbb-design-systems/lyne-components/blob/main/docs/CODE_OF_CONDUCT.md)
    that this project adheres to.
    - [ ] I have searched the [pull request
    tracker](https://github.com/sbb-design-systems/lyne-components/pulls)
    for a Pull Request (PR) that matches the one I want to submit, without
    success.
    
    ## Issue
    
    This PR Closes #
    
    ## Pull request checklist
    
    Please check if your PR fulfills the following requirements:
    
    - [ ] Tests for the changes have been added (for bug fixes / features)
    - [ ] Docs have been reviewed and added / updated if needed (for bug
    fixes / features)
    
    See [Review Guidelines](../docs/REVIEW.md) for more information on what
    is checked during the review process.
    
    ## Changes
    
    Changes in this pull request:
    
    -
    
    ## Browsers
    
    I tested the build on the following browsers:
    
    - [ ] Firefox Desktop
    - [ ] Chrome Desktop
    - [ ] Edge Desktop
    - [ ] Safari Desktop
    - [ ] Chrome Mobile
    - [ ] Safari Mobile
    
    ## Screen readers
    
    I tested the build on the following browsers:
    
    - [ ] JAWS Firefox Desktop
    - [ ] JAWS Chrome Desktop
    - [ ] NVDA Firefox Desktop
    - [ ] NVDA Chrome Desktop
    - [ ] VoiceOver Safari Desktop
    - [ ] VoiceOver Chrome Desktop
    - [ ] VoiceOver Safari Mobile
    - [ ] Android Accessibility Suite Chrome Mobile
    
    ## Pull request type
    
    <!-- Please do not submit updates to dependencies unless it fixes an
    issue. -->
    
    <!-- Please try to limit your pull request to one type, submit multiple
    pull requests if needed. -->
    
    Please check the type of change your PR introduces:
    
    - [ ] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, renaming)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] Documentation content changes
    - [ ] Other (please describe):
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [ ] No
    
    <!-- If this introduces a breaking change, please describe the impact
    and migration path for existing applications below. -->
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as
    screenshots of how the component looks before and after the change. -->

--- src/elements-experimental/seat-reservation/seat-reservation-navigation/seat-reservation-navigation-services/seat-reservation-navigation-services.ts
+++ src/elements-experimental/seat-reservation/seat-reservation-navigation/seat-reservation-navigation-services/seat-reservation-navigation-services.ts
@@ -1,10 +1,13 @@
+import { forceType } from '@sbb-esta/lyne-elements/core/decorators/force-type';
 import { type CSSResultGroup, nothing, type TemplateResult } from 'lit';
 import { html, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import { classMap } from 'lit/directives/class-map.js';
 
 import '../../seat-reservation-graphic.js';
 
 import style from './seat-reservation-navigation-services.scss?lit&inline';
+
 import '@sbb-esta/lyne-elements/screen-reader-only.js';
 
 /**
@@ -19,9 +22,24 @@ class SbbSeatReservationNavigationServicesElement extends LitElement {
   @property({ attribute: 'property-ids', type: Array })
   public accessor propertyIds: string[] = [];
 
+  @forceType()
+  @property({ type: Boolean })
+  public accessor vertical: boolean = false;
+
+  /**
+   * render a maximum of 3 of the service sign icons (slice(0,3)) regardless of the input from Backend,
+   * otherwise the layout could be destroyed
+   *
+   * @protected
+   */
   protected override render(): TemplateResult {
-    return html`<div class="sbb-seat-reservation-navigation__signs">
-      ${this.propertyIds?.map((signIcon: string) => {
+    return html`<div
+      class="${classMap({
+        'sbb-seat-reservation-navigation__signs': true,
+        'sbb-seat-reservation-navigation__signs--vertical': this.vertical,
+      })}"
+    >
+      ${this.propertyIds?.slice(0, 3)?.map((signIcon: string) => {
         return html`
           <sbb-seat-reservation-graphic
             name=${signIcon ?? nothing}
