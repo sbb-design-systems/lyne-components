110513bbf37ffec71f1ddcb87fa33f20e68ab651
Jeri Peier <jeremias.peier@sbb.ch>
2025-04-23 10:40:05 +0200

    feat(sbb-table-wrapper): introduce focusable property (#3624)
    Closes #3623

--- src/elements/table/table-wrapper/table-wrapper.spec.ts
+++ src/elements/table/table-wrapper/table-wrapper.spec.ts
@@ -43,21 +43,66 @@ describe(`sbb-table-wrapper`, () => {
 
     // Expand the table so that the scrollbar appears
     const table = element.querySelector('table')!;
-    const tableWrapper = element.shadowRoot!.querySelector('.sbb-table-wrapper')!;
 
     table.style.setProperty('width', '140%');
-    tableWrapper.scroll(20, 0);
+    element.scroll(20, 0);
 
     // ResizeObserver callbacks run outside the usual render flow
     await waitForCondition(() => element.classList.contains('sbb-table-wrapper-offset-both'));
     expect(element).to.have.class('sbb-table-wrapper-offset-both');
 
-    tableWrapper.scroll(-20, 0);
+    element.scroll(-20, 0);
     await waitForCondition(() => element.classList.contains('sbb-table-wrapper-offset-right'));
     expect(element).to.have.class('sbb-table-wrapper-offset-right');
 
-    tableWrapper.scroll(1000, 0);
+    element.scroll(1000, 0);
     await waitForCondition(() => element.classList.contains('sbb-table-wrapper-offset-left'));
     expect(element).to.have.class('sbb-table-wrapper-offset-left');
+  });
+
+  it('should set tabindex 0 when focusable', async () => {
+    element.focusable = true;
+    await waitForLitRender(element);
+
+    expect(element).to.have.attribute('tabindex', '0');
+  });
+
+  it('should not touch user set tabindex when focusable', async () => {
+    element.tabIndex = 2;
+    expect(element).to.have.attribute('tabindex', '2');
+
+    element.focusable = true;
+    await waitForLitRender(element);
+
+    expect(element).to.have.attribute('tabindex', '2');
+  });
+
+  it('should remove tabindex when not focusable anymore', async () => {
+    element.focusable = true;
+    await waitForLitRender(element);
+
+    expect(element).to.have.attribute('tabindex', '0');
+
+    element.focusable = false;
+    await waitForLitRender(element);
+
+    expect(element).not.to.have.attribute('tabindex');
+  });
+
+  it('should sync scrollbar classes', async () => {
+    expect(element).to.have.class('sbb-scrollbar-thick-track-visible');
+    expect(element).not.to.have.class('sbb-scrollbar-thick-negative-track-visible');
+
+    element.negative = true;
+    await waitForLitRender(element);
+
+    expect(element).not.to.have.class('sbb-scrollbar-thick-track-visible');
+    expect(element).to.have.class('sbb-scrollbar-thick-negative-track-visible');
+
+    element.negative = false;
+    await waitForLitRender(element);
+
+    expect(element).to.have.class('sbb-scrollbar-thick-track-visible');
+    expect(element).not.to.have.class('sbb-scrollbar-thick-negative-track-visible');
   });
 });
