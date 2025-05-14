6fbe421b05ad143086d82ead50f630bfca6d2bf0
Tommaso Menga <tommaso.menga-94@hotmail.it>
2025-05-14 16:21:15 +0200

    fix(sbb-toast): fix a11y on windows browsers (#3706)

--- src/elements/toast/toast.snapshot.spec.ts
+++ src/elements/toast/toast.snapshot.spec.ts
@@ -1,44 +1,52 @@
 import { expect } from '@open-wc/testing';
 import { html } from 'lit/static-html.js';
 
-import { isFirefox } from '../core/dom.js';
 import { fixture, testA11yTreeSnapshot } from '../core/testing/private.js';
-import { describeIf } from '../core/testing.js';
 
 import type { SbbToastElement } from './toast.component.js';
 
 import './toast.component.js';
+import '../link/link.js';
 
 describe(`sbb-toast`, () => {
-  describe('renders', () => {
-    let elem: SbbToastElement;
+  let elem: SbbToastElement;
 
+  describe('renders', () => {
     beforeEach(async () => {
       elem = await fixture(html`
-        <sbb-toast icon-name="circle-tick-small" dismissible> 'Lorem ipsum dolor' </sbb-toast>
+        <sbb-toast icon-name="circle-tick-small" dismissible> Lorem ipsum dolor </sbb-toast>
       `);
     });
 
-    describeIf(!isFirefox, 'Chrome-Safari', async () => {
-      it('DOM', async () => {
-        await expect(elem).dom.to.be.equalSnapshot();
-      });
-
-      it('Shadow DOM', async () => {
-        await expect(elem).shadowDom.to.be.equalSnapshot();
-      });
+    it('DOM', async () => {
+      await expect(elem).dom.to.be.equalSnapshot();
     });
 
-    describeIf(isFirefox, 'Firefox', async () => {
-      it('DOM', async () => {
-        await expect(elem).dom.to.be.equalSnapshot();
-      });
-
-      it('Shadow DOM', async () => {
-        await expect(elem).shadowDom.to.be.equalSnapshot();
-      });
+    it('Shadow DOM', async () => {
+      await expect(elem).shadowDom.to.be.equalSnapshot();
     });
 
     testA11yTreeSnapshot();
+  });
+
+  describe('renders with action', () => {
+    beforeEach(async () => {
+      elem = await fixture(html`
+        <sbb-toast icon-name="circle-tick-small" dismissible>
+          Lorem ipsum dolor
+          <sbb-link slot="action" sbb-toast-close href="https://www.sbb.ch" target="_blank">
+            Link action
+          </sbb-link>
+        </sbb-toast>
+      `);
+    });
+
+    it('DOM', async () => {
+      await expect(elem).dom.to.be.equalSnapshot();
+    });
+
+    it('Shadow DOM', async () => {
+      await expect(elem).shadowDom.to.be.equalSnapshot();
+    });
   });
 });
