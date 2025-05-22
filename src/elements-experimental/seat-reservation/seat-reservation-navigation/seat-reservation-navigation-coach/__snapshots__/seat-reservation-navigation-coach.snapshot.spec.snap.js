959e42fe6a18f598d39278d0b185ace3152afa67
e528920 <thomas.hamm@sbb.ch>
2025-05-22 07:30:18 +0200

    test(seat-reservation): test use new class names

--- src/elements-experimental/seat-reservation/seat-reservation-navigation/seat-reservation-navigation-coach/__snapshots__/seat-reservation-navigation-coach.snapshot.spec.snap.js
+++ src/elements-experimental/seat-reservation/seat-reservation-navigation/seat-reservation-navigation-coach/__snapshots__/seat-reservation-navigation-coach.snapshot.spec.snap.js
@@ -11,26 +11,26 @@ snapshots["sbb-seat-reservation-navigation-coach renders a navigation coach DOM"
 /* end snapshot sbb-seat-reservation-navigation-coach renders a navigation coach DOM */
 
 snapshots["sbb-seat-reservation-navigation-coach renders a navigation coach Shadow DOM"] = 
-`<div class="sbb-seat-reservation-navigation__item-coach">
+`<div class="sbb-sr-navigation__item-coach">
   <sbb-screen-reader-only id="nav-coach-service-descriptions-0">
     en:Verfügbare Services: Velozone,Silence area
   </sbb-screen-reader-only>
   <button
     aria-describedby="nav-coach-service-descriptions-0"
-    class="sbb-seat-reservation-navigation__control-button"
+    class="sbb-sr-navigation__ctrl-button"
     title="Navigate to train compartment 85"
     type="button"
   >
-    <div class="sbb-seat-reservation-navigation__additional-information">
+    <div class="sbb-sr-navigation__additional-information">
       <div
         aria-hidden="true"
-        class="sbb-seat-reservation-navigation__item-coach-number"
+        class="sbb-sr-navigation__item-coach-number"
       >
         85
       </div>
       <div
         aria-hidden="true"
-        class="sbb-seat-reservation-navigation__item-coach-travelclass"
+        class="sbb-sr-navigation__item-coach-travelclass"
       >
       </div>
     </div>
