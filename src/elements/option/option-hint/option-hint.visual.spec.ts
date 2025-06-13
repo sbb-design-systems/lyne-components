c332d391dbeb44889a909df7b99a5a58c9ec7bb3
Tommmaso Menga <tommaso.menga-94@hotmail.it>
2025-06-13 11:18:03 +0200

    test(sbb-option-hint): fix visual spec

--- src/elements/option/option-hint/option-hint.visual.spec.ts
+++ src/elements/option/option-hint/option-hint.visual.spec.ts
@@ -36,20 +36,23 @@ describe('sbb-option-hint', () => {
       it(
         'Autocomplete',
         visualDiffDefault.with(async (setup) => {
-          await setup.withFixture(html`
-            <sbb-form-field ?negative=${negative} size=${size}>
-              <label>Autocomplete</label>
-              <input />
-              <sbb-autocomplete>
-                <sbb-option value="1"> Option 1 </sbb-option>
-                <sbb-option value="2"> Option 2 </sbb-option>
-                <sbb-option value="3"> Option 3 </sbb-option>
-                <sbb-option value="4"> Option 4 </sbb-option>
-                ${divider ? html`<sbb-divider></sbb-divider>` : nothing}
-                <sbb-option-hint>42 more hits</sbb-option-hint>
-              </sbb-autocomplete>
-            </sbb-form-field>
-          `);
+          await setup.withFixture(
+            html`
+              <sbb-form-field ?negative=${negative} size=${size}>
+                <label>Autocomplete</label>
+                <input />
+                <sbb-autocomplete>
+                  <sbb-option value="1"> Option 1 </sbb-option>
+                  <sbb-option value="2"> Option 2 </sbb-option>
+                  <sbb-option value="3"> Option 3 </sbb-option>
+                  <sbb-option value="4"> Option 4 </sbb-option>
+                  ${divider ? html`<sbb-divider></sbb-divider>` : nothing}
+                  <sbb-option-hint>42 more hits</sbb-option-hint>
+                </sbb-autocomplete>
+              </sbb-form-field>
+            `,
+            { minHeight: '500px' },
+          );
           setup.withPostSetupAction(() => openAutocomplete(setup));
         }),
       );
@@ -57,25 +60,28 @@ describe('sbb-option-hint', () => {
       it(
         'Autocomplete with group',
         visualDiffDefault.with(async (setup) => {
-          await setup.withFixture(html`
-            <sbb-form-field ?negative=${negative} size=${size}>
-              <label>Autocomplete</label>
-              <input />
-              <sbb-autocomplete preserve-icon-space>
-                <sbb-optgroup>
-                  <sbb-option value="1" icon-name="clock-small"> Option 1 </sbb-option>
-                  <sbb-option value="2" icon-name="clock-small"> Option 2 </sbb-option>
-                  <sbb-option-hint>Group hint</sbb-option-hint>
-                </sbb-optgroup>
-                <sbb-optgroup>
-                  <sbb-option value="3" icon-name="clock-small"> Option 3 </sbb-option>
-                  <sbb-option value="4"> Option 4 </sbb-option>
-                </sbb-optgroup>
-                ${divider ? html`<sbb-divider></sbb-divider>` : nothing}
-                <sbb-option-hint>42 more hits</sbb-option-hint>
-              </sbb-autocomplete>
-            </sbb-form-field>
-          `);
+          await setup.withFixture(
+            html`
+              <sbb-form-field ?negative=${negative} size=${size}>
+                <label>Autocomplete</label>
+                <input />
+                <sbb-autocomplete preserve-icon-space>
+                  <sbb-optgroup>
+                    <sbb-option value="1" icon-name="clock-small"> Option 1 </sbb-option>
+                    <sbb-option value="2" icon-name="clock-small"> Option 2 </sbb-option>
+                    <sbb-option-hint>Group hint</sbb-option-hint>
+                  </sbb-optgroup>
+                  <sbb-optgroup>
+                    <sbb-option value="3" icon-name="clock-small"> Option 3 </sbb-option>
+                    <sbb-option value="4"> Option 4 </sbb-option>
+                  </sbb-optgroup>
+                  ${divider ? html`<sbb-divider></sbb-divider>` : nothing}
+                  <sbb-option-hint>42 more hits</sbb-option-hint>
+                </sbb-autocomplete>
+              </sbb-form-field>
+            `,
+            { minHeight: '500px' },
+          );
           setup.withPostSetupAction(() => openAutocomplete(setup));
         }),
       );
@@ -83,19 +89,22 @@ describe('sbb-option-hint', () => {
       it(
         'Select',
         visualDiffDefault.with(async (setup) => {
-          await setup.withFixture(html`
-            <sbb-form-field ?negative=${negative} size=${size}>
-              <label>Select</label>
-              <sbb-select>
-                <sbb-option value="1"> Option 1 </sbb-option>
-                <sbb-option value="2"> Option 2 </sbb-option>
-                <sbb-option value="3"> Option 3 </sbb-option>
-                <sbb-option value="4"> Option 4 </sbb-option>
-                ${divider ? html`<sbb-divider></sbb-divider>` : nothing}
-                <sbb-option-hint>42 more hits</sbb-option-hint>
-              </sbb-select>
-            </sbb-form-field>
-          `);
+          await setup.withFixture(
+            html`
+              <sbb-form-field ?negative=${negative} size=${size}>
+                <label>Select</label>
+                <sbb-select>
+                  <sbb-option value="1"> Option 1 </sbb-option>
+                  <sbb-option value="2"> Option 2 </sbb-option>
+                  <sbb-option value="3"> Option 3 </sbb-option>
+                  <sbb-option value="4"> Option 4 </sbb-option>
+                  ${divider ? html`<sbb-divider></sbb-divider>` : nothing}
+                  <sbb-option-hint>42 more hits</sbb-option-hint>
+                </sbb-select>
+              </sbb-form-field>
+            `,
+            { minHeight: '500px' },
+          );
           setup.withPostSetupAction(() =>
             setup.snapshotElement.querySelector('sbb-select')!.open(),
           );
