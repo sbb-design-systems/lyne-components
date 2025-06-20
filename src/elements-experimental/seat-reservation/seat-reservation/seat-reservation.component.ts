38515cc11635346782a5bb29da9b82c02cc297b4
e528920 <thomas.hamm@sbb.ch>
2025-06-20 11:05:03 +0200

    fix(sbb-seat-reservation-popover): initial version; without screenreader attention

--- src/elements-experimental/seat-reservation/seat-reservation/seat-reservation.component.ts
+++ src/elements-experimental/seat-reservation/seat-reservation/seat-reservation.component.ts
@@ -522,7 +522,6 @@ class SbbSeatReservationElement extends SeatReservationBaseElement {
           z-index="${serviceElement.position.z}"
         >
           <sbb-popover-trigger id="${triggerId}">
-            <div class="ASDFASDFASDFASDFAS">
             <sbb-seat-reservation-graphic
               name=${serviceElement.icon ?? nothing}
               width=${serviceElement.dimension.w * this.baseGridSize}
@@ -531,7 +530,6 @@ class SbbSeatReservationElement extends SeatReservationBaseElement {
               role="img"
               aria-hidden="true"
             ></sbb-seat-reservation-graphic>
-            </div>
           </sbb-popover-trigger>
           ${this._popover(triggerId, titleDescription)}
         </sbb-seat-reservation-scoped>
