176eb24053f74422802e676422316adbbc2fcddd
Tommaso Menga <tommaso.menga-94@hotmail.it>
2025-07-10 10:41:30 +0200

    refactor(sbb-menu, sbb-menu-button, sbb-menu-link): assigned specific role (#3802)

--- src/elements/menu/menu-button/__snapshots__/menu-button.snapshot.spec.snap.js
+++ src/elements/menu/menu-button/__snapshots__/menu-button.snapshot.spec.snap.js
@@ -74,7 +74,7 @@ snapshots["sbb-menu-button renders component with icon A11y tree Chrome"] =
   "name": "",
   "children": [
     {
-      "role": "button",
+      "role": "menuitem",
       "name": "Action"
     }
   ]
@@ -90,8 +90,14 @@ snapshots["sbb-menu-button renders component with icon A11y tree Firefox"] =
   "name": "",
   "children": [
     {
-      "role": "button",
-      "name": "Action"
+      "role": "text container",
+      "name": "",
+      "children": [
+        {
+          "role": "text leaf",
+          "name": "Action"
+        }
+      ]
     }
   ]
 }
