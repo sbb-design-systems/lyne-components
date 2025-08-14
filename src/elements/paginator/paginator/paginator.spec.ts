c6edf5f40f52b8ce82d95a2ecabdf79cae3ce1c8
Jeremias Peier <jeremias.peier@sbb.ch>
2025-08-14 12:24:16 +0200

    fix: fix test

--- src/elements/paginator/paginator/paginator.spec.ts
+++ src/elements/paginator/paginator/paginator.spec.ts
@@ -230,7 +230,7 @@ describe('sbb-paginator', () => {
 
     const comboBoxElement = element.shadowRoot!.querySelector('[role="combobox"]')!;
 
-    expect(comboBoxElement).to.have.attribute('aria-label', 'Items per Page');
+    expect(comboBoxElement).to.have.attribute('aria-label', 'Items per page');
 
     const lang = document.documentElement.getAttribute('lang')!;
     document.documentElement.setAttribute('lang', 'de');
