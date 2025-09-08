58d919c5e6791c5b886941d5bfdf715c7e4afee1
Jeremias Peier <jeremias.peier@sbb.ch>
2025-09-08 13:45:40 +0200

    refactor: loading indicator circle

--- src/elements/loading-indicator-circle/loading-indicator-circle.stories.ts
+++ src/elements/loading-indicator-circle/loading-indicator-circle.stories.ts
@@ -96,7 +96,9 @@ const meta: Meta = {
   ],
   parameters: {
     backgroundColor: (context: StoryContext) =>
-      context.args.color === 'white' ? 'var(--sbb-color-iron)' : 'var(--sbb-color-white)',
+      context.args.color === 'white'
+        ? 'var(--sbb-background-color-1-negative)'
+        : 'var(--sbb-background-color-1)',
     docs: {
       extractComponentDescription: () => readme,
     },
