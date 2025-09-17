7fab16dee998e536a832b81f8e70f7932988fb3f
e567547 <grass.leon@sbb.ch>
2025-09-17 12:25:03 +0200

    feat(seat-reservation): added firefox-test to snapshot

---
+++ src/elements-experimental/seat-reservation/seat-reservation-navigation-services/__snapshots__/seat-reservation-navigation-services.snapshot.spec.snap.js
@@ -0,0 +1,64 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["sbb-seat-reservation-navigation-services renders DOM"] = 
+`<sbb-seat-reservation-navigation-services>
+</sbb-seat-reservation-navigation-services>
+`;
+/* end snapshot sbb-seat-reservation-navigation-services renders DOM */
+
+snapshots["sbb-seat-reservation-navigation-services renders Shadow DOM"] = 
+`<div class="sbb-sr-navigation__signs">
+  <sbb-screen-reader-only>
+    Available services:Bike area, Quiet zone
+  </sbb-screen-reader-only>
+  <sbb-seat-reservation-graphic
+    aria-hidden="true"
+    name="BICYCLE"
+    style="--sbb-seat-reservation-graphic-width:20;--sbb-seat-reservation-graphic-height:20;"
+    title="Bike area"
+  >
+  </sbb-seat-reservation-graphic>
+  <sbb-seat-reservation-graphic
+    aria-hidden="true"
+    name="SILENCE"
+    style="--sbb-seat-reservation-graphic-width:20;--sbb-seat-reservation-graphic-height:20;"
+    title="Quiet zone"
+  >
+  </sbb-seat-reservation-graphic>
+</div>
+`;
+/* end snapshot sbb-seat-reservation-navigation-services renders Shadow DOM */
+
+snapshots["sbb-seat-reservation-navigation-services renders A11y tree Chrome"] = 
+`<p>
+  {
+  "role": "WebArea",
+  "name": "",
+  "children": [
+    {
+      "role": "text",
+      "name": "Available services:Bike area, Quiet zone"
+    }
+  ]
+}
+</p>
+`;
+/* end snapshot sbb-seat-reservation-navigation-services renders A11y tree Chrome */
+
+snapshots["sbb-seat-reservation-navigation-services renders A11y tree Firefox"] = 
+`<p>
+  {
+  "role": "document",
+  "name": "",
+  "children": [
+    {
+      "role": "text leaf",
+      "name": "Available services:Bike area, Quiet zone"
+    }
+  ]
+}
+</p>
+`;
+/* end snapshot sbb-seat-reservation-navigation-services renders A11y tree Firefox */
+
