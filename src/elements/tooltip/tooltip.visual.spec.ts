b57399980ec7cd10533f772f8dff6dcfababe489
Tommmaso Menga <tommaso.menga-94@hotmail.it>
2025-10-06 10:19:07 +0200

    test(sbb-tooltip): add 'rtl' tests

--- src/elements/tooltip/tooltip.visual.spec.ts
+++ src/elements/tooltip/tooltip.visual.spec.ts
@@ -5,39 +5,71 @@ import { describeViewports, visualDiffDefault } from '../core/testing/private.js
 import './tooltip.component.js';
 import '../button/button.js';
 
+const physicalSupportedPositions = [
+  'top',
+  'bottom',
+  'left',
+  'right',
+  'top span-left',
+  'top span-right',
+  'bottom span-left',
+  'bottom span-right',
+  'left span-top',
+  'left span-bottom',
+  'right span-top',
+  'right span-bottom',
+];
+
+// Note: if you add new logical supported positions, update also the 'rtlPositionMapping'
+const logicalSupportedPositions = [
+  'block-start',
+  'block-end',
+  'inline-start',
+  'inline-end',
+  'block-start span-inline-start',
+  'block-start span-inline-end',
+  'block-end span-inline-start',
+  'block-end span-inline-end',
+  'inline-start span-block-start',
+  'inline-start span-block-end',
+  'inline-end span-block-start',
+  'inline-end span-block-end',
+];
+
 describe('sbb-tooltip', () => {
-  const positions = [
-    'top',
-    'bottom',
-    'left',
-    'right',
-    'top span-left',
-    'top span-right',
-    'bottom span-left',
-    'bottom span-right',
-    'left span-top',
-    'left span-bottom',
-    'right span-top',
-    'right span-bottom',
-    'block-start',
-    'block-end',
-    'inline-start',
-    'inline-end',
-    'block-start span-inline-start',
-    'block-start span-inline-end',
-    'block-end span-inline-start',
-    'block-end span-inline-end',
-    'inline-start span-block-start',
-    'inline-start span-block-end',
-    'inline-end span-block-start',
-    'inline-end span-block-end',
-  ];
+  const positions = [...physicalSupportedPositions, ...logicalSupportedPositions];
 
   describeViewports({ viewports: ['medium'], viewportHeight: 250 }, () => {
+    afterEach(async () => {
+      document.documentElement.removeAttribute('dir');
+    });
+
     for (const position of positions) {
       it(
         `position=${position} ${visualDiffDefault.name}`,
         visualDiffDefault.with(async (setup) => {
+          await setup.withFixture(html`
+            <div style="padding-block: 5rem; padding-inline: 10rem">
+              <sbb-button icon-name="pen-small" id="trigger">Button</sbb-button>
+              <sbb-tooltip
+                trigger="trigger"
+                style="--sbb-overlay-controller-position-area: ${position}"
+                >I am a tooltip with a message</sbb-tooltip
+              >
+            </div>
+          `);
+          setup.withPostSetupAction(async () => {
+            document.querySelector('sbb-tooltip')!.open();
+          });
+        }),
+      );
+    }
+
+    for (const position of logicalSupportedPositions) {
+      it(
+        `position=${position} rtl`,
+        visualDiffDefault.with(async (setup) => {
+          document.documentElement.dir = 'rtl';
           await setup.withFixture(html`
             <div style="padding-block: 5rem; padding-inline: 10rem">
               <sbb-button icon-name="pen-small" id="trigger">Button</sbb-button>
