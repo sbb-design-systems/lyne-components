b0b2e4599e14377728afe31bdecf0f0e2e0b7447
Jeremias Peier <jeremias.peier@sbb.ch>
2025-10-07 12:33:58 +0200

    fix: snapshots

--- src/elements/tabs/tab-group/__snapshots__/tab-group.snapshot.spec.snap.js
+++ src/elements/tabs/tab-group/__snapshots__/tab-group.snapshot.spec.snap.js
@@ -1,7 +1,7 @@
 /* @web/test-runner snapshot v1 */
 export const snapshots = {};
 
-snapshots["sbb-tab-group renders DOM"] =
+snapshots["sbb-tab-group renders DOM"] = 
 `<sbb-tab-group initial-selected-index="0">
   <sbb-tab-label
     active=""
@@ -65,7 +65,7 @@ snapshots["sbb-tab-group renders DOM"] =
 `;
 /* end snapshot sbb-tab-group renders DOM */
 
-snapshots["sbb-tab-group renders Shadow DOM"] =
+snapshots["sbb-tab-group renders Shadow DOM"] = 
 `<div
   class="sbb-tab-group"
   role="tablist"
@@ -80,7 +80,7 @@ snapshots["sbb-tab-group renders Shadow DOM"] =
 `;
 /* end snapshot sbb-tab-group renders Shadow DOM */
 
-snapshots["sbb-tab-group renders A11y tree Firefox"] =
+snapshots["sbb-tab-group renders A11y tree Firefox"] = 
 `<p>
   {
   "role": "document",
@@ -119,7 +119,7 @@ snapshots["sbb-tab-group renders A11y tree Firefox"] =
 `;
 /* end snapshot sbb-tab-group renders A11y tree Firefox */
 
-snapshots["sbb-tab-group renders A11y tree Chrome"] =
+snapshots["sbb-tab-group renders A11y tree Chrome"] = 
 `<p>
   {
   "role": "WebArea",
