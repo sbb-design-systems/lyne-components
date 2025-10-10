8c19ff36f40c3ce2cc76f1107f4172cb95380e31
Davide Mininni <davide.mininni@finconsgroup.com>
2025-10-10 10:53:29 +0200

    fix: improve visual test names

--- src/elements/mini-calendar/mini-calendar-day/mini-calendar-day.visual.spec.ts
+++ src/elements/mini-calendar/mini-calendar-day/mini-calendar-day.visual.spec.ts
@@ -11,7 +11,7 @@ import './mini-calendar-day.component.js';
 describe('sbb-mini-calendar-day', () => {
   describeViewports({ viewports: ['zero', 'medium'] }, () => {
     for (const marker of ['', 'circle', 'target', 'slash', 'cross']) {
-      describe(`marker=${marker}`, () => {
+      describe(`marker=${marker === '' ? 'default' : marker}`, () => {
         for (const state of visualDiffStandardStates) {
           it(
             state.name,
@@ -30,7 +30,7 @@ describe('sbb-mini-calendar-day', () => {
 
     for (const color of ['', 'charcoal', 'cloud', 'orange', 'red', 'sky']) {
       it(
-        `color=${color}`,
+        `color=${color === '' ? 'default' : color}`,
         visualDiffDefault.with(async (setup) => {
           await setup.withFixture(
             html`<sbb-mini-calendar-day date="2025-01-01" color=${color}></sbb-mini-calendar-day>`,
