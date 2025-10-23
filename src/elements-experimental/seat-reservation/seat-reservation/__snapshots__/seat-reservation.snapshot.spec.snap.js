e36bea61e53d0bfa28c5d8731920e56b8d282852
e528920 <thomas.hamm@sbb.ch>
2025-10-23 07:43:36 +0200

    fix(sbb-seat-reservation-restanzen): add full list of place properties w/ translations (TIMO-43054)

--- src/elements-experimental/seat-reservation/seat-reservation/__snapshots__/seat-reservation.snapshot.spec.snap.js
+++ src/elements-experimental/seat-reservation/seat-reservation/__snapshots__/seat-reservation.snapshot.spec.snap.js
@@ -94,6 +94,36 @@ snapshots["sbb-seat-reservation renders Shadow DOM"] =
 `;
 /* end snapshot sbb-seat-reservation renders Shadow DOM */
 
+snapshots["sbb-seat-reservation renders A11y tree Chrome"] = 
+`<p>
+  {
+  "role": "WebArea",
+  "name": "",
+  "children": [
+    {
+      "role": "contentinfo",
+      "name": "Start Graphic Seat Reservation",
+      "disabled": true
+    },
+    {
+      "role": "list",
+      "name": "Seat reservation navigation"
+    },
+    {
+      "role": "generic",
+      "name": ""
+    },
+    {
+      "role": "contentinfo",
+      "name": "Exit Graphic Seat Reservation",
+      "disabled": true
+    }
+  ]
+}
+</p>
+`;
+/* end snapshot sbb-seat-reservation renders A11y tree Chrome */
+
 snapshots["sbb-seat-reservation renders A11y tree Firefox"] = 
 `<p>
   {
@@ -129,34 +159,4 @@ snapshots["sbb-seat-reservation renders A11y tree Firefox"] =
 </p>
 `;
 /* end snapshot sbb-seat-reservation renders A11y tree Firefox */
-
-snapshots["sbb-seat-reservation renders A11y tree Chrome"] = 
-`<p>
-  {
-  "role": "WebArea",
-  "name": "",
-  "children": [
-    {
-      "role": "contentinfo",
-      "name": "Start Graphic Seat Reservation",
-      "disabled": true
-    },
-    {
-      "role": "list",
-      "name": "Seat reservation navigation"
-    },
-    {
-      "role": "generic",
-      "name": ""
-    },
-    {
-      "role": "contentinfo",
-      "name": "Exit Graphic Seat Reservation",
-      "disabled": true
-    }
-  ]
-}
-</p>
-`;
-/* end snapshot sbb-seat-reservation renders A11y tree Chrome */
 
