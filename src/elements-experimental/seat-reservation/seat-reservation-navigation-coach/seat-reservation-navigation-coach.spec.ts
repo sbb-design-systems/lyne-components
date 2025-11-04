133e153f219b59914f5b2496e4dc81a9b596d30c
e528920 <thomas.hamm@sbb.ch>
2025-11-04 09:04:05 +0100

    test(sbb-seat-reservation): test keyboard interactions + aria-label

--- src/elements-experimental/seat-reservation/seat-reservation-navigation-coach/seat-reservation-navigation-coach.spec.ts
+++ src/elements-experimental/seat-reservation/seat-reservation-navigation-coach/seat-reservation-navigation-coach.spec.ts
@@ -1,5 +1,6 @@
 import { assert, expect, fixture } from '@open-wc/testing';
 import { EventSpy, waitForLitRender } from '@sbb-esta/lyne-elements/core/testing.js';
+import { sendKeys } from '@web/test-runner-commands';
 import { html } from 'lit/static-html.js';
 
 import { SbbSeatReservationNavigationCoachElement } from './seat-reservation-navigation-coach.component.js';
@@ -25,7 +26,6 @@ describe('sbb-seat-reservation-navigation-coach', () => {
 
   it('should have a button', () => {
     const el = element.shadowRoot?.querySelector('.sbb-sr-navigation__ctrl-button');
-
     assert.instanceOf(el, HTMLButtonElement);
   });
 
@@ -44,7 +44,7 @@ describe('sbb-seat-reservation-navigation-coach', () => {
     await expect(element.coachId).to.be.equal('coach-id');
   });
 
-  it('check host attributes and content', () => {
+  it('check child content', () => {
     expect(
       element.shadowRoot!.firstElementChild!.classList.contains('sbb-sr-navigation__item-coach'),
     ).to.be.true;
@@ -64,6 +64,21 @@ describe('sbb-seat-reservation-navigation-coach', () => {
 
     await expect(btn.hasAttribute('disabled')).to.be.true;
     await expect(clickSpy.count).to.be.equal(0);
+  });
+
+  it('should stop propagating click if disabled', async () => {
+    element.disable = true;
+    await waitForLitRender(element);
+
+    const clickSpy = new EventSpy('click');
+    const btn = element.shadowRoot?.querySelector(
+      '.sbb-sr-navigation__ctrl-button',
+    ) as HTMLButtonElement;
+
+    btn.click();
+
+    btn.dispatchEvent(new PointerEvent('click'));
+    expect(clickSpy.count).not.to.be.greaterThan(0);
   });
 
   it('should have a driver area', async () => {
@@ -166,27 +181,57 @@ describe('sbb-seat-reservation-navigation-coach', () => {
     await expect(getComputedStyle(btn).borderEndEndRadius).to.be.equal('16px');
   });
 
-  it('should call selectcoach on interaction', async () => {
+  it('should call @selectcoach on interaction', async () => {
     const selectSpy = new EventSpy('selectcoach');
     const btn = element.shadowRoot?.querySelector(
       '.sbb-sr-navigation__ctrl-button',
     ) as HTMLButtonElement;
 
     btn.click();
-    await waitForLitRender(element);
     await selectSpy.calledOnce();
     await expect(selectSpy.count).to.be.equal(1);
   });
 
-  it('should call focuscoach on interaction', async () => {
-    const focuscoach = new EventSpy('focuscoach');
+  it('should dispatch click event on pressing Enter', async () => {
     const btn = element.shadowRoot?.querySelector(
       '.sbb-sr-navigation__ctrl-button',
     ) as HTMLButtonElement;
 
+    const changeSpy = new EventSpy('click');
     btn.focus();
+    await sendKeys({ press: 'Enter' });
+    expect(changeSpy.count).to.be.greaterThan(0);
+  });
+
+  it('should dispatch click event on pressing Space', async () => {
+    const btn = element.shadowRoot?.querySelector(
+      '.sbb-sr-navigation__ctrl-button',
+    ) as HTMLButtonElement;
+
+    const changeSpy = new EventSpy('click');
+    btn.focus();
+    await sendKeys({ press: ' ' });
+    expect(changeSpy.count).to.be.greaterThan(0);
+  });
+
+  it('should take freePlacesByType settings into account for title-Attribute', async () => {
+    element.freePlacesByType = { seats: 20, bicycles: 10 };
+
     await waitForLitRender(element);
-    await focuscoach.calledOnce();
-    await expect(focuscoach.count).to.be.equal(1);
+
+    const btn = element.shadowRoot?.querySelector(
+      '.sbb-sr-navigation__ctrl-button',
+    ) as HTMLButtonElement;
+
+    await expect(btn.getAttribute('title')).to.be.equal(
+      'Navigate to coach coach-id. 20 seats available. 10 available bicycle spaces.',
+    );
+  });
+
+  it('should render the aria label in the sbb-screen-reader-only element', async () => {
+    element.setAttribute('property-ids', '["BISTRO"]');
+    await waitForLitRender(element);
+    const screenReaderOnlyElement = element.shadowRoot?.querySelector('sbb-screen-reader-only');
+    expect(screenReaderOnlyElement?.innerHTML).to.include('Available services: Bistro');
   });
 });
