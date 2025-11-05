beb48197ffc6c8367f75e122b9b71b445ffa5f1f
e528920 <thomas.hamm@sbb.ch>
2025-11-05 12:31:34 +0100

    test(sbb-seat-reservation): visual tests for different states

--- src/elements-experimental/seat-reservation/seat-reservation-navigation-coach/seat-reservation-navigation-coach.visual.spec.ts
+++ src/elements-experimental/seat-reservation/seat-reservation-navigation-coach/seat-reservation-navigation-coach.visual.spec.ts
@@ -2,45 +2,51 @@ import {
   describeEach,
   describeViewports,
   visualDiffDefault,
-  visualDiffStandardStates,
+  visualDiffFocus,
+  visualDiffHover,
+  visualRegressionFixture,
 } from '@sbb-esta/lyne-elements/core/testing/private.js';
 import './seat-reservation-navigation-coach.component.js';
 import { html } from 'lit';
 
 describe('sbb-seat-reservation-navigation-coach', () => {
+  let root: HTMLElement;
+
   const cases = {
     selected: [false, true],
-    focused: [false, true],
-    disable: [false, true],
+    disabled: [false, true],
   };
 
-  describeViewports({ viewports: ['zero', 'large'] }, () => {
-    for (const state of visualDiffStandardStates) {
-      it(
-        `${state.name}`,
-        state.with(async (setup) => {
-          await setup.withFixture(
-            html`<sbb-seat-reservation-navigation-coach
-              coach-id="CI85"
-            ></sbb-seat-reservation-navigation-coach>`,
-          );
-        }),
-      );
-    }
-    describeEach(cases, ({ selected, focused, disable }) => {
-      it(
-        `test selected=${selected} focused=${focused} disable=${disable}`,
-        visualDiffDefault.with(async (setup) => {
-          await setup.withFixture(html`
+  // Standard visual diff states to be tested;
+  // own states array because the visual-regression-snapshot.ts constant "visualDiffStandardStates"
+  // includes more than we need
+  const visualDiffStandardStates = [visualDiffDefault, visualDiffFocus, visualDiffHover] as const;
+
+  // large only viewport because we don't use any other breakpoint media queries
+  describeViewports({ viewports: ['large'] }, () => {
+    describeEach(cases, ({ selected, disabled }) => {
+      beforeEach(async function () {
+        root = await visualRegressionFixture(
+          html`
             <sbb-seat-reservation-navigation-coach
-              coach-id="CI85"
+              coach-id="CI"
               ?selected=${selected}
-              ?disable=${disable}
-              ?focused=${focused}
+              ?disable=${disabled}
+              travel-class=["FIRST"]
             ></sbb-seat-reservation-navigation-coach>
-          `);
-        }),
-      );
+          `,
+        );
+      });
+
+      for (const state of visualDiffStandardStates) {
+        it(
+          `${state.name}`,
+          state.with((setup) => {
+            setup.withSnapshotElement(root);
+            setup.withStateElement(root.querySelector('.sbb-sr-navigation__ctrl-button')!);
+          }),
+        );
+      }
     });
   });
 });
