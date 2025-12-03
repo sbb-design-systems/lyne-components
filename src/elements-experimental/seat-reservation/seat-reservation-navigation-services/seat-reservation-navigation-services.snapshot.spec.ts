58f4b6b08add5cdcb1dab4cf61c88879b3eba6b1
Thomas <thomas.hamm@sbb.ch>
2025-12-03 13:44:14 +0100

    test(sbb-seat-reservation): merge some tests from branch to dev-branch

--- src/elements-experimental/seat-reservation/seat-reservation-navigation-services/seat-reservation-navigation-services.snapshot.spec.ts
+++ src/elements-experimental/seat-reservation/seat-reservation-navigation-services/seat-reservation-navigation-services.snapshot.spec.ts
@@ -24,11 +24,11 @@ describe(`sbb-seat-reservation-navigation-services`, () => {
     });
 
     it('DOM', async () => {
-      await expect(element).dom.to.be.equalSnapshot();
+      expect(element).dom.to.be.equalSnapshot();
     });
 
     it('Shadow DOM', async () => {
-      await expect(element).shadowDom.to.be.equalSnapshot();
+      expect(element).shadowDom.to.be.equalSnapshot();
     });
 
     testA11yTreeSnapshot();
