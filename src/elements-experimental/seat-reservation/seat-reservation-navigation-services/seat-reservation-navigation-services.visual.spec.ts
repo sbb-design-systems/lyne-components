a9a342702288a742e26a9fd3d3780730301994d8
thm-sbb <thomas.hamm@sbb.ch>
2025-12-05 10:32:33 +0100

    test(sbb-seat-reservation): merge some tests from branch to dev-branch (#4265)
    ## Preflight Checklist
    
    <!-- Please ensure you've completed the following steps by replacing [ ]
    with [x]-->
    
    - [ ] I have read the [Contributing
    Guidelines](https://github.com/sbb-design-systems/lyne-components/blob/main/docs/CONTRIBUTING.md)
    for this project.
    - [ ] I agree to follow the [Code of
    Conduct](https://github.com/sbb-design-systems/lyne-components/blob/main/docs/CODE_OF_CONDUCT.md)
    that this project adheres to.
    - [ ] I have searched the [pull request
    tracker](https://github.com/sbb-design-systems/lyne-components/pulls)
    for a Pull Request (PR) that matches the one I want to submit, without
    success.
    
    ## Issue
    
    This PR Closes #
    
    ## Pull request checklist
    
    Please check if your PR fulfills the following requirements:
    
    - [ ] Tests for the changes have been added (for bug fixes / features)
    - [ ] Docs have been reviewed and added / updated if needed (for bug
    fixes / features)
    
    See [Review Guidelines](../docs/REVIEW.md) for more information on what
    is checked during the review process.
    
    ## Changes
    
    Changes in this pull request:
    
    -
    
    ## Browsers
    
    I tested the build on the following browsers:
    
    - [ ] Firefox Desktop
    - [ ] Chrome Desktop
    - [ ] Edge Desktop
    - [ ] Safari Desktop
    - [ ] Chrome Mobile
    - [ ] Safari Mobile
    
    ## Screen readers
    
    I tested the build on the following browsers:
    
    - [ ] JAWS Firefox Desktop
    - [ ] JAWS Chrome Desktop
    - [ ] NVDA Firefox Desktop
    - [ ] NVDA Chrome Desktop
    - [ ] VoiceOver Safari Desktop
    - [ ] VoiceOver Chrome Desktop
    - [ ] VoiceOver Safari Mobile
    - [ ] Android Accessibility Suite Chrome Mobile
    
    ## Pull request type
    
    <!-- Please do not submit updates to dependencies unless it fixes an
    issue. -->
    
    <!-- Please try to limit your pull request to one type, submit multiple
    pull requests if needed. -->
    
    Please check the type of change your PR introduces:
    
    - [ ] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, renaming)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] Documentation content changes
    - [ ] Other (please describe):
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [ ] No
    
    <!-- If this introduces a breaking change, please describe the impact
    and migration path for existing applications below. -->
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as
    screenshots of how the component looks before and after the change. -->

--- src/elements-experimental/seat-reservation/seat-reservation-navigation-services/seat-reservation-navigation-services.visual.spec.ts
+++ src/elements-experimental/seat-reservation/seat-reservation-navigation-services/seat-reservation-navigation-services.visual.spec.ts
@@ -1,23 +1,40 @@
-import { describeViewports } from '@sbb-esta/lyne-elements/core/testing/private.js';
-
+import {
+  describeEach,
+  describeViewports,
+  visualDiffDefault,
+  visualRegressionFixture,
+} from '@sbb-esta/lyne-elements/core/testing/private.js';
 import './seat-reservation-navigation-services.component.ts';
+import { html } from 'lit';
 
 describe('sbb-seat-reservation-navigation-services', () => {
-  /**
-   * Add the `viewports` param to test only specific viewport;
-   * add the `viewportHeight` param to set a fixed height for the browser.
-   */
-  describeViewports(() => {
-    // Create visual tests considering the implemented states (default, hover, active, focus)
-    // for (const state of visualDiffStandardStates) {
-    //   it(
-    //     `${state.name}`,
-    //     state.with(async (setup) => {
-    //       await setup.withFixture(
-    //         html`<sbb-seat-reservation-navigation-services></sbb-seat-reservation-navigation-services>`,
-    //       );
-    //     }),
-    //   );
-    // }
+  const propertyIds = ['BISTRO', 'WIFI', 'PRAM'];
+
+  const cases = {
+    forcedColors: [false, true],
+    darkMode: [false, true],
+  };
+
+  describeViewports({ viewports: ['large'] }, () => {
+    describeEach(cases, ({ forcedColors, darkMode }) => {
+      for (const state of [visualDiffDefault]) {
+        it(
+          `state=${state.name}`,
+          state.with(async () => {
+            await visualRegressionFixture(
+              html`
+                <sbb-seat-reservation-navigation-services
+                  .propertyIds=${propertyIds}
+                ></sbb-seat-reservation-navigation-services>
+              `,
+              {
+                darkMode,
+                forcedColors,
+              },
+            );
+          }),
+        );
+      }
+    });
   });
 });
