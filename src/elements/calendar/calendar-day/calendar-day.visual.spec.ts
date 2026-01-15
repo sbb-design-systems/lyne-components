288e865ecc4425d4c370ca9c7cc60fb5601753d4
Davide Mininni <davide.mininni@finconsgroup.com>
2026-01-15 10:56:29 +0100

    test: add visual tests on calendar-day

--- src/elements/calendar/calendar-day/calendar-day.visual.spec.ts
+++ src/elements/calendar/calendar-day/calendar-day.visual.spec.ts
@@ -1,18 +1,101 @@
-import { html } from 'lit';
+import { html, nothing } from 'lit';
+import { repeat } from 'lit/directives/repeat.js';
+import { type SinonStub, stub } from 'sinon';
 
-import { describeViewports, visualDiffStandardStates } from '../../core/testing/private.ts';
+import { defaultDateAdapter } from '../../core/datetime.ts';
+import {
+  describeViewports,
+  visualDiffActive,
+  visualDiffDefault,
+  visualDiffFocus,
+  visualDiffHover,
+} from '../../core/testing/private.ts';
 
 import './calendar-day.component.ts';
+import '../calendar-enhanced/calendar-enhanced.component.ts';
 
 describe('sbb-calendar-day', () => {
+  let todayStub: SinonStub;
+
+  before(() => {
+    todayStub = stub(defaultDateAdapter, 'today').returns(new Date(2025, 0, 1, 0, 0, 0));
+  });
+
+  after(() => {
+    todayStub.restore();
+  });
+
   describeViewports(() => {
-    for (const state of visualDiffStandardStates) {
-      it(
-        `${state.name}`,
-        state.with(async (setup) => {
-          await setup.withFixture(html`<sbb-calendar-day slot="2025-01-01"></sbb-calendar-day>`);
-        }),
-      );
+    it(
+      'default',
+      visualDiffDefault.with(async (setup) => {
+        await setup.withFixture(
+          html` <sbb-calendar-enhanced
+            .min=${defaultDateAdapter.toIso8601(new Date(2025, 0, 9))}
+            .selected=${defaultDateAdapter.toIso8601(new Date(2025, 0, 15))}
+            .dateFilter=${(d: Date): boolean => d.getDate() % 2 === 1}
+          >
+            ${repeat(new Array(31), (_, index) => {
+              const slotName = defaultDateAdapter.toIso8601(new Date(`2025-01-${index + 1}`));
+              return html` <sbb-calendar-day slot=${slotName}></sbb-calendar-day>`;
+            })}
+          </sbb-calendar-enhanced>`,
+        );
+      }),
+    );
+
+    for (const content of [false, true]) {
+      for (const state of [visualDiffHover, visualDiffActive, visualDiffFocus]) {
+        it(
+          `today state=${state.name} content=${content}`,
+          state.with(async (setup) => {
+            await setup.withFixture(
+              html` <sbb-calendar-enhanced>
+                ${repeat(new Array(31), (_, index) => {
+                  const slotName = defaultDateAdapter.toIso8601(new Date(`2025-01-${index + 1}`));
+                  return html` <sbb-calendar-day slot=${slotName}>
+                    ${content
+                      ? html` <span
+                          class="sbb-text-xxs"
+                          style="display: flex; flex-direction: column; justify-content: center; color: var(--sbb-color-metal);"
+                          >99.-</span
+                        >`
+                      : nothing}
+                  </sbb-calendar-day>`;
+                })}
+              </sbb-calendar-enhanced>`,
+            );
+            setup.withStateElement(
+              setup.snapshotElement.querySelector('sbb-calendar-day[slot="2025-01-01"]')!,
+            );
+          }),
+        );
+
+        it(
+          `selected state=${state.name} content=${content}`,
+          state.with(async (setup) => {
+            await setup.withFixture(
+              html` <sbb-calendar-enhanced .selected=${new Date('2025-01-15')}>
+                ${repeat(new Array(31), (_, index) => {
+                  const slotName = defaultDateAdapter.toIso8601(new Date(`2025-01-${index + 1}`));
+                  return html` <sbb-calendar-day slot=${slotName}>
+                    ${content
+                      ? html` <span
+                          class="sbb-text-xxs"
+                          style="display: flex; flex-direction: column; justify-content: center; color: var(--sbb-color-metal);"
+                          >99.-</span
+                        >`
+                      : nothing}
+                  </sbb-calendar-day>`;
+                })}
+              </sbb-calendar-enhanced>`,
+            );
+            setup.withStateElement(
+              setup.snapshotElement.querySelector('sbb-calendar-day[slot="2025-01-15"]')!,
+            );
+          }),
+        );
+      }
     }
   });
 });
