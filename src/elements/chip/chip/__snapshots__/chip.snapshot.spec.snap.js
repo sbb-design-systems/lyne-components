ae1478988743d5e71e709e66062d2055ef0015c5
Davide Mininni <davide.mininni@finconsgroup.com>
2026-01-26 12:28:10 +0100

    refactor(sbb-chip): scss refactoring

--- src/elements/chip/chip/__snapshots__/chip.snapshot.spec.snap.js
+++ src/elements/chip/chip/__snapshots__/chip.snapshot.spec.snap.js
@@ -11,27 +11,56 @@ snapshots["sbb-chip renders DOM"] =
 /* end snapshot sbb-chip renders DOM */
 
 snapshots["sbb-chip renders Shadow DOM"] = 
-`<div class="sbb-chip">
-  <div class="sbb-chip__label-wrapper">
-    <span class="sbb-chip__label">
-      <slot>
-        Value
-      </slot>
-    </span>
-  </div>
-  <sbb-mini-button
-    aria-hidden="true"
-    class="sbb-chip__delete"
-    icon-name="cross-tiny-small"
-    tabindex="0"
-  >
-  </sbb-mini-button>
-  <sbb-screen-reader-only>
-    , Press the Delete button to remove the chip
-  </sbb-screen-reader-only>
+`<div class="sbb-chip__label-wrapper">
+  <span class="sbb-chip__label">
+    <slot>
+      Value
+    </slot>
+  </span>
 </div>
+<sbb-mini-button
+  aria-hidden="true"
+  class="sbb-chip__delete"
+  icon-name="cross-tiny-small"
+  tabindex="0"
+>
+</sbb-mini-button>
+<sbb-screen-reader-only>
+  , Press the Delete button to remove the chip
+</sbb-screen-reader-only>
 `;
 /* end snapshot sbb-chip renders Shadow DOM */
+
+snapshots["sbb-chip renders with label DOM"] = 
+`<sbb-chip
+  tabindex="-1"
+  value="Value"
+>
+  Value label
+</sbb-chip>
+`;
+/* end snapshot sbb-chip renders with label DOM */
+
+snapshots["sbb-chip renders with label Shadow DOM"] = 
+`<div class="sbb-chip__label-wrapper">
+  <span class="sbb-chip__label">
+    <slot>
+      Value
+    </slot>
+  </span>
+</div>
+<sbb-mini-button
+  aria-hidden="true"
+  class="sbb-chip__delete"
+  icon-name="cross-tiny-small"
+  tabindex="0"
+>
+</sbb-mini-button>
+<sbb-screen-reader-only>
+  , Press the Delete button to remove the chip
+</sbb-screen-reader-only>
+`;
+/* end snapshot sbb-chip renders with label Shadow DOM */
 
 snapshots["sbb-chip renders disabled DOM"] = 
 `<sbb-chip
@@ -43,43 +72,25 @@ snapshots["sbb-chip renders disabled DOM"] =
 /* end snapshot sbb-chip renders disabled DOM */
 
 snapshots["sbb-chip renders disabled Shadow DOM"] = 
-`<div class="sbb-chip">
-  <div class="sbb-chip__label-wrapper">
-    <span class="sbb-chip__label">
-      <slot>
-        Value
-      </slot>
-    </span>
-  </div>
-  <sbb-mini-button
-    aria-hidden="true"
-    class="sbb-chip__delete"
-    icon-name="cross-tiny-small"
-    tabindex="0"
-  >
-  </sbb-mini-button>
-  <sbb-screen-reader-only>
-    , Press the Delete button to remove the chip
-  </sbb-screen-reader-only>
+`<div class="sbb-chip__label-wrapper">
+  <span class="sbb-chip__label">
+    <slot>
+      Value
+    </slot>
+  </span>
 </div>
+<sbb-mini-button
+  aria-hidden="true"
+  class="sbb-chip__delete"
+  icon-name="cross-tiny-small"
+  tabindex="0"
+>
+</sbb-mini-button>
+<sbb-screen-reader-only>
+  , Press the Delete button to remove the chip
+</sbb-screen-reader-only>
 `;
 /* end snapshot sbb-chip renders disabled Shadow DOM */
-
-snapshots["sbb-chip renders A11y tree Chrome"] = 
-`<p>
-  {
-  "role": "WebArea",
-  "name": "",
-  "children": [
-    {
-      "role": "option",
-      "name": "Value , Press the Delete button to remove the chip"
-    }
-  ]
-}
-</p>
-`;
-/* end snapshot sbb-chip renders A11y tree Chrome */
 
 snapshots["sbb-chip renders A11y tree Firefox"] = 
 `<p>
@@ -110,6 +121,68 @@ snapshots["sbb-chip renders A11y tree Firefox"] =
 </p>
 `;
 /* end snapshot sbb-chip renders A11y tree Firefox */
+
+snapshots["sbb-chip renders A11y tree Chrome"] = 
+`<p>
+  {
+  "role": "WebArea",
+  "name": "",
+  "children": [
+    {
+      "role": "option",
+      "name": "Value , Press the Delete button to remove the chip"
+    }
+  ]
+}
+</p>
+`;
+/* end snapshot sbb-chip renders A11y tree Chrome */
+
+snapshots["sbb-chip renders with label A11y tree Firefox"] = 
+`<p>
+  {
+  "role": "document",
+  "name": "",
+  "children": [
+    {
+      "role": "text container",
+      "name": "",
+      "children": [
+        {
+          "role": "text leaf",
+          "name": "Value label"
+        },
+        {
+          "role": "text leaf",
+          "name": ", "
+        },
+        {
+          "role": "text leaf",
+          "name": "Press the Delete button to remove the chip"
+        }
+      ]
+    }
+  ]
+}
+</p>
+`;
+/* end snapshot sbb-chip renders with label A11y tree Firefox */
+
+snapshots["sbb-chip renders with label A11y tree Chrome"] = 
+`<p>
+  {
+  "role": "WebArea",
+  "name": "",
+  "children": [
+    {
+      "role": "option",
+      "name": "Value label , Press the Delete button to remove the chip"
+    }
+  ]
+}
+</p>
+`;
+/* end snapshot sbb-chip renders with label A11y tree Chrome */
 
 snapshots["sbb-chip renders disabled A11y tree Chrome"] = 
 `<p>
@@ -158,83 +231,4 @@ snapshots["sbb-chip renders disabled A11y tree Firefox"] =
 </p>
 `;
 /* end snapshot sbb-chip renders disabled A11y tree Firefox */
-
-snapshots["sbb-chip renders with label DOM"] = 
-`<sbb-chip
-  tabindex="-1"
-  value="Value"
->
-  Value label
-</sbb-chip>
-`;
-/* end snapshot sbb-chip renders with label DOM */
-
-snapshots["sbb-chip renders with label Shadow DOM"] = 
-`<div class="sbb-chip">
-  <div class="sbb-chip__label-wrapper">
-    <span class="sbb-chip__label">
-      <slot>
-        Value
-      </slot>
-    </span>
-  </div>
-  <sbb-mini-button
-    aria-hidden="true"
-    class="sbb-chip__delete"
-    icon-name="cross-tiny-small"
-    tabindex="0"
-  >
-  </sbb-mini-button>
-  <sbb-screen-reader-only>
-    , Press the Delete button to remove the chip
-  </sbb-screen-reader-only>
-</div>
-`;
-/* end snapshot sbb-chip renders with label Shadow DOM */
-
-snapshots["sbb-chip renders with label A11y tree Chrome"] = 
-`<p>
-  {
-  "role": "WebArea",
-  "name": "",
-  "children": [
-    {
-      "role": "option",
-      "name": "Value label , Press the Delete button to remove the chip"
-    }
-  ]
-}
-</p>
-`;
-/* end snapshot sbb-chip renders with label A11y tree Chrome */
-
-snapshots["sbb-chip renders with label A11y tree Firefox"] = 
-`<p>
-  {
-  "role": "document",
-  "name": "",
-  "children": [
-    {
-      "role": "text container",
-      "name": "",
-      "children": [
-        {
-          "role": "text leaf",
-          "name": "Value label"
-        },
-        {
-          "role": "text leaf",
-          "name": ", "
-        },
-        {
-          "role": "text leaf",
-          "name": "Press the Delete button to remove the chip"
-        }
-      ]
-    }
-  ]
-}
-</p>
-`;
-/* end snapshot sbb-chip renders with label A11y tree Firefox */
 
