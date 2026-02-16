b90af6fa1e66b72ec8e263b649a6158a7709d20d
Jeri Peier <jeremias.peier@sbb.ch>
2026-02-16 16:01:00 +0100

    fix(sbb-tab-group): show focus outline and fix focus trap (#4501)

--- src/elements/tabs/tab-group/tab-group.component.ts
+++ src/elements/tabs/tab-group/tab-group.component.ts
@@ -114,30 +114,30 @@ class SbbTabGroupElement extends SbbElementInternalsMixin(SbbHydrationMixin(LitE
 
   /**
    * Disables a tab by index.
-   * @param tabIndex The index of the tab you want to disable.
+   * @param index The index of the tab you want to disable.
    */
-  public disableTab(tabIndex: number): void {
-    if (this.labels[tabIndex]) {
-      this.labels[tabIndex].disabled = true;
+  public disableTab(index: number): void {
+    if (this.labels[index]) {
+      this.labels[index].disabled = true;
     }
   }
 
   /**
    * Enables a tab by index.
-   * @param tabIndex The index of the tab you want to enable.
+   * @param index The index of the tab you want to enable.
    */
-  public enableTab(tabIndex: number): void {
-    if (this.labels[tabIndex]) {
-      this.labels[tabIndex].disabled = false;
+  public enableTab(index: number): void {
+    if (this.labels[index]) {
+      this.labels[index].disabled = false;
     }
   }
 
   /**
    * Activates a tab by index.
-   * @param tabIndex The index of the tab you want to activate.
+   * @param index The index of the tab you want to activate.
    */
-  public activateTab(tabIndex: number): void {
-    this.labels[tabIndex]?.activate();
+  public activateTab(index: number): void {
+    this.labels[index]?.activate();
   }
 
   private _enabledTabs(): SbbTabLabelElement[] {
@@ -220,9 +220,13 @@ class SbbTabGroupElement extends SbbElementInternalsMixin(SbbHydrationMixin(LitE
       >
         <slot name="tab-bar" @slotchange=${this._onLabelSlotChange}></slot>
       </div>
-      <div class="sbb-tab-group-content">
-        <slot @slotchange=${throttle(this._onContentSlotChange, 150)}></slot>
-      </div>
+      ${!this.fixedHeight
+        ? html`
+            <div class="sbb-tab-group-content">
+              <slot @slotchange=${throttle(this._onContentSlotChange, 150)}></slot>
+            </div>
+          `
+        : html`<slot @slotchange=${throttle(this._onContentSlotChange, 150)}></slot>`}
     `;
   }
 }
