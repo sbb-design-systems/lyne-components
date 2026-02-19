729a4d0621a836b0ae121da4831b71b00f5d8736
Davide Mininni <davide.mininni@finconsgroup.com>
2026-02-19 14:22:50 +0100

    refactor(file-selector): css refactoring

--- src/elements/file-selector/file-selector-dropzone/__snapshots__/file-selector-dropzone.snapshot.spec.snap.js
+++ src/elements/file-selector/file-selector-dropzone/__snapshots__/file-selector-dropzone.snapshot.spec.snap.js
@@ -8,43 +8,88 @@ snapshots["sbb-file-selector-dropzone renders DOM"] =
 /* end snapshot sbb-file-selector-dropzone renders DOM */
 
 snapshots["sbb-file-selector-dropzone renders Shadow DOM"] = 
-`<div class="sbb-file-selector">
-  <div class="sbb-file-selector__input-container">
-    <label>
-      <span class="sbb-file-selector__dropzone-area">
-        <span class="sbb-file-selector__dropzone-area--icon">
-          <sbb-icon name="folder-open-medium">
-          </sbb-icon>
-        </span>
-        <span class="sbb-file-selector__dropzone-area--title">
-        </span>
-        <span class="sbb-file-selector__dropzone-area--subtitle">
-          Drag & Drop your file here
-        </span>
-        <span class="sbb-file-selector__dropzone-area--button">
-          <sbb-secondary-button-static size="m">
-            Choose a file
-          </sbb-secondary-button-static>
-        </span>
+`<div class="sbb-file-selector__input-container">
+  <label>
+    <span class="sbb-file-selector__dropzone-area">
+      <span class="sbb-file-selector__dropzone-area--icon">
+        <sbb-icon name="folder-open-medium">
+        </sbb-icon>
       </span>
-      <input
-        class="sbb-file-selector__visually-hidden"
-        type="file"
-      >
-    </label>
-  </div>
-  <p
-    class="sbb-file-selector__visually-hidden"
-    role="status"
-  >
-  </p>
-  <div class="sbb-file-selector__error">
-    <slot name="error">
-    </slot>
-  </div>
+      <span class="sbb-file-selector__dropzone-area--title">
+      </span>
+      <span class="sbb-file-selector__dropzone-area--subtitle">
+        Drag & Drop your file here
+      </span>
+      <span class="sbb-file-selector__dropzone-area--button">
+        <sbb-secondary-button-static size="m">
+          Choose a file
+        </sbb-secondary-button-static>
+      </span>
+    </span>
+    <input
+      class="sbb-file-selector__visually-hidden"
+      type="file"
+    >
+  </label>
+</div>
+<p
+  class="sbb-file-selector__visually-hidden"
+  role="status"
+>
+</p>
+<div class="sbb-file-selector__error">
+  <slot name="error">
+  </slot>
 </div>
 `;
 /* end snapshot sbb-file-selector-dropzone renders Shadow DOM */
+
+snapshots["sbb-file-selector-dropzone renders multiple DOM"] = 
+`<sbb-file-selector-dropzone
+  multiple=""
+  size="m"
+>
+</sbb-file-selector-dropzone>
+`;
+/* end snapshot sbb-file-selector-dropzone renders multiple DOM */
+
+snapshots["sbb-file-selector-dropzone renders multiple Shadow DOM"] = 
+`<div class="sbb-file-selector__input-container">
+  <label>
+    <span class="sbb-file-selector__dropzone-area">
+      <span class="sbb-file-selector__dropzone-area--icon">
+        <sbb-icon name="folder-open-medium">
+        </sbb-icon>
+      </span>
+      <span class="sbb-file-selector__dropzone-area--title">
+      </span>
+      <span class="sbb-file-selector__dropzone-area--subtitle">
+        Drag & Drop your files here
+      </span>
+      <span class="sbb-file-selector__dropzone-area--button">
+        <sbb-secondary-button-static size="m">
+          Choose files
+        </sbb-secondary-button-static>
+      </span>
+    </span>
+    <input
+      class="sbb-file-selector__visually-hidden"
+      multiple=""
+      type="file"
+    >
+  </label>
+</div>
+<p
+  class="sbb-file-selector__visually-hidden"
+  role="status"
+>
+</p>
+<div class="sbb-file-selector__error">
+  <slot name="error">
+  </slot>
+</div>
+`;
+/* end snapshot sbb-file-selector-dropzone renders multiple Shadow DOM */
 
 snapshots["sbb-file-selector-dropzone renders A11y tree Chrome"] = 
 `<p>
@@ -61,23 +106,21 @@ snapshots["sbb-file-selector-dropzone renders A11y tree Chrome"] =
           "role": "none",
           "children": [
             {
-              "ignored": true,
-              "role": "none",
-              "children": [
-                {
-                  "role": "LabelText",
-                  "name": ""
-                }
-              ]
-            },
-            {
-              "role": "status",
-              "name": "",
-              "live": "polite",
-              "atomic": true,
-              "relevant": "additions text"
+              "role": "LabelText",
+              "name": ""
             }
           ]
+        },
+        {
+          "role": "status",
+          "name": "",
+          "live": "polite",
+          "atomic": true,
+          "relevant": "additions text"
+        },
+        {
+          "ignored": true,
+          "role": "none"
         }
       ]
     }
@@ -86,55 +129,6 @@ snapshots["sbb-file-selector-dropzone renders A11y tree Chrome"] =
 </p>
 `;
 /* end snapshot sbb-file-selector-dropzone renders A11y tree Chrome */
-
-snapshots["sbb-file-selector-dropzone renders multiple DOM"] = 
-`<sbb-file-selector-dropzone
-  multiple=""
-  size="m"
->
-</sbb-file-selector-dropzone>
-`;
-/* end snapshot sbb-file-selector-dropzone renders multiple DOM */
-
-snapshots["sbb-file-selector-dropzone renders multiple Shadow DOM"] = 
-`<div class="sbb-file-selector">
-  <div class="sbb-file-selector__input-container">
-    <label>
-      <span class="sbb-file-selector__dropzone-area">
-        <span class="sbb-file-selector__dropzone-area--icon">
-          <sbb-icon name="folder-open-medium">
-          </sbb-icon>
-        </span>
-        <span class="sbb-file-selector__dropzone-area--title">
-        </span>
-        <span class="sbb-file-selector__dropzone-area--subtitle">
-          Drag & Drop your files here
-        </span>
-        <span class="sbb-file-selector__dropzone-area--button">
-          <sbb-secondary-button-static size="m">
-            Choose files
-          </sbb-secondary-button-static>
-        </span>
-      </span>
-      <input
-        class="sbb-file-selector__visually-hidden"
-        multiple=""
-        type="file"
-      >
-    </label>
-  </div>
-  <p
-    class="sbb-file-selector__visually-hidden"
-    role="status"
-  >
-  </p>
-  <div class="sbb-file-selector__error">
-    <slot name="error">
-    </slot>
-  </div>
-</div>
-`;
-/* end snapshot sbb-file-selector-dropzone renders multiple Shadow DOM */
 
 snapshots["sbb-file-selector-dropzone renders multiple A11y tree Chrome"] = 
 `<p>
@@ -151,23 +145,21 @@ snapshots["sbb-file-selector-dropzone renders multiple A11y tree Chrome"] =
           "role": "none",
           "children": [
             {
-              "ignored": true,
-              "role": "none",
-              "children": [
-                {
-                  "role": "LabelText",
-                  "name": ""
-                }
-              ]
-            },
-            {
-              "role": "status",
-              "name": "",
-              "live": "polite",
-              "atomic": true,
-              "relevant": "additions text"
+              "role": "LabelText",
+              "name": ""
             }
           ]
+        },
+        {
+          "role": "status",
+          "name": "",
+          "live": "polite",
+          "atomic": true,
+          "relevant": "additions text"
+        },
+        {
+          "ignored": true,
+          "role": "none"
         }
       ]
     }
