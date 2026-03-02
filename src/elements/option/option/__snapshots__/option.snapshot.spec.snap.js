57d4aa3e31f73f34f799b5531a5cefbcdbddfb18
Tommaso Menga <tommaso.menga-94@hotmail.it>
2026-03-02 16:07:46 +0100

    refactor(sbb-autocomplete, sbb-select): css refactoring (#4441)
    Part of #3969

--- src/elements/option/option/__snapshots__/option.snapshot.spec.snap.js
+++ src/elements/option/option/__snapshots__/option.snapshot.spec.snap.js
@@ -13,20 +13,18 @@ snapshots["sbb-option autocomplete renders selected DOM"] =
 /* end snapshot sbb-option autocomplete renders selected DOM */
 
 snapshots["sbb-option autocomplete renders selected Shadow DOM"] = 
-`<div class="sbb-option__container">
-  <div class="sbb-option">
-    <span class="sbb-option__icon">
-      <slot name="icon">
-      </slot>
+`<div class="sbb-option">
+  <span class="sbb-option__icon">
+    <slot name="icon">
+    </slot>
+  </span>
+  <span class="sbb-option__label">
+    <slot>
+    </slot>
+    <span>
+      Option 1
     </span>
-    <span class="sbb-option__label">
-      <slot>
-      </slot>
-      <span>
-        Option 1
-      </span>
-    </span>
-  </div>
+  </span>
 </div>
 `;
 /* end snapshot sbb-option autocomplete renders selected Shadow DOM */
@@ -43,20 +41,18 @@ snapshots["sbb-option autocomplete renders disabled DOM"] =
 /* end snapshot sbb-option autocomplete renders disabled DOM */
 
 snapshots["sbb-option autocomplete renders disabled Shadow DOM"] = 
-`<div class="sbb-option__container">
-  <div class="sbb-option">
-    <span class="sbb-option__icon">
-      <slot name="icon">
-      </slot>
+`<div class="sbb-option">
+  <span class="sbb-option__icon">
+    <slot name="icon">
+    </slot>
+  </span>
+  <span class="sbb-option__label">
+    <slot>
+    </slot>
+    <span>
+      Option 1
     </span>
-    <span class="sbb-option__label">
-      <slot>
-      </slot>
-      <span>
-        Option 1
-      </span>
-    </span>
-  </div>
+  </span>
 </div>
 `;
 /* end snapshot sbb-option autocomplete renders disabled Shadow DOM */
