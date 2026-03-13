1f0ea34505e8bd8a4ff122ba9637bdfc55ea477a
thm-sbb <thomas.hamm@sbb.ch>
2026-03-13 15:26:46 +0100

    fix(sbb-seat-reservation): gap problems in navigation + others (#4619)
    - fix gap problems in navigation
    - replace SVG icon for bicycle
    - rename test data to "private" usage
    - some more unit-tests

--- src/elements-experimental/seat-reservation/seat-reservation.stories.ts
+++ src/elements-experimental/seat-reservation/seat-reservation.stories.ts
@@ -6,8 +6,8 @@ import type { InputType } from 'storybook/internal/types';
 
 import { sbbSpread } from '../../storybook/helpers/spread.ts';
 
-import { MOCK_GIRUNO_TRAIN } from './common/mapper/sample-data/seat-reservation-sample-data-giruno.ts';
-import { MOCK_TRAIN_LOCOMOTIVE_LAYOUT } from './common/mapper/sample-data/seat-reservation-sample-data-others.ts';
+import { MOCK_GIRUNO_TRAIN } from './common/mapper/sample-data/seat-reservation-sample-data-giruno.private.ts';
+import { MOCK_TRAIN_LOCOMOTIVE_LAYOUT } from './common/mapper/sample-data/seat-reservation-sample-data-others.private.ts';
 import { mapIconToSvg, mapRawDataToSeatReservation } from './common/mapper.ts';
 import type { CoachItem, CoachItemDetails, SeatReservation } from './common/types.ts';
 import readme from './readme.md?raw';
