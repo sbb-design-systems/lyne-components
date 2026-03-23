5d63fc68af85ea3020de5549c41cde06e14a4333
jeripeierSBB <jeremias.peier@sbb.ch>
2026-03-23 13:23:06 +0100

    fix: review

--- src/elements/core/a11y/focus-trap-controller.ts
+++ src/elements/core/a11y/focus-trap-controller.ts
@@ -120,13 +120,11 @@ export class SbbFocusTrapController implements ReactiveController {
       return root;
     }
 
-    const children = root.shadowRoot
-      ? root.shadowRoot.children
-      : root.localName === 'slot'
-        ? (root as HTMLSlotElement).assignedElements().length > 0
-          ? (root as HTMLSlotElement).assignedElements()
-          : root.children
-        : root.children;
+    const children =
+      root.shadowRoot?.children ??
+      (root.localName === 'slot' && (root as HTMLSlotElement).assignedElements().length > 0
+        ? (root as HTMLSlotElement).assignedElements()
+        : root.children);
 
     for (let i = 0; i < children.length; i++) {
       const tabbableChild =
@@ -154,13 +152,11 @@ export class SbbFocusTrapController implements ReactiveController {
       result = root;
     }
 
-    const children = root.shadowRoot
-      ? root.shadowRoot.children
-      : root.localName === 'slot'
-        ? (root as HTMLSlotElement).assignedElements().length > 0
-          ? (root as HTMLSlotElement).assignedElements()
-          : root.children
-        : root.children;
+    const children =
+      root.shadowRoot?.children ??
+      (root.localName === 'slot' && (root as HTMLSlotElement).assignedElements().length > 0
+        ? (root as HTMLSlotElement).assignedElements()
+        : root.children);
 
     // Iterate in reverse DOM order.
     for (let i = children.length - 1; i >= 0; i--) {
