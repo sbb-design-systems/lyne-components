23632bdaa67e966bcc9658fa9a1c09f64ced057f
jeripeierSBB <jeremias.peier@sbb.ch>
2026-03-30 18:30:05 +0200

    fix: review inline story

--- src/storybook/guides/inline-story.tsx
+++ src/storybook/guides/inline-story.tsx
@@ -2,16 +2,7 @@ import React from 'react';
 
 export const InlineStory = (props: unknown) => (
   // @ts-expect-error tsx not configured, but supported for storybook stories
-  <div
-    style={{
-      border:
-        'var(--sbb-border-width-1x) solid light-dark(var(--sbb-color-aluminium), var(--sbb-color-anthracite))',
-      padding: 'var(--sbb-spacing-fixed-6x)',
-      borderRadius: 'var(--sbb-border-radius-2x)',
-      marginBlockEnd: 'var(--sbb-spacing-fixed-6x)',
-    }}
-    className="docs-story"
-  >
+  <div className="docs-story sbb-inline-story">
     {
       // @ts-expect-error tsx not configured, but supported for storybook stories
       <div {...props} className="sb-story sb-unstyled"></div>
