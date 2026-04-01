b5cd5dcc23519f5179beddec61899a782f664545
Jeri Peier <jeremias.peier@sbb.ch>
2026-04-01 17:45:13 +0200

    feat(sbb-form-field): add `<sbb-hint>` and `<sbb-form-field-text-counter>` components (#4579)
    Closes #4512

---
+++ src/elements/form-field/form-field-text-counter/__snapshots__/form-field-text-counter.snapshot.spec.snap.js
@@ -0,0 +1,166 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["sbb-form-field-text-counter renders DOM"] = 
+`<sbb-form-field
+  error-space="none"
+  size="m"
+  width="default"
+>
+  <label slot="label">
+    Description
+  </label>
+  <textarea
+    aria-describedby=""
+    maxlength="100"
+    rows="3"
+  >
+  </textarea>
+  <sbb-form-field-text-counter slot="hint">
+  </sbb-form-field-text-counter>
+</sbb-form-field>
+`;
+/* end snapshot sbb-form-field-text-counter renders DOM */
+
+snapshots["sbb-form-field-text-counter renders Shadow DOM"] = 
+`100
+<slot>
+  characters remaining
+</slot>
+`;
+/* end snapshot sbb-form-field-text-counter renders Shadow DOM */
+
+snapshots["sbb-form-field-text-counter renders A11y tree Chrome"] = 
+`<p>
+  {
+  "role": "generic",
+  "name": "Fixture Container",
+  "children": [
+    {
+      "ignored": true,
+      "role": "none",
+      "children": [
+        {
+          "role": "generic",
+          "name": ""
+        },
+        {
+          "role": "generic",
+          "name": ""
+        },
+        {
+          "ignored": true,
+          "role": "none",
+          "children": [
+            {
+              "ignored": true,
+              "role": "none",
+              "children": [
+                {
+                  "ignored": true,
+                  "role": "none"
+                },
+                {
+                  "ignored": true,
+                  "role": "none",
+                  "children": [
+                    {
+                      "ignored": true,
+                      "role": "none"
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+</p>
+`;
+/* end snapshot sbb-form-field-text-counter renders A11y tree Chrome */
+
+snapshots["sbb-form-field-text-counter renders with custom slot text DOM"] = 
+`<sbb-form-field
+  error-space="none"
+  size="m"
+  width="default"
+>
+  <label slot="label">
+    Description
+  </label>
+  <textarea
+    aria-describedby=""
+    maxlength="100"
+    rows="3"
+  >
+  </textarea>
+  <sbb-form-field-text-counter slot="hint">
+    characters left
+  </sbb-form-field-text-counter>
+</sbb-form-field>
+`;
+/* end snapshot sbb-form-field-text-counter renders with custom slot text DOM */
+
+snapshots["sbb-form-field-text-counter renders with custom slot text Shadow DOM"] = 
+`100
+<slot>
+  characters remaining
+</slot>
+`;
+/* end snapshot sbb-form-field-text-counter renders with custom slot text Shadow DOM */
+
+snapshots["sbb-form-field-text-counter renders with custom slot text A11y tree Chrome"] = 
+`<p>
+  {
+  "role": "generic",
+  "name": "Fixture Container",
+  "children": [
+    {
+      "ignored": true,
+      "role": "none",
+      "children": [
+        {
+          "role": "generic",
+          "name": ""
+        },
+        {
+          "role": "generic",
+          "name": ""
+        },
+        {
+          "ignored": true,
+          "role": "none",
+          "children": [
+            {
+              "ignored": true,
+              "role": "none",
+              "children": [
+                {
+                  "ignored": true,
+                  "role": "none"
+                },
+                {
+                  "ignored": true,
+                  "role": "none",
+                  "children": [
+                    {
+                      "ignored": true,
+                      "role": "none"
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+</p>
+`;
+/* end snapshot sbb-form-field-text-counter renders with custom slot text A11y tree Chrome */
+
