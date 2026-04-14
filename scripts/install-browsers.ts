829efc4ff90f027dcaffef750286dc4ba2e82a38
Lukas Spirig <lukas.spirig@sbb.ch>
2026-04-14 12:27:19 +0200

    refactor: inline postinstall

--- scripts/install-browsers.ts
+++
@@ -1,8 +0,0 @@
-import { spawn } from 'node:child_process';
-
-if (!process['CI']) {
-  const binary = process.env['npm_execpath'] || 'npx';
-  spawn(binary, ['playwright', 'install'], { stdio: 'inherit' });
-} else {
-  console.log('Running in CI environment, skipping browser installation.');
-}
