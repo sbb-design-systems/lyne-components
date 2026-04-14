dd123ba443804e0da995a466c8e9be7c5899cc28
jeripeierSBB <jeremias.peier@sbb.ch>
2026-04-14 18:09:34 +0200

    refactor: tests for vertical wagon

--- src/elements/train/train-wagon/train-wagon.visual.spec.ts
+++ src/elements/train/train-wagon/train-wagon.visual.spec.ts
@@ -1,6 +1,7 @@
 import { html, nothing, type TemplateResult } from 'lit';
 
 import { describeEach, describeViewports, visualDiffDefault } from '../../core/testing/private.ts';
+import type { SbbTrainWagonElement, SbbTrainFormationElement } from '../../train.ts';
 
 import '../../train.ts';
 
@@ -15,114 +16,142 @@ describe(`sbb-train-wagon`, () => {
       'restaurant',
       'locomotive',
       'closed',
-    ],
-    view: ['side', 'top'],
+    ] satisfies SbbTrainWagonElement['type'][],
+    view: ['side', 'top'] satisfies SbbTrainFormationElement['view'][],
     active: [false, true],
     forcedColors: [false, true],
   };
 
-  const occupancyCases = ['high', 'medium', 'low', 'none', null];
-  const wagonClassCases = ['1', '2', '1-2', '2-1'];
+  const occupancyCases = [
+    'high',
+    'medium',
+    'low',
+    'none',
+    null,
+  ] satisfies SbbTrainWagonElement['occupancy'][];
+  const wagonClassCases = ['1', '2', '1-2', '2-1'] satisfies SbbTrainWagonElement['wagonClass'][];
 
-  const trainFormationWrapper = (content: TemplateResult): TemplateResult =>
-    html`<sbb-train-formation><sbb-train>${content}</sbb-train></sbb-train-formation>`;
-
-  describeViewports({ viewports: ['zero', 'large'] }, () => {
-    for (const occupancy of occupancyCases) {
-      it(
-        `occupancy=${occupancy}`,
-        visualDiffDefault.with(async (setup) => {
-          await setup.withFixture(
-            trainFormationWrapper(html`
-              <sbb-train-wagon occupancy=${occupancy || nothing}></sbb-train-wagon>
-            `),
-          );
-        }),
-      );
-    }
-
-    for (const wagonClass of wagonClassCases) {
-      it(
-        `class=${wagonClass}`,
-        visualDiffDefault.with(async (setup) => {
-          await setup.withFixture(
-            trainFormationWrapper(
-              html`<sbb-train-wagon wagon-class=${wagonClass}></sbb-train-wagon>`,
-            ),
-          );
-        }),
-      );
-    }
-
-    it(
-      `one icon`,
-      visualDiffDefault.with(async (setup) => {
-        await setup.withFixture(
-          trainFormationWrapper(html`
-            <sbb-train-wagon wagon-class="2" label="38">
-              <sbb-icon name="sa-bz"></sbb-icon>
-            </sbb-train-wagon>
-          `),
-        );
-      }),
-    );
-
-    it(
-      `multiple icons`,
-      visualDiffDefault.with(async (setup) => {
-        await setup.withFixture(
-          trainFormationWrapper(html`
-            <sbb-train-wagon wagon-class="2" label="38">
-              <sbb-icon name="sa-rs"></sbb-icon>
-              <sbb-icon name="sa-nf"></sbb-icon>
-              <sbb-icon name="sa-bz"></sbb-icon>
-            </sbb-train-wagon>
-          `),
-        );
-      }),
-    );
-
-    it(
-      `multiple icons without label`,
-      visualDiffDefault.with(async (setup) => {
-        await setup.withFixture(
-          trainFormationWrapper(html`
-            <sbb-train-wagon wagon-class="2">
-              <sbb-icon name="sa-rs"></sbb-icon>
-              <sbb-icon name="sa-nf"></sbb-icon>
-              <sbb-icon name="sa-bz"></sbb-icon>
-            </sbb-train-wagon>
-          `),
-        );
-      }),
-    );
-  });
+  const trainFormationWrapper = (
+    content: TemplateResult,
+    orientation: SbbTrainFormationElement['orientation'] = 'horizontal',
+    view: SbbTrainFormationElement['view'] = 'side',
+  ): TemplateResult =>
+    html`<sbb-train-formation orientation=${orientation} view=${view}
+      ><sbb-train>${content}</sbb-train></sbb-train-formation
+    >`;
 
   describeViewports({ viewports: ['zero'] }, () => {
-    describeEach(wagonTypeCases, ({ type, view, forcedColors, active }) => {
-      it(
-        '',
-        visualDiffDefault.with(async (setup) => {
-          await setup.withFixture(
-            html`<sbb-train-formation view=${view}>
-              <sbb-train>
-                ${type === 'sleeping' || type === 'couchette' || type === 'restaurant'
-                  ? html`<sbb-train-wagon
-                      type=${type}
-                      class=${active ? 'sbb-active' : nothing}
-                    ></sbb-train-wagon>`
-                  : html`<sbb-train-wagon
-                      type=${type}
-                      occupancy="medium"
-                      wagon-class="1"
-                      class=${active ? 'sbb-active' : nothing}
-                    ></sbb-train-wagon>`}
-              </sbb-train>
-            </sbb-train-formation>`,
-            { forcedColors },
+    for (const orientation of [
+      'horizontal',
+      'vertical',
+    ] satisfies SbbTrainFormationElement['orientation'][]) {
+      describe(`orientation=${orientation}`, () => {
+        describeEach(wagonTypeCases, ({ type, view, forcedColors, active }) => {
+          it(
+            '',
+            visualDiffDefault.with(async (setup) => {
+              await setup.withFixture(
+                trainFormationWrapper(
+                  html` ${type === 'sleeping' || type === 'couchette' || type === 'restaurant'
+                    ? html`<sbb-train-wagon
+                        type=${type}
+                        class=${active ? 'sbb-active' : nothing}
+                      ></sbb-train-wagon>`
+                    : html`<sbb-train-wagon
+                        type=${type}
+                        occupancy="medium"
+                        wagon-class="1"
+                        class=${active ? 'sbb-active' : nothing}
+                      ></sbb-train-wagon>`}`,
+                  orientation,
+                  view,
+                ),
+                { forcedColors },
+              );
+            }),
           );
-        }),
-      );
-    });
+        });
+
+        for (const occupancy of occupancyCases) {
+          it(
+            `occupancy=${occupancy}`,
+            visualDiffDefault.with(async (setup) => {
+              await setup.withFixture(
+                trainFormationWrapper(
+                  html` <sbb-train-wagon occupancy=${occupancy || nothing}></sbb-train-wagon> `,
+                  orientation,
+                ),
+              );
+            }),
+          );
+        }
+
+        for (const wagonClass of wagonClassCases) {
+          it(
+            `class=${wagonClass}`,
+            visualDiffDefault.with(async (setup) => {
+              await setup.withFixture(
+                trainFormationWrapper(
+                  html`<sbb-train-wagon wagon-class=${wagonClass}></sbb-train-wagon>`,
+                  orientation,
+                ),
+              );
+            }),
+          );
+        }
+
+        it(
+          `one icon`,
+          visualDiffDefault.with(async (setup) => {
+            await setup.withFixture(
+              trainFormationWrapper(
+                html`
+                  <sbb-train-wagon wagon-class="2" label="38">
+                    <sbb-icon name="sa-bz"></sbb-icon>
+                  </sbb-train-wagon>
+                `,
+                orientation,
+              ),
+            );
+          }),
+        );
+
+        it(
+          `multiple icons`,
+          visualDiffDefault.with(async (setup) => {
+            await setup.withFixture(
+              trainFormationWrapper(
+                html`
+                  <sbb-train-wagon wagon-class="2" label="38">
+                    <sbb-icon name="sa-rs"></sbb-icon>
+                    <sbb-icon name="sa-nf"></sbb-icon>
+                    <sbb-icon name="sa-bz"></sbb-icon>
+                  </sbb-train-wagon>
+                `,
+                orientation,
+              ),
+            );
+          }),
+        );
+
+        it(
+          `multiple icons without label`,
+          visualDiffDefault.with(async (setup) => {
+            await setup.withFixture(
+              trainFormationWrapper(
+                html`
+                  <sbb-train-wagon wagon-class="2">
+                    <sbb-icon name="sa-rs"></sbb-icon>
+                    <sbb-icon name="sa-nf"></sbb-icon>
+                    <sbb-icon name="sa-bz"></sbb-icon>
+                  </sbb-train-wagon>
+                `,
+                orientation,
+              ),
+            );
+          }),
+        );
+      });
+    }
   });
 });
