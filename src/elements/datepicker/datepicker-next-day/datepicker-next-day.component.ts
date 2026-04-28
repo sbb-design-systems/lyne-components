edc164d087d17052f0d903539ecb036cc8dbb299
jeripeierSBB <jeremias.peier@sbb.ch>
2026-04-28 17:42:57 +0200

    refactor: datepicker

--- src/elements/datepicker/datepicker-next-day/datepicker-next-day.component.ts
+++ src/elements/datepicker/datepicker-next-day/datepicker-next-day.component.ts
@@ -1,19 +1,11 @@
-import type { CSSResultGroup } from 'lit';
-
-import { miniButtonStyle } from '../../button.pure.ts';
-import { boxSizingStyles, i18nNextDay, i18nSelectNextDay } from '../../core.ts';
-import { datepickerButtonStyle, SbbDatepickerButton } from '../common/datepicker-button.ts';
+import { i18nNextDay, i18nSelectNextDay } from '../../core.ts';
+import { SbbDatepickerButton } from '../common/datepicker-button.ts';
 
 /**
  * Combined with a `sbb-datepicker`, it can be used to move the date ahead.
  */
 export class SbbDatepickerNextDayElement<T = Date> extends SbbDatepickerButton<T> {
   public static override readonly elementName: string = 'sbb-datepicker-next-day';
-  public static override styles: CSSResultGroup = [
-    boxSizingStyles,
-    miniButtonStyle,
-    datepickerButtonStyle,
-  ];
 
   protected iconName: string = 'chevron-small-right-small';
   protected i18nOffBoundaryDay: Record<string, string> = i18nNextDay;
