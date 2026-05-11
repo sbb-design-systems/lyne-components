011a5b4f1d9b988445c7cd41950bc37c447c6477
jeripeierSBB <jeremias.peier@sbb.ch>
2026-05-11 17:55:47 +0200

    refactor: remove lean context function

--- src/elements/core/dom/lean-context.ts
+++
@@ -1,9 +0,0 @@
-import { isServer } from 'lit';
-
-/**
- * Checks if the document has the `sbb-lean` class.
- * @deprecated
- */
-export function isLean(): boolean {
-  return !isServer && document.documentElement.classList.contains('sbb-lean');
-}
