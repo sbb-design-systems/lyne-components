56697808f3d6a67900fd9fb4d330540fce99e4e0
Lukas Spirig <lukas.spirig@sbb.ch>
2026-06-23 13:15:11 +0200

    ci: extract common bulding blocks

--- scripts/clean-docs-files.ts
+++ scripts/clean-docs-files.ts
@@ -2,10 +2,10 @@ import { readdirSync, unlinkSync } from 'node:fs';
 import { extname, join } from 'node:path';
 
 const allowedExtensions =
-  /^\.(s?css|html?|m?js|json|ts|map|ico|jpe?g|png|svg|woff2|txt|gitignore|gitkeep|stackblitzrc)$/;
+  /^\.(s?css|html?|m?js|json|ts|md|map|ico|jpe?g|png|svg|woff2|txt|gitignore|gitkeep|stackblitzrc)$/;
 // Removes all files not matching allowed extensions from given directory.
-readdirSync(new URL('../dist/storybook', import.meta.url), { withFileTypes: true, recursive: true })
-  .filter((d) => d.isFile() && !allowedExtensions.test(extname(d.name)))
+readdirSync(join(process.cwd(), 'dist', 'docs'), { withFileTypes: true, recursive: true })
+  .filter((d) => d.isFile() && !allowedExtensions.test(extname(d.name) || d.name))
   .forEach((d) => {
     console.log(`Removing ${join(d.parentPath, d.name)}`);
     unlinkSync(join(d.parentPath, d.name));
