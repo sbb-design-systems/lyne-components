8b9e6b5310ed28c48b91397c2666ac344221ac42
reneboms <r_boms@web.de>
2026-07-28 12:22:38 +0200

    fix(seat-reservation): add handling for incorrect seat-reservation height

--- src/elements-experimental/seat-reservation/seat-reservation/seat-reservation.component.ts
+++ src/elements-experimental/seat-reservation/seat-reservation/seat-reservation.component.ts
@@ -94,7 +94,11 @@ export class SbbSeatReservationElement extends SeatReservationBaseElement {
   private _initVehicleSeatReservationConstruction(): void {
     this._coachesHtmlTemplate = html`
       ${this._renderTravelDirection()}
-      <div class="sbb-sr__component">
+      <div
+        class="sbb-sr__component ${classMap({
+          'sbb-sr__component--has-travel-direction': this.hasTravelDirection(),
+        })}"
+      >
         ${this._renderNavigation()}
         <div
           class="sbb-sr__wrapper-coach-decks"
@@ -188,7 +192,7 @@ export class SbbSeatReservationElement extends SeatReservationBaseElement {
   }
 
   private _renderTravelDirection(): TemplateResult | null {
-    if (!this.travelDirection || this.travelDirection === 'NONE') {
+    if (!this.hasTravelDirection()) {
       return null;
     }
 
