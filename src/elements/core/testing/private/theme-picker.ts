47400ea3b4a39599d7cc0ddad5a80e5dcb6ea960
jeripeierSBB <jeremias.peier@sbb.ch>
2026-09-07 17:11:43 +0200

    fix: review

--- src/elements/core/testing/private/theme-picker.ts
+++ src/elements/core/testing/private/theme-picker.ts
@@ -2,6 +2,11 @@ import leanTheme from '../../styles/lean-theme.scss?inline';
 import offBrandTheme from '../../styles/off-brand-theme.scss?inline';
 import safetyTheme from '../../styles/safety-theme.scss?inline';
 
+const interFontElement = document.createElement('link');
+interFontElement.href =
+  'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap';
+interFontElement.rel = 'stylesheet';
+
 /**
  * Picks another theme than standard and applies it to the document head.
  * Only for tests!
@@ -23,20 +28,28 @@ export async function overrideStandardThemeWith(
       break;
   }
 
-  const styleBlock = Array.from(document.head.querySelectorAll('style')).find((e) =>
-    e.textContent.includes('@font-face'),
-  )!;
-  const preloadedFontFaces =
-    styleBlock.textContent.match(/@font-face\s*{[\s\S]*?}/g)?.join('\n\n') ?? '';
-
-  const leanStyleBlock = document.createElement('style');
-  leanStyleBlock.textContent = themeFile.replace(
-    /@font-face\b\s*\{[\s\S]*?\}/g,
-    preloadedFontFaces,
+  const styleBlock = Array.from(document.head.querySelectorAll('style')).find(
+    (e) => e.textContent.includes('@font-face') && e.textContent.includes('font-family: SBB'),
   );
 
-  styleBlock.remove();
-  document.head.appendChild(leanStyleBlock);
+  const themeStyleBlock = document.createElement('style');
+  if (styleBlock) {
+    const preloadedFontFaces =
+      styleBlock?.textContent.match(/@font-face\s*{[\s\S]*?}/g)?.join('\n\n') ?? '';
+
+    themeStyleBlock.textContent = themeFile.replace(
+      /@font-face\b\s*\{[\s\S]*?\}/g,
+      preloadedFontFaces,
+    );
+
+    interFontElement.remove();
+    styleBlock.remove();
+  } else {
+    document.head.appendChild(interFontElement);
+    themeStyleBlock.textContent = themeFile;
+  }
+
+  document.head.appendChild(themeStyleBlock);
 
   await document.fonts.ready;
 }
