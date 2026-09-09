ad281e9291145902afde97c2fe83ef50b7571c2c
jeripeierSBB <jeremias.peier@sbb.ch>
2026-09-09 09:40:30 +0200

    fix: german text

--- src/elements/core/i18n/i18n.ts
+++ src/elements/core/i18n/i18n.ts
@@ -904,3 +904,10 @@ export const i18nRemainingCharacters: Record<string, string> = {
   fr: 'caractères restants',
   it: 'caratteri rimanenti',
 };
+
+export const i18nLogo125Anniversary: Record<string, string> = {
+  de: 'Logo, wir verbinden die Schweiz seit 125 Jahren.',
+  en: 'Logo, connecting Switzerland for 125 years.',
+  fr: 'Logo, 125 ans à tisser des liens qui font la Suisse.',
+  it: 'Logo, da 125 anni uniamo la Svizzera.',
+};
