8eed2fbe78199ad915291d659fb0af9856d275b9
grass.leon@sbb.ch <e567547@sbb.ch>
2026-09-11 15:15:33 +0200

    feat(sbb-seat-reservation): refactor icon names

--- src/elements-experimental/seat-reservation/common/svgs.ts
+++ src/elements-experimental/seat-reservation/common/svgs.ts
@@ -19,8 +19,8 @@ import interiorPlaceSeatNotBookable from './svgs/interior-place-seat-not-bookabl
 import interiorPlaceSeatSelected from './svgs/interior-place-seat-selected.svg?raw';
 import interiorPlaceSeatUnavailable from './svgs/interior-place-seat-unavailable.svg?raw';
 import layoutEntrance from './svgs/layout-entrance.svg?raw';
-import mobileForbidden from './svgs/mobile-forbidden.svg?raw';
-import mobile from './svgs/mobile.svg?raw';
+import mobilePhoneForbidden from './svgs/mobile-phone-forbidden.svg?raw';
+import mobilePhone from './svgs/mobile-phone.svg?raw';
 import toddler from './svgs/toddler.svg?raw';
 
 const svgs = {
@@ -45,8 +45,8 @@ const svgs = {
   interiorPlaceSeatSelected,
   interiorPlaceSeatUnavailable,
   layoutEntrance,
-  mobile,
-  mobileForbidden,
+  mobilePhone,
+  mobilePhoneForbidden,
   toddler,
 };
 
