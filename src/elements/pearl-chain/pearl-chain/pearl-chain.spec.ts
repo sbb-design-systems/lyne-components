480f8cc419658a3e4a1960af2e4a6f34174466e5
Tommmaso Menga <tommaso.menga-94@hotmail.it>
2026-09-14 10:11:08 +0200

    feat(pearl-chain): add line gap

--- src/elements/pearl-chain/pearl-chain/pearl-chain.spec.ts
+++ src/elements/pearl-chain/pearl-chain/pearl-chain.spec.ts
@@ -1,4 +1,4 @@
-import { assert } from '@open-wc/testing';
+import { assert, expect } from '@open-wc/testing';
 import { html } from 'lit';
 
 import { fixture } from '../../core/testing/private.ts';
@@ -26,5 +26,31 @@ describe(`sbb-pearl-chain`, () => {
     );
     const node = element.querySelector('sbb-pearl-chain-node')!;
     element.removeChild(node);
+  });
+
+  it('sets :state(horizontal) when nodes are spread more horizontally than vertically', async () => {
+    element = await fixture(html`
+      <sbb-pearl-chain>
+        <div style="display: flex; width: 200px;">
+          <sbb-pearl-chain-node type="start"></sbb-pearl-chain-node>
+          <span style="flex: 1;"></span>
+          <sbb-pearl-chain-node type="end"></sbb-pearl-chain-node>
+        </div>
+      </sbb-pearl-chain>
+    `);
+    expect(element.matches(':state(horizontal)')).to.be.true;
+  });
+
+  it('does not set :state(horizontal) when nodes are stacked vertically', async () => {
+    element = await fixture(html`
+      <sbb-pearl-chain>
+        <div style="display: flex; flex-direction: column; height: 200px;">
+          <sbb-pearl-chain-node type="start"></sbb-pearl-chain-node>
+          <span style="flex: 1;"></span>
+          <sbb-pearl-chain-node type="end"></sbb-pearl-chain-node>
+        </div>
+      </sbb-pearl-chain>
+    `);
+    expect(element.matches(':state(horizontal)')).to.be.false;
   });
 });
