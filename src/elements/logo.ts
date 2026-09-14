4e04f383d39b507d7ae79aa08669caa8f55904ab
Davide Mininni <101575400+DavideMininni-Fincons@users.noreply.github.com>
2026-09-14 17:17:59 +0200

    feat(logo): add SBB Cargo and Elvetino logos (#5240)

--- src/elements/logo.ts
+++ src/elements/logo.ts
@@ -1,6 +1,14 @@
 /** @entrypoint */
-import { SbbLogoElement } from './logo.pure.ts';
+import {
+  SbbLogoCargoElement,
+  SbbLogoCargoInternationalElement,
+  SbbLogoElvetinoElement,
+  SbbLogoElement,
+} from './logo.pure.ts';
 
 export * from './logo.pure.ts';
 
 SbbLogoElement.define();
+SbbLogoCargoElement.define();
+SbbLogoCargoInternationalElement.define();
+SbbLogoElvetinoElement.define();
