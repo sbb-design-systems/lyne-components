71129b650d2b3b532cc61408c09bc812a7acac39
Tommmaso Menga <tommaso.menga-94@hotmail.it>
2026-09-15 14:29:23 +0200

    feat(pearl-chain-node): add type implementation

--- src/elements/pearl-chain/pearl-chain-node/pearl-chain-node.component.ts
+++ src/elements/pearl-chain/pearl-chain-node/pearl-chain-node.component.ts
@@ -141,18 +141,70 @@ export class SbbPearlChainNodeElement extends SbbElement {
       case 'end':
       case 'commercial':
         return svg`
-          <circle cx="50%" cy="50%" r="50%" fill="currentcolor" />
+          <circle r="50%" fill="currentColor" />
         `;
       case 'stop':
         return svg`
           <circle
-            cx="50%"
-            cy="50%"
-            stroke="currentcolor"
+            stroke="currentColor"
           />
         `;
+      case 'skip':
+        return svg`
+          <path d="M3.65 2.59c-.36.36-.59.86-.59 1.41 0 1.1.9 2 2 2 .55 0 1.05-.22 1.41-.59L7.89 6.83C7.16 7.55 6.17 8 5.06 8c-2.21 0-4-1.79-4-4 0-1.1.45-2.1 1.17-2.83L3.65 2.59ZM5.06 0c2.21 0 4 1.79 4 4 0 .55-.11 1.08-.32 1.56L7.05 3.87c-.06-1-.86-1.8-1.87-1.87L3.5.32C3.98.11 4.51 0 5.06 0Z" fill="currentColor"/>
+          <path class="dash" d="M1.06 0 8.84 7.78 7.78 8.84 0 1.06 1.06 0Z"/>
+        `;
+      case 'stop-duty':
+        return svg`
+          <circle
+            class="outer-circle"
+            stroke="currentColor"
+          />
+        `;
+      case 'boarding':
+        return svg`
+          <circle
+            class="outer-circle"
+            stroke="currentColor"
+          />
+          <path d="M1.85 5C1.85 5.84 2.19 6.64 2.77 7.23 3.36 7.81 4.16 8.15 5 8.15 5.84 8.15 6.64 7.81 7.23 7.23 7.81 6.64 8.15 5.84 8.15 5L5 5 1.85 5Z" fill="currentColor"/>
+        `;
+      case 'alighting':
+        return svg`
+          <circle
+            class="outer-circle"
+            stroke="currentColor"
+          />
+          <path d="M8.15 5C8.15 4.16 7.81 3.36 7.23 2.77 6.64 2.19 5.84 1.85 5 1.85 4.16 1.85 3.36 2.19 2.77 2.77 2.19 3.36 1.85 4.16 1.85 5L5 5 8.15 5Z" fill="currentColor"/>
+        `;
+      case 'stop-on-demand':
+        return svg`
+          <circle
+            class="outer-circle"
+            stroke="currentColor"
+          />
+          <circle
+            fill="currentColor"
+            r="20%"
+          />
+        `;
+      case 'boarding-on-demand':
+        return svg`
+          <circle
+            class="outer-circle"
+            stroke="currentColor"
+          />
+          <path d="M3 5C3 5.53 3.21 6.04 3.59 6.41 3.96 6.79 4.47 7 5 7 5.53 7 6.04 6.79 6.41 6.41 6.79 6.04 7 5.53 7 5L5 5H3Z" fill="currentColor"/>
+        `;
+      case 'alighting-on-demand':
+        return svg`
+          <circle
+            class="outer-circle"
+            stroke="currentColor"
+          />
+          <path d="M7 5C7 4.47 6.79 3.96 6.41 3.59 6.04 3.21 5.53 3 5 3 4.47 3 3.96 3.21 3.59 3.59 3.21 3.96 3 4.47 3 5L5 5H7Z" fill="currentColor"/>
+        `;
       default:
-        // ponytail: other types deferred until Figma designs are confirmed.
         return nothing;
     }
   }
