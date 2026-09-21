d7766899ea382a985a27f90d2fb93e92bb5a8a33
Tommmaso Menga <tommaso.menga-94@hotmail.it>
2026-09-21 10:47:01 +0200

    test(pearl-chain): optimize snapshots

--- src/elements/pearl-chain/pearl-chain/pearl-chain.visual.spec.ts
+++ src/elements/pearl-chain/pearl-chain/pearl-chain.visual.spec.ts
@@ -1,4 +1,5 @@
 import { html } from 'lit';
+import { repeat } from 'lit/directives/repeat.js';
 
 import {
   describeEach,
@@ -26,7 +27,6 @@ describe(`sbb-pearl-chain`, () => {
       'start',
       'end',
     ],
-    now: ['before', 'in-progress', 'after'],
     orientation: ['horizontal', 'vertical'],
     state: ['', 'disrupted', 'irrelevant', 'walk', 'unsure', 'disrupted+irrelevant'],
     emulateMedia: [
@@ -36,44 +36,50 @@ describe(`sbb-pearl-chain`, () => {
     ],
   };
 
+  // "now" fully in the past, "now" in progress, "now" fully in the future
+  const times = ['2026-09-14T08:00:00', '2026-09-14T12:00:00', '2026-09-14T15:00:00'];
+
   describeViewports({ viewports: ['small'] }, () => {
     describeEach(
       cases,
-      ({ type, now, orientation, state, emulateMedia: { forcedColors, darkMode } }) => {
+      ({ type, orientation, state, emulateMedia: { forcedColors, darkMode } }) => {
         beforeEach(async function () {
-          const currentTime =
-            now === 'before'
-              ? '2026-09-14T08:00:00'
-              : now === 'in-progress'
-                ? '2026-09-14T12:00:00'
-                : '2026-09-14T15:00:00';
           root = await visualRegressionFixture(
             html`
-              <sbb-pearl-chain now=${currentTime}>
-                <div
-                  style="display: flex; flex-direction: ${orientation === 'horizontal' ? 'row' : 'column'}; align-items: center; width: 400px; height: 400px;"
-                >
-                  <sbb-pearl-chain-node
-                    type="start"
-                    departure="2026-09-14T09:00:00"
-                  ></sbb-pearl-chain-node>
-                  <span style="flex: 1;"></span>
-                  <sbb-pearl-chain-node
-                    type=${type}
-                    ?disrupted=${state.includes('disrupted')}
-                    ?irrelevant=${state.includes('irrelevant')}
-                    ?walk=${state.includes('walk')}
-                    ?unsure=${state.includes('unsure')}
-                    arrival="2026-09-14T11:00:00"
-                    departure="2026-09-14T11:10:00"
-                  ></sbb-pearl-chain-node>
-                  <span style="flex: 1;"></span>
-                  <sbb-pearl-chain-node
-                    type="end"
-                    arrival="2026-09-14T14:00:00"
-                  ></sbb-pearl-chain-node>
-                </div>
-              </sbb-pearl-chain>
+              <div
+                style="display: flex; flex-direction: ${orientation === 'horizontal' ? 'column' : 'row'}; gap: 2rem; width: 400px; height: 400px;"
+              >
+                ${repeat(
+                  times,
+                  (now) => html`
+                    <sbb-pearl-chain now=${now} style="flex: 1">
+                      <div
+                        style="display: flex; flex-direction: ${orientation === 'horizontal' ? 'row' : 'column'}; align-items: center; width: 100%; height: 100%;"
+                      >
+                        <sbb-pearl-chain-node
+                          type="start"
+                          departure="2026-09-14T09:00:00"
+                        ></sbb-pearl-chain-node>
+                        <span style="flex: 1;"></span>
+                        <sbb-pearl-chain-node
+                          type=${type}
+                          ?disrupted=${state.includes('disrupted')}
+                          ?irrelevant=${state.includes('irrelevant')}
+                          ?walk=${state.includes('walk')}
+                          ?unsure=${state.includes('unsure')}
+                          arrival="2026-09-14T11:00:00"
+                          departure="2026-09-14T11:10:00"
+                        ></sbb-pearl-chain-node>
+                        <span style="flex: 1;"></span>
+                        <sbb-pearl-chain-node
+                          type="end"
+                          arrival="2026-09-14T14:00:00"
+                        ></sbb-pearl-chain-node>
+                      </div>
+                    </sbb-pearl-chain>
+                  `,
+                )}
+              </div>
             `,
             {
               forcedColors,
